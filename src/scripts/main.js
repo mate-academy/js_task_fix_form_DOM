682e2b3b9f16c1844599fb87210bd2d39670d12e
Liudmyla Ilchenko <liudmyla.ilchenko7@gmail.com>
2020-07-12 21:46:05 +0300

    Labels and placeholders added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+function decamelize(camelString) {
+  return camelString.replace(/([A-Z]+)*([A-Z][a-z])/g, '$1 $2').toLowerCase();
+}
+
+function capitalize(string) {
+  return string.replace(
+    /\w\S*/g,
+    (text) => text.charAt(0).toUpperCase() + text.substr(1)
+  );
+}
+
+for (const input of inputs) {
+  const parent = input.parentElement;
+
+  input.placeholder = capitalize(decamelize(input.name));
+
+  parent.insertAdjacentHTML(
+    'beforeend',
+    `<label class="field-label" for=${input.id}>${decamelize(
+      input.name
+    )}<label>`
+  );
+}
