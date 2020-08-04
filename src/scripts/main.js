2f5816209b892ff4448a38bedb8e127a2e9aa9eb
Bohdan Polienko <mignatov756@gmail.com>
2020-08-04 23:17:48 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const forms = document.querySelectorAll('input');
+
+for (const form of forms) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = form.id;
+  label.textContent = form.name;
+
+  form.placeholder = form.name;
+
+  form.before(label);
+}
