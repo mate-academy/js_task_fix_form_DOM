1f085ab50e7ce7e6390edea095a6f7959a4e9c7a
Bohdan Polienko <mignatov756@gmail.com>
2020-08-10 22:07:26 +0300

    toUpperCase Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,24 @@
 const forms = document.querySelectorAll('input');
 
 for (const form of forms) {
+  let result = '';
   const label = document.createElement('label');
 
   label.className = 'field-label';
   label.htmlFor = form.id;
   label.textContent = form.name;
 
-  form.placeholder = form.name;
+  for (let i = 0; i < form.name.length; i++) {
+    if (form.name[i] === form.name[i].toUpperCase() && i > 0) {
+      result += ' ';
+    }
+    result += form.name[i];
+  }
+  result = result.toLowerCase();
+
+  result = result[0].toUpperCase() + result.slice(1);
+
+  form.placeholder = result;
 
   form.before(label);
 }
