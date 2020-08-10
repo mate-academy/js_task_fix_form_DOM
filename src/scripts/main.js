e92a425c12d2bad426119fdfbdc347a4f41d04e0
Victor-Zinchenko-mate <65622349+Victor-Zinchenko-mate@users.noreply.github.com>
2020-08-10 20:03:04 +0300

    SolutionTask3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('input');
+
+for (const key of list) {
+  const label = document.createElement('label');
+
+  label.setAttribute('class', 'field-label');
+  label.setAttribute('for', key.id);
+  key.placeholder = key.name.charAt().toUpperCase() + key.name.slice(1);
+  label.textContent = key.name;
+  key.after(label);
+};
