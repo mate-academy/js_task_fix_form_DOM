8e9b206ebba140bc8f9c98e66ffa2d7695faad9b
Dennis Priadka <dennis.priadka@gmail.com>
2020-08-16 15:31:09 +0300

    +

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+for (const input of [...document.querySelectorAll('input')]) {
+  input.placeholder = input.name;
+
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+  input.parentNode.append(label);
+}
