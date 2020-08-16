c77f84e87ce0003c3a1509c6197eea5afc86919b
Dennis Priadka <dennis.priadka@gmail.com>
2020-08-16 15:49:58 +0300

    added normal case

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 for (const input of [...document.querySelectorAll('input')]) {
-  input.placeholder = input.name;
+  input.placeholder = normalCase(input.name);
 
   const label = document.createElement('label');
 
@@ -9,4 +9,13 @@ for (const input of [...document.querySelectorAll('input')]) {
   label.htmlFor = input.id;
   label.textContent = input.name;
   input.parentNode.append(label);
+}
+
+function normalCase(str) {
+  const noCamel = str.replace(/([A-Z])/g, ' $1').replace(/\s|_/g, ' ')
+    .toLowerCase().split('');
+
+  noCamel[0] = noCamel[0].toUpperCase();
+
+  return noCamel.join('');
 }
