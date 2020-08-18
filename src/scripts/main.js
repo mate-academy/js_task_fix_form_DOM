b22359efe4594fb301f693fd666f069929b0fcef
dan3si <63787275+dan3si@users.noreply.github.com>
2020-08-18 13:40:42 +0300

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
-for (const input of [...document.querySelectorAll('input')]) {
-  input.placeholder = normalCase(input.name);
+for (const input of document.querySelectorAll('input')) {
+  input.placeholder = toNormalCase(input.name);
 
   const label = document.createElement('label');
 
@@ -11,11 +11,9 @@ for (const input of [...document.querySelectorAll('input')]) {
   input.parentNode.append(label);
 }
 
-function normalCase(str) {
-  const noCamel = str.replace(/([A-Z])/g, ' $1').replace(/\s|_/g, ' ')
-    .toLowerCase().split('');
+function toNormalCase(str) {
+  const noCamel = str.replace(/([A-Z])/g, ' $1')
+    .replace(/\s|_/g, ' ');
 
-  noCamel[0] = noCamel[0].toUpperCase();
-
-  return noCamel.join('');
+  return noCamel[0].toUpperCase() + noCamel.slice(1).toLowerCase();
 }
