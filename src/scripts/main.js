190742215cda43812d25b7537a294e7a9766ef19
Alisa <aliceblunt8@gmail.com>
2020-08-20 16:33:07 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+// Create label and append to input container
+inputs.forEach((input) => {
+  const label = `
+  <label for="${input.id}" class="field-label">
+    ${input.name}
+  </label>`;
+
+  input.parentElement.insertAdjacentHTML('afterbegin', label);
+});
+
+// Set placeholder for each input
+inputs.forEach(input => {
+  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+});
