f4ceb08fb33c16c0064bcb79409afe02531bc375
Alisa <aliceblunt8@gmail.com>
2020-08-21 11:42:08 +0300

    fix placeholder

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,5 +14,11 @@ inputs.forEach((input) => {
 
 // Set placeholder for each input
 inputs.forEach(input => {
-  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+  if (input.name.includes('Name')) {
+    const inputValue = input.name.split('N').join(' n');
+
+    input.placeholder = inputValue[0].toUpperCase() + inputValue.slice(1);
+  } else {
+    input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+  }
 });
