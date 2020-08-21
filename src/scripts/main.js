95791413118fdda21d28fdeef49ab616ef8b62d9
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-08-21 11:47:44 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,8 +11,7 @@ for (let i = 0; i < inputs.length; i++) {
   label.textContent = inputs[i].name;
   label.className = 'field-label';
   inputs[i].parentElement.append(label);
-  
-  const placeholder = inputs[i].name[0].toUpperCase() + inputs[i].name.slice(1);
 
-  inputs[i].placeholder = placeholder;
+  inputs[i].placeholder = inputs[i].name[0].toUpperCase()
+    + inputs[i].name.slice(1).replace(/([A-Z])/g, ' $1').toLowerCase();
 }
