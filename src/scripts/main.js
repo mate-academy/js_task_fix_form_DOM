569c4e6b53db6c7e48ffc9fe6d5bddb767cfa3fc
RusTron <rusan.tron@gmail.com>
2020-08-21 15:26:16 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
+const inputs = document.querySelectorAll('input');
+
+const fields = document.querySelectorAll('.field');
+
+const labels = [];
+
+inputs.forEach((x, i) => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = x.id;
+  label.innerText = x.name.toUpperCase();
+  labels.push(label);
+  x.placeholder = x.name;
+  fields[i].append(labels[i]);
+});
+
 // write code here
