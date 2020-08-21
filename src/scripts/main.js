44b004fb05e236714ef555d53bf63e1fa09e0e8e
Elena-brv <leena.brv@gmail.com>
2020-08-21 17:00:16 +0300

    forms

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+const divs = document.querySelectorAll('.field');
+
+inputs.forEach((input, ind) => {
+  let placeholderName = '';
+
+  placeholderName += input.name[0].toUpperCase();
+
+  for (let i = 1; i < input.name.length; i++) {
+    if (input.name[i] === input.name[i].toUpperCase()) {
+      placeholderName += ' ' + input.name[i].toLowerCase();
+      continue;
+    }
+    placeholderName += input.name[i];
+  }
+
+  input.placeholder = placeholderName;
+
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.getAttribute('id');
+  divs[ind].append(label);
+});
