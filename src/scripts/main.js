f13bf19d999af693d38977110c3e13cb41c366aa
Elena-brv <leena.brv@gmail.com>
2020-08-22 11:24:38 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,9 @@
 'use strict';
 
 const inputs = document.querySelectorAll('input');
-const divs = document.querySelectorAll('.field');
 
 inputs.forEach((input, ind) => {
-  let placeholderName = '';
-
-  placeholderName += input.name[0].toUpperCase();
+  let placeholderName = input.name[0].toUpperCase();
 
   for (let i = 1; i < input.name.length; i++) {
     if (input.name[i] === input.name[i].toUpperCase()) {
@@ -21,7 +18,7 @@ inputs.forEach((input, ind) => {
   const label = document.createElement('label');
 
   label.className = 'field-label';
-  label.htmlFor = input.getAttribute('id');
+  label.htmlFor = input.id;
   label.textContent = placeholderName;
-  divs[ind].append(label);
+  input.parentElement.append(label);
 });
