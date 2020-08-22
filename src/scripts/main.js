849a6ce4d72e28ed23cf1325a8a0d15e3e6c1ca6
Roman <romikkus@gmail.com>
2020-08-22 23:55:24 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const input = document.querySelectorAll('input');
+const formArea = document.querySelectorAll('.field');
+
+for (let i = 0; i < formArea.length; i++) {
+  formArea[i].insertAdjacentHTML('afterbegin', `
+    <label class="field-label" for=${input[i].id}>
+        ${input[i].name.replace(/([A-Z])/g, ' $1').trim()}
+    </label>
+  `);
+}
+
+for (let i = 0; i < input.length; i++) {
+  input[i].placeholder = (input[i].name[0].toUpperCase() + input[i].name
+    .slice(1)).replace(/([A-Z])/g, ' $1').trim();
+}
