d41a54e888b653a77134f1aa4881b9bdd877ec17
Mikle25 <mikle2593@gmail.com>
2020-08-22 19:17:59 +0300

    v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+(function() {
+  const inputAll = document.querySelectorAll('input');
+
+  [...inputAll].forEach(input => {
+    const label = document.createElement('label');
+    const namePlaceholder = input.name
+      .replace(/([A-Z][a-z]+)/g, ' $1 ')
+      .toLowerCase()
+      .trim();
+
+    input.placeholder = (namePlaceholder.replace(
+      namePlaceholder[0], namePlaceholder[0].toUpperCase()
+    ));
+
+    label.className = 'field-label';
+    label.htmlFor = input.id;
+    label.textContent = namePlaceholder;
+    input.parentElement.append(label);
+  });
+}());
