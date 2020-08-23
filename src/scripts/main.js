d9fd8627c9065b5af40b2bacf3ff6a62688826bc
Roman <romikkus@gmail.com>
2020-08-23 20:12:52 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,15 +3,20 @@
 const input = document.querySelectorAll('input');
 const formArea = document.querySelectorAll('.field');
 
+function unCamelize(string) {
+  return string.replace(/([A-Z])/g, ' $1').trim();
+}
+
 for (let i = 0; i < formArea.length; i++) {
   formArea[i].insertAdjacentHTML('afterbegin', `
     <label class="field-label" for=${input[i].id}>
-        ${input[i].name.replace(/([A-Z])/g, ' $1').trim()}
+        ${unCamelize(input[i].name)}
     </label>
   `);
 }
 
 for (let i = 0; i < input.length; i++) {
-  input[i].placeholder = (input[i].name[0].toUpperCase() + input[i].name
-    .slice(1)).replace(/([A-Z])/g, ' $1').trim();
+  input[i].placeholder = unCamelize(input[i].name[0]
+    .toUpperCase() + input[i].name
+    .slice(1));
 }
