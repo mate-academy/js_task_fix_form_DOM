0040e349f50da10309ce7d2de4adf67990aa0d6f
nanccyy <lidiapriymak@gmail.com>
2020-08-23 23:08:58 +0800

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const field = document.querySelectorAll('.field-text');
+
+[...field].map(input =>
+  input.setAttribute('placeholder', isCapitalized(splitNames(input.name))));
+
+const fieldsWrapper = document.querySelectorAll('.field');
+
+[...fieldsWrapper].forEach(div => {
+  div.insertAdjacentHTML('afterbegin', `
+    <label for="${div.lastElementChild.id}"
+      class="field-label">
+        ${splitNames(div.lastElementChild.name)}
+    </label>
+  `);
+});
+
+function isCapitalized(string) {
+  return string[0].toUpperCase() + string.slice(1);
+}
+
+function splitNames(input) {
+  return input === 'firstName'
+    || input === 'lastName'
+    ? input.slice(0, input.length - 4) + ' Name'
+    : input;
+}
