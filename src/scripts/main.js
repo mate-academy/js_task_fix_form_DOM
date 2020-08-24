8dc5f5bf0334f53ffd268f58894dc65cbf26d2cf
DmJSCourse <dimonua@gmail.com>
2020-08-24 16:19:28 +0300

    completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const inputs = document.getElementsByTagName('input');
+
+for (const input of inputs) {
+  const toAdd = document.createElement('label');
+
+  toAdd.className = 'field-label';
+  toAdd.htmlFor = input.id;
+  toAdd.textContent = input.name;
+  input.closest('div').appendChild(toAdd);
+  input.placeholder = input.name.toUpperCase();
+}
