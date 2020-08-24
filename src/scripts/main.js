a7ca64f9f0ca2009f7b006eeb33dacc8e4fe47b3
Anatasia <krajnova.stasy@gmail.com>
2020-08-24 17:32:00 +0300

    first solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
 // write code here
+
+const inputs = [ ...document.querySelectorAll('input') ];
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = toNormalizeText(input.name);
+  input.before(label);
+
+  input.placeholder = toNormalizeText(input.name);
+}
+
+function toNormalizeText(text) {
+  let result = '';
+
+  result += text[0].toUpperCase();
+
+  for (let i = 1; i < text.length; i++) {
+    if (text[i] === text[i].toUpperCase()) {
+      result += ' ';
+    }
+
+    result += text[i].toLowerCase();
+  }
+
+  return result;
+}
