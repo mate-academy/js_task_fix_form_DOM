49cf740eeaf84789b3344eb65dca2118f979218e
uni-8 <julia.zhol.a@gmail.com>
2020-08-24 21:35:25 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = `${input.id}`;
+  label.textContent = `${fromCamelToNormal(input.name)}`;
+
+  input.placeholder = `${fromCamelToNormal(input.name)}`;
+  input.before(label);
+});
+
+function fromCamelToNormal(str) {
+  return str
+    .split(/(?=[A-Z])/)
+    .map(word => word.codePointAt(0) > 90
+      ? word.replace(/^./, l => l.toUpperCase())
+      : word.toLowerCase())
+    .join(' ');
+}
