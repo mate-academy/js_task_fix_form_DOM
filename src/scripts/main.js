7330ed33e579c0fd2add8530d89689998c27f8ec
Zarva <zarva6596@gmail.com>
2020-08-24 20:27:10 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+[...document.querySelectorAll('.field-text')]
+  .map(x => x.setAttribute('placeholder', `${x.name}`));
+
+const input = document.querySelectorAll('.field-text');
+
+input.forEach(x => {
+  const label = document.createElement('label');
+  const box = x.parentElement;
+
+  label.className = 'field-label';
+  label.htmlFor = x.id;
+  label.innerText = x.name.toUpperCase();
+
+  box.prepend(label);
+});
