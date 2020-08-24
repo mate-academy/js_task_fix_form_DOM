8ae5dd8b292afe9ecdf1d4bb58102e01aceb30c7
Anastasiia Bortnichuk <feilanheid@gmail.com>
2020-08-24 17:02:48 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('.field')
+  .forEach(field => {
+    const lab = document.createElement('label');
+    const input = field.firstElementChild;
+
+    lab.className = 'field-label';
+    lab.htmlFor = input.id;
+    lab.textContent = input.name.toUpperCase();
+
+    field.prepend(lab);
+
+    input.setAttribute('placeholder', input.name[0].toUpperCase()
+    + input.name.slice(1));
+  });
