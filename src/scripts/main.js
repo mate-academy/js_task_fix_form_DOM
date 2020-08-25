850d00171e2f1cc8ba688b08c736909a2afaba7e
Vasyl_Zarichnyi <vasylzarichnyi@gmail.com>
2020-08-25 21:29:28 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const input = [...document.querySelectorAll('input')];
+let label;
+
+for (let i = 0; input.length > i; i++) {
+  label = input[i]
+    .insertAdjacentElement('beforebegin', document.createElement('label'));
+  label.className = 'field-label';
+  label.htmlFor = input[i].id;
+  label.innerText = (input[i].name).split('N').join(' N');
+
+  input[i].placeholder = (input[i].name[0]).toUpperCase()
+    + (input[i].name).split('N').join(' n').slice(1);
+}
