54d1d23042e7566dd3410b277d90e429217c674e
Maxym Chernyavskiy <masyatya@gmail.com>
2020-08-25 16:27:12 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,8 @@ const inputs = document.querySelectorAll('.field-text');
 const fields = document.querySelectorAll('.field');
 
 for (let i = 0; i < inputs.length; i++) {
-  inputs[i].placeholder = inputs[i].name.charAt(0).toUpperCase()
-    + inputs[i].name.replace(/([A-Z])/g, ' $1').slice(1);
+  inputs[i].placeholder = inputs[i].name[0].toUpperCase()
+    + inputs[i].name.replace(/([A-Z])/g, ' $1').slice(1).toLowerCase();
 
   const label = document.createElement('label');
 
