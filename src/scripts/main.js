66809035a2d78ebb2b74e1c5b89847258b4a8325
karinaKudatska <karina.kudatska@gmail.com>
2020-08-25 19:34:53 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.htmlFor = input.id;
+  label.innerText = input.name;
+  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+
+  input.before(label);
+});
