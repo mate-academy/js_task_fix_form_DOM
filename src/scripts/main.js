12e1590789fefb3c4e9e8af58bb02c5ac70ef068
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-08-25 21:56:52 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const fieldContainers = document.querySelectorAll('.field');
+
+for (const field of [...fieldContainers]) {
+  const thisFieldInput = field.querySelector('input');
+
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.textContent = thisFieldInput.name;
+  label.htmlFor = thisFieldInput.id;
+
+  field.prepend(label);
+
+  thisFieldInput.placeholder = (thisFieldInput.name)
+    .split('')
+    .map((letter, index) => {
+      if (index === 0) {
+        return letter.toUpperCase();
+      } else {
+        return letter.toLowerCase();
+      }
+    })
+    .join('');
+}
