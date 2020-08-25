8735da996dd73597d5e71018b91e32e51e328f55
yuriimovchko <movchko.yuriy@gmail.com>
2020-08-25 14:54:54 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const allInputs = [...document.querySelectorAll('input')];
+
+for (const item of allInputs) {
+  let name = item.name.replace(/([A-Z])/g, ' $1');
+
+  name = name.charAt(0).toUpperCase() + name.substr(1);
+
+  item.insertAdjacentHTML('beforebegin', `
+  <label for='${item.id}' 
+    style='position: absolute; top: 16px;'>${name.toUpperCase()}
+  </label>
+  `);
+
+  item.placeholder = name;
+}
