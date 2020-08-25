51eb5710dccf0f2299d7b254b0acdc9d251858cd
unknown <pavlo.com.5@gmail.com>
2020-08-25 22:56:21 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('.field-text')];
+const fields = [...document.querySelectorAll('.field')];
+
+inputs
+  .map(input => input
+    .setAttribute(
+      'placeholder',
+      `${input.name[0].toUpperCase()}${input.name.slice(1)}`
+    )
+  );
+
+fields
+  .map(field => field
+    .insertAdjacentHTML(
+      'afterbegin',
+      `
+        <label
+          for="${field.lastElementChild.id}"
+          class="field-label"
+        >${field.lastElementChild.name.toUpperCase()}
+
+        </label>
+      `
+    )
+  );
