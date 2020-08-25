949155f0700ceaad3c304db594393048a8c5e187
nznmvs <nznmvs@gmail.com>
2020-08-25 18:53:42 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+for (const input of document.querySelectorAll('input')) {
+  input.placeholder = toNormalCase(input.name);
+
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = toNormalCase(input.name);
+  input.parentNode.append(label);
+}
+
+function toNormalCase(str) {
+  const noCamel = str.replace(/([A-Z])/g, ' $1')
+    .replace(/\s|_/g, ' ');
+
+  return noCamel[0].toUpperCase() + noCamel.slice(1).toLowerCase();
+}
