8c5363fb9c7bd51c380fd9ae8846754a1191011b
artsashko <artyomsashko@gmail.com>
2020-08-26 18:16:35 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,10 +5,19 @@ const inputs = document.querySelectorAll('input');
 for (const input of inputs) {
   input.insertAdjacentHTML('beforebegin', `
     <label class="field-label" for="${input.id}">
-      ${input.name.toUpperCase()}
+      ${spacedName(input.name)}
     </label>
   `);
 
-  input.placeholder = input.name[0].toUpperCase()
-    + input.name.slice(1);
+  input.placeholder = spacedName(
+    input.name[0].toUpperCase() + input.name.slice(1)
+  );
+}
+
+function spacedName(string) {
+  if (string.includes('Name')) {
+    return string.slice(0, -4) + ' ' + string.slice(-4).toLowerCase();
+  }
+
+  return string;
 }
