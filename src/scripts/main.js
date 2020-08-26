4d88a8edf8411fae3d9ac1c059529f8188fd8cf5
Anton <a.v.sorokin95@gmail.com>
2020-08-26 11:39:17 +0300

    after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,9 +4,18 @@ const inputList = document.querySelectorAll('input');
 
 for (const input of inputList) {
   const label = document.createElement('label');
+  const labelContent = [];
+
+  for (const char of input.name.split('')) {
+    if (char === char.toUpperCase()) {
+      labelContent.push(' ', char);
+    } else {
+      labelContent.push(char);
+    }
+  }
 
   label.className = 'field-label';
-  label.textContent = input.name;
+  label.textContent = labelContent.join('');
   label.htmlFor = input.id;
   input.before(label);
 
