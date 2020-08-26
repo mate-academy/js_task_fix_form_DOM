7bd3dd641d51b9c1003fc51148f77d860e9ed42a
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-08-26 15:01:32 +0300

    changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,28 +4,29 @@ const inputContainers = document.querySelectorAll('.field');
 
 for (const inputContainer of [...inputContainers]) {
   const input = inputContainer.querySelector('input');
-
-  const label = document.createElement('label');
-
-  label.className = 'field-label';
-  label.textContent = input.name;
-  label.htmlFor = input.id;
-
-  inputContainer.prepend(label);
-
-  const formattedPlaceholder = [];
+  let inputName = [];
 
   (input.name)
     .split('')
     .forEach((letter, index) => {
       if (index === 0) {
-        formattedPlaceholder.push(letter.toUpperCase());
+        inputName.push(letter.toUpperCase());
       } else if (letter.toUpperCase() === letter) {
-        formattedPlaceholder.push(` ${letter}`);
+        inputName.push(` ${letter}`);
       } else {
-        formattedPlaceholder.push(letter);
+        inputName.push(letter);
       }
     });
 
-  input.placeholder = formattedPlaceholder.join('');
+  inputName = inputName.join('');
+
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.textContent = inputName.toUpperCase();
+  label.htmlFor = input.id;
+
+  inputContainer.prepend(label);
+
+  input.placeholder = inputName;
 }
