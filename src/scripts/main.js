171f0c2e876f5865fbc3be6de1dc75677ec83f82
tykhan <tykhanvasyl@gmail.com>
2020-08-26 09:58:27 +0300

    fixed after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,15 +3,22 @@
 const inputs = document.querySelectorAll('input');
 
 inputs.forEach(input => {
+  let getName = `${input.name[0].toUpperCase()}`
+  + `${input.name.slice(1).toLowerCase()}`;
+
+  for (let i = 0; i < input.name.length; i++) {
+    if (input.name[i] === input.name[i].toUpperCase()) {
+      getName = `${input.name[0].toUpperCase()}`
+        + `${input.name.slice(1, i)} `
+        + `${input.name.slice(i)}`;
+    }
+  }
+  input.setAttribute('placeholder', getName);
+
   const newLabel = document.createElement('label');
 
   newLabel.classList.add('field-label');
   newLabel.setAttribute('for', `${input.getAttribute('id')}`);
-  newLabel.textContent = input.name;
+  newLabel.textContent = getName;
   input.before(newLabel);
-
-  const getName = `${input.name[0].toUpperCase()}`
-  + `${input.name.slice(1).toLowerCase()}`;
-
-  input.setAttribute('placeholder', getName);
 });
