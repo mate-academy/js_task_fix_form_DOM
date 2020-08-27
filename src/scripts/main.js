d1692012b29adb3e3ee39834d2b06d168f3a157c
ArtemiiKab <artemiikab@gmail.com>
2020-08-27 13:14:34 +0300

    sorry didnt get it firt time

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,7 @@ for (let i = 0; i < inputs.length; i++) {
   const label = document.createElement('label');
 
   label.className = 'field-label';
-  label.textContent = inputs[i].name;
+  label.textContent = inputs[i].name.replace(/([A-Z])/g, ' $1').toUpperCase();
   label.htmlFor = inputs[i].id;
 
   inputs[i].parentElement.append(label);
