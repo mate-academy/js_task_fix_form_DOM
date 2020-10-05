77ce2c5cb0ad87b202ba7df35657fc80a2079250
Alona <aionovaionova@gmail.com>
2020-10-05 16:17:23 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('.field-text')];
+
+for (const input of inputs) {
+  const inputLabel = document.createElement('label');
+
+  inputLabel.classList.add('field-label');
+
+  inputLabel.textContent = input.name;
+
+  inputLabel.for = input.id;
+
+  input.parentElement.appendChild(inputLabel);
+
+  input.placeholder = input.name.toUpperCase();
+}
