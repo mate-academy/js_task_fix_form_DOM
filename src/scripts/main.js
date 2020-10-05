392cce8fb32f102c50b29b46a5ccf1ac316a79b6
Yazheviks <vicsergeyeva@gmail.com>
2020-10-05 15:34:13 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+const parent = document.querySelectorAll('.field');
+
+inputs.forEach((item, index) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', item.getAttribute('id'));
+
+  parent[index].insertBefore(label, item);
+
+  let placeholder = item.getAttribute('name').split('');
+
+  placeholder[0] = placeholder[0].toUpperCase();
+  placeholder = placeholder.join('');
+
+  item.setAttribute('placeholder', placeholder);
+});
