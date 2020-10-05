d6a377f570cf434c5e05852ae4d9b3b3ee98a8e9
Yazheviks <vicsergeyeva@gmail.com>
2020-10-05 19:14:24 +0300

    fix case, label name assignment

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,12 +7,12 @@ inputs.forEach((item, index) => {
   const label = document.createElement('label');
 
   label.classList.add('field-label');
-  label.setAttribute('for', item.getAttribute('id'));
+  label.htmlFor = item.id;
   label.textContent = item.getAttribute('name');
 
   parent[index].insertBefore(label, item);
 
-  let placeholder = item.getAttribute('name').split('');
+  let placeholder = item.getAttribute('name').toLowerCase().split('');
 
   placeholder[0] = placeholder[0].toUpperCase();
   placeholder = placeholder.join('');
