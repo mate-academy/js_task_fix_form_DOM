893b672c018277e448819410285495747e52309d
Artur Bespalyy <zkhomers@gmail.com>
2020-10-07 14:41:49 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+const stringFormat = function(str) {
+  const result = str.name.split('N').join(' n');
+
+  return result[0].toUpperCase() + result.slice(1);
+};
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.className = ('field-label');
+  label.htmlFor = input.id;
+  label.innerText = stringFormat(input);
+  input.before(label);
+  input.placeholder = stringFormat(input);
+};
