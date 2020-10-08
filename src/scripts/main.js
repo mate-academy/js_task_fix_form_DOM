c4bc2850d06dd67cdfc3f85cfbc20a1837ab2b4c
Alina <alina90565@gmail.com>
2020-10-08 18:08:56 +0300

    done fix form DOM task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+
+  input.placeholder = input.name;
+
+  const parentForInput = input.closest('.field');
+
+  parentForInput.prepend(label);
+});
