4a4fede13fdf09dc7657475a40d125081f9ef395
Alina <alina90565@gmail.com>
2020-10-08 18:24:51 +0300

    created fixedName function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,15 +3,27 @@
 const inputs = document.querySelectorAll('input');
 
 inputs.forEach(input => {
+  const { name, id } = input;
   const label = document.createElement('label');
 
   label.className = 'field-label';
-  label.htmlFor = input.id;
-  label.textContent = input.name;
+  label.htmlFor = id;
+  label.textContent = fixedName(name);
 
-  input.placeholder = input.name;
+  input.placeholder = fixedName(name);
 
   const parentForInput = input.closest('.field');
 
   parentForInput.prepend(label);
 });
+
+function fixedName(name) {
+  const indexOfBigLetter = [...name].findIndex(letter => {
+    return letter.toUpperCase() === letter;
+  });
+
+  const firstWord = name.substring(0, indexOfBigLetter);
+  const secondWord = name.substring(indexOfBigLetter).toLowerCase();
+
+  return firstWord + ' ' + secondWord;
+}
