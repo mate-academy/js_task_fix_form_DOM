d39585f61fe677d17eda991e6e0c359842f48119
Andrii Medziak <58214135+Andrii1996@users.noreply.github.com>
2020-10-11 16:01:10 +0300

    solution form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+for (const input of inputs) {
+  const lable = document.createElement('lable');
+
+  lable.classList.add('field-lable');
+
+  lable.textContent = input.name.toUpperCase();
+  lable.setAttribute('for', input.id);
+  input.parentElement.prepend(lable);
+
+  const placeHolder = input.name.replace(/([A-Z])/g, ` $1`).toLowerCase();
+
+  input.placeholder = placeHolder;
+}
