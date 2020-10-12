0577945537df03247a14c3ebf68ae00162bbcc7a
andlap24 <andrew.laptev24@gmail.com>
2020-10-12 17:28:36 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('.field-text')];
+
+function createLabel(list) {
+  for (const item of list) {
+    const label = document.createElement('label');
+
+    label.classList.add('field-label');
+    label.htmlFor = item.id;
+    label.textContent = item.name;
+
+    item.parentElement.appendChild(label);
+
+    const placeholder = item.name[0].toUpperCase() + item.name.slice(1);
+
+    item.placeholder = placeholder;
+  }
+}
+
+createLabel(inputs);
