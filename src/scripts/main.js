408de34c9b3e7e7033f66597500ae819b5391287
Tania Omelko <tanyaomelko39@gmail.com>
2020-10-13 15:09:51 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.className = ['field-label'];
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+  input.placeholder = convertString(input.name);
+  input.parentNode.append(label);
+}
+
+function convertString(string) {
+  let stringConverted = '';
+
+  for (const i of string) {
+    if (i === i.toUpperCase() && string.indexOf('i') !== 0) {
+      stringConverted += ' ';
+    }
+    stringConverted += i;
+  }
+
+  return stringConverted[0].toUpperCase()
+    + stringConverted.slice(1).toLowerCase();
+}
