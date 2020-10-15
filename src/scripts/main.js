ff03201d7f0115b357f86b3ff85d1aeb53f0d5ca
KirillLutsenko <mnrovski68@gmail.com>
2020-10-15 17:19:31 +0300

    Fix Form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.className = ['field-label'];
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+
+  input.parentNode.append(label);
+  input.placeholder = editingPlaceholder(input.name);
+}
+
+function editingPlaceholder(string) {
+  const letters = [];
+
+  for (const symbol of string.slice(1)) {
+    if (symbol === symbol.toUpperCase()) {
+      letters.push(' ', symbol.toLowerCase());
+    } else {
+      letters.push(symbol);
+    }
+  }
+
+  return string[0].toUpperCase() + letters.join('');
+}
