38b94d1912895f47bdbe3499bd5bbcb664c73641
Sofiia Kucherenko <kucherenko.sv13@gmail.com>
2020-10-15 16:55:37 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const fields = document.querySelectorAll('.field');
+
+for (const field of fields) {
+  const label = document.createElement('label');
+  const input = field.querySelector('input');
+  const inputName = toNormalCase(input.name);
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = inputName;
+
+  input.placeholder = inputName;
+
+  field.append(label);
+}
+
+function toNormalCase(inputName) {
+  let stringToNormalCase = '';
+
+  for (const letter of inputName) {
+    if (letter === letter.toUpperCase()) {
+      stringToNormalCase += ' ';
+    }
+
+    stringToNormalCase += letter.toLowerCase();
+  }
+
+  return stringToNormalCase[0].toUpperCase()
+    + stringToNormalCase.slice(1);
+}
