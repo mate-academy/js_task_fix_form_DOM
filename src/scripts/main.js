c4e3ebd047e09b0ac04284f65050d44768ac5282
Vladyslav Pavlik <pavliklaw7@gmail.com>
2020-10-15 22:10:17 +0300

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const allInputs = document.getElementsByTagName('input');
+
+for (let i = 0, len = allInputs.length; i < len; i++) {
+  const newLabel = document.createElement('label');
+
+  newLabel.className = 'field-label';
+  allInputs[i].before(newLabel);
+  newLabel.innerText = allInputs[i].name;
+  allInputs[i].setAttribute('placeholder', allInputs[i].name);
+}
