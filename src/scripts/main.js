a8cc68139726ed6078b23cd50eb150936bf9588c
martakupka <marta-k2008@ukr.net>
2020-10-15 16:29:26 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const inputElements = document.querySelectorAll('.field-text');
+
+for (const inputElement of inputElements) {
+  let inputName = inputElement.name;
+
+  if (inputElement.name.indexOf('Name') !== -1) {
+    inputName = inputElement.name.replace('Name', '') + ' Name';
+  }
+
+  inputElement.setAttribute('placeholder', `${inputName.toUpperCase()}`);
+
+  const labelElement = document.createElement('label');
+
+  labelElement.className = 'field-label';
+  labelElement.htmlFor = inputElement.id;
+  labelElement.textContent = inputName;
+
+  inputElement.closest('.field').append(labelElement);
+}
