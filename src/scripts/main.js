db37676f366a20864c1e7e61f4e3f279a76ffb60
Oleksandr Rohatnov <mr.rogatnov@gmail.com>
2020-10-15 09:56:35 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,17 @@
 'use strict';
 
-const inputs = document.querySelectorAll('input');
+const fields = document.querySelectorAll('input');
 
-for (const element of inputs) {
+for (const element of fields) {
+  const { id, name } = element;
+
   const label = document.createElement('label');
 
   label.className = 'field-label';
-  label.htmlFor = element.id;
-  label.textContent = element.name.replace(/([A-Z])/g, ' $1');
+  label.htmlFor = id;
+  label.textContent = name.replace(/([A-Z])/g, ' $1');
   element.before(label);
 
-  element.placeholder
-    = element.name.charAt(0).toUpperCase()
-    + element.name.slice(1).replace(/([A-Z])/g, ' $1');
+  element.placeholder = name[0].toUpperCase()
+    + name.slice(1).replace(/([A-Z])/g, ' $1');
 }
