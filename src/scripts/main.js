9a4d2a395a8f3709d2f524623ea8e36d5cdc22e2
KhrystynaYelyseyeva <play1409@gmail.com>
2020-10-15 15:56:09 +0300

    add script for changing inputs look

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const inputsNodes = document.querySelectorAll('.field-text');
+
+for (const input of inputsNodes) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+
+  label.htmlFor = input.id;
+
+  label.textContent = input.name.toUpperCase();
+
+  const placeholder = input.name.toLowerCase();
+
+  input.placeholder = placeholder[0].toUpperCase() + placeholder.slice(1);
+
+  input.before(label);
+}
