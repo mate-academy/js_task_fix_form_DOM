eba8fabbe4dd8c92dabfa7f6a232cb62a70c3d0a
milla-romankova <milla-romankova@gmail.com>
2020-10-15 16:42:32 +0300

    js solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const inputCovers = document.querySelectorAll('.field');
+
+for (const inputCover of inputCovers) {
+  const input = inputCover.querySelector('input');
+  const inputName = input.name.replace(/([A-Z])/g, ' $1');
+
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.textContent = inputName;
+  label.htmlFor = input.id;
+
+  inputCover.append(label);
+
+  let placeholder = inputName.toLowerCase();
+
+  placeholder = placeholder[0].toUpperCase() + placeholder.slice(1);
+  input.placeholder = placeholder;
+}
