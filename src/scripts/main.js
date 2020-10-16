5c701acc453b55934309d9f659b3f9413d3020b7
Olena kuzii <okuziy@gmail.com>
2020-10-16 19:54:19 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+// const input = [...document.querySelectorAll('input')];
+
+// for (const field of input) {
+//   field.placeholder = field.name;
+
+//   const fieldLabel = document.createElement('label');
+
+//   fieldLabel.className = 'field-label';
+//   fieldLabel.textContent = field.name;
+//   fieldLabel.htmlFor = field.id;
+
+//   input.parentElement.append(fieldLabel);
+// }
+const input = [...document.querySelectorAll('input')];
+
+for (const field of input) {
+  const fieldLabel = document.createElement('label');
+
+  fieldLabel.className = 'field-label';
+  fieldLabel.textContent = field.name.replace(/([A-Z])/g, ' $1');
+  fieldLabel.htmlFor = field.id;
+  field.before(fieldLabel);
+
+  field.placeholder = field.name;
+}
