db0a38080309151198831856fcfd8a4916bc24c5
Atteroot <yuora556@gmail.com>
2020-10-16 13:57:50 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const labelCreator = document.createElement('label');
+  const nameSpaceReplacer = input.name.replace(/Name/, ' Name');
+
+  labelCreator.className = 'field-label';
+  labelCreator.htmlFor = input.id;
+  labelCreator.textContent = nameSpaceReplacer;
+  input.before(labelCreator);
+
+  input.placeholder = nameSpaceReplacer[0].toUpperCase()
+    + nameSpaceReplacer.slice(1);
+}
