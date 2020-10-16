99e9f65702d2564ec17411593fa0333e169e7299
Evgenii <voznukzhenia@gmail.com>
2020-10-16 11:43:50 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // write code here
+const parent = document.querySelectorAll('.field');
+const fields = document.querySelectorAll('.field-text');
+
+fields.forEach((input, index) => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+  input.placeholder = input.name;
+
+  parent[index].append(label);
+});
