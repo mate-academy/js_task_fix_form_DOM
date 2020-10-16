c832268ff6bcbc9d1220a01ce495403f85aa3315
Vladyslav Pavlik <pavliklaw7@gmail.com>
2020-10-16 12:32:45 +0300

    variable name changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,12 +4,12 @@ const allInputs = document.getElementsByTagName('input');
 
 for (let i = 0; i < allInputs.length; i++) {
   const newLabel = document.createElement('label');
-  const CapitalizedPlaceholder = allInputs[i].name
+  const placeholderName = allInputs[i].name
     .charAt(0)
     .toUpperCase() + allInputs[i].name.slice(1);
 
   newLabel.className = 'field-label';
   allInputs[i].before(newLabel);
   newLabel.innerText = allInputs[i].name;
-  allInputs[i].setAttribute('placeholder', CapitalizedPlaceholder);
+  allInputs[i].setAttribute('placeholder', placeholderName);
 }
