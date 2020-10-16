45da1781dc72f62fa03f46bf9795c26fd9a04b95
Vladyslav Pavlik <pavliklaw7@gmail.com>
2020-10-16 12:16:54 +0300

    loop fix, placeholder capitalized

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,11 +2,14 @@
 
 const allInputs = document.getElementsByTagName('input');
 
-for (let i = 0, len = allInputs.length; i < len; i++) {
+for (let i = 0; i < allInputs.length; i++) {
   const newLabel = document.createElement('label');
+  const placeholderName = allInputs[i].name
+    .charAt(0)
+    .toUpperCase() + allInputs[i].name.slice(1);
 
   newLabel.className = 'field-label';
   allInputs[i].before(newLabel);
   newLabel.innerText = allInputs[i].name;
-  allInputs[i].setAttribute('placeholder', allInputs[i].name);
+  allInputs[i].setAttribute('placeholder', placeholderName);
 }
