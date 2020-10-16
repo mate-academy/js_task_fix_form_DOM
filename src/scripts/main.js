577e0ea76ab1c4f213db6860b598fb97bc008e69
Yurii <yurastt1@gmail.com>
2020-10-16 04:15:38 +0300

    solution with bad placeholders

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const input = document.querySelectorAll('input');
+
+for (const elem of [...input]) {
+  elem.placeholder = elem.name;
+  elem.placeholder[0].toUpperCase();
+
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.textContent = elem.name;
+  label.setAttribute('for', elem.id);
+
+  elem.parentElement.append(label);
+}
