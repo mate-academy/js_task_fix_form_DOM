ea7167a4cac7dc639f8c5f0037beee34f6c9a039
Olena kuzii <okuziy@gmail.com>
2020-10-19 11:49:30 +0300

    delete comment

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,5 @@
 'use strict';
 
-// const input = [...document.querySelectorAll('input')];
-
-// for (const field of input) {
-//   field.placeholder = field.name;
-
-//   const fieldLabel = document.createElement('label');
-
-//   fieldLabel.className = 'field-label';
-//   fieldLabel.textContent = field.name;
-//   fieldLabel.htmlFor = field.id;
-
-//   input.parentElement.append(fieldLabel);
-// }
 const input = [...document.querySelectorAll('input')];
 
 for (const field of input) {
