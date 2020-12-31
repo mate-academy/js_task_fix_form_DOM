853d3ef16f80ebdbf60761001db02510aaa27cab
Serhii Alieksieievych <kajot1863@gmail.com>
2020-12-31 15:54:12 +0200

    completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+for (const input of inputs) {
+  const labelForInput = document.createElement('label');
+
+  labelForInput.className = 'field-label';
+  labelForInput.htmlFor = input.id;
+  labelForInput.textContent = input.name;
+
+  input.placeholder = input.name;
+
+  input.parentElement.append(labelForInput);
+}
