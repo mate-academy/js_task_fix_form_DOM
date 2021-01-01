69dd81ce791e5b3b8339a4050aeadce25e309674
Khrulenko Vitalii <khrulenko.v@gmail.com>
2021-01-01 22:47:39 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const inputs = [...document.getElementsByTagName('input')];
+
+for (let i = 0; i < inputs.length; i++) {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+
+  label.htmlFor = `${inputs[i].id}`;
+  label.textContent = `${inputs[i].name}`;
+  inputs[i].placeholder = `${inputs[i].name}`;
+
+  inputs[i].before(label);
+}
