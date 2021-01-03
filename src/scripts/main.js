48033128f1abaa0c85c2264286003615b053699e
5Mountains <V.Piatyhor@gmail.com>
2021-01-04 00:10:28 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
+
+inputs.forEach(input => {
+  input.setAttribute('placeholder', capitalize(input.name));
+
+  input.insertAdjacentHTML('beforebegin',
+    `<label for="${input.id}" class="field-label">${input.name}</label>`);
+});
