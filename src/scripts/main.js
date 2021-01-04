4ad6432de486c1d02c8888d646a571a111ec054f
MarchD <dubchmar@gmail.com>
2021-01-04 17:53:47 +0200

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const fields = document.getElementsByClassName('field');
+
+for (const field of fields) {
+  const fieldText = field.querySelector('.field-text');
+  const fieldName = fieldText.name;
+
+  field.insertAdjacentHTML('afterbegin',
+    `<label class="field-label" for="${fieldText.id}">
+            ${fieldName.toUpperCase()}
+    </label>`
+  );
+
+  fieldText.placeholder = fieldName[0].toUpperCase() + fieldName.slice(1);
+};
