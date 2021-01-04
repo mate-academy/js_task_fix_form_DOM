01a2d460e724f93bfd9ad55a65b696e51e633ba2
Dima Bond <neverpooh@gmail.com>
2021-01-04 19:33:23 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const inputList = document.getElementsByTagName('input');
+
+for (const element of inputList) {
+  element.setAttribute('placeholder',
+    `${formatPlaceholder(element.name)}`);
+
+  element.insertAdjacentHTML('beforebegin', `
+    <label class="field-label" for="${element.id}">
+      ${element.name}
+    </label>
+  `);
+}
+
+function formatPlaceholder(str) {
+  const newStr = str.slice(1);
+  let result = '';
+
+  for (let i = 0; i < newStr.length; i++) {
+    if (newStr.charCodeAt(i) <= 90) {
+      result += ' ';
+    }
+    result += newStr[i];
+  }
+
+  return (str[0].toUpperCase() + result);
+}
