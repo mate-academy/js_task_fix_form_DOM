ac6032ba60744b8b02fa58d93105da283457ae16
Dima Semenov <semenovdima998@gmail.com>
2021-01-04 23:43:58 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const arrName = document.querySelectorAll('input');
+
+for (const key of arrName) {
+  key.placeholder = key.name[0].toUpperCase() + key.name.slice(1);
+
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.textContent = key.name;
+  label.htmlFor = key.id;
+
+  key.before(label);
+}
