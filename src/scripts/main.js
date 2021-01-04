50bee15eee1b2150daa311af4cc494cb32c108af
Zhenya <e.mezhueva@gmail.com>
2021-01-04 19:23:23 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,7 @@ const inputs = document.querySelectorAll('input');
 
 for (const inp of inputs) {
   inp.insertAdjacentHTML('beforebegin', `
-  <label class="field-label" for="${inp.getAttribute('id')}">${inp.name}</label>
+  <label class="field-label" for="${inp.id}">${inp.name}</label>
   `);
   inp.placeholder = inp.name[0].toUpperCase() + inp.name.slice(1);
 }
