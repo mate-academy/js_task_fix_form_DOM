7c9bfabf94ec08bc9e9f568fd42a5113825f6a6a
tamara ostapets <tamara.ostapec@gmail.com>
2021-01-04 17:15:16 +0200

    Form is fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+inputs.forEach(input => {
+  input.placeholder = splitWord(input.name);
+
+  const label = document.createElement('label');
+
+  label.for = input.id;
+  label.textContent = splitWord(input.name);
+  label.className = `field-label`;
+
+  input.parentElement.prepend(label);
+});
+
+function splitWord(string) {
+  const arr = string.split('');
+  const index = arr.findIndex(char => char === char.toUpperCase());
+
+  if (index !== -1) {
+    arr.splice(index, 0, ' ');
+  }
+
+  arr[0] = arr[0].toUpperCase();
+
+  return arr.join('');
+}
