159c357251618c2e24f16b58f1d9c6992087261d
Ruslan Shmadchenko <ruslanwpua@gmail.com>
2021-01-04 14:27:52 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+const inputsArray = [...inputs];
+
+for (const input of inputsArray) {
+  input.htmlFor = input.id;
+
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.textContent = input.name;
+
+  input.before(label);
+  input.placeholder = input.name;
+};
