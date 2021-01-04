8abe27cc1f476b170c44e32b4db75f5b8f610919
Nazar Valchyshyn <nvalja@gmail.com>
2021-01-05 01:59:17 +0200

    made task fix-form-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const wrapper = document.querySelectorAll('.field');
+
+for (const item of wrapper) {
+  const textPlaceholder = item
+    .children[0]
+    .name[0]
+    .toUpperCase() + item.children[0].name.slice(1);
+
+  const text = item.children[0].name;
+
+  item.children[0].placeholder = textPlaceholder;
+
+  item.insertAdjacentHTML('afterbegin', `
+      <label class="field-label" for="${item.children[0].id}">
+      ${text}
+      </label>
+      `);
+}
