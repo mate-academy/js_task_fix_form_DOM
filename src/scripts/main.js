37f9073fb14d3220f12369ea630d7e9f6cb21107
Alex Lemeshchuk <alexyskr@gmail.com>
2021-01-04 17:26:09 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputFields = document.querySelectorAll('input');
+
+for (let i = 0; i < inputFields.length; i++) {
+  const label = document.createElement('label');
+
+  label.setAttribute('class', 'field-label');
+  label.setAttribute('for', inputFields[i].id);
+  label.innerText = inputFields[i].name;
+
+  inputFields[i].setAttribute('placeholder',
+    inputFields[i].name.slice(0, 1).toUpperCase() + inputFields[i].name.slice(
+      1, inputFields[i].name.length).toLowerCase());
+
+  inputFields[i].parentNode.appendChild(label);
+}
