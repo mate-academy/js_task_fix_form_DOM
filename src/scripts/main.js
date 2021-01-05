81f281df52fc454121b2ee58d61d67f15490e9b2
Tetiana Zotova <zotova.tetiana@gmail.com>
2021-01-05 23:28:46 +0200

    suggestion applied

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,7 @@ const inputsArr = [...inputs];
 for (let i = 0; i < inputsArr.length; i++) {
   inputsArr[i].insertAdjacentHTML('beforebegin', `
   <label class="field-label" for="${inputsArr[i].id}
-  ">${inputsArr[i].name}</label>`);
+  ">${inputsArr[i].name.replace(/Name/, ' Name')}</label>`);
 
   inputsArr[i].placeholder
   = inputsArr[i].name[0].toUpperCase() + inputsArr[i].name.slice(1);
