306b163b832f4f700413a1c0d436a1c4a4991636
Dima Semenov <semenovdima998@gmail.com>
2021-01-05 20:46:09 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,12 +3,23 @@
 const arrName = document.querySelectorAll('input');
 
 for (const key of arrName) {
-  key.placeholder = key.name[0].toUpperCase() + key.name.slice(1);
+  let res;
+
+  for (let i = 0; i < key.name.length; i++) {
+    if (key.name[i] === key.name[i].toUpperCase()) {
+      res = [...key.name].slice(0, i).join('') + ' '
+      + [...key.name].slice(i, key.name.length).join('');
+    }
+  }
+
+  key.placeholder = res === undefined
+    ? key.name[0].toUpperCase() + key.name.slice(1)
+    : res[0].toUpperCase() + res.slice(1);
 
   const label = document.createElement('label');
 
   label.className = 'field-label';
-  label.textContent = key.name;
+  label.textContent = res;
   label.htmlFor = key.id;
 
   key.before(label);
