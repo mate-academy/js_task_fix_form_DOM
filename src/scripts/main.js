76992c2cdb07ab26abfc86dc7d6cdcaa8a216465
troospieler <troospieler1990@gmail.com>
2021-01-05 21:13:39 +0200

    improved solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,15 +3,22 @@
 const inputs = document.querySelectorAll('.field-text');
 
 for (const input of inputs) {
-  const correctPlaceholder = input.name[0].toUpperCase()
+  let correctPlaceholder = input.name[0].toUpperCase()
     + input.name.slice(1).toLowerCase();
+
+  const n = correctPlaceholder.indexOf('name');
+
+  if (correctPlaceholder.includes('name')) {
+    correctPlaceholder = correctPlaceholder.slice(0, n)
+    + ' ' + 'name';
+  }
 
   input.placeholder = correctPlaceholder;
 
   const label = document.createElement('label');
 
   label.className = 'field-label';
-  label.textContent = input.name.toUpperCase();
+  label.textContent = correctPlaceholder.toUpperCase();
   label.htmlFor = input.name;
 
   input.parentElement.append(label);
