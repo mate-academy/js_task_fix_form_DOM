5c6b3f69cbd14fcc1658fed048973e1d6fb25e55
Svitlana Haiduk <svitlana.gaiduk@gmail.com>
2021-01-05 23:42:48 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,9 +4,11 @@ const allInputs = document.querySelectorAll('input');
 
 for (const input of allInputs) {
   input.insertAdjacentHTML('beforebegin',
-    `<label class="field-label" for="${input.id}">${input.name}</label>`
+    `<label class="field-label" for="${input.id}">
+      ${input.name.replace(/Name/, ' Name')}
+    </label>`
   );
 
-  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
-  input.placeholder = input.placeholder.replace(/Name/, ' Name');
+  input.placeholder = (input.name[0].toUpperCase() + input.name.slice(1))
+    .replace(/Name/, ' Name');
 }
