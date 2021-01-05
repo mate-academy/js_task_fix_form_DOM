f902faed669ba60df4e33b09692df689fa8d1920
Nazar Valchyshyn <nvalja@gmail.com>
2021-01-05 21:31:14 +0200

    corrections ater review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,18 +3,21 @@
 const wrapper = document.querySelectorAll('.field');
 
 for (const item of wrapper) {
-  const textPlaceholder = item
-    .children[0]
-    .name[0]
-    .toUpperCase() + item.children[0].name.slice(1);
+  const inputName = item.children[0].name;
 
-  const text = item.children[0].name;
+  const textPlaceholder = inputName.split('').map((letter, index) => {
+    return letter === letter.toUpperCase()
+      ? ' ' + letter
+      : index === 0
+        ? letter.toUpperCase()
+        : letter;
+  }).join('');
 
   item.children[0].placeholder = textPlaceholder;
 
   item.insertAdjacentHTML('afterbegin', `
       <label class="field-label" for="${item.children[0].id}">
-      ${text}
+      ${inputName}
       </label>
       `);
 }
