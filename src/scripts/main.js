5d68d936083947b52e3fa1ae0e7840d677acedd8
Zhenya <e.mezhueva@gmail.com>
2021-01-06 01:54:14 +0200

    label text fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,9 @@ const inputs = document.querySelectorAll('input');
 
 for (const inp of inputs) {
   inp.insertAdjacentHTML('beforebegin', `
-  <label class="field-label" for="${inp.id}">${inp.name}</label>
+    <label class="field-label" for="${inp.id}">
+        ${inp.name.replace(/([A-Z])/g, ' $1')}
+    </label>
   `);
   inp.placeholder = inp.name[0].toUpperCase() + inp.name.slice(1);
 }
