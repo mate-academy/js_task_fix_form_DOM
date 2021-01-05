103ac79d2b57514924873553e7eca613fb8d44b6
Iryna Cherpak <cherpak.iryna@gmail.com>
2021-01-05 20:29:47 +0200

    Fixed input name

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,11 @@
 const inputs = document.querySelectorAll('input');
 
 inputs.forEach(input => {
+  const inputName = input.name.replace(/Name/, ' Name');
+
   input.insertAdjacentHTML('beforebegin',
-    `<label class="field-label" for="${input.id}">${input.name}</label>`
+    `<label class="field-label" for="${input.id}">${inputName}</label>`
   );
 
-  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+  input.placeholder = inputName[0].toUpperCase() + inputName.slice(1);
 });
