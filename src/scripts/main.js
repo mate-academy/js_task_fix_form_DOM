768cb2076a006e8dcf42691b14a9f6bd552e4e41
Iryna Kozhura <ivladimirovna713@gmail.com>
2021-01-05 19:38:02 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('.field')];
+
+inputs.forEach(div => {
+  div.insertAdjacentHTML('afterbegin', '<label></label>');
+
+  div.children[0].className = 'field-label';
+  div.children[0].htmlFor = div.children[1].id;
+  div.children[0].textContent = div.children[1].name;
+
+  div.children[1].placeholder = div.children[1].name[0].toUpperCase()
+  + div.children[1].name.slice(1);
+});
