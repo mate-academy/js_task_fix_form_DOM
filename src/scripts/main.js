7b27d8f93fc4e288ad0063d3834707ab0d05b0d9
Andrey Gavriluk <bugabuga.008@gmail.com>
2021-01-06 01:21:00 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  input.placeholder = toUpper(input.name);
+
+  input.insertAdjacentHTML('beforebegin',
+    `<label for="${input.id}" class="field-label">
+      ${input.name}
+    </label>`);
+});
+
+function toUpper(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1).replace(/Name/, ' Name');
+}
