4ab79fa4e4ca136fc04cd5393392336937e3ca00
MarchD <dubchmar@gmail.com>
2021-01-05 23:52:24 +0200

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,7 +4,9 @@ const fields = document.getElementsByClassName('field');
 
 for (const field of fields) {
   const fieldText = field.querySelector('.field-text');
-  const fieldName = fieldText.name;
+  const fieldName = /[A-Z]/.test(fieldText.name)
+    ? toSplitString(fieldText.name, /[A-Z]/)
+    : fieldText.name;
 
   field.insertAdjacentHTML('afterbegin',
     `<label class="field-label" for="${fieldText.id}">
@@ -14,3 +16,9 @@ for (const field of fields) {
 
   fieldText.placeholder = fieldName[0].toUpperCase() + fieldName.slice(1);
 };
+
+function toSplitString(str, separator) {
+  const index = str.indexOf(str.match(separator));
+
+  return `${str.slice(0, index)} ${str.slice(index)}`;
+}
