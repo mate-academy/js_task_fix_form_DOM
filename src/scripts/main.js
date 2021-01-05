c211b7f199d0b2920ba90945a37a7b007b4cceb7
DarMatt <daryamatviienko@gmail.com>
2021-01-05 20:47:39 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,7 +4,9 @@ const inputs = document.querySelectorAll('input');
 
 for (const input of inputs) {
   input.insertAdjacentHTML('beforebegin', `
-    <label class = "field-label" for= "${input.id}"> ${input.name.replace(/Name/, ' Name')}</label>
+    <label class = "field-label" for= "${input.id}"> 
+      ${input.name.replace(/Name/, ' Name')}
+    </label>
   `);
   input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
 }
