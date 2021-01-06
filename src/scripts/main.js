81b7fae03096555816bff03e2648b21bd2d742ad
Tetiana Zotova <zotova.tetiana@gmail.com>
2021-01-06 13:56:35 +0200

    suggestion applied

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,5 +9,6 @@ for (let i = 0; i < inputsArr.length; i++) {
   ">${inputsArr[i].name.replace(/Name/, ' Name')}</label>`);
 
   inputsArr[i].placeholder
-  = inputsArr[i].name[0].toUpperCase() + inputsArr[i].name.slice(1);
+      = inputsArr[i].name[0].toUpperCase() + inputsArr[i].name
+      .slice(1).replace(/Name/, ' Name');
 }
