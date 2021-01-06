c1417dc898bbff35ccf406cb61af97a09bb16ef4
Olesia <olesia.tk@gmail.com>
2021-01-06 08:56:27 +0200

    Fixed with replace

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,18 +4,9 @@ const inputs = document.querySelectorAll('input');
 
 for (const input of inputs) {
   const labelElement = document.createElement('label');
-  let lebelName = '';
-
-  for (const letter of input.name) {
-    if (letter.charCodeAt(0) < 97) {
-      lebelName += ` ${letter}`;
-    } else {
-      lebelName += letter;
-    }
-  }
 
   labelElement.className = 'field-label';
-  labelElement.textContent = lebelName;
+  labelElement.textContent = input.name.replace(/Name/, ' Name');
   input.after(labelElement);
 
   const inputAtr = input.getAttribute('id');
@@ -23,7 +14,7 @@ for (const input of inputs) {
   labelElement.setAttribute('for', inputAtr);
 
   const textPlaceholder = input.name.slice(0, 1).toUpperCase()
-    + input.name.slice(1);
+    + input.name.slice(1).replace(/Name/, ' Name');
 
   input.setAttribute('placeholder', textPlaceholder);
 }
