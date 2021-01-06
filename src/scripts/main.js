12b6913b3b08677e659ac84132cd7f9974f8bd31
Khrulenko Vitalii <khrulenko.v@gmail.com>
2021-01-06 19:50:42 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,28 @@
 
 const inputs = [...document.getElementsByTagName('input')];
 
+function toClassicCase(camelWord) {
+  let upperLetter = '';
+
+  for (const letter of camelWord) {
+    if (letter >= 'A' && letter <= 'Z') {
+      upperLetter = letter;
+    }
+  }
+
+  return camelWord
+    .replace(`${upperLetter}`, ` ${upperLetter}`)
+    .replace(`${camelWord[0]}`, `${camelWord[0].toUpperCase()}`);
+}
+
 for (let i = 0; i < inputs.length; i++) {
   const label = document.createElement('label');
 
   label.classList.add('field-label');
 
   label.htmlFor = `${inputs[i].id}`;
-  label.textContent = `${inputs[i].name}`;
-  inputs[i].placeholder = `${inputs[i].name}`;
+  label.textContent = `${toClassicCase(inputs[i].name)}`;
+  inputs[i].placeholder = `${toClassicCase(inputs[i].name)}`;
 
   inputs[i].before(label);
 }
