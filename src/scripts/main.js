1e5cd8f7cb7e0b9fbd42f26119026fac84b737f5
chri911 <delightsome@bigmir.net>
2021-01-06 20:09:02 +0200

    fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,14 +3,21 @@
 const input = document.querySelectorAll('.field-text');
 const arrOfInputs = [...input];
 
+function fromCamelCase(str) {
+  const newStr = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+
+  return newStr;
+}
+
 arrOfInputs.map((el) => (
-  el.placeholder = el.name[0].toUpperCase() + el.name.slice(1)
+  el.placeholder = fromCamelCase(el.name[0]).toUpperCase()
+    + fromCamelCase(el.name.slice(1))
 ));
 
 for (const key of arrOfInputs) {
   const label = document.createElement('label');
 
   label.htmlFor = key.id;
-  label.innerText = key.name.toUpperCase();
+  label.innerText = fromCamelCase(key.name).toUpperCase();
   key.before(label);
 }
