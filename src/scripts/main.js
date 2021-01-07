c356f909cdcbdc62dc0b433e4bc7cb6f255af768
Maryna Zinchenko <pandoz16@gmail.com>
2021-01-07 12:37:52 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,11 +7,11 @@ for (let i = 0; i < allInputs.length; i++) {
   const label = document.createElement('label');
 
   label.className = 'field-label';
-  label.textContent = allInputs[i].name;
+  label.textContent = allInputs[i].name.replace(/Name/, ' Name');
   label.htmlFor = allInputs[i].id;
 
   rootElement[i].append(label);
 
   allInputs[i].placeholder = allInputs[i].name[0].toUpperCase()
-  + allInputs[i].name.slice(1);
+  + allInputs[i].name.slice(1).replace(/Name/, ' Name');
 }
