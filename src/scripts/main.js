7853459dc2d265b552f57ff2d7bc66d49da0b279
MaximZhuravlov <maksym.zhuravlov1995@gmail.com>
2021-01-08 16:52:50 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,12 +6,11 @@ inputs.forEach(input => {
   const label = document.createElement('label');
 
   label.classList.add('field-label');
-  label.setAttribute('for', input.id);
+  label.htmlFor = input.id;
 
   label.textContent = input.name;
 
-  input.setAttribute('placeholder',
-    input.name.substring(0, 1).toUpperCase()
-    + input.name.substring(1).toLowerCase());
+  input.placeholder = input.name.substring(0, 1).toUpperCase()
+    + input.name.substring(1).toLowerCase();
   input.parentNode.appendChild(label);
 });
