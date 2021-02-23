adff47206c1d2fc3045c9c0308321ecaa75a5025
Misha Lysak <myhaillysak@gmail.com>
2021-02-23 20:59:23 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,10 +6,13 @@ const label = document.createElement('label');
 for (let i = 0; i < input.length; i++) {
   label[i] = document.createElement('label');
 
+  const firstLetter = input[i].name.slice(0, 1).toUpperCase();
+  const wordWithoutFirstLetter = input[i].name.slice(1).split('t').join('t ');
+
   label[i].className = 'field-label';
   label[i].htmlFor = input[i].id;
-  label[i].innerText = input[i].name;
-  input[i].placeholder = input[i].name;
+  label[i].innerText = input[i].name.split('t').join('t ');
+  input[i].placeholder = firstLetter + wordWithoutFirstLetter;
 
   input[i].insertAdjacentElement('beforebegin', label[i]);
 }
