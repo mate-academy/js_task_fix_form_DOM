7b85da4209171eb2f093fb76cc09860886c76bc8
Misha Lysak <myhaillysak@gmail.com>
2021-02-23 18:27:19 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
+const input = document.querySelectorAll('input');
+const label = document.createElement('label');
+
+for (let i = 0; i < input.length; i++) {
+  label[i] = document.createElement('label');
+
+  label[i].className = 'field-label';
+  label[i].htmlFor = input[i].id;
+  label[i].innerText = input[i].name;
+  input[i].placeholder = input[i].name;
+
+  input[i].insertAdjacentElement('beforebegin', label[i]);
+}
+
 // write code here
