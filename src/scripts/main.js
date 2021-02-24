1917080b62246fab728472e162582d974dfe37e7
Misha Lysak <myhaillysak@gmail.com>
2021-02-24 12:38:24 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,13 +4,14 @@ const input = document.querySelectorAll('input');
 const label = document.createElement('label');
 
 for (let i = 0; i < input.length; i++) {
-  const word = input[i].name.split('').map(letter => {
-    if (letter === letter.toUpperCase()) {
-      return ` ${letter}`;
-    }
+  const word = input[i].name.split('')[0].toUpperCase()
+    + input[i].name.split('').slice(1).map(letter => {
+      if (letter === letter.toUpperCase()) {
+        return ` ${letter}`;
+      }
 
-    return letter;
-  }).join('');
+      return letter;
+    }).join('');
 
   label[i] = document.createElement('label');
 
