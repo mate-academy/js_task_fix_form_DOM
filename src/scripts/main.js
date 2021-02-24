718744b0251a9b3b85b9ea2231ac1f1c048e4fae
Bohdan Dobak <chudolipton@gmail.com>
2021-02-24 12:20:04 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,17 +4,20 @@ const input = document.querySelectorAll('input');
 
 const label = document.createElement('label');
 
-for (let i = 0; i < input.length; i++) {
-  label[i] = document.createElement('label');
-  label[i].className = 'field-label';
-  label[i].htmlFor = input[i].id;
-  label[i].innerText = input[i].name;
-
-  input[i].placeholder = input[i].name.split(' ').map(word => (
+function capitalize(string) {
+  return string.split(' ').map(word => (
     word.substring(0, 1).toUpperCase() + word.substring(1)
   )).join(' ').split('').map(letter => (
     letter === letter.toUpperCase() ? ' ' + letter : letter
   )).join('');
+}
+
+for (let i = 0; i < input.length; i++) {
+  label[i] = document.createElement('label');
+  label[i].className = 'field-label';
+  label[i].htmlFor = input[i].id;
+  label[i].innerText = capitalize(input[i].name);
+  input[i].placeholder = capitalize(input[i].name);
 
   input[i].insertAdjacentElement('beforebegin', label[i]);
 }
