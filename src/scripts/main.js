2db50735e325dd1d47de20a9178ab69165decacc
Bohdan Dobak <chudolipton@gmail.com>
2021-02-24 11:49:01 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,7 +9,10 @@ for (let i = 0; i < input.length; i++) {
   label[i].className = 'field-label';
   label[i].htmlFor = input[i].id;
   label[i].innerText = input[i].name;
-  input[i].placeholder = input[i].name.toUpperCase().split('T').join('T ');
+
+  input[i].placeholder = input[i].name.split('').map(letter => (
+    letter === letter.toUpperCase() ? ' ' + letter : letter
+  )).join('').toUpperCase();
 
   input[i].insertAdjacentElement('beforebegin', label[i]);
 }
