b3cc14e71ec8802ab0f0f9f01ad39252b15c1d57
Bohdan Dobak <chudolipton@gmail.com>
2021-02-24 12:11:35 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,9 +10,11 @@ for (let i = 0; i < input.length; i++) {
   label[i].htmlFor = input[i].id;
   label[i].innerText = input[i].name;
 
-  input[i].placeholder = input[i].name.split('').map(letter => (
+  input[i].placeholder = input[i].name.split(' ').map(word => (
+    word.substring(0, 1).toUpperCase() + word.substring(1)
+  )).join(' ').split('').map(letter => (
     letter === letter.toUpperCase() ? ' ' + letter : letter
-  )).join('').toUpperCase();
+  )).join('');
 
   input[i].insertAdjacentElement('beforebegin', label[i]);
 }
