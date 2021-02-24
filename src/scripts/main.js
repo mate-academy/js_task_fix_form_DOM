99bdbbef62a4606475498de2ece201ef78cb76d4
Misha Lysak <myhaillysak@gmail.com>
2021-02-24 11:46:13 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,7 @@ const label = document.createElement('label');
 for (let i = 0; i < input.length; i++) {
   const word = input[i].name.split('').map(letter => {
     if (letter === letter.toUpperCase()) {
-      return ' ' + letter;
+      return ` ${letter}`;
     }
 
     return letter;
