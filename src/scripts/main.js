05d59b756fe3ac88e038ce3376b7fe1538b49d39
Taras <seman6745@gmail.com>
2021-02-26 16:31:29 +0200

    forms-first

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+function formatingText(text = 'email') {
+  const newText = text.split('');
+  const i = newText.findIndex(el => el === el.toUpperCase());
+
+  newText[0] = newText[0].toUpperCase();
+
+  if (i > 0) {
+    newText.splice(i, 0, ' ');
+  }
+
+  return newText.join('');
+};
+
+[...inputs].forEach(element => {
+  element.placeholder = formatingText(element.name);
+
+  element.insertAdjacentHTML('beforebegin', `
+    <label
+    for="${element.id}"
+    class="field-label"
+    >
+      ${formatingText(element.name)}
+    </label>
+  `);
+});
