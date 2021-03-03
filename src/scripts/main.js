a059cae1b2405f75228e45c4ad4a0ce979108491
Sofiia Asieieva <sofiia.asieieva@gmail.com>
2021-03-03 17:34:52 +0200

    added a label and placeholder to the page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.setAttribute('for', input.id);
+  label.setAttribute('class', 'field-label');
+  input.setAttribute('placeholder', capitalizeFirstLetter(input.name));
+  input.before(label);
+  label.innerText = capitalizeFirstLetter(input.name);
+}
+
+function capitalizeFirstLetter(string) {
+  let result = '';
+
+  for (const letter of string) {
+    if (letter === letter.toUpperCase()) {
+      result += ' ';
+    }
+    result += letter;
+  }
+
+  return result.charAt(0).toUpperCase() + result.slice(1);
+}
