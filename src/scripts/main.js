d1935e73ca4721e47bfab83782e3bc597921c233
anastasiia <anastasiia.aliinyk@gmail.com>
2021-03-04 15:56:13 +0200

    fix form DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const listOfInputs = Array.from(document.querySelectorAll('input'));
+
+listOfInputs.forEach(input => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', `${input.id}`);
+  label.textContent = `${input.name}`;
+
+  input.setAttribute('placeholder', `${input.name}`);
+  input.before(label);
+});
