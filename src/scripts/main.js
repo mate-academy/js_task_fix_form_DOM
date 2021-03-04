a3999e508ae3f8b50042bcfbcd2bc05f2a33e3d6
Olexandr Bieliaiev <Shurasava17@gmail.com>
2021-03-04 20:41:54 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((element) => {
+  const field = document.createElement('label');
+
+  field.htmlFor = element.id;
+  field.className = 'field-label';
+  field.textContent = capitalize(element.name);
+  element.parentElement.append(field);
+  element.placeholder = capitalize(element.name);
+});
+
+function capitalize(string) {
+  const words = string.split('');
+
+  words.forEach((element, index) => {
+    if (index === 0) {
+      words[index] = words[index].toUpperCase();
+    }
+
+    if (words[index].charCodeAt(0) < 91 && words[index].charCodeAt(0) > 64) {
+      words[index] = ' ' + words[index].toUpperCase();
+    }
+  });
+
+  return words.join('').trim();
+}
+
+capitalize('nextTask');
