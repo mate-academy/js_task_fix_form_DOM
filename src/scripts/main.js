89f6797c04d3acfafcee5451d4c53820426601fc
anastasiia <anastasiia.aliinyk@gmail.com>
2021-03-04 16:18:47 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,28 @@
 'use strict';
 
+function capitalizeFirstLetter(string) {
+  let splitedTitle = '';
+
+  for (const letter of string) {
+    if (letter === letter.toUpperCase()) {
+      splitedTitle += ' ';
+    }
+
+    splitedTitle += letter;
+  }
+
+  return splitedTitle.charAt(0).toUpperCase() + splitedTitle.slice(1);
+}
+
 const listOfInputs = Array.from(document.querySelectorAll('input'));
 
 listOfInputs.forEach(input => {
   const label = document.createElement('label');
 
   label.className = 'field-label';
-  label.setAttribute('for', `${input.id}`);
-  label.textContent = `${input.name}`;
+  label.setAttribute('for', input.id);
+  label.textContent = capitalizeFirstLetter(input.name);
 
-  input.setAttribute('placeholder', `${input.name}`);
+  input.setAttribute('placeholder', capitalizeFirstLetter(input.name));
   input.before(label);
 });
