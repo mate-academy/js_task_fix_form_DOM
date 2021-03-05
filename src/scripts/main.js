b27006c3091c24a70cfba35ceb5cf900fc218489
Ping <4ivrik@ukr.net>
2021-03-05 13:58:55 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = splitWords(input.name);
+  input.placeholder = splitWords(input.name);
+  input.placeholder = input.placeholder.toUpperCase();
+  input.parentElement.append(label);
+}
+
+function splitWords(word) {
+  return word.replace(/([a-z])([A-Z])/g, '$1 $2');
+}
