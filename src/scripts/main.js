72dec5610bed8a9cb45c47488d6442b428640b6f
Taras Chirkov <chirkovtaras@gmail.com>
2021-03-05 14:32:05 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const placeholderAddSpace = input.name.replace(/([A-Z])/g, ' $1');
+
+  const placeholder = placeholderAddSpace
+    .charAt(0).toUpperCase()
+  + placeholderAddSpace
+    .slice(1);
+
+  input.setAttribute('placeholder', placeholder);
+
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = placeholder;
+
+  input.before(label);
+}
