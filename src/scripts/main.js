b48d08dc93cfbb6b5f3efba4601e6c1ab1e7ed8f
Vladyslav Semyrekov <semyrekov.vladyslav@gmail.com>
2021-03-05 17:39:22 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+for (const input of inputs) {
+  input.insertAdjacentHTML('afterend',
+    `<label
+      for='addGap(${input.name})'
+      class='field-label' >${input.name}
+    </label>`
+  );
+
+  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+}
