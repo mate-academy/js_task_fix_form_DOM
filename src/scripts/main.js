3bc06ba885323670f500d4ddb5e5926fb320c639
Nikita Puhovskii <cheliojul@gmail.com>
2021-03-05 17:59:14 +0200

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+function capitalizing(word) {
+  return word.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+}
+
+function splitWords(word) {
+  return word.replace(/[A-Z]/g, ' $&');
+}
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = splitWords(input.name).toUpperCase();
+
+  input.parentElement.append(label);
+  input.placeholder = capitalizing(splitWords(input.name));
+}
