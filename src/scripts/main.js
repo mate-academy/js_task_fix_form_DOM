dc14d3beaa07db212519f25f0868c93aa15b0693
Artur Shpontak <ashpontak97@gmail.com>
2021-03-08 19:12:52 +0200

    Solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+class Label {
+  constructor(id, inputName) {
+    const newLabel = document.createElement('label');
+
+    newLabel.className = 'field-label';
+    newLabel.htmlFor = id;
+    newLabel.textContent = inputName.toUpperCase();
+
+    return newLabel;
+  }
+}
+
+const fields = [...document.querySelectorAll('.field')];
+
+for (const field of fields) {
+  const input = field.children[0];
+  let inputName = input.name.split('');
+
+  inputName[0] = inputName[0].toUpperCase();
+  inputName = inputName.join('');
+
+  input.placeholder = inputName;
+
+  const inputLabel = new Label(input.id, inputName);
+
+  field.insertBefore(inputLabel, input);
+}
