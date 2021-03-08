bc7dae894cda7b388bee87da6e909e3a4c683f6f
Владислав Сандык <nubia@MacBook-Pro-Vladislav.local>
2021-03-08 19:00:22 +0200

    added labels and placeholders in form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+function capitalizeFirstLetter(string) {
+  let modifiedString = '';
+
+  for (const letter of string) {
+    if (letter === letter.toUpperCase()) {
+      modifiedString += ' ';
+    }
+    modifiedString += letter;
+  }
+
+  return modifiedString[0].toUpperCase() + modifiedString.slice(1);
+}
+
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.setAttribute('for', input.id);
+  label.setAttribute('class', 'field-label');
+
+  input.setAttribute('placeholder', capitalizeFirstLetter(input.name));
+  input.before(label);
+
+  label.innerText = capitalizeFirstLetter(input.name);
+}
