35fc22f46fda82c89e815fe9f1e43a4914782a0a
Yevhenii Yashnyi <y.yashnyi@gmail.com>
2021-03-08 15:12:22 +0200

    fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,12 +6,12 @@ for (const input of inputs) {
   input.insertAdjacentHTML('beforebegin',
     `<label class="field-label" for="${input.id}">
         ${splitWords(input.name)}
-    </label>`);
-
-  input.placeholder = splitWords(
-    input.name[0].toUpperCase() + input.name.slice(1)
+    </label>`
   );
-};
+
+  input.placeholder = input.name[0].toUpperCase()
+    + splitWords(input.name.slice(1));
+}
 
 function splitWords(word) {
   return word.replace(/[A-Z]/g, ' $&');
