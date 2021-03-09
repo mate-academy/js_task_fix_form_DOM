f3cb2240cc092a984d5de9a0f6728715dca3d2d3
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-03-09 11:50:00 +0200

    Fixes after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,14 +3,27 @@
 const inputs = [...document.querySelectorAll('input')];
 
 function capitalize(string) {
-  return string.toUpperCase();
+  const letters = string.split('');
+  const formattedLetters = [];
+
+  formattedLetters.push(letters[0].toUpperCase());
+
+  for (let i = 1; i < letters.length; i++) {
+    if (letters[i] === letters[i].toUpperCase()) {
+      formattedLetters.push(' ', letters[i]);
+    } else {
+      formattedLetters.push(letters[i]);
+    }
+  }
+
+  return formattedLetters.join('');
 }
 
 inputs.map((input) => {
   const label = document.createElement('label');
 
   label.className = 'field-label';
-  label.setAttribute('for', input.id);
+  label.htmlfor = input.id;
   label.textContent = capitalize(input.name);
 
   input.parentElement.append(label);
