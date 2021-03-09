52fcdf08519c8889ca5123608ba07d6e72740336
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-03-09 17:49:23 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,28 +23,33 @@ function formatingWord(indexOfUpperSymbol, placeholderValue) {
   return newWord;
 }
 
+function formatAllText(placeholder) {
+  let newText = '';
+  const indexOfUpperSymbol = placeholder
+    .indexOf(placeholder.match(/[A-Z]/g));
+
+  if (indexOfUpperSymbol > 0) {
+    newText = formatingWord(indexOfUpperSymbol, placeholder);
+  } else {
+    newText = replacePlaceHolder(placeholder, 1);
+  }
+
+  return newText;
+}
+
 const dataInputs = document.querySelectorAll('.field-text');
 
 for (let i = 0; i < dataInputs.length; i++) {
-  let placeholderValue = dataInputs[i].name;
+  const placeholderValue = dataInputs[i].name;
 
   const inputParent = dataInputs[i].parentElement;
-
-  const indexOfUpperSymbol = placeholderValue
-    .indexOf(placeholderValue.match(/[A-Z]/g));
-
-  if (indexOfUpperSymbol > 0) {
-    placeholderValue = formatingWord(indexOfUpperSymbol, placeholderValue);
-  } else {
-    placeholderValue = replacePlaceHolder(placeholderValue, 1);
-  }
 
   const label = document.createElement('label');
 
   inputParent.prepend(label);
   label.htmlFor = dataInputs[i].id;
   label.className = 'field-label';
-  label.innerText = placeholderValue;
+  label.innerText = formatAllText(placeholderValue);
 
-  dataInputs[i].placeholder = `${placeholderValue}`;
+  dataInputs[i].placeholder = `${formatAllText(placeholderValue)}`;
 }
