9727dba0425324e0c18333ca4cd0d26f84482c56
Olha Briukhanova <oaleksandrovva@gmail.com>
2021-03-09 11:57:56 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,18 @@
 
 const inputs = document.querySelectorAll('.field-text');
 
-const wordWithCapitalLetter = (word) => {
-  return word.charAt(0).toUpperCase() + word.slice(1);
+const formatingWord = (word) => {
+  let newWord = '';
+
+  for (const letter of word) {
+    if (letter === letter.toUpperCase()) {
+      newWord += ' ';
+    }
+
+    newWord += letter;
+  }
+
+  return newWord.charAt(0).toUpperCase() + newWord.slice(1);
 };
 
 for (const input of inputs) {
@@ -11,8 +21,8 @@ for (const input of inputs) {
 
   label.className = 'field-label';
   label.htmlFor = input.id;
-  label.textContent = input.name.toUpperCase();
-  input.placeholder = wordWithCapitalLetter(input.name);
+  label.textContent = formatingWord(input.name);
+  input.placeholder = formatingWord(input.name);
 
   input.parentElement.append(label);
 }
