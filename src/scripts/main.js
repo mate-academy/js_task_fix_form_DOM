9cee2f2dd0dc34b5f4afd603c06de31fd5ff7f06
Anna <garsinaanna32778@gmail.com>
2021-03-09 15:58:24 +0200

    replaced map with forEach

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,24 +2,26 @@
 
 const inputs = document.querySelectorAll('input');
 
-[...inputs].map((input) => {
+function addSpaces(string) {
+  const splittedName = string.split('');
+
+  for (let i = 0; i < splittedName.length; i++) {
+    if (splittedName[i] === splittedName[i].toUpperCase()) {
+      splittedName[i] = ` ${splittedName[i]}`;
+    }
+  }
+
+  return splittedName.join('');
+}
+
+[...inputs].forEach((input) => {
   const label = document.createElement('label');
 
   label.className = 'field-label';
   label.htmlFor = input.id;
-  label.textContent = input.name.toUpperCase();
 
-  function addSpaces(string) {
-    const splittedName = string.split('');
-
-    for (let i = 0; i < splittedName.length; i++) {
-      if (splittedName[i] === splittedName[i].toUpperCase()) {
-        splittedName[i] = ` ${splittedName[i]}`;
-      }
-    }
-
-    return splittedName.join('');
-  }
+  label.textContent = addSpaces(input.name)[0].toUpperCase()
+  + addSpaces(input.name).slice(1).toUpperCase();
 
   input.placeholder = addSpaces(input.name)[0].toUpperCase()
   + addSpaces(input.name).slice(1);
