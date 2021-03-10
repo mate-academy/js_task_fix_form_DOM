c112ce43de47a04035dd2d8db8731b635bb19035
Oleg Bezriadin <muhachov86@gmail.com>
2021-03-10 14:41:47 +0200

    No RegExp no problem?

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,9 +5,7 @@ const inputs = document.querySelectorAll('input');
 for (const input of inputs) {
   const label = document.createElement('label');
 
-  const match = input.name.match(/([A-Z]?[^A-Z]*)/g);
-
-  const inputName = `${match[0]} ${match[1]}`;
+  const inputName = splitWord(input.name);
 
   label.textContent = inputName;
 
@@ -18,4 +16,17 @@ for (const input of inputs) {
 
   input.placeholder = inputName.substring(0, 1).toUpperCase()
     + inputName.substring(1);
+}
+
+function splitWord(word) {
+  let cuttedWord = '';
+
+  word.split('').forEach(letter => {
+    if (letter.charCodeAt() < 91) {
+      cuttedWord += ` `;
+    }
+    cuttedWord += letter;
+  });
+
+  return cuttedWord;
 }
