d1d9a3caa50369f368f389a63acf4d111c393dad
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-03-10 12:20:54 +0200

    fix hardCoing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,37 +1,14 @@
 'use strict';
 
-function replacePlaceHolder(
-  placeholder, charIndexFrom, charIndexTo = placeholder.length
-) {
-  return placeholder[0]
-    .toUpperCase() + placeholder.slice(charIndexFrom, charIndexTo);
-}
-
-function formatingWord(indexOfUpperSymbol, placeholderValue) {
-  let newWord = '';
-
-  const firstPart = replacePlaceHolder(
-    placeholderValue, 1, indexOfUpperSymbol
-  );
-
-  const secondPart = replacePlaceHolder(
-    placeholderValue, indexOfUpperSymbol, placeholderValue.length
-  );
-
-  newWord = `${firstPart} ${secondPart}`;
-
-  return newWord;
-}
-
 function formatAllText(placeholder) {
   let newText = '';
-  const indexOfUpperSymbol = placeholder
-    .indexOf(placeholder.match(/[A-Z]/g));
 
-  if (indexOfUpperSymbol > 0) {
-    newText = formatingWord(indexOfUpperSymbol, placeholder);
-  } else {
-    newText = replacePlaceHolder(placeholder, 1);
+  for (let i = 0; i < placeholder.length; i++) {
+    if (placeholder[i] === placeholder[i].toUpperCase()) {
+      newText += ' ';
+    }
+
+    newText += placeholder[i];
   }
 
   return newText;
