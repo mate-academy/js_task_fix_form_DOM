5f7bc9e43acbeaac0153109a8b305588d12c6833
Onishchuk <onishchuk.danil@gmail.com>
2021-03-17 13:44:07 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const allInput = [...document.querySelectorAll('input')];
+const allFiled = [...document.querySelectorAll('.field')];
+
+for (let i = 0; i < allInput.length; i++) {
+  const holder = allInput[i].name.split('');
+
+  holder[0] = holder[0].toUpperCase();
+  allInput[i].placeholder = holder.join('');
+
+  allFiled[i].insertAdjacentHTML('afterbegin', `
+  <label class="field-label" for="${allInput[i].id}">
+    ${allInput[i].name.toUpperCase()}
+  </label>
+`);
+}
