be24d6e8fc6d08e488d185809902b2724e9be8f0
Ivan Bilozerov <vaniabelozerov@gmail.com>
2021-03-19 16:48:44 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const fields = document.querySelectorAll('.field');
+const fieldTexts = document.querySelectorAll('.field-text');
+
+const fieldTextIds = [...fieldTexts].map(item => item.id);
+const fieldTextNames = [...fieldTexts].map(item => item.name);
+
+const splittedWords = fieldTextNames.map(item => {
+  return item[0]
+    .toUpperCase() + item
+    .slice(1)
+    .replace(/([A-Z][a-z]+)/g, ' $1 ')
+    .trim();
+});
+
+fields.forEach((item, i) => {
+  item.insertAdjacentHTML('beforeend', `
+  <label class="field-label" for="${fieldTextIds[i]}">
+    ${splittedWords[i].toUpperCase()}
+  </label>
+  `);
+});
+
+fieldTexts.forEach((item, i) => {
+  item.setAttribute('placeholder', splittedWords[i]);
+});
