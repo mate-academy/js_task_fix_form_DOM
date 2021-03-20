21272c470c1fc2f5912ccc4a545abe8cd1fe12bd
Vitalii Nepomiashchyi <nepomyaschyi1@gmail.com>
2021-03-20 23:20:47 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const camelToString = function(camelCase) {
+  let string = camelCase.replace(/([A-Z])/g, ' $1');
+
+  string = string[0].toUpperCase() + string.substring(1);
+
+  return string;
+};
+
+const fields = document.querySelectorAll('.field');
+
+for (let i = 0; i < fields.length; i++) {
+  const input = fields[i].querySelector('.field-text');
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = `${input.id}`;
+  label.textContent = camelToString(input.name);
+
+  fields[i].prepend(label);
+
+  input.placeholder = camelToString(input.name);
+}
