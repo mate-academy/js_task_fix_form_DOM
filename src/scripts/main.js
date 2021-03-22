694e3909612583327c22548dd7549374479b3b01
Onishchuk <onishchuk.danil@gmail.com>
2021-03-22 15:13:44 +0200

    fix bug

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,13 @@ const allFiled = [...document.querySelectorAll('.field')];
 for (let i = 0; i < allInput.length; i++) {
   const holder = allInput[i].name.split('');
 
+  for (let j = 0; j < holder.length; j++) {
+    if (holder[j] === holder[j].toUpperCase()) {
+      holder.splice(holder.indexOf(holder[j]), 0, ' ');
+      j++;
+    }
+  }
+
   holder[0] = holder[0].toUpperCase();
   allInput[i].placeholder = holder.join('');
 
