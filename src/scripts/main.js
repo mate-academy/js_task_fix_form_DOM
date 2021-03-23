fc1bec681632099cd269e887dfd3b34b8b627b7f
Makhmut <grupry@gmail.com>
2021-03-23 23:00:11 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+[...document.querySelectorAll('.field-text')]
+  .map(item => {
+    const lable = document.createElement('label');
+
+    lable.className = 'field-label';
+    lable.textContent = `${item.name}`;
+    lable.setAttribute('for', `${item.id}`);
+
+    item.placeholder = `${item.name}`;
+    item.before(lable);
+  });
