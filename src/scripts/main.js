771f743c45740d1d390a0cdd998437690b51f577
Vlad Dezhniuk <vladdezhniuk@gmail.com>
2021-03-24 00:42:44 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,10 @@ const result = document.querySelectorAll('.field');
 for (let a = 0; a < result.length; a++) {
   const selector = result[a].querySelector('.field-text');
   const placeholder = selector.name[0]
-    .toUpperCase() + selector.name.slice(1);
+    .toUpperCase() + selector.name
+    .slice(1)
+    .replace(/([A-Z][a-z]+)/g, ' $1 ')
+    .trim();
 
   result[a].insertAdjacentHTML('afterbegin', `
   <label for="${selector.id}" class = "field-label">${selector.name}</label>
