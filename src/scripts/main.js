af229441029e3cd8e9b14738eccfc2823dd7cd13
Vlad Dezhniuk <vladdezhniuk@gmail.com>
2021-03-24 00:04:58 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const result = document.querySelectorAll('.field');
+
+for (let a = 0; a < result.length; a++) {
+  const selector = result[a].querySelector('.field-text');
+  const placeholder = selector.name[0]
+    .toUpperCase() + selector.name.slice(1);
+
+  result[a].insertAdjacentHTML('afterbegin', `
+  <label for="${selector.id}" class = "field-label">${selector.name}</label>
+  `);
+
+  selector.placeholder = placeholder;
+}
