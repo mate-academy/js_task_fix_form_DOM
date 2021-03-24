51c005a4530c3ab1770f3efc27eb7ab1e324b6e2
Iryna Haiduk <$ git config --global user.name Iryna>
2021-03-24 14:32:47 +0200

    Added labels and placeholders to forms

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const inputsList = document.querySelectorAll('input');
+
+function splitWords(string) {
+  const secondWordIndex = string.split('').findIndex(
+    item => item === item.toUpperCase());
+  let splittedWords;
+
+  if (secondWordIndex !== -1) {
+    splittedWords = `${string.slice(0, 1)
+      .toUpperCase()}`
+    + `${string.slice(1, secondWordIndex)}`
+    + ` ${string.slice(secondWordIndex)}`;
+  } else {
+    splittedWords = `${string.slice(0, 1).toUpperCase()}`
+    + `${string.slice(1)}`;
+  }
+
+  return splittedWords;
+}
+
+inputsList.forEach(input => {
+  input.insertAdjacentHTML('beforebegin',
+    `<label class="field-label" for="${input.id}">`
+  + `${splitWords(input.name)}</label>`);
+  input.setAttribute('placeholder', splitWords(input.name));
+}
+);
