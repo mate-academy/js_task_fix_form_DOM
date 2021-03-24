dc65da7bbff9505ba133df97a255e2136368c1e5
Oleg Hluhk <glug.oleg@gmail.com>
2021-03-24 13:25:08 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+for (let i = 0; i < inputs.length; i++) {
+  let placeholder = inputs[i].name.replace(/([A-Z])/g, ' $1');
+
+  placeholder = placeholder[0].toUpperCase() + placeholder.slice(1);
+
+  inputs[i].placeholder = placeholder;
+
+  const label = document.createElement('label');
+
+  label.htmlFor = inputs[i].id;
+  label.textContent = placeholder;
+  label.className = 'field-label';
+
+  inputs[i].parentElement.prepend(label);
+}
