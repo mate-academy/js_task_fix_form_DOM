cf49a6f390b1f98cbe1ff3a0b7c2518a6709f17f
Ruslan Kalyniak <krv241085krv@icloud.com>
2021-03-24 17:57:03 +0200

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
 // write code here
+
+function convert(str) {
+  const newStr = str[0].toUpperCase() + str.slice(1);
+  let result = '';
+
+  for (let i = 0; i < newStr.length; i++) {
+    if (newStr[i] === newStr[i].toUpperCase() && i !== 0) {
+      result += ' ' + newStr[i];
+    } else {
+      result += newStr[i];
+    }
+  }
+
+  return result;
+}
+
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+
+  const placeholder = convert(input.name);
+
+  input.placeholder = placeholder;
+  label.innerText = placeholder;
+
+  input.parentElement.append(label);
+};
