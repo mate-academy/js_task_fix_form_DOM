c6d9ed6e5477ad7b899cef681cdaade87b9f5913
Serhii-Nikitin <serg.niki@i.ua>
2021-03-24 14:21:50 +0200

    atribute for was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,8 +5,8 @@ const inputs = document.querySelectorAll('input');
 for (let i = 0; i < inputs.length; i++) {
   const label = document.createElement('label');
 
-  label.textContent = inputs[i].id;
-  label.innerText = inputs[i].name;
+  label.htmlFor = inputs[i].id;
+  label.textContent = inputs[i].name;
   label.className = 'field-label';
 
   const placeholder = inputs[i].name[0].toUpperCase() + inputs[i].name.slice(1);
