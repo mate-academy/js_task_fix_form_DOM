7a05ca131a0e5193035cbd5ae4d8eec09386e8e2
Alexander Levchenko <a.levchenko603@gmail.com>
2021-03-24 14:16:02 +0200

    dividing was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,7 @@ const allInputs = document.querySelectorAll('.field-text');
 for (const input of allInputs) {
   input.insertAdjacentHTML('beforebegin',
     `<label class = 'field-label' for = '${input.id}'>
-  ${input.name}</label>`
+  ${input.name.replace(/([A-Z]+)/g, ' $1')}</label>`
   );
 
   input.setAttribute('placeholder',
