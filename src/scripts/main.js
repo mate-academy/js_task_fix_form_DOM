bce01285aa39ef422d96990049d660048cea3167
romandubovskyi <rdubowskij@gmail.com>
2021-05-10 16:29:52 +0300

    added place holders and lables for form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+for (let i = 0; i < inputs.length; i++) {
+  const currentInput = inputs[i];
+
+  currentInput.placeholder = currentInput.name;
+
+  const item = document.createElement('label');
+
+  item.setAttribute('for', currentInput.id);
+  item.className = 'field-label';
+  item.textContent = currentInput.name.toUpperCase();
+
+  currentInput.parentNode.append(item);
+}
