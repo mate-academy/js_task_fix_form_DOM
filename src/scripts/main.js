a09f738eddd71fcea6763f51cff5d8a476fff112
Maksym Maksymenko <m.maksymenko@smartyads.com>
2021-05-14 13:19:26 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputsList = [...document.querySelectorAll('input')];
+
+function helperFunc(list) {
+  list.forEach(item => {
+    const label = document.createElement('label');
+    const inputText = item.name.slice(0, 1).toUpperCase() + item.name.slice(1);
+
+    label.className = 'field-label';
+    label.setAttribute('for', item.id);
+    label.textContent = item.name.toUpperCase();
+    item.before(label);
+    item.setAttribute('placeholder', inputText);
+  });
+}
+
+helperFunc(inputsList);
