ea844987b1c0f7b35eafdc187f8cf26b000ec89a
Inna <inna_temis@ukr.net>
2021-05-18 20:16:24 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write code here
+const input = [...document.querySelectorAll('input')];
+
+for (const elem of input) {
+  const label = document.createElement('label');
+
+  label.setAttribute('class', 'field-label');
+  label.setAttribute('for', elem.id);
+  label.setAttribute('textContent', elem.name);
+  elem.setAttribute('placeholder', firstLetter(elem.name));
+}
+
+function firstLetter(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
