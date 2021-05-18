ab343d36fc473dae41d8ad284f924ab910996bd6
Ihor Karpyn <igor82200@gmail.com>
2021-05-18 15:41:45 +0300

    added label and placeholder

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,39 +1,14 @@
 'use strict';
 
-const firstName = document.getElementById('sign-up-name');
-const lastName = document.getElementById('sign-up-last-name');
-const emailSignUp = document.getElementById('sign-up-email');
-const emailSignIn = document.getElementById('sign-in-email');
-const passwordSignUp = document.getElementById('sign-up-password');
-const passwordSignIn = document.getElementById('sign-in-password');
+const inputs = document.querySelectorAll('input');
 
-const changeList = [
-  {
-    element: firstName, text: 'First Name', id: 'sign-up-name',
-  },
-  {
-    element: lastName, text: 'Last Name', id: 'sign-up-last-name',
-  },
-  {
-    element: emailSignUp, text: 'Email', id: 'sign-up-email',
-  },
-  {
-    element: emailSignIn, text: 'Email', id: 'sign-in-email',
-  },
-  {
-    element: passwordSignUp, text: 'Password', id: 'sign-up-password',
-  },
-  {
-    element: passwordSignIn, text: 'Password', id: 'sign-in-password',
-  },
-];
+for (const input of inputs) {
+  const inputText = input.name.split('N').join(' N');
+  const label = document.createElement('label');
 
-for (const form of changeList) {
-  form.element.placeholder = form.text;
-
-  form.element.parentElement.insertAdjacentHTML('afterbegin',
-    `
-    <label for="${form.id}" class="field-label" >${form.text}</label>
-    `
-  );
+  input.placeholder = inputText[0].toUpperCase() + inputText.slice(1);
+  label.className = 'field-label';
+  label.textContent = inputText;
+  label.htmlFor = input.id;
+  input.parentElement.append(label);
 }
