041b91166769e566db0c96b40fdcabb51dd77444
Andriy Fesych <andriyhimself@gmail.com>
2021-05-29 16:45:11 +0300

    Solution_0.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+function addingLabel(element) {
+  const newLabel = document.createElement('label');
+
+  newLabel.className = 'field-label';
+  newLabel.textContent = `${element.name}`;
+  newLabel.for = element.id;
+
+  element.placeholder = `${element.name.charAt(0).toUpperCase()
+  + element.name.slice(1)}`; // Capitalizing & setting placeholder
+  element.parentElement.append(newLabel);
+}
+
+Array.from(document.querySelectorAll('input')).map(node => {
+  addingLabel(node);
+});
