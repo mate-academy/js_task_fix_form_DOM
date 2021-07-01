a9c5234d20b8f55d6b1e382bc7f9a572b5b1ae2d
Artem Yemelyanov <ae1666528@gmail.com>
2021-07-01 17:10:39 +0300

    Fixed auth form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+inputs.forEach(input => {
+  const inputParent = input.parentElement;
+  const inputLabel = document.createElement('label');
+
+  inputLabel.for = input.id;
+  inputLabel.textContent = input.name.toUpperCase();
+  inputLabel.className += 'field-label';
+
+  inputParent.appendChild(inputLabel);
+});
+
+inputs.forEach(input => {
+  switch (input.name) {
+    case 'firstName': {
+      input.placeholder = 'First Name';
+      break;
+    }
+
+    case 'lastName': {
+      input.placeholder = 'Last Name';
+      break;
+    }
+
+    case 'email': {
+      input.placeholder = 'Email';
+      break;
+    }
+
+    case 'password': {
+      input.placeholder = 'Password';
+      break;
+    }
+  }
+});
