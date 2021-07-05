1533f1834a652ccf5f2401ca156a3522b9238ed4
korzh <korzh.alexey@yahoo.com>
2021-07-05 19:49:09 +0300

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+const fields = document.querySelectorAll('.field');
+
+for (let i = 0; i < inputs.length; i++) {
+  const label = document.createElement('label');
+  const input = inputs[i];
+  const field = fields[i];
+  let title = input.name[0].toUpperCase() + input.name.slice(1);
+  const splittedTitleForCycle = title.split('');
+
+  field.style.paddingTop = '20px';
+  label.htmlFor = input.id;
+  title = title.split('');
+
+  for (let j = 1; j < splittedTitleForCycle.length; j++) {
+    if (
+      splittedTitleForCycle[j].toUpperCase()
+      === splittedTitleForCycle[j]
+    ) {
+      title.splice(j, 0, ' ');
+    }
+  }
+
+  title = title.join('');
+  label.innerText = title;
+  input.placeholder = title;
+
+  field.prepend(label);
+}
