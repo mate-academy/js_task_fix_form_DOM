5d6bd442ff8cfafff69ea26105424377e44d1b8e
Artem Dzhereleiko <dgerik2015@gmail.com>
2021-07-05 21:14:02 +0300

    fix placeholder

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,13 +5,15 @@ const inputs = document.querySelectorAll('.field-text');
 
 for (let i = 0; i < inputs.length; i++) {
   const label = document.createElement('label');
-  const nameOfInput = inputs[i].name.toUpperCase();
+  const inputName = inputs[i].name;
 
   label.className = 'field-label';
-  label.innerText = nameOfInput;
+  label.innerText = inputName.toUpperCase();
   label.htmlFor = inputs[i].id;
 
   inputsContainer[i].prepend(label);
 
-  inputs[i].placeholder = nameOfInput;
+  inputs[i].placeholder
+    = (inputName.charAt(0).toUpperCase() + inputName.slice(1))
+      .replace(/([A-Z]+)*([A-Z][a-z])/g, '$1 $2');
 }
