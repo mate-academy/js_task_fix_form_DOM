c79976de24adcaba01c034bebc2e75ce68c77b4c
John <jozaman96@gmail.com>
2021-07-05 19:34:11 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.for = input.id;
+  label.textContent = input.name;
+  input.parentElement.prepend(label);
+
+  if (input.name === 'firstName') {
+    input.placeholder = 'First Name';
+  } else if (input.name === 'lastName') {
+    input.placeholder = 'Last Name';
+  } else if (input.name === 'email') {
+    input.placeholder = 'Email';
+  } else {
+    input.placeholder = 'Password';
+  }
+});
