b5c2cefdfdbfef9b08109dd1e0ff10d418fbbec4
Yaslik1033 <55955258+Yaslik1033@users.noreply.github.com>
2021-07-05 16:59:13 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+function getPlaceholder(str) {
+  const placeholder = str.split('');
+
+  placeholder[0] = placeholder[0].toUpperCase();
+
+  return placeholder.join('');
+}
+
+const fields = document.querySelectorAll('.field');
+
+for (const field of fields) {
+  const input = field.querySelector('input');
+  const label = document.createElement('label');
+
+  input.placeholder = getPlaceholder(input.name);
+
+  label.textContent = input.placeholder;
+  label.htmlFor = input.id;
+  label.classList.add('field-label');
+
+  field.prepend(label);
+}
