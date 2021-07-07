b78509d0ff90c9184d8d70282cbf22dd5450cc52
Андрій Наливайко <andrijnalivajko@MacBook-Pro-Andrij.local>
2021-07-07 16:48:53 +0300

    Check me again

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,6 @@
 'use strict';
 
 const getAllInputs = document.querySelectorAll('.field');
-const firstForm = document.body.children[0].children[0];
-const secondForm = document.body.children[0].children[1];
 
 for (let i = 0; i < getAllInputs.length; i++) {
   const child = getAllInputs[i].children[0];
@@ -15,15 +13,5 @@ for (let i = 0; i < getAllInputs.length; i++) {
   child.placeholder = child.name.charAt(0).toUpperCase()
   + child.name.slice(1).split('t').join('t ');
 
-  if (i < 4) {
-    firstForm.children[i].append(createLable);
-  };
-
-  if (i === 4) {
-    secondForm.children[0].append(createLable);
-  };
-
-  if (i === 5) {
-    secondForm.children[1].append(createLable);
-  };
+  getAllInputs[i].append(createLable);
 };
