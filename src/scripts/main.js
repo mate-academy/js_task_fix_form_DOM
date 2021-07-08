3c48abd6edfbfb7efab35f489dce009ba03294ff
Bishko <yurik322412@gmail.com>
2021-07-08 14:25:42 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+const label = document.createElement('label');
+
+inputs.forEach(input => {
+  label.classList.add('field-label');
+  label.innerText = input.name;
+  label.htmlFor = input.id;
+  input.insertAdjacentHTML('beforebegin', label.outerHTML);
+  input.placeholder = capitalize(input.name);
+});
+
+function capitalize(text) {
+  const splitedWord = text.replace(/([A-Z]+)/g, ' $1');
+
+  return splitedWord[0].toUpperCase() + splitedWord.slice(1);
+}
