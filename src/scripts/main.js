eb84728fbe9aae2356786a7aad947ff872d86f80
David Dohru <tronsmartmy@gmail.com>
2021-07-08 16:12:51 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const forLabel = [ ...document.querySelectorAll('.field') ];
+const allInputs = [ ...document.querySelectorAll('input') ];
+
+for (let i = 0; i < allInputs.length; i++) {
+  const elementLabel = document.createElement('label');
+  const value = allInputs[i];
+  const index = value.name.indexOf('N');
+
+  elementLabel.setAttribute('for', value.id);
+
+  value.placeholder = value.name[0].toUpperCase()
+  + value.name.slice(1);
+
+  if (value.placeholder.includes('N')) {
+    value.placeholder = value.name[0]
+      .toUpperCase() + value.name.slice(1, index)
+    + ' ' + value.name.slice(index, value.name.length);
+  }
+  elementLabel.textContent = value.placeholder;
+  elementLabel.textContent = value.name;
+  elementLabel.classList.add('field-label');
+  forLabel[i].append(elementLabel);
+}
