651b180cc6aba43643389dceeb08206007b9ce0d
David Dohru <tronsmartmy@gmail.com>
2021-07-09 15:16:02 +0300

    add function capitalize and fixed loop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,17 @@
 'use strict';
 
-const forLabel = [ ...document.querySelectorAll('.field') ];
+const capitalize = (str) => {
+  return str.replace(/([A-Z]+)/g, ' $1').toLowerCase();
+};
+
 const allInputs = [ ...document.querySelectorAll('input') ];
 
-for (let i = 0; i < allInputs.length; i++) {
+for (const item of allInputs) {
   const elementLabel = document.createElement('label');
-  const value = allInputs[i];
-  const index = value.name.indexOf('N');
 
-  elementLabel.setAttribute('for', value.id);
-
-  value.placeholder = value.name[0].toUpperCase() + value.name.slice(1);
-
-  if (value.placeholder.includes('N')) {
-    value.placeholder = value.name[0]
-      .toUpperCase() + value.name.slice(1, index)
-    + ' ' + value.name.slice(index, value.name.length);
-  }
-  elementLabel.textContent = value.placeholder;
+  item.placeholder = capitalize(item.name);
+  elementLabel.textContent = capitalize(item.name);
   elementLabel.classList.add('field-label');
-  forLabel[i].append(elementLabel);
+  elementLabel.setAttribute('for', item.id);
+  item.parentElement.prepend(elementLabel);
 }
