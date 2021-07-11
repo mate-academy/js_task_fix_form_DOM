80b787d6b8889a70200c457e84c43ba87ab2a31e
Oleksii Blezniuk <oleksii.blezniuk@gmail.com>
2021-07-11 19:47:54 +0300

    task is completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // write code here
+const inputs = [...document.querySelectorAll('input')];
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+  const container = input.parentElement;
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+  input.placeholder = input.name.toUpperCase();
+
+  container.append(label);
+}
