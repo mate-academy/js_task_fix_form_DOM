ff08004e0c0ce14dcdb3ad4f66e3ac94aaec9685
Oleksii Blezniuk <oleksii.blezniuk@gmail.com>
2021-07-12 17:53:32 +0300

    format of placeholders was changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,19 @@
 
 // write code here
 const inputs = [...document.querySelectorAll('input')];
+const stringToNewFormat = function(string) {
+  const newWordIndex = [...string].findIndex(letter => {
+    return letter === letter.toUpperCase();
+  });
+  let formatedString = string.charAt(0).toUpperCase() + string.slice(1);
+
+  if (newWordIndex >= 0) {
+    formatedString = formatedString.slice(0, newWordIndex)
+    + ' ' + formatedString.slice(newWordIndex).toLowerCase();
+  }
+
+  return formatedString;
+};
 
 for (const input of inputs) {
   const label = document.createElement('label');
@@ -9,8 +22,8 @@ for (const input of inputs) {
 
   label.className = 'field-label';
   label.setAttribute('for', input.id);
-  label.textContent = input.name;
-  input.placeholder = input.name.toUpperCase();
+  label.textContent = stringToNewFormat(input.name);
+  input.placeholder = stringToNewFormat(input.name);
 
   container.append(label);
 }
