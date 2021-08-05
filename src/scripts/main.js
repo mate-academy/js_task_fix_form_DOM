66b00a8bca69cbac69df0d543b1c962f988dce2e
Raz-Dva <my.poshta.goncharov@gmail.com>
2021-08-05 18:58:37 +0200

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputsText = document.querySelectorAll('.field-text');
+
+[...inputsText].forEach((input) => {
+  const idInput = input.getAttribute('id');
+  const nameInput = input.getAttribute('name');
+  const label = document.createElement('label');
+  const parentInput = input.closest('.field');
+
+  label.className = 'field-label';
+  label.setAttribute('for', idInput);
+  label.textContent = nameInput;
+
+  parentInput.insertBefore(label, input);
+  input.placeholder = nameInput;
+});
