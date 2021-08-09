49481ec20c0354e03fe223c75983bc7a2c3515d4
Taras Pyda <Tapac1991@icloud.com>
2021-08-09 22:45:21 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+function unCamelCase(string) {
+  let newStirng = '';
+
+  for (let letter of string) {
+    if (letter === letter.toUpperCase()) {
+      letter = ` ${letter}`;
+    }
+    newStirng += letter;
+  }
+
+  return newStirng.charAt(0).toUpperCase() + newStirng.slice(1).toLowerCase();
+};
+
+const inputs = document.getElementsByTagName('input');
+
+for (const input of inputs) {
+  input.insertAdjacentHTML('beforebegin', `
+      <label class="field-label" for="${input.id}">
+        ${unCamelCase(input.name)}
+      </label>
+    `);
+
+  input.placeholder = unCamelCase(input.name);
+}
