e3e074fe0df2c9e6629c0a9969853055d5314033
Ihor Nahnybida <igor653279@gmail.com>
2021-08-15 12:56:04 +0300

    changed main.js; labels added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const pageForms = document.getElementsByTagName('form');
+
+const normalizeInputField = (forms) => {
+  const formsArray = [...forms];
+
+  for (const form of formsArray) {
+    for (const input of form) {
+      if (input.className !== 'field-text') {
+        continue;
+      }
+
+      const labelText = input.name.replace(
+        /([a-z])([A-Z])/g,
+        '$1 $2'
+      ).toLowerCase();
+
+      input.placeholder = labelText.slice(0, 1).toUpperCase()
+        + labelText.slice(1);
+
+      input.insertAdjacentHTML('afterend', `
+        <label
+          class="field-label"
+          for=${input.id}
+        >
+          ${labelText}
+        </label>
+      `);
+    }
+  }
+};
+
+normalizeInputField(pageForms);
