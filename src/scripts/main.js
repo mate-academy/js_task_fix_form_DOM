0f0ed6de35e751d988dfe8b0c88e8f3f2744a14a
Ihor Nahnybida <igor653279@gmail.com>
2021-08-16 11:53:02 +0300

    simplified

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,26 @@
 'use strict';
 
-const pageForms = document.getElementsByTagName('form');
+const pageInputs = document.getElementsByTagName('input');
 
 const normalizeInputField = (forms) => {
-  const formsArray = [...forms];
+  for (const input of [...forms]) {
+    const labelText = input.name.replace(
+      /([a-z])([A-Z])/g,
+      '$1 $2'
+    ).toLowerCase();
 
-  for (const form of formsArray) {
-    for (const input of form) {
-      if (input.className !== 'field-text') {
-        continue;
-      }
+    input.placeholder = labelText.slice(0, 1).toUpperCase()
+      + labelText.slice(1);
 
-      const labelText = input.name.replace(
-        /([a-z])([A-Z])/g,
-        '$1 $2'
-      ).toLowerCase();
-
-      input.placeholder = labelText.slice(0, 1).toUpperCase()
-        + labelText.slice(1);
-
-      input.insertAdjacentHTML('afterend', `
-        <label
-          class="field-label"
-          for=${input.id}
-        >
-          ${labelText}
-        </label>
-      `);
-    }
+    input.insertAdjacentHTML('afterend', `
+      <label
+        class="field-label"
+        for=${input.id}
+      >
+        ${labelText}
+      </label>
+    `);
   }
 };
 
-normalizeInputField(pageForms);
+normalizeInputField(pageInputs);
