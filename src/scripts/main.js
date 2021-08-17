5ab1b6742664712e67ee296fcc877ad6b7e11286
Alex Hlotov <alexhlotov67@gmail.com>
2021-08-17 16:46:45 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const inputFields = [...document.querySelectorAll('.field')];
+const formInputs = [...document.querySelectorAll('input')];
+const classFieldLabel = 'field-label';
+
+for (let i = 0; i < formInputs.length; i++) {
+  let fieldName = formInputs[i].name
+    .replace(/([a-z])([A-Z])/g, '$1 $2')
+    .split('');
+
+  fieldName[0] = fieldName[0].toUpperCase();
+
+  fieldName = fieldName.join('');
+
+  inputFields[i].insertAdjacentHTML('afterbegin', `
+    <label class=${classFieldLabel} for=${formInputs[i].id}>
+      ${fieldName}
+    </label>
+  `);
+  formInputs[i].setAttribute('placeholder', fieldName);
+}
