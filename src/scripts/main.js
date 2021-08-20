a95effc4b92b5e8382f3b3bb338b72c2e952f4f0
Alexandr Somov <aleksandr.somov2011@gmail.com>
2021-08-20 22:00:21 +0300

    'solution'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+const upperChart = (str) => {
+  return str[0].toUpperCase() + str.slice(1);
+};
+
+[...inputs].map((item) => {
+  item.insertAdjacentHTML('beforebegin',
+    `<label for="${item.id}" class="field-label">${item.name}</label>`);
+  item.placeholder = upperChart(item.name);
+});
