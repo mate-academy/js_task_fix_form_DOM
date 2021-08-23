92c35814e764990bfb450129a1435720e5e4590c
Raz-Dva <my.poshta.goncharov@gmail.com>
2021-08-23 18:43:47 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,13 +5,13 @@ const inputsText = document.querySelectorAll('.field-text');
 [...inputsText].forEach((input) => {
   const idInput = input.getAttribute('id');
   const nameInput = input.getAttribute('name');
-  const label = document.createElement('label');
+  const labelElement = document.createElement('label');
   const parentInput = input.closest('.field');
 
-  label.className = 'field-label';
-  label.setAttribute('for', idInput);
-  label.textContent = nameInput;
+  labelElement.className = 'field-label';
+  labelElement.setAttribute('for', idInput);
+  labelElement.textContent = nameInput;
 
-  parentInput.insertBefore(label, input);
+  parentInput.insertBefore(labelElement, input);
   input.placeholder = nameInput;
 });
