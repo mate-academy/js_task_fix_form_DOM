23c3e2ee838164193322ada56607b15e095ca71c
Roman Minchenko <minchenko.poma@gmail.com>
2021-08-24 20:17:07 +0300

    fix_form_DOM-done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const parentInput = document.querySelectorAll('.field');
+
+const capitalize = (string) => {
+  const firstLetter = string.substr(0, 1).toLocaleUpperCase();
+  const restLetter = string.substr(1, string.length);
+  const newWord = firstLetter + restLetter;
+
+  return newWord;
+};
+
+[...parentInput].forEach(element => {
+  const input = element.querySelector('input');
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+  input.placeholder = capitalize(input.name);
+  element.append(label);
+
+  return element;
+});
