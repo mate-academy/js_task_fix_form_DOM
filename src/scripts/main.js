0b14a83d1de8be085c388b7c074a60c72e27be4f
Roman <romakelemen@gmail.com>
2021-09-04 11:04:46 +0300

    solved_task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,23 @@
 'use strict';
 
-const item = document.getElementsByTagName('input');
+const items = document.getElementsByTagName('input');
 
-[...item].map((item) => { 
+[...items].map((item) => {
+  const placeholder
+    = item.name.split(/(?=[A-Z])/).length >= 2
+      ? item.name.split(/(?=[A-Z])/)[0].charAt(0).toLocaleUpperCase()
+        + item.name.split(/(?=[A-Z])/)[0].slice(1) + ' '
+        + item.name.split(/(?=[A-Z])/)[1]
+      : item.name.split(/(?=[A-Z])/)[0].charAt(0).toLocaleUpperCase()
+        + item.name.split(/(?=[A-Z])/)[0].slice(1);
 
-const placeholder = 
-  item.name.split(/(?=[A-Z])/).length >=2
-  ?item.name.split(/(?=[A-Z])/)[0].charAt(0).toLocaleUpperCase()
-    + item.name.split(/(?=[A-Z])/)[0].slice(1) + ' ' +item.name.split(/(?=[A-Z])/)[1]
-  :item.name.split(/(?=[A-Z])/)[0].charAt(0).toLocaleUpperCase()
-    + item.name.split(/(?=[A-Z])/)[0].slice(1);
+  const labelName
+    = item.name.split(/(?=[A-Z])/).length >= 2
+      ? item.name.split(/(?=[A-Z])/)[0].toUpperCase()
+        + ' ' + item.name.split(/(?=[A-Z])/)[1].toUpperCase()
+      : item.name.split(/(?=[A-Z])/)[0].toUpperCase();
 
-const labelName = 
-  item.name.split(/(?=[A-Z])/).length >= 2
-  ?item.name.split(/(?=[A-Z])/)[0].toUpperCase()+ ' ' + item.name.split(/(?=[A-Z])/)[1].toUpperCase()
-  :item.name.split(/(?=[A-Z])/)[0].toUpperCase();
-
-
-item.parentElement.innerHTML = `
+  item.parentElement.innerHTML = `
   <label 
     class="field-label" 
     for="${item.name}">
