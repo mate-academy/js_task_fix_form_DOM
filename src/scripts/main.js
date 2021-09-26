293fcdd163b3ad554c8410b3eff37c8080029122
bohdan_kochetov <bodja1208@gmail.com>
2021-09-26 12:10:34 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.setAttribute('for', input.id);
+  label.className = 'field-label';
+  input.placeholder = firstLetterToUpperCase(input.name);
+
+  input.before(label);
+}
+
+function firstLetterToUpperCase(string) {
+  const stringSplit = string.split('');
+
+  stringSplit[0] = stringSplit[0].toUpperCase();
+
+  return stringSplit.join('');
+}
