e1a09b689c80d8ae984caee9f904f05c4d21c022
Roman Prokopiv <rprokopiv@gmail.com>
2021-10-01 21:35:10 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const elements = document.querySelectorAll('.field');
+
+for (const element of elements) {
+  const nameInput = convertName(element.children[0].name);
+
+  element.children[0].placeholder = nameInput;
+
+  element.insertAdjacentHTML('afterbegin', `
+  <label for="${element.children[0].id}"
+    class="field-label">
+    ${nameInput}
+  </label>
+`);
+}
+
+function convertName(names) {
+  let result = '';
+
+  for (let i = 0; i < names.length; i++) {
+    let letter = names[i];
+
+    if (letter === names[i].toUpperCase()) {
+      result += ' ';
+    };
+
+    if (i === 0) {
+      letter = names[i].toUpperCase();
+    };
+
+    result += letter;
+  };
+
+  return result;
+}
