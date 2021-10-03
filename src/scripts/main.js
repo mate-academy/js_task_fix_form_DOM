0b14536dac88b8598944153c7f638708eeb0bfe6
Andrii Malyshev <andrey.malyshev.97@gmail.com>
2021-10-03 16:04:53 +0300

    js_task_fix_form_DOM done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const inputFields = document.querySelectorAll('input');
+
+for (const inputs of inputFields) {
+  const labelTag = document.createElement('label');
+  const capitalize = string => string[0].toUpperCase() + string.slice(1);
+
+  labelTag.className = 'field-inputLabel';
+  labelTag.setAttribute('for', inputs.id);
+  labelTag.innerText = capitalize(inputs.name);
+  inputs.placeholder = capitalize(inputs.name);
+
+  inputs.before(labelTag);
+};
