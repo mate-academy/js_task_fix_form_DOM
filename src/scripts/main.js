fd74b65419eee1d722a4ac4bc7603342290dc645
Andrii Malyshev <andrey.malyshev.97@gmail.com>
2021-10-03 16:38:03 +0300

    update code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,6 @@
 'use strict';
 
-const inputFields = document.querySelectorAll('input');
-
-for (const inputs of inputFields) {
+document.querySelectorAll('input').forEach(inputs => {
   const labelTag = document.createElement('label');
   const capitalize = string => string[0].toUpperCase() + string.slice(1);
 
@@ -10,6 +8,5 @@ for (const inputs of inputFields) {
   labelTag.setAttribute('for', inputs.id);
   labelTag.innerText = capitalize(inputs.name);
   inputs.placeholder = capitalize(inputs.name);
-
   inputs.before(labelTag);
-};
+});
