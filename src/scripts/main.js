e9ec08be2de6295d189767d1179dceb2f248c2db
Andrii Malyshev <andrey.malyshev.97@gmail.com>
2021-10-03 16:14:08 +0300

    fixed class name

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,7 @@ for (const inputs of inputFields) {
   const labelTag = document.createElement('label');
   const capitalize = string => string[0].toUpperCase() + string.slice(1);
 
-  labelTag.className = 'field-inputLabel';
+  labelTag.className = 'field-label';
   labelTag.setAttribute('for', inputs.id);
   labelTag.innerText = capitalize(inputs.name);
   inputs.placeholder = capitalize(inputs.name);
