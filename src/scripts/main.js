9e077274d38c5ed1fa94afc320ccfe6d7c759bea
Dima Narozhnyi <dmitriy.narozhnyi@gmail.com>
2021-10-16 09:08:23 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+function capitalize(string) {
+  const capitalizeString = string.charAt(0).toUpperCase()
+  + string.slice(1).toLowerCase();
+
+  return capitalizeString;
+}
+
+const inputs = document.querySelectorAll('form input');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = `${input.id}`;
+  label.textContent = `${input.name}`;
+  input.setAttribute('placeholder', `${capitalize(input.name)}`);
+
+  const parentContainer = input.parentElement;
+
+  parentContainer.prepend(label);
+}
