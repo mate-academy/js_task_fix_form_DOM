03538deafb6fdda83e79ad7ba38f679f50649999
Ilona Shulha <ilon.sh.ilo@gmail.com>
2021-10-17 18:32:03 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll(`input`);
+
+[...inputs].forEach(item => {
+  const thisName = item.getAttribute('name');
+
+  item.setAttribute(
+    'placeholder',
+    thisName[0].toUpperCase() + thisName.slice(1)
+  );
+
+  const sticker = document.createElement(`label`);
+
+  sticker.className = 'field-label';
+  sticker.setAttribute('for', item.getAttribute('id'));
+
+  sticker.textContent = thisName;
+
+  item.parentNode.append(sticker);
+});
