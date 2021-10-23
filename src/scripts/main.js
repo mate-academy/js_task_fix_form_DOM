11117a6e666edd9c5907f99ced3a8c908b2b9b2d
Serhii Dvorets <dvorets.sergey@gmail.com>
2021-10-23 18:15:31 +0300

    1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
 // write code here
+
+[...document.querySelectorAll('input')].map(input => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+  input.append(label);
+  input.placeholder = input.name;
+});
