d571cc5ca6ba3945dbcdbe9c78f851cb9325d136
Dima Permiakov <dimapermyakov2532@gmail.com>
2021-11-09 21:03:13 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,28 @@
 const inputs = document.querySelectorAll('input');
 
 [...inputs].forEach(input => {
-  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+  const newName = () => {
+    const transformName = input.name.split('');
+
+    for (const key in transformName) {
+      if (transformName[key] === transformName[key].toUpperCase()) {
+        transformName.splice(key, 0, ' ');
+        break;
+      } else if (+key === 0) {
+        transformName[key] = transformName[key].toUpperCase();
+      }
+    }
+
+    return transformName.join('');
+  };
+
+  input.placeholder = newName();
 
   const label = document.createElement('label');
 
   label.className = 'field-label';
   label.htmlFor = input.id;
-  label.textContent = input.name;
+  label.textContent = newName();
 
   input.parentElement.append(label);
 });
