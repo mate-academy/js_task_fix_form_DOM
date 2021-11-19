25e70169ba0e68584a7789a2ac0615e5f043fa4f
Dariia Romanova <dariia.romanova@gmail.com>
2021-11-19 15:08:40 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+function convertCamelCase(text) {
+  return text.replace(/([A-Z])/g, ' $1');
+}
+
+const inputs = document.querySelectorAll('.field > input');
+
+for (const input of [...inputs]) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = convertCamelCase(input.name);
+  input.parentElement.append(label);
+
+  input.placeholder = convertCamelCase(input.name).toUpperCase();
+}
