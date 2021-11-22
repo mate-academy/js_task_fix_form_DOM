8e08b8b10779b4b67d49fd195309980a470714ea
unknown <skirik0117@gmail.com>
2021-11-22 14:48:25 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+function placeholder(input) {
+  const nameOfInput = getName(input);
+
+  input.placeholder = nameOfInput[0].toUpperCase() + nameOfInput.slice(1);
+}
+
+function getName(input) {
+  return input.name.replace(/([A-Z])/g, ' $1');
+}
+
+[...inputs].forEach((input) => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = getName(input);
+
+  input.before(label);
+
+  placeholder(input);
+});
