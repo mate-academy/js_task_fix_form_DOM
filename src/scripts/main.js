ead229765ff39a8ba47daa6cdeca436d59180769
Andrey Grigoriev <nkgrand25@gmail.com>
2021-11-22 13:57:22 +0200

    Fixed sticky words

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,13 +2,27 @@
 
 const inputs = document.querySelectorAll('input');
 
-for (const input of inputs) {
-  const label = document.createElement('label');
+function fillForms() {
+  for (const input of inputs) {
+    const label = document.createElement('label');
 
-  label.className = 'field-label';
-  label.htmlFor = input.id;
-  label.textContent = input.name.toUpperCase();
+    label.className = 'field-label';
+    label.htmlFor = input.id;
+    label.textContent = addTextContent(input.name);
 
-  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
-  input.before(label);
+    input.placeholder = addPlaceholder(input);
+    input.before(label);
+  }
 }
+
+function addTextContent(input) {
+  return input.split(/(?=[A-Z])/g).join(' ');
+}
+
+function addPlaceholder(input) {
+  const newPlaceholder = input.name[0].toUpperCase() + input.name.slice(1);
+
+  return addTextContent(newPlaceholder);
+}
+
+fillForms();
