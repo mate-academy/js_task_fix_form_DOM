20514df17ebf2c257e3b201905b0d500926fc348
Vova Alputov <alputov1998@gmail.com>
2021-12-22 00:37:21 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+[...inputs].forEach(
+  x => x.setAttribute(
+    'placeholder', x.name[0].toUpperCase() + x.name.slice(1)
+  )
+);
+
+[...inputs].forEach(
+  x => x.insertAdjacentHTML(
+    'beforebegin', `<label class = field-label for = ${x.id}>${x.name}</label>`
+  )
+);
