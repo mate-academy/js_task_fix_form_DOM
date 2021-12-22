6f3412e9f39039e02991ba48c0741d97fe44a87f
Vova Alputov <alputov1998@gmail.com>
2021-12-22 17:51:19 +0200

    done2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,13 @@
 
 const inputs = document.querySelectorAll('.field-text');
 
-[...inputs].forEach(
-  x => x.setAttribute(
+[...inputs].forEach((x) => {
+  x.setAttribute(
     'placeholder', x.name[0].toUpperCase() + x.name.slice(1)
-  )
-);
+  );
 
-[...inputs].forEach(
-  x => x.insertAdjacentHTML(
+  x.insertAdjacentHTML(
     'beforebegin', `<label class = field-label for = ${x.id}>${x.name}</label>`
-  )
+  );
+}
 );
