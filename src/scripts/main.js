482541f53df5fd06ced082ee9e52a551e3b42c84
Dmytro Brushnivskyi <dimalvivatc@gmail.com>
2021-12-24 14:03:16 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const input = document.querySelectorAll('input');
+
+for (let i = 0; i < input.length; i++) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input[i].id);
+
+  input[i].setAttribute('placeholder',
+    input[i].name.charAt(0).toUpperCase()
+    + input[i].name.slice(1));
+
+  label.textContent = input[i].name;
+
+  input[i].append(label);
+}
