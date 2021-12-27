68138dd796768fa07ee0bdc28f4afec16944dbf1
KHTRE <KHTREE@gmail.com>
2021-12-27 09:21:29 +0200

    works

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const line of inputs) {
+  line.setAttribute('placeholder', line.name.toUpperCase());
+
+  const labelForInput = document.createElement('label');
+
+  labelForInput.setAttribute('class', 'field-label');
+  labelForInput.setAttribute('for', line.id);
+  labelForInput.textContent = line.name;
+
+  line.append(labelForInput);
+}
