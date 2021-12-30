f7386ddb2ef680124adcb0874e302d89edcfe57f
E-Sharpilo <evgeniy.sharpilo@gmail.com>
2021-12-30 11:54:45 +0200

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  let inputName = input.name.split(/(?=[A-Z])/).join(' ');
+
+  inputName = inputName[0].toUpperCase() + inputName.slice(1);
+
+  input.parentElement.insertAdjacentHTML('afterbegin', `
+  <label class="field-label" for="${input.id}">
+    ${inputName}
+  </label>
+  `);
+
+  input.placeholder = inputName;
+};
