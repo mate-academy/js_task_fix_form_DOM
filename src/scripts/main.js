a33135b3ab92ececc0a2b15917bde2fdba8b0264
Anna Morozevych <annyshka2210@gmail.com>
2022-01-02 17:54:20 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputElements = document.querySelectorAll('input');
+
+for (let i = 0; i < inputElements.length; i++) {
+  const labelElement = document.createElement('label');
+  const splitString = inputElements[i].name.split(/(?=[A-Z])/).join(' ');
+
+  labelElement.className = 'field-label';
+  labelElement.htmlFor = inputElements[i].id;
+  labelElement.textContent = splitString;
+
+  inputElements[i].placeholder
+    = splitString[0].toUpperCase()
+    + splitString.slice(1);
+
+  const parentElement = inputElements[i].parentElement;
+
+  parentElement.append(labelElement);
+}
