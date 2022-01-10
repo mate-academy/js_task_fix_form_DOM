d4286754b31c38ccba3ca9a539fbbe9e8b0f4444
Andriy Yurkiv <88943977+andriy-yurkiv@users.noreply.github.com>
2022-01-10 12:55:51 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const fields = document.querySelectorAll('.field');
+
+for (const field of fields) {
+  const input = field.querySelector('.field-text');
+
+  input.placeholder = input.name;
+
+  input.placeholder = input.placeholder.replace(/([A-Z])/g, ' $1');
+  input.placeholder = input.placeholder.charAt(0).toUpperCase()
+    + input.placeholder.substr(1);
+
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+
+  field.append(label);
+}
