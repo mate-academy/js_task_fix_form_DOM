b1ab70dcaf4681cd5d20a74305f5d4be8712f51f
Daria Veretyak <dariaveretyak@gmail.com>
2022-01-12 22:00:44 +0200

    Changed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,13 @@
 const inputs = document.querySelectorAll('input');
 
 [...inputs].map((el) => {
-  el.insertAdjacentHTML(
-    'beforebegin', `<label class="field-label" for="${el.id}">
-      ${el.name}
+  el.insertAdjacentHTML('beforebegin',
+    `<label class="field-label" for="${el.id}">
+      ${el.name.split(/(?=[A-Z])/).join(' ')}
     </label>`
   );
 
-  el.setAttribute('placeholder', el.name[0].toUpperCase() + el.name.slice(1));
+  el.setAttribute('placeholder',
+    el.name[0].toUpperCase() + el.name.slice(1).split(/(?=[A-Z])/).join(' ')
+  );
 });
