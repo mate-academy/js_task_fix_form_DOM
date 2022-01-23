737ed3e5f8e8b71de76ffdaca8544414e502ad6c
Luhovyi <95681104+Luhovyi@users.noreply.github.com>
2022-01-23 16:12:14 +0200

    js_task_fix_form_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write code here
+const inputs = document.querySelectorAll('.field-text');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.className = `field-label`;
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+  // eslint-disable-next-line max-len
+  input.setAttribute('placeholder', input.name[0].toUpperCase() + input.name.slice(1));
+  input.closest('.field').append(label);
+}
