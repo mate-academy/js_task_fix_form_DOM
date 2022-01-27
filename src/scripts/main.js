c518a24c554373477fee41d3e19cb12ed896ef7c
Ivan Shulhan <92580512+IvanShulhan@users.noreply.github.com>
2022-01-27 14:13:32 +0200

    my-solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (let i = 0; i < inputs.length; i++) {
+  const label = document.createElement('label');
+
+  label.className = `field-label`;
+  label.setAttribute('for', inputs[i].id);
+  label.textContent = inputs[i].name;
+
+  inputs[i].append(label);
+
+  const placeholderText = inputs[i].name[0].toUpperCase()
+  + inputs[i].name.slice(1);
+
+  inputs[i].setAttribute('placeholder', placeholderText);
+}
