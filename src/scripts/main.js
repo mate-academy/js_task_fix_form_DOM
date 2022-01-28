773819ceddf4b3dd55f5f0139220dedb7b826b44
Ladyha Roman <ladrom.ua@gmail.com>
2022-01-28 15:25:35 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+for (const input of inputs) {
+  const inputParent = input.parentElement;
+  const label = document.createElement('label');
+
+  inputParent.append(label);
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.innerText = input.name;
+  input.placeholder = toCapitalize(input.name);
+}
+
+function toCapitalize(word) {
+  return word[0].toUpperCase() + word.slice(1);
+}
