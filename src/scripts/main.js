242d110f115c4db41213956d5ea4897cceb86bdb
Oleh Rak <rakole86@gmail.com>
2022-01-29 10:27:12 +0200

    Fix_Form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('input');
+
+const input = document.querySelectorAll('input');
+
+for (const item of input) {
+  item.placeholder = item.name.charAt(0).toLocaleUpperCase(0)
+  + item.name.slice(1);
+
+  const elem = document.createElement('label');
+
+  elem.setAttribute('class', 'field-label');
+  elem.setAttribute('id', item.id);
+
+  elem.textContent = item.name.charAt(0).toLocaleUpperCase(0)
+  + item.name.slice(1);
+  item.parentElement.prepend(elem);
+}
