974d59f5081926387fbc74352be932ce4ba56eda
Roma Afanasiev <romafanasiev123@gmail.com>
2022-02-03 19:56:35 +0200

    add placeholders for forms

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
 // write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(el => {
+  const label = document.createElement('label');
+  const inputName = el.getAttribute('name').toLowerCase();
+  const placeholderName = inputName[0].toUpperCase()
+  + inputName.substring(1);
+
+  el.setAttribute('placeholder', placeholderName);
+
+  el.before(label);
+  label.setAttribute('class', 'field-label');
+  label.setAttribute('for', el.getAttribute('id'));
+  label.textContent = el.getAttribute('name');
+});
