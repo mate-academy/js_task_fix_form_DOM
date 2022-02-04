1f58f65d0e3bdb4c2c30b57734d121f3e2d891a4
Roma Afanasiev <romafanasiev123@gmail.com>
2022-02-04 18:56:01 +0200

    fix lastNames and Firsnames for placeholders and labels

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,13 +6,26 @@ const inputs = document.querySelectorAll('input');
 inputs.forEach(el => {
   const label = document.createElement('label');
   const inputName = el.getAttribute('name').toLowerCase();
-  const placeholderName = inputName[0].toUpperCase()
+  let placeholderName = inputName[0].toUpperCase()
   + inputName.substring(1);
+
+  if (el.getAttribute('name') === 'firstName') {
+    placeholderName = 'First name';
+  } else if (el.getAttribute('name') === 'lastName') {
+    placeholderName = 'Last name';
+  }
 
   el.setAttribute('placeholder', placeholderName);
 
   el.before(label);
   label.setAttribute('class', 'field-label');
   label.setAttribute('for', el.getAttribute('id'));
-  label.textContent = el.getAttribute('name');
+
+  if (el.getAttribute('name') === 'firstName') {
+    label.textContent = 'First name';
+  } else if (el.getAttribute('name') === 'lastName') {
+    label.textContent = 'Last name';
+  } else {
+    label.textContent = el.getAttribute('name');
+  }
 });
