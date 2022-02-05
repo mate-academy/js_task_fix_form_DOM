85d05c08b322061323b951cd45c911b3119e4f8f
Roma Afanasiev <romafanasiev123@gmail.com>
2022-02-05 15:01:18 +0200

    add functions for 3 values in name

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,11 +13,17 @@ inputs.forEach(el => {
     + el.getAttribute('name').substring(1);
     label.textContent = el.getAttribute('name');
   } else {
-    placeholderName = inputName[0][0].toUpperCase() + inputName[0].substring(1)
-    + ' ' + inputName[1][0].toUpperCase() + inputName[1].substring(1);
+    let str1 = '';
+    let str2 = '';
 
-    label.textContent = inputName[0].toUpperCase()
-    + ' ' + inputName[1].toUpperCase();
+    for (const key in inputName) {
+      str1 += inputName[key][0].toUpperCase()
+      + inputName[key].substring(1) + ' ';
+      str2 += inputName[key].toUpperCase() + ' ';
+    }
+
+    placeholderName = str1;
+    label.textContent = str2;
   }
 
   el.before(label);
