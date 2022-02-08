a7dafec46eceb937719fdb6bf46663098cf55e0a
KristinaTar <kristina.tarashchuk@gmail.com>
2022-02-08 22:22:42 +0200

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputsArr = Array.from(document.getElementsByTagName('input'));
+
+for (const element of inputsArr) {
+  const label = document.createElement('label');
+
+  const parrentElem = element.parentElement;
+
+  parrentElem.insertBefore(label, element);
+  label.className = 'field-label';
+  label.textContent = element.attributes.name.nodeValue;
+
+  element.setAttribute('placeholder', `${
+    label.textContent.charAt(0).toUpperCase() + label.textContent.slice(1)}`);
+
+  const forAttr = element.attributes.id.nodeValue;
+
+  label.setAttribute('for', `${forAttr}`);
+};
