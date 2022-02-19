ea468e49b57d0fcf64067cd27faf1a68f9b1e62c
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-02-19 15:35:00 +0200

    The script was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const inputsCollection = document.querySelectorAll('input');
+
+for (const elem of inputsCollection) {
+  const labelForInput = document.createElement('label');
+  const parentElem = elem.parentNode;
+
+  labelForInput.className = 'field-label';
+  labelForInput.textContent = elem.name.toUpperCase();
+  labelForInput.setAttribute('for', elem.id);
+  parentElem.prepend(labelForInput);
+
+  elem.setAttribute('placeholder',
+    elem.name[0].toUpperCase() + elem.name.slice(1));
+}
