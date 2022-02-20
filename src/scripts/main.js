a45eef08eb28661c19d4a39d6a236c654e329cb2
Mykhailo Molnar <mmolnar@atlassian.com>
2022-02-20 17:14:45 +0200

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const placeholder = input.name.charAt(0).toUpperCase() 
+  + input.name.slice(1).replace(/([A-Z])/g, ' $1');
+
+  input.insertAdjacentHTML('beforebegin',
+  `<label class = 'field-label' for=${input.id}>
+  ${placeholder};
+  <label/>`
+  )
+  input.placeholder = placeholder;
+}
+
 // write code here
