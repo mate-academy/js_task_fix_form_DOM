59e041cc72ad0c8ce4087ad2a6d60fd99157e382
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-02-22 21:06:35 +0200

    Words in first name and last name were separated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,10 +7,34 @@ for (const elem of inputsCollection) {
   const parentElem = elem.parentNode;
 
   labelForInput.className = 'field-label';
-  labelForInput.textContent = elem.name.toUpperCase();
+
+  let nextWordIndex = null;
+
+  // the code below would work correct only for the name,
+  // which consist of one or two words
+  // in other cases a universal function is required
+  // const labelTextArray = elem.name.split('');
+
+  for (let i = 0; i < elem.name.length; i++) {
+    if (elem.name[i].toUpperCase() === elem.name[i]) {
+      nextWordIndex = i;
+    }
+  }
+
+  let text = '';
+
+  if (nextWordIndex) {
+    text = elem.name.slice(0, nextWordIndex)
+    + ' ' + elem.name.slice(nextWordIndex);
+  } else {
+    text = elem.name.toUpperCase();
+  }
+
   labelForInput.setAttribute('for', elem.id);
   parentElem.prepend(labelForInput);
 
+  labelForInput.textContent = text.toUpperCase();
+
   elem.setAttribute('placeholder',
-    elem.name[0].toUpperCase() + elem.name.slice(1));
+    text[0].toUpperCase() + text.slice(1));
 }
