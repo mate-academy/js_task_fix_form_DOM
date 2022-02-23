d7da5681d53e35c528d5da5bc38b737fa3b9c5f3
Dmytro Semenenko <dmitriy.semenenko@gmail.com>
2022-02-23 13:53:46 +0200

    Solution JS Task Fix Form DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const fields = [...document.querySelectorAll('.field')];
+
+fields.forEach(field => {
+  field.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <label class="field-label" for="${field.children[0].id}">
+      ${field.children[0].name}
+    </label>
+  `
+  );
+
+  field.children[1].placeholder
+    = field.children[1].name.toUpperCase()[0]
+    + field.children[1].name.slice(1);
+});
