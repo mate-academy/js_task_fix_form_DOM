e2eedd8318831180114dfa8441fd9efae3ff0ac0
Yuri Taraday <KaIIRu3@gmail.com>
2022-03-13 20:58:47 +0200

    Append

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('.field')];
+
+inputs.forEach((field) => {
+  field.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <label class ="field-label" for = "${field.children[0].id}">
+      ${field.children[0].name}
+    </label>
+    `
+  );
+
+  field.children[1].placeholder
+    = field.children[1].name.toUpperCase()[0]
+    + field.children[1].name.slice(1);
+});
