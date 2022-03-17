b2444de990ac588598676da0e0dc7906794d53b0
Mykhailo Ianush <yanushmisha@gmail.com>
2022-03-17 17:10:05 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,11 +4,13 @@ const inputs = document.querySelectorAll('input');
 
 inputs.forEach(input => {
   const inputlabel = document.createElement('Label');
+  const inputName = input.name
+    .replace(/([a-z])([A-Z])/, '$1 $2').replace(/\b\w/g, c => c.toUpperCase());
 
   inputlabel.htmlFor = input.id;
-  inputlabel.textContent = input.name;
+  inputlabel.textContent = inputName;
   inputlabel.className = 'field-label';
-  input.placeholder = input.name.toUpperCase();
+  input.placeholder = inputName;
 
   input.before(inputlabel);
 });
