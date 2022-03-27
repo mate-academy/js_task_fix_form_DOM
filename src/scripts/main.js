8d1aa8910459274858385341dbdc0172869f9606
Maria Luschik <marialyschik@Marias-MacBook-Air.local>
2022-03-27 21:40:00 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  const label = document.createElement('label');
+  const cutText = input.name.replace(/([A-Z])/g, ' $1');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = cutText.charAt(0).toUpperCase() + cutText.slice(1);
+
+  input.parentElement.append(label);
+
+  input.placeholder = cutText.charAt(0).toUpperCase() + cutText.slice(1);
+});
