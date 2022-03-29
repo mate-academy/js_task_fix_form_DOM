a2832849183cf1a00d50deaf6a3fd4879de5474c
Hlib Schebalkov <glebusha228@gmail.com>
2022-03-29 19:31:38 +0300

    delete console

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,11 @@
 'use strict';
+
 const input = document.querySelectorAll('input');
 const div = document.querySelectorAll('.field');
 
 for (let i = 0; i < input.length; i++) {
   const label = document.createElement('label');
+
   label.className = 'field-label';
   label.setAttribute('for', input[i].id);
   label.innerHTML = input[i].name;
@@ -11,12 +13,12 @@ for (let i = 0; i < input.length; i++) {
   const slpitName = input[i].name.split('');
   const toUpper = slpitName[0].toLocaleUpperCase();
   const rest = [...slpitName];
+
   rest.splice(0, 1);
+
   const capitalize = [toUpper, ...rest].join('');
 
   input[i].setAttribute('placeholder', capitalize);
 
   div[i].append(label);
 }
-
-console.log(div);
