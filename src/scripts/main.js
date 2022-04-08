080f2e98c9ac2bc2774d8a3628609c03fcf38c49
Ostap Tomashivskyi <o.tomash650416@gmail.com>
2022-04-09 02:09:05 +0300

    Fixed form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const fields = document.querySelectorAll('input');
+
+for (let i = 0; i < fields.length; i++) {
+  const label = document.createElement('label');
+  const placeHolder = fields[i].name[0].toUpperCase() + fields[i].name.slice(1);
+
+  label.className = 'field-label';
+  label.textContent = fields[i].name;
+  label.htmlFor = fields[i].id;
+
+  fields[i].placeholder = placeHolder;
+
+  fields[i].before(label);
+}
