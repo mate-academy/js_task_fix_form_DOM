30e0998d373a3868b7ce20f4da5acdb8506eea08
Ostap Tomashivskyi <o.tomash650416@gmail.com>
2022-04-11 15:59:21 +0300

    Fix related to comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,12 +2,34 @@
 
 const fields = document.querySelectorAll('input');
 
+const getUpperCaseIndex = str => {
+  let index = -1;
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i].toUpperCase();
+
+    if (char === str[i]) {
+      index = i;
+    }
+  }
+
+  return index;
+};
+
 for (let i = 0; i < fields.length; i++) {
   const label = document.createElement('label');
-  const placeHolder = fields[i].name[0].toUpperCase() + fields[i].name.slice(1);
+  const indexBigChar = getUpperCaseIndex(fields[i].name);
+  let placeHolder = fields[i].name[0].toUpperCase() + fields[i].name.slice(1);
+
+  if (indexBigChar > -1) {
+    const firstPart = placeHolder.slice(0, indexBigChar);
+    const secondPart = placeHolder.slice(indexBigChar);
+
+    placeHolder = `${firstPart} ${secondPart}`;
+  }
 
   label.className = 'field-label';
-  label.textContent = fields[i].name;
+  label.textContent = placeHolder;
   label.htmlFor = fields[i].id;
 
   fields[i].placeholder = placeHolder;
