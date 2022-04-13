45238bb2dc9d52dd0ba8cc46890c22c7706aaa43
Max Shylenko <maxshylenko@gmail.com>
2022-04-13 14:02:11 +0200

    fix form DOM is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const fields = [...document.querySelectorAll('.field')];
+
+for (const field of fields) {
+  const input = field.firstElementChild;
+
+  input.placeholder = `${input.name.toUpperCase()}`;
+
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = `${input.id}`;
+  label.textContent = `${input.name.toUpperCase()}`;
+
+  field.append(label);
+}
