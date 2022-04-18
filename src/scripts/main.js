a181c57cf8373ff45bdcc913fde6030b3d36cd10
Lilia Mazur <liliamazur2508@gmail.com>
2022-04-18 16:35:48 +0300

    first

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const fields = [...document.querySelectorAll('.field')];
+
+fields.forEach(field => {
+  const input = field.firstElementChild;
+  const label = document.createElement('label');
+
+  const toUpper = attributeName => {
+    let separator;
+
+    for (const letter of attributeName) {
+      if (letter === letter.toUpperCase()) {
+        separator = letter;
+      }
+    }
+
+    if (separator) {
+      return attributeName.split(separator).join(` ${separator}`).toUpperCase();
+    }
+
+    return attributeName.toUpperCase();
+  };
+
+  input.placeholder = `${toUpper(input.name)}`;
+
+  label.className = 'field-label';
+  label.htmlFor = `${input.id}`;
+  label.textContent = `${toUpper(input.name)}`;
+
+  field.append(label);
+});
