5122ac242d046c8eaa5fa0c6d7e1193e7cded273
Lilia Mazur <liliamazur2508@gmail.com>
2022-04-19 15:30:35 +0300

    third

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,28 +1,27 @@
 'use strict';
 
 const fields = [...document.querySelectorAll('.field')];
+const toUpper = attributeName => {
+  let separator;
+
+  const newName = attributeName[0].toUpperCase() + attributeName.slice(1);
+
+  for (const letter of attributeName) {
+    if (letter === letter.toUpperCase()) {
+      separator = letter;
+    }
+  }
+
+  if (separator) {
+    return newName.split(separator).join(` ${separator}`);
+  }
+
+  return newName;
+};
 
 fields.forEach(field => {
   const input = field.firstElementChild;
   const label = document.createElement('label');
-
-  const toUpper = attributeName => {
-    let separator;
-
-    const newName = attributeName[0].toUpperCase() + attributeName.slice(1);
-
-    for (const letter of attributeName) {
-      if (letter === letter.toUpperCase()) {
-        separator = letter;
-      }
-    }
-
-    if (separator) {
-      return newName.split(separator).join(` ${separator}`);
-    }
-
-    return newName;
-  };
 
   input.placeholder = `${toUpper(input.name)}`;
 
