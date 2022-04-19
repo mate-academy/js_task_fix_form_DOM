1cf43145a19a35f88ee166b9bfcaf3a5db9dc7a6
MariaBondar <marybondar777@gmail.com>
2022-04-19 11:16:49 +0200

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+inputs
+  .forEach(elem => elem
+    .setAttribute('placeholder',
+      elem.name.slice(0, 1).toUpperCase() + elem.name.slice(1)));
+
+inputs.forEach(input => input.insertAdjacentHTML('afterend', `
+  <label for="${input.id}" class="field-label">
+    ${input.name}
+  </label>
+`));
