56d1d7c66784bc96208c2bca2d829f3a6563e11f
Roman Nikitenko <Lomkaaa@ukr.net>
2022-05-09 18:34:59 +0300

    Task complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(el => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = el.id;
+  label.textContent = el.name;
+  el.parentElement.prepend(label);
+  el.placeholder = el.name[0].toUpperCase() + el.name.slice(1);
+});
