b6ee989afe791cc9ec6754a880d1869831298fba
Alex <asultan4ik@gmail.com>
2022-05-12 14:12:55 +0300

    Initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+[...inputs].forEach(input => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.for = input.id;
+  label.textContent = input.name;
+
+  input.before(label);
+  input.placeholder = input.name.toLocaleUpperCase();
+});
