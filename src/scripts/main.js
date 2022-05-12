7e9819dc83aba3b9dc7f03f9c5123ccf65b68d0d
Nazar Muliar <nazarmuliar26@gmail.com>
2022-05-12 20:32:20 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('[name]')].slice(1);
+
+for (const input of inputs) {
+  input.setAttribute('placeholder', `${(input.name).slice(0, 1).toUpperCase()
+    + input.name.slice(1)}`);
+
+  input.parentElement.append(document.createElement('label'));
+  input.parentElement.querySelector('label').setAttribute('for', `${input.id}`);
+}
