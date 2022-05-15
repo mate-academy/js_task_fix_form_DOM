d5ca50285b0c1b8310623ae1c4afe33917f25a6e
ShapovalDenys <denys.igorevich@gmail.com>
2022-05-15 13:10:12 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  input.placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  input.insertAdjacentHTML('afterend', `
+    <label class='field-label' for='${input.id}'>${input.name}</label>
+  `);
+}
