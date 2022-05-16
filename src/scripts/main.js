18429f5a298735bb2c72fd9de47121230308d597
Kostiantyn <havryshkostya@gmail.com>
2022-05-16 16:37:54 +0400

    wo vu paciki na moczikah s polnum bakom benzika ...

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (let i = 0; i < inputs.length; i++) {
+  const label = document.createElement('label');
+
+  label.className = `field-label`;
+  label.htmlFor = inputs[i].id;
+  label.textContent = inputs[i].name;
+
+  inputs[i].parentElement.append(label);
+
+  inputs[i].placeholder = capitalize(inputs[i].name);
+}
+
+function capitalize(inputName) {
+  return inputName[0].toUpperCase() + inputName.slice(1);
+}
