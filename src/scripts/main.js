8dc899cac113887c2fbb344e04f5e353a3a62307
Kostiantyn <havryshkostya@gmail.com>
2022-05-18 13:14:52 +0400

    splitted

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,13 +7,28 @@ for (let i = 0; i < inputs.length; i++) {
 
   label.className = `field-label`;
   label.htmlFor = inputs[i].id;
-  label.textContent = inputs[i].name;
+  label.textContent = capitalize(inputs[i].name);
 
+  if (label.textContent === 'FirstName'
+    || label.textContent === 'LastName') {
+    label.textContent = splitByCamelCase(inputs[i].name);
+  }
   inputs[i].parentElement.append(label);
 
   inputs[i].placeholder = capitalize(inputs[i].name);
+
+  if (inputs[i].placeholder === 'FirstName'
+  || inputs[i].placeholder === 'LastName') {
+    inputs[i].placeholder = splitByCamelCase(inputs[i].name);
+}
 }
 
 function capitalize(inputName) {
   return inputName[0].toUpperCase() + inputName.slice(1);
+}
+
+function splitByCamelCase(stringToSplit) {
+  const splitted = stringToSplit.replace(/([A-Z])/g, ' $1');
+
+  return splitted.charAt(0).toUpperCase() + splitted.slice(1);
 }
