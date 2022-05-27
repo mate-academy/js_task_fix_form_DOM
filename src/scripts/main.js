a0a1b797ba18d3f7f510b638cdfe35f8bda603d0
Alina Yermolenko <alina.yermolenko27@gmail.com>
2022-05-27 18:04:24 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const input = document.querySelectorAll('input');
+
+input.forEach(one => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', `${one.id}`);
+  label.textContent = `${one.name}`;
+
+  const done = one.name;
+
+  one.placeholder = done.toUpperCase();
+  one.before(label);
+}
+);
