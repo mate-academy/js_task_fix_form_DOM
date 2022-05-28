65f50016387e6eaaf20698d1a9917a0d41365b27
Alina Yermolenko <alina.yermolenko27@gmail.com>
2022-05-28 17:55:32 +0300

    final

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,9 +7,9 @@ input.forEach(one => {
 
   label.classList.add('field-label');
   label.setAttribute('for', `${one.id}`);
-  label.textContent = `${one.name}`;
+  label.textContent = `${one.name.replace('N', ' n')}`;
 
-  const done = one.name;
+  const done = one.name.replace('N', ' n');
 
   one.placeholder = done.toUpperCase();
   one.before(label);
