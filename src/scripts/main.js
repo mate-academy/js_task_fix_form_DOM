c3ef318f442c2f23bbb73d0726522fe33877d795
Volodumur Vitinskiy <vovahunter1989@gmail.com>
2022-06-10 12:31:26 +0300

    first try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const input = document.querySelectorAll('input');
+
+for (const i of input) {
+  const label = document.createElement('label');
+
+  label.htmlFor = i.id;
+  label.textContent = i.name;
+  label.className = 'field-label';
+
+  i.placeholder = i.name.charAt(0).toUpperCase()
+  + i.name.toLowerCase().slice(1);
+
+  i.parentElement.append(label);
+}
