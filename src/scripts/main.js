ce51cc1d56449a7ee564d724332bde2210a20f41
Oleksiy Lyashenko <oleksij.lyashenko@gmail.com>
2022-06-10 12:00:37 +0300

    changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+[...inputs].map(input => {
+  const labelForInput = document.createElement('label');
+
+  labelForInput.setAttribute('class', 'field-label');
+  labelForInput.setAttribute('for', `${input.id}`);
+  labelForInput.textContent = `${input.name}`;
+
+  input.parentElement.append(labelForInput);
+
+  const inputName = input.name[0].toLocaleUpperCase() + input.name.slice(1);
+
+  input.placeholder = `${inputName}`;
+});
