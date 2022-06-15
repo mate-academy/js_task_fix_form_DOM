728f8a6e9609bb1ed2f4c9604fee7d730cb74268
Artem Furdela <furdela.artem@gmail.com>
2022-06-15 21:31:41 +0300

    Fix form first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write code here
+
+const inputs = [...document.querySelectorAll('input')];
+
+inputs.forEach(input => {
+  const label = document.createElement('label')
+  label.className = ['field-label'];
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+
+  input.parentElement.append(label);
+  
+  input.placeholder = input.name[0].toLocaleUpperCase()
+    + input.name.substring(1);
+});
