5cc433cff657edc5fff640d41f5c268e92fb8fe9
Vitalii-Heiko <papus@ukr.net.com>
2022-06-15 09:25:59 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  input.insertAdjacentElement('beforebegin', label);
+
+  input.setAttribute(
+    'placeholder', `${input.name[0].toUpperCase() + input.name.slice(1)}`
+  );
+  label.className = 'field-label';
+  label.setAttribute('for', `${input.id}`);
+  label.textContent = `${input.name}`;
+});
