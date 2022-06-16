af5943023a11ce5576a52f2eb843bf69158e7021
Olexandr Istomin <gn.53@ukr.net>
2022-06-16 22:24:33 +0300

    Solution_2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,12 +2,31 @@
 
 const input = [...document.querySelectorAll('.field-text')];
 
+function capitalize(str) {
+  const newArr = [...str];
+  const indexOfCapitalize = newArr.findIndex(function(el) {
+    if (el === el.toUpperCase()) {
+      return true;
+    }
+  });
+
+  const newStr = str[0].toUpperCase()
+  + str.slice(1, indexOfCapitalize) + ' '
+  + str.slice(indexOfCapitalize);
+
+  const oldStr = str[0].toUpperCase() + str.slice(1);
+
+  return indexOfCapitalize > -1 ? newStr : oldStr;
+}
+
 for (let i = 0; i < input.length; i++) {
   const label = document.createElement('label');
 
   label.setAttribute('class', 'field-label');
   label.setAttribute('for', `${input[i].id}`);
-  label.textContent = input[i].name;
-  input[i].setAttribute('placeholder', `${input[i].name.toUpperCase()}`);
+  label.textContent = capitalize(input[i].name).toUpperCase();
+
+  input[i].setAttribute('placeholder',
+    `${capitalize(input[i].name)}`);
   input[i].before(label);
 }
