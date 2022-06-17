bdb5bc33262e05b9ee2edbcb45fd20ecec24d95f
Vitalii-Heiko <papus@ukr.net.com>
2022-06-17 10:34:04 +0300

    solution 1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,14 +4,16 @@
 const inputs = document.querySelectorAll('input');
 
 inputs.forEach((input) => {
+  const placeholderName = input.name.replace(/[A-Z]/g, ' ' + '$&');
   const label = document.createElement('label');
 
   input.insertAdjacentElement('beforebegin', label);
 
   input.setAttribute(
-    'placeholder', `${input.name[0].toUpperCase() + input.name.slice(1)}`
+    'placeholder',
+    `${placeholderName[0].toUpperCase() + placeholderName.slice(1)}`
   );
   label.className = 'field-label';
   label.setAttribute('for', `${input.id}`);
-  label.textContent = `${input.name}`;
+  label.textContent = `${placeholderName}`;
 });
