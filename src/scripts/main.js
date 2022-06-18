596e17f0a1f608a6b65e2fb733ec8cae709f555e
Max <59259022+WhiteAshDc@users.noreply.github.com>
2022-06-18 18:52:48 +0200

    result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const input = document.querySelectorAll('input');
+
+input[4].placeholder = 'Email';
+input[5].placeholder = 'Password';
+
+for (const el of input) {
+  el.insertAdjacentHTML(
+    'afterend',
+    `<label class="field-label" for=""></label>`
+  );
+}
+
+const label = [...document.querySelectorAll('label')];
+
+label[0].htmlFor = 'sign-up-name';
+label[1].htmlFor = 'sign-up-last-name';
+label[2].htmlFor = 'sign-up-email';
+label[3].htmlFor = 'sign-up-password';
