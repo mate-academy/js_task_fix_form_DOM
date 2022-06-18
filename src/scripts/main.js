4689c6ee8a4ebcb3d8d668b9bc243be1bf568685
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-06-18 16:28:27 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,12 +9,15 @@ for (let i = 0; i < inputs.length; i++) {
   const label = document.createElement('label');
 
   label.className = 'field-label';
+  label.textContent = inputs[i].name;
   label.htmlFor = inputs[i].id;
 
   if (inputs[i].name === 'firstName') {
     inputs[i].placeholder = 'First Name';
+    label.textContent = 'First Name';
   } else if (inputs[i].name === 'lastName') {
     inputs[i].placeholder = 'Last Name';
+    label.textContent = 'First Name';
   } else {
     inputs[i].placeholder = inputs[i].name.toUpperCase();
   }
