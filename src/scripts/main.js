5aa6a2929757357ea81ee42782eb5016b16b66e7
Vitaliy Anochin <anuhin.vital@gmail.com>
2022-06-19 13:55:34 +0300

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const element = document.querySelectorAll('.field-text');
+
+for (const el of element) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', el.id);
+  label.textContent = el.name;
+  el.setAttribute('placeholder', el.name[0].toUpperCase() + el.name.slice(1));
+  el.closest('.field').append(label);
+}
