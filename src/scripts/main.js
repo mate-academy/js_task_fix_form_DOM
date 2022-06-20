1cfa338d8407311c981e968be838fd03318d453a
Kirill Polozenko <kir.polozenko@icloud.com>
2022-06-21 00:42:25 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const input = document.querySelectorAll('input');
+
+input.forEach(x => {
+  const label = document.createElement('label');
+
+  label.setAttribute('for', x.id);
+  label.classList.add('field-label');
+
+  label.innerHTML = x.name.split('N').join(' n').toLocaleUpperCase();
+  x.setAttribute('placeholder', `${x.name.split('N').join(' n')}`);
+  x.before(label);
+});
