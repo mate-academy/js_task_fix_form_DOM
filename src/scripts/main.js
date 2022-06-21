374168a964b3a4e775fdef808b127422e24cf215
Stanislav Topikha <stanislav.topikha@gmail.com>
2022-06-21 14:53:49 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+[...inputs].forEach(input => {
+  const label = document.createElement('label');
+  const inputNameSplited = input.name.split(/(?=[A-Z])/);
+
+  label.htmlFor = input.id;
+  label.classList.toggle('field-label');
+  label.textContent = inputNameSplited.join('').toUpperCase();
+
+  input.parentElement.append(label);
+
+  input.placeholder = inputNameSplited
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+});
