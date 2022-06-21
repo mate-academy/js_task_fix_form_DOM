e0e6c03e54436cd4edc28fa3f3e66fa105a98e77
Bogdan Prokopenko <pb.dvlpr@gmail.com>
2022-06-21 11:18:12 +0300

    fix form DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write code here
+const allForms = [...document.querySelectorAll('input')];
+
+for (const value of allForms) {
+  value.insertAdjacentHTML('beforebegin', `
+    <label class='field-label' for='${value.id}'>
+      ${value.name}
+    </label>
+  `);
+
+  value.placeholder = value.name[0].toUpperCase()
+    + value.name.slice(1);
+}
