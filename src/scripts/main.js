d09574c3fe1d465c90171dd0d3c422cc9c755af3
Bogdan Prokopenko <pb.dvlpr@gmail.com>
2022-06-22 22:58:07 +0300

    Tree From Object DOM Third Run

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,20 +4,30 @@
 const allForms = [...document.querySelectorAll('input')];
 
 for (const value of allForms) {
-  value.insertAdjacentHTML('beforebegin', `
-    <label class='field-label' for='${value.id}'>
-      ${value.name}
-    </label>
-  `);
-
   value.placeholder = value.name[0].toUpperCase()
     + value.name.slice(1);
 
   if (value.name === 'firstName') {
     value.placeholder = 'First Name';
-  }
 
-  if (value.name === 'lastName') {
+    value.insertAdjacentHTML('beforebegin', `
+    <label class='field-label' for='${value.id}'>
+      FIRST NAME
+    </label>
+  `);
+  } else if (value.name === 'lastName') {
     value.placeholder = 'Last Name';
+
+    value.insertAdjacentHTML('beforebegin', `
+    <label class='field-label' for='${value.id}'>
+      LAST NAME
+    </label>
+  `);
+  } else {
+    value.insertAdjacentHTML('beforebegin', `
+    <label class='field-label' for='${value.id}'>
+      ${value.name}
+    </label>
+  `);
   }
 }
