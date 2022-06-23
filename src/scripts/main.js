7b9ae384583df7fd116beb6288dcbf21be56de9d
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2022-06-23 12:34:03 +0300

    firstCommit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write code here
+
+const list = document.querySelectorAll('input');
+
+for (const item of list) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', item.id);
+  label.textContent = item.name;
+
+  item.placeholder
+  = item.name.charAt(0).toUpperCase() + item.name.slice(1);
+  item.parentElement.append(label);
+}
