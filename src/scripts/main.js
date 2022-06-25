b2090405c6f492e1fc17fe689cce37755e4f5203
Pavel Maistrenko <maystrenko.p@gmail.com>
2022-06-25 18:39:04 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const fields = [...document.querySelectorAll('.field')];
+
+fields.map(function(element) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+
+  const input = element.querySelector('input');
+
+  const attributeName = input.getAttribute('name');
+  const idName = input.getAttribute('id');
+
+  label.setAttribute('for', idName);
+  label.innerText = attributeName;
+  input.setAttribute('placeholder', attributeName);
+  element.append(label);
+});
