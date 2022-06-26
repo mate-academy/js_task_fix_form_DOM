6d2bd92e7aea1bdb7db7b2705a44783c8b099db7
mira1zu <mira1zu733@gmail.com>
2022-06-26 15:23:43 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+function camelCaseConvert(string) {
+  return string.replace(/([A-Z])/g, (match) => ` ${match}`)
+    .replace(/^./, (match) => match.toUpperCase())
+    .trim();
+}
+
+const forms = document.querySelectorAll('form');
+
+for (const form of forms) {
+  const fieldsInput = form.querySelectorAll('.field-text');
+
+  for (const fieldInput of fieldsInput) {
+    fieldInput.placeholder = camelCaseConvert(fieldInput.name);
+
+    const elementHTML = `
+      <label class="field-label" for=${fieldInput.id}>
+        ${camelCaseConvert(fieldInput.name)}
+      </label>
+    `;
+
+    fieldInput.insertAdjacentHTML('afterend', elementHTML);
+  }
+}
