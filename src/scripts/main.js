7ec220003e83134e8af1d3f95ff427a9c8b7594f
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-06-28 00:26:34 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
 // write code here
+const fields = document.querySelectorAll(`.field`);
+
+const fieldsArray = [...fields];
+
+const stringHandler = (inputName) => {
+  const string = inputName;
+  let newString = string;
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === string[i].toUpperCase()) {
+      newString = string.slice(0, i) + ' ' + string.slice(i);
+    }
+  }
+
+  return newString.slice(0, 1).toUpperCase() + newString.slice(1);
+};
+
+fieldsArray.forEach(field => {
+  const input = field.querySelector(`.field-text`);
+  const inputName = input.getAttribute(`name`);
+  const text = stringHandler(inputName);
+  const label = document.createElement('label');
+
+  label.classList.add(`field-label`);
+  label.textContent = text;
+  field.appendChild(label);
+  input.setAttribute('placeholder', text);
+});
