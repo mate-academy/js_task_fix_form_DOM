819320d059a28d90253c6db1a4f08164409825b9
DimaShm <shmagindmitry@gmail.com>
2022-06-30 12:26:54 +0300

    Solution fix form DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const listInput = document.querySelectorAll('input');
+
+for (let i = 0; i < listInput.length; i++) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.textContent = listInput[i].name;
+  label.htmlFor = listInput[i].id;
+
+  const specialName = listInput[i].name[0].toUpperCase()
+  + listInput[i].name.slice(1);
+
+  listInput[i].placeholder = specialName;
+  listInput[i].parentElement.append(label);
+}
