043514c4c935589a9b9275b12488d42fba0e961d
Alyona Pisotska <pisotska.o@gmail.com>
2022-07-01 15:27:22 +0300

    Fix form DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const inputsList = document.querySelectorAll('input');
+
+for (const inputsElement of inputsList) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = inputsElement.id;
+
+  let placeholderName = '';
+
+  for (const letter of inputsElement.name) {
+    if (letter !== letter.toUpperCase()) {
+      placeholderName += letter;
+    } else {
+      placeholderName += ` ${letter}`;
+    }
+  }
+
+  placeholderName = placeholderName[0].toUpperCase() + placeholderName.slice(1);
+
+  inputsElement.placeholder = placeholderName;
+  label.textContent = placeholderName.toUpperCase();
+
+  inputsElement.parentElement.append(label);
+}
