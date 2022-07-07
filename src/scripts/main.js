a89771caff5dc1abdd9ec7edd166f86bb101d42c
Max Shylenko <maxshylenko@gmail.com>
2022-07-07 18:11:26 +0200

    fix form DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  const label = document.createElement('label');
+  const inputField = input.closest('.field');
+  let inputName = '';
+
+  for (let i = 0; i < input.name.length; i++) {
+    const currentLetter = input.name[i];
+    const nextLetter = input.name[i + 1];
+
+    if (i === 0) {
+      inputName += currentLetter.toUpperCase();
+      continue;
+    }
+
+    if (nextLetter) {
+      if (nextLetter === nextLetter.toUpperCase()) {
+        inputName += `${currentLetter} `;
+        continue;
+      }
+    }
+
+    inputName += currentLetter;
+  }
+
+  input.placeholder = inputName;
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = inputName;
+
+  inputField.append(label);
+});
