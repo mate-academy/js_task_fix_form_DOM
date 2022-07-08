65031495f8dc8cfb8c17a1da44e309da9ed34419
Tanya Hryn <tanyagryn1984@gmail.com>
2022-07-08 23:32:10 +0300

    fix-form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const listInput = document.querySelectorAll('input');
+
+[...listInput].forEach(input => {
+  const labels = document.createElement('label');
+
+  labels.className = 'field-label';
+  labels.htmlFor = input.id;
+  labels.textContent = input.name;
+
+  input.placeholder = input.name[0].toUpperCase()
+  + input.name.toLowerCase().slice(1);
+
+  input.parentElement.prepend(labels);
+});
