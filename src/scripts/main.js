0538a65f6e9472574fb9872566357978d5c84ea0
Kostya Berehoviy <kostyaber3@gmail.com>
2022-07-10 23:15:10 +0300

    Form has been Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+  
+  label.className = 'field-label';
+  
+  label.setAttribute('for', input.id);
+  
+  label.textContent = input.name;
+  
+  const placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+  input.setAttribute('placeholder', placeholder);
+
+  input.parentNode.append(label);
+}
