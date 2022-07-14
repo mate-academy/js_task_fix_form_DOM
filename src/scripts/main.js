f94084e657d05e67b8f52db5c338314c440f198e
Vladimir Yurkov <vladimir.yurkov.91@gmail.com>
2022-07-14 12:40:55 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const inputList = document.querySelectorAll('input');
+
+inputList.forEach(item => {
+  item.insertAdjacentHTML(
+    'beforebegin',
+    `<label class="field-label" for="${item.id}">
+      ${item.name.toUpperCase()}
+    </label>`
+  );
+
+  item.placeholder = item.name.charAt(0).toUpperCase()
+    + item.name.substring(1).toLowerCase();
+});
