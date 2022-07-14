3a34ca8f48ba73ce7fbec9f3310e88b3e1f54458
Taras Kozii <taras_kozii@ukr.net>
2022-07-14 09:13:08 +0300

    solution v02

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,15 +2,29 @@
 
 const inputs = [...document.querySelectorAll('form input')];
 
+function getSliceStr(str) {
+  let result = '';
+
+  for (const elem of str) {
+    if (elem.toUpperCase() !== elem) {
+      result += elem;
+    } else {
+      result += ` ${elem}`;
+    }
+  }
+
+  return result[0].toUpperCase() + result.slice(1);
+}
+
 for (const elem of inputs) {
   const label = document.createElement('label');
 
   label.className = 'field-label';
   label.htmlFor = elem.id;
-  label.textContent = elem.name;
+  label.textContent = getSliceStr(elem.name);
   elem.parentElement.prepend(label);
 }
 
 for (const elem of inputs) {
-  elem.placeholder = elem.name[0].toUpperCase() + elem.name.slice(1);
+  elem.placeholder = getSliceStr(elem.name);
 }
