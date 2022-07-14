3cb57c1aaff01f57e08b060b940b60fff442ad2b
Tanya Hryn <tanyagryn1984@gmail.com>
2022-07-14 20:23:35 +0300

    fixed-forma-v3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,12 +5,22 @@ const listInput = document.querySelectorAll('input');
 [...listInput].forEach(input => {
   const labels = document.createElement('label');
 
+  let resultString = '';
+
+  for (const letter of input.name) {
+    if (letter === letter.toUpperCase()) {
+      resultString += ' ';
+    };
+
+    resultString += letter;
+  }
+
   labels.className = 'field-label';
   labels.htmlFor = input.id;
-  labels.textContent = input.name;
+  labels.textContent = resultString[0].toUpperCase() + resultString.slice(1);
 
-  input.placeholder = input.name[0].toUpperCase()
-  + input.name.slice(1);
+  input.placeholder = resultString[0].toUpperCase()
+  + resultString.slice(1);
 
   input.parentElement.prepend(labels);
 });
