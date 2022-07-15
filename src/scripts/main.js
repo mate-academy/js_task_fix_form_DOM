a74fcc3be431a19693b7f3da9ae0ee8832f3b511
Aleksandr Kovalenko <dias.koval@gmail.com>
2022-07-15 20:28:42 +0300

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const input = document.querySelectorAll('input');
+
+for (const key of input) {
+  const newLabel = document.createElement('label');
+
+  newLabel.className = 'field-label';
+  newLabel.htmlFor = key.id;
+  newLabel.textContent = key.name;
+
+  key.parentElement.append(newLabel);
+
+  key.placeholder = key.name[0].toUpperCase() + key.name.slice(1);
+}
