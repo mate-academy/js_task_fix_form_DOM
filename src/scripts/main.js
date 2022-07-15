f32d45ddab655f50d7412a0a8e35d5a112bb0b9d
Olha Chebotarova <chebotarovaolha@gmail.com>
2022-07-14 20:23:12 -0400

    Form fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of [...inputs]) {
+  const newLabel = document.createElement('Label');
+
+  newLabel.className = 'field-label';
+  newLabel.htmlFor = input.id;
+  newLabel.innerHTML = input.name;
+
+  input.parentElement.append(newLabel);
+  input.placeholder = input.name.toUpperCase();
+};
