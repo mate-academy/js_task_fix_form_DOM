04cb9dd2c1c7643f4481776640ebfa3764159975
Vlad Skuratovskyi <vladskur0369@gmail.com>
2022-07-16 20:54:14 +0300

    Retry load after reinstall node_modules

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 // write code here
+const core = document.querySelector('.login-wrap');
+
+const padding = core.querySelectorAll('.field');
+const input = core.querySelectorAll('input');
+
+for (const distance of padding) {
+  distance.setAttribute('style', 'padding-top: 22px');
+}
+
+for (const text of input) {
+  const word = text.name.split('');
+
+  word[0] = word[0].toUpperCase();
+
+  text.insertAdjacentHTML('beforebegin',
+    `<h6 style="margin: 3px;">${word.join('').toUpperCase()}</h6>`);
+  text.setAttribute('placeholder', word.join(''));
+}
