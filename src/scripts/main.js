79e46cffc55ac13f521792e7e4e1b7104634e790
lentyaeev <daniilshaliuk@gmail.com>
2022-07-19 11:14:01 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const forms = document.querySelectorAll('form');
+
+for (const form of forms) {
+  for (const element of form) {
+    if (element.name) {
+      element.setAttribute('placeholder', splitter(capitalize(element.name)));
+
+      const label = document.createElement('label');
+
+      label.classList.add('field-label');
+      label.setAttribute('for', element.id);
+      label.textContent = splitter(element.name);
+      element.parentElement.append(label);
+    }
+  }
+}
+
+function capitalize(element) {
+  return element.charAt(0).toUpperCase() + element.slice(1);
+}
+
+function splitter(element) {
+  return element.split(/(?=[A-Z])/).join(' ');
+}
