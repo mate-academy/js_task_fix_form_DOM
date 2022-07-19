b18a603401ab0ce9af4081f139f79f5ee4e2cc22
Denys Dmytruk <dmytrukdenysan@gmail.com>
2022-07-19 13:44:34 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const allInputs = document.querySelectorAll('input');
+
+allInputs.forEach(input => {
+  const label = document.createElement('label');
+  const inputName = input.name;
+
+  label.classList.add('field-label');
+  label.htmlFor = input.id;
+  label.textContent = inputName;
+
+  input.placeholder = inputName[0].toUpperCase() + inputName.slice(1);
+  input.insertAdjacentElement('beforebegin', label);
+});
