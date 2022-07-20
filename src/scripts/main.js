aae0782142d40543b4a945bcbf81b16701ab7a5e
DashaPoliazz <dasha.poliazzz@gmail.com>
2022-07-20 19:43:54 +0300

    form was fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+const capitalizer = (word) => {
+  return [...word]
+    .map((char, i) => i === 0 ? char.toLocaleUpperCase() : char)
+    .join('');
+};
+
+inputs.forEach(input => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+
+  label.setAttribute('for', input.id);
+
+  label.textContent = input.name;
+
+  input.setAttribute('placeholder', capitalizer(input.name));
+
+  input.parentNode.append(label);
+});
