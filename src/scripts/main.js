babcdc12d243caeb8096ad93973cd67d6404fa35
Olha Stakhova <olha.stakhova@gmail.com>
2022-07-23 00:16:59 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const template = `<lable class="field-label" for="${
+    input.id}">${input.textContent = createTextLable(input.name)}</lable>`;
+
+  input.placeholder = createTextPlaceholder(input.name);
+
+  input.insertAdjacentHTML('beforebegin', template);
+}
+
+function createTextLable(str) {
+  return str.split('N').join(' N').toUpperCase();
+}
+
+function createTextPlaceholder(str) {
+  const firstLater = str.charAt(0).toUpperCase();
+  const reformStr = (firstLater + str.slice(1)).split('N').join(' N');
+
+  return reformStr;
+}
