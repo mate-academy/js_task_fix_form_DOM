bbddebc4bf848f8b3065ef47dfa8ec6be33b1331
Claudia Kalnitskaya <kalnitskii.family@gmail.com>
2022-07-22 17:44:02 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const inputList = document.querySelectorAll('input');
+
+for (const element of inputList) {
+  const label = document.createElement('label');
+
+  label.htmlFor = element.id;
+  label.className = 'field-label';
+  label.innerText = element.name;
+  element.placeholder = element.name[0].toUpperCase() + element.name.slice(1);
+  element.before(label);
+}
