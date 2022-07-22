4dd18a630a2a5cd4b090d8430d387a145c03e61e
Natalya Tsybulko <tsybulko.natalya395@gmail.com>
2022-07-22 13:01:31 +0300

    solve the fixFormDOM problem

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+const parents = document.querySelectorAll('.field');
+
+for (let i = 0; i < inputs.length; i++) {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.htmlFor = inputs[i].id;
+  label.textContent = inputs[i].name;
+
+  inputs[i].setAttribute(
+    'placeholder',
+    inputs[i].name[0].toUpperCase() + inputs[i].name.substring(1)
+  );
+
+  parents[i].append(label);
+}
