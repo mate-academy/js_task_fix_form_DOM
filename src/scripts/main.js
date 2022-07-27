4e0bf2c4902dfadfc096416478f9b79ecd0c4284
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2022-07-27 11:47:51 +0300

    Help with capitalizing inputs placeholder

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  const label = document.createElement('label');
+  label.for = input.id;
+  label.class = `field-label`;
+  label.textContent = input.name.toUpperCase();
+
+  input.placeholder = input.name.replace(/([A-Z])/g, ' $1')
+  // please help to come up with easier solution to turn camelCase into Camel case
+  .replace(/^./, function(str){ return str.toUpperCase(); });
+
+  input.textContent = input.name;
+
+  input.parentElement.insertAdjacentElement('afterbegin', label);
+
+  console.log(input.parentElement.innerHTML)
+})
