d51fcc51100f58d41164b968f7a359b2aa293444
Denis Koval <denisdkoval@gmail.com>
2022-07-29 11:27:12 +0300

    addspaces

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,14 +3,40 @@
 const inputs = document.querySelectorAll('input');
 
 for (const input of inputs) {
+  let text = input.name;
+
+  for (let i = text.length - 1; i >= 0; i -= 1) {
+    if (text[i] === text[i].toUpperCase()) {
+      text = text.slice(0, i) + ' ' + text.slice(i);
+    }
+  }
+
   const label = document.createElement('label');
 
   label.className = 'field-label';
   label.htmlFor = input.id;
-  label.textContent = input.name;
+  label.textContent = text;
 
-  input.placeholder = input.name[0].toUpperCase()
-  + input.name.toLowerCase().slice(1);
+  input.placeholder = text[0].toUpperCase()
+  + text.slice(1);
 
   input.parentElement.append(label);
 };
+// const inputs = document.querySelectorAll('input');
+
+// for (const input of inputs) {
+//   let text = input.name;
+
+//   for (let i = text.length - 1; i >= 0; i -= 1) {
+//     if (text[i] === text[i].toUpperCase()) {
+//       text = text.slice(0, i) + ' ' + text.slice(i);
+//     }
+//   }
+
+//   input.insertAdjacentHTML('beforebegin',
+//     `<label class="field-label"
+//     for="${input.id}">${text.toUpperCase()}</label>`);
+
+//   input.setAttribute('placeholder',
+//     `${text[0].toUpperCase() + text.slice(1)}`);
+// }
