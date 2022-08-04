685e66e7197af05795245abc7cd8fa2b51d6c629
Hovera-Liubomyr <ljusuk@gmail.com>
2022-08-04 19:22:29 +0300

    fix_form_dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const key of inputs) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = key.id;
+  label.textContent = key.name;
+  key.placeholder = capitalizeFirstLetter(key.name);
+  key.append(label);
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
