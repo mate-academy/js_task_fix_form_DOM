be087f0f338cf56543340e0bb3b60df8c9c581ba
Andrian Melnyk <andrian_m@ukr.net>
2022-08-05 23:50:24 +0300

    First commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(el => {
+  el.insertAdjacentHTML(
+    'beforebegin',
+    `<label class="field-label" for="${el.id}">
+        ${el.name.toUpperCase()}
+      </label>`
+  );
+  el.placeholder = `${el.name.toUpperCase()}`;
+});
