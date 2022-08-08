a44b43120eba797334f738aac1f347a9740a1316
Andrian Melnyk <andrian_m@ukr.net>
2022-08-08 21:05:39 +0300

    Second commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,23 @@
 const inputs = document.querySelectorAll('input');
 
 inputs.forEach(el => {
+  const nameValue = el.name;
+  let fixedNameValue = el.name[0].toUpperCase();
+
+  for (let i = 1; i < nameValue.length; i++) {
+    if (nameValue[i] === nameValue[i].toUpperCase()) {
+      fixedNameValue += ' ';
+    }
+
+    fixedNameValue += nameValue[i];
+  }
+
   el.insertAdjacentHTML(
     'beforebegin',
     `<label class="field-label" for="${el.id}">
-        ${el.name.toUpperCase()}
+        ${fixedNameValue}
       </label>`
   );
-  el.placeholder = `${el.name.toUpperCase()}`;
+
+  el.placeholder = fixedNameValue;
 });
