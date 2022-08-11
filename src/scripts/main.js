cdbf9ed7ecd1f4d2cb66c80449a41d05fba00ac8
sergii-nosachenko <s.nosachenko@gmail.com>
2022-08-11 16:22:33 +0300

    Task solution code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const forms = document.querySelectorAll('form');
+
+forms.forEach(form => {
+  const inputs = form.querySelectorAll('input');
+
+  inputs.forEach(input => {
+    /* eslint-disable-next-line no-shadow */
+    const { name, id } = input;
+    const label = document.createElement('label');
+
+    label.className = 'field-label';
+    label.htmlFor = id;
+    label.innerText = name;
+
+    input.setAttribute(
+      'placeholder',
+      name[0].toUpperCase() + name.slice(1)
+    );
+    input.before(label);
+  });
+});
