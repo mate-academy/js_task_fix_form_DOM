6496884192fd46efe87e609a3778dc273dafc8b1
Andrii Kuzmenko <andriikuzmenko35@gmail.com>
2022-08-12 13:47:57 +0300

    words were separated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,10 +4,22 @@ const inputElements = document.querySelectorAll('.field-text');
 
 for (const element of inputElements) {
   element.insertAdjacentHTML('beforebegin', `
-  <label for="${element.id}" class="field-label">${element.name}</label>`);
-  element.setAttribute('placeholder', capitalize(element.name));
+  <label for="${element.id}" class="field-label">
+    ${getDivided(element.name)}
+  </label>`);
+  element.setAttribute('placeholder', getCapitalize(getDivided(element.name)));
 }
 
-function capitalize(string) {
+function getCapitalize(string) {
   return string[0].toUpperCase() + string.slice(1);
+}
+
+function getDivided(string) {
+  return string.split('').map(letter => {
+    if (letter.toUpperCase() === letter) {
+      return ` ${letter}`;
+    } else {
+      return letter;
+    }
+  }).join('');
 }
