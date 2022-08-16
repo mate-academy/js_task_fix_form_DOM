3dae8459cd69ef044e706e3700bcbf778d5591ae
Yuliia Parashchenko <yuliia.parashchenko@gmail.com>
2022-08-16 13:17:25 +0300

    Fix form DOM task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+const inputsArray = [...inputs];
+
+inputsArray.forEach(item => {
+  const label = document.createElement('label');
+  const itemName = item.name;
+
+  const fieldName = itemName.split('').map((letter, index) => {
+    if (itemName.charCodeAt(index)
+      === itemName.toUpperCase().charCodeAt(index)) {
+      return ' ' + letter;
+    }
+
+    return letter;
+  }).join('');
+
+  label.className = 'field-label';
+  label.htmlFor = item.id;
+  label.textContent = fieldName;
+
+  item.before(label);
+
+  item.placeholder = fieldName[0].toUpperCase() + fieldName.slice(1);
+});
