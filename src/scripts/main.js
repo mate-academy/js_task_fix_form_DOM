5677378f90357d042c8e1b1f9d3b7746c51878d7
Denys Topchyi <miliarder97@gmail.com>
2022-08-19 10:13:52 +0300

    formFixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+function capitalize(param) {
+  return param.slice(0, 1).toUpperCase()
+    + param.slice(1).split(/(?=[A-Z])/g).join(' ');
+}
+
+inputs.forEach(el => {
+  el.insertAdjacentHTML('beforebegin',
+    `<label 
+    class='field-label' 
+    for="${el.id}">
+    ${el.name}
+  </label>`);
+  el.placeholder = capitalize(el.name);
+});
