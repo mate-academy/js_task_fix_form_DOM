ef3a98f65b76a4639f5f640f3e5ca8d569a114b9
Viktoria Neimesh <valencia88vika@gmail.com>
2022-08-21 10:29:23 +0300

    function getSeperateWord was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,26 @@
 
 const inputs = document.querySelectorAll('input');
 
-for (const input of [...inputs]) {
+function getSeperateWord(str) {
+  let result = '';
+
+  for (const letter of str) {
+    if (letter.toUpperCase() !== letter) {
+      result += letter;
+    } else {
+      result += ` ${letter}`;
+    }
+  }
+
+  return result[0].toLocaleUpperCase() + result.slice(1);
+}
+
+for (const input of inputs) {
   const label = document.createElement('label');
 
   label.className = 'field-label';
   label.htmlFor = input.id;
-  label.innerHTML = input.name;
-
-  input.placeholder = input.name.toUpperCase();
-
+  label.innerHTML = getSeperateWord(input.name);
   input.parentElement.append(label);
+  input.placeholder = getSeperateWord(input.name);
 }
