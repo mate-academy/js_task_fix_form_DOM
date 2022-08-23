cef581c9a7a5e88357f09311a27d24704e496bd2
Nazar Kulchevych <gamebandit38@gmail.com>
2022-08-23 12:12:37 +0300

    added function to modify camelCase

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,26 @@
 
 // write code here
 
+function removeCamelCase(str) {
+  let result = '';
+
+  for (const char of str) {
+    if (char === char.toUpperCase()) {
+      result += ' ' + char;
+      continue;
+    }
+
+    result += char;
+  }
+
+  return result;
+}
+
 const inputs = [...document.querySelectorAll('input')];
 
 inputs.forEach(input => {
-  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+  input.placeholder = input.name[0].toUpperCase()
+    + removeCamelCase(input.name.slice(1));
 
   const label = document.createElement('label');
 
