efcd6bda7919beb90cdd3b3ee568f0fe91409a11
Dima <dimak119988@gmail.com>
2022-08-23 23:57:55 +0300

    First Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const inputList = document.querySelectorAll('input');
+
+const inputArray = [...inputList];
+
+for (let i = 0; i < inputArray.length; i++) {
+  const parentElement = inputArray[i].parentElement;
+
+  const label = document.createElement('label');
+
+  label.className = `field-label`;
+  label.htmlFor = inputArray[i].id;
+  label.innerText = inputArray[i].name.toLocaleUpperCase();
+
+  const placeholder
+  = inputArray[i].name.toLowerCase().split('').map((item, position) => {
+    if (position === 0) {
+      return item.toLocaleUpperCase();
+    } else {
+      return item;
+    }
+  }).join('');
+
+  inputArray[i].placeholder = placeholder;
+
+  parentElement.append(label);
+}
+
+// console.log(inputList);
