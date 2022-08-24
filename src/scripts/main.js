63c543681865d1db145a407e417bb566a8c3021f
Andriy Kachalkin <kachalkin1306@gmail.com>
2022-08-24 10:43:03 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
+/* eslint-disable no-shadow */
 'use strict';
 
-// write code here
+const forms = document.querySelectorAll('form');
+
+forms.forEach(form => {
+  const inputs = document.querySelectorAll('input');
+
+  inputs.forEach(input => {
+    const { name, id } = input;
+
+    const label = document.createElement('label');
+
+    label.className = 'field-label';
+    label.htmlFor = id;
+    label.textContent = name;
+
+    input.setAttribute(
+      `placeholder`,
+      name[0].toUpperCase() + name.slice(1).replace(/([A-Z])/g, ' $1').trim()
+    );
+
+    input.before(label);
+  });
+});
