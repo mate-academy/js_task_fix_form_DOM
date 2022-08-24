734d81a3e3a6a27e78d845f8976af5ef6a93ea7e
Hovera-Liubomyr <ljusuk@gmail.com>
2022-08-24 16:36:28 +0300

    fix_form_dom_1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,11 +7,19 @@ for (const key of inputs) {
 
   label.className = 'field-label';
   label.htmlFor = key.id;
-  label.textContent = key.name;
-  key.placeholder = capitalizeFirstLetter(key.name);
+  label.textContent = wordSeparator(key.name);
+  key.placeholder = wordSeparator(key.name);
   key.append(label);
 }
 
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+function wordSeparator(string) {
+  const phrase = string.split('');
+
+  for (let i = 0; i < phrase.length; i++) {
+    if (phrase[i].toUpperCase() === phrase[i]) {
+      phrase[i] = ' ' + phrase[i];
+    }
+  }
+
+  return phrase[0].toUpperCase() + phrase.join('').slice(1);
 }
