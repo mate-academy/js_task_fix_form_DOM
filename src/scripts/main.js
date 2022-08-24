799cd0075acf925430a7d2b28dc490bfba2522ec
Sasha Kolomiiets <kinbean444@gmail.com>
2022-08-24 19:13:32 +0300

    add spaces

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,11 +2,18 @@
 
 const inputs = document.querySelectorAll('input');
 
-inputs.forEach((item, i) => {
+inputs.forEach((item) => {
   item.insertAdjacentHTML('beforebegin', `
   <label class="field-label" for="${item.id}">${item.name}</label>
   `);
 
-  item.placeholder = item.name[0].toUpperCase()
-    + item.name.slice(1);
+  item.placeholder = item.name[0].toUpperCase();
+
+  for (let i = 1; i < item.name.length; i++) {
+    if (item.name[i] === item.name[i].toUpperCase()) {
+      item.placeholder += ` ${item.name[i]}`;
+    } else {
+      item.placeholder += item.name[i];
+    }
+  }
 });
