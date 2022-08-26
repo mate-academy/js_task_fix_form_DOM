c4359ddeba3e0e061011c1ad62ba8e63e7e31062
Dolhov Andrey <andrej.dolgov.042110@gmail.com>
2022-08-26 17:54:19 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const inputs = [...document.getElementsByClassName('field-text')];
+
+inputs.forEach((item) => {
+  item.setAttribute(
+    'placeholder',
+    `${item.getAttribute('name').toUpperCase()}`);
+
+  const label = document.createElement('label');
+
+  label.innerHTML = item.getAttribute('name');
+  label.className = 'field-label';
+  label.setAttribute('for', `${item.id}`);
+
+  item.before(label);
+});
+
+// Have a good day! :3
