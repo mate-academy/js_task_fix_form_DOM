093f9eb67b59c6123431af28587d554b953697d5
Yevhen Pavlovskyi <pavlovskiy07@gmail.com>
2022-08-26 15:03:46 +0300

    all done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 // write code here
+const inputs = [...document.querySelectorAll('input')];
+
+inputs.forEach(input => {
+  const label = document.createElement('label');
+
+  label.className = ['field-label'];
+  label.htmlFor = input.id;
+
+  label.textContent = input.name.includes('Name')
+    ? input.name.replace('Name', ' Name')
+    : input.name;
+
+  input.parentElement.append(label);
+
+  input.placeholder = label.textContent[0].toLocaleUpperCase()
+    + label.textContent.substring(1);
+});
