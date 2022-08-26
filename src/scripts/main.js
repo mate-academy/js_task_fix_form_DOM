eaffdd9b1de08460eda073efd1b95c1b1146c639
Mark Shapka <mark.shapka1@gmail.com>
2022-08-26 15:00:58 +0300

    deleted nested loop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,36 +1,39 @@
 'use strict';
 
 const form = document.querySelectorAll('form');
+const elementsOfForm = [];
 
-for (let i = 0; i < form.length; i++) {
-  for (const input of form[i]) {
-    const label = document.createElement('label');
+for (const childElements of form) {
+  elementsOfForm.push(...childElements);
+}
 
-    label.classList = `field-label`;
+for (const input of elementsOfForm) {
+  const label = document.createElement('label');
 
-    if (input.getAttribute('name') === 'firstName') {
-      label.setAttribute('for', 'sign-up-name');
-      label.innerHTML = `First Name`;
-      input.setAttribute('placeholder', 'First Name');
-    }
+  label.classList = `field-label`;
 
-    if (input.getAttribute('name') === 'lastName') {
-      label.setAttribute('for', 'sign-up-last-name');
-      label.innerHTML = `Last Name`;
-      input.setAttribute('placeholder', 'Last Name');
-    }
-
-    if (input.getAttribute('name') === 'email') {
-      label.setAttribute('for', 'sign-up-email');
-      label.innerHTML = `Email`;
-      input.setAttribute('placeholder', 'Email');
-    }
-
-    if (input.getAttribute('name') === 'password') {
-      label.setAttribute('for', 'sign-up-password');
-      label.innerHTML = `Password`;
-      input.setAttribute('placeholder', 'Password');
-    }
-    input.before(label);
+  if (input.getAttribute('name') === 'firstName') {
+    label.setAttribute('for', 'sign-up-name');
+    label.innerHTML = `First Name`;
+    input.setAttribute('placeholder', 'First Name');
   }
+
+  if (input.getAttribute('name') === 'lastName') {
+    label.setAttribute('for', 'sign-up-last-name');
+    label.innerHTML = `Last Name`;
+    input.setAttribute('placeholder', 'Last Name');
+  }
+
+  if (input.getAttribute('name') === 'email') {
+    label.setAttribute('for', 'sign-up-email');
+    label.innerHTML = `Email`;
+    input.setAttribute('placeholder', 'Email');
+  }
+
+  if (input.getAttribute('name') === 'password') {
+    label.setAttribute('for', 'sign-up-password');
+    label.innerHTML = `Password`;
+    input.setAttribute('placeholder', 'Password');
+  }
+  input.before(label);
 }
