77baeb6f976b176635cb986b49f29d4843bdad16
Oleksandr Malyshev <sanyokmalyshev@gmail.com>
2022-09-05 12:45:56 +0800

    Task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+[...inputs].map(input => {
+  const label = document.createElement('label');
+  const placeholder
+    = input.name.charAt(0).toUpperCase()
+    + input.name.slice(1).toLowerCase();
+
+  label.innerHTML = `
+    <label for='${input.id}' class='field-label'>
+      ${input.name}
+    </label>
+  `;
+  input.setAttribute('placeholder', placeholder);
+  input.parentElement.append(label);
+});
