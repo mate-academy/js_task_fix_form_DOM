8db46d1d8328f153f7793d4a02e82b26f856df97
Boris Kerekesha <kerekesha90@ukr.net>
2022-09-06 15:03:00 +0300

    fix_form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const fieldInputs = document.getElementsByClassName('field');
+
+for (let i = 0; i < fieldInputs.length; i++) {
+  const label = document.createElement('label');
+  label.className = 'field-label';
+  label.htmlFor = fieldInputs[i].getElementsByClassName('field-text')[0].id;
+  label.textContent = fieldInputs[i].getElementsByClassName('field-text')[0].name;
+  fieldInputs[i].prepend(label);
+  fieldInputs[i].getElementsByClassName('field-text')[0].placeholder = label.textContent[0].toLocaleUpperCase() + label.textContent.slice(1);
+}
