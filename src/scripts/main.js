3f27dae27ee8697b31b82e3fc78446dc2f7c97bf
Oleksii <oleksii@it.com>
2022-09-08 17:59:53 +0300

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+inputs.forEach(inp => {
+  const label = document.createElement('label');
+
+  label.classList = 'field-label';
+  label.htmlFor = inp.id;
+
+  label.textContent = inp.name.includes('val')
+    ? inp.name.replace('val', 'val')
+    : inp.name;
+
+  inp.parentElement.append(label);
+
+  inp.placeholder = label.textContent[0].toUpperCase()
+    + label.textContent.substring(1);
+});
