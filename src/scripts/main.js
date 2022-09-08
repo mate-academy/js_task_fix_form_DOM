4360963a5ce735110d50b91efdadb505a62b570c
fenderukr <priststoreua@gmail.com>
2022-09-08 13:50:46 +0300

    authFormFix0.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+  input.setAttribute('placeholder', input.name.toUpperCase());
+
+  input.prepend(label);
+}
