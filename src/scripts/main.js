c0fc80ce4b8fc4be5ad02c91b52a14651beadbce
Boris Kerekesha <kerekesha90@ukr.net>
2022-09-09 09:45:29 +0300

    fix form__with separation different words with space

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,9 +4,18 @@ const fieldInputs = document.getElementsByClassName('field');
 
 for (let i = 0; i < fieldInputs.length; i++) {
   const label = document.createElement('label');
+
   label.className = 'field-label';
   label.htmlFor = fieldInputs[i].getElementsByClassName('field-text')[0].id;
-  label.textContent = fieldInputs[i].getElementsByClassName('field-text')[0].name;
+
+  label.textContent = fieldInputs[i]
+    .getElementsByClassName('field-text')[0].name
+    .split('')
+    .map(letter => (letter === letter.toUpperCase() ? ` ${letter}` : letter))
+    .join('');
+
   fieldInputs[i].prepend(label);
-  fieldInputs[i].getElementsByClassName('field-text')[0].placeholder = label.textContent[0].toLocaleUpperCase() + label.textContent.slice(1);
+
+  fieldInputs[i].getElementsByClassName('field-text')[0].placeholder
+    = label.textContent[0].toLocaleUpperCase() + label.textContent.slice(1);
 }
