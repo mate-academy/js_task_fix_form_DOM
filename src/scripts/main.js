b7cf2aaf31f4f0e882c7b91b8730dc8afec5174a
Dima Korol <korovaidima@gmail.com>
2022-09-10 19:32:15 +0200

    2nd commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+function forLable(word) {
+  let label = '';
+
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === word[i].toUpperCase()) {
+      label += ' ' + word[i];
+      i++;
+    }
+    label += word[i];
+  }
+
+  return label;
+}
+
+[...inputs].map(elem => {
+  elem.insertAdjacentHTML('beforebegin', `
+    <label class="field-label" for="${elem.id}">
+    ${forLable(elem.name)}</label>
+  `);
+
+  elem.placeholder = elem.name[0].toUpperCase();
+
+  for (let i = 1; i < elem.name.length; i++) {
+    if (elem.name[i] === elem.name[i].toUpperCase()) {
+      elem.placeholder += ` ${elem.name[i].toLowerCase()}`;
+      i++;
+    }
+    elem.placeholder += elem.name[i];
+  }
+});
