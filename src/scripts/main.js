811b59064169c1e00007e26a873a569fb321b3f5
Inna <dragotaiv@gmail.com>
2022-09-12 22:37:20 +0300

    labels

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+[...inputs].forEach(input => {
+  const label = document.createElement('label');
+
+  label.className = ['field-label'];
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+
+  input.placeholder = input.name.charAt(0).toUpperCase()
+                    + input.name.substring(1).toLowerCase();
+
+  input.parentElement.append(label);
+});
