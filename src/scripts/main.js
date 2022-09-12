b1f25c60fc1d2d735aee61b26bfda5e7dc344afa
Misha Kostov <kostov.mihail321@gmail.com>
2022-09-12 14:26:17 +0300

    Separate words

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,12 +4,15 @@ const inputs = document.querySelectorAll('input');
 
 for (const input of inputs) {
   const label = document.createElement('label');
-  let placeholder = input.name.toLocaleLowerCase();
+  let placeholder = input.name
+    .split(/(?=[A-Z])/)
+    .map(str => str.toLowerCase())
+    .join(' ');
 
   placeholder = placeholder.charAt(0).toUpperCase() + placeholder.slice(1);
   label.setAttribute('for', input.id);
   label.classList.add('field-label');
-  label.innerHTML = input.name;
+  label.innerHTML = placeholder;
 
   input.before(label);
   input.setAttribute('placeholder', placeholder);
