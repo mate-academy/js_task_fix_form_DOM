2f02cbeab067ae573fd0b85a8353b00bb56071db
Anastasiia Mahalias <anastasiabatychenko@gmail.com>
2022-09-13 16:31:02 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  input.placeholder = capitalizePhrase(input.name);
+
+  input.insertAdjacentHTML('beforebegin', `
+    <label class="field-label" for="${input.id}">
+    ${input.name.toUpperCase()}
+    </label>
+  `);
+}
+
+function capitalizePhrase(word) {
+  const result = word.charAt(0).toUpperCase()
+    + word.slice(1);
+
+  return result;
+}
