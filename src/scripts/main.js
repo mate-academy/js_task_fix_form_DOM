f15c037769556c8ea1d6b6e956446a394a43c540
Sheremet Oleksandra <sandasheremet@gmail.com>
2022-09-15 12:23:57 +0300

    form was fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  input.parentElement.insertAdjacentHTML(
+    'afterbegin',
+    `<label class="field-label" for="${input.id}">
+    ${input.name.toUpperCase()}
+    </label>`
+  );
+
+  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+}
