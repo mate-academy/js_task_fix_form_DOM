8cf63073c9e72f73f5f36392eade4cbfa6b14394
Max Bovtun <maxbovtun@gmail.com>
2022-09-17 22:49:38 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write code here
+const fields = document.querySelectorAll('.field');
+
+for (const field of fields) {
+  const newLabel = document.createElement('label');
+
+  newLabel.setAttribute('for', field.firstElementChild.id);
+  newLabel.classList.add('field-label');
+  newLabel.innerHTML = field.firstElementChild.name;
+  field.firstElementChild.placeholder = field.firstElementChild.name;
+  field.insertBefore(newLabel, field.firstElementChild);
+}
