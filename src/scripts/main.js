247a1725f610ddae702487c670e1a91d1d6f803e
Andriy Andrieiev <proph7000@gmail.com>
2022-09-24 18:00:44 +0300

    First try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+function capitalize(string) {
+  return string.slice(0, 1).toUpperCase() + string.slice(1);
+}
+
+inputs.forEach(input => {
+  const inputName = input.getAttribute('name');
+
+  input.placeholder = capitalize(inputName);
+
+  input.insertAdjacentHTML('beforebegin', `
+    <label class="field-label" for=${input.getAttribute('id')}>
+      ${inputName.toUpperCase()}
+    </label>
+  `);
+});
