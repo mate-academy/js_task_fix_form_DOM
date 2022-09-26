69eff8a3ded1c9033c2f1cf3a05998b4e24073a2
dmitriyn3679 <dmitriyn3679@gmail.com>
2022-09-26 13:46:27 +0300

    done!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+const splitLabel = text => {
+  switch (true) {
+    case text.toLowerCase() === 'firstname':
+      return 'First Name';
+
+    case text.toLowerCase() === 'lastname':
+      return 'Last Name';
+
+    default:
+      return text[0].toUpperCase() + text.slice(1);
+  }
+};
+
+for (const input of inputs) {
+  input.insertAdjacentHTML('beforebegin', `
+    <label class="field-label" for="${input.id}">
+        ${splitLabel(input.name)}
+    </label>
+  `);
+  input.placeholder = splitLabel(input.name);
+}
