cf525c40e3d20ebe6a3730bced9f03fbc4761728
Andriy Andrieiev <proph7000@gmail.com>
2022-09-26 18:42:27 +0300

    Added split for camelCase

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,13 @@
 const inputs = document.querySelectorAll('input');
 
 function capitalize(string) {
+  for (let i = 1; i < string.length; i++) {
+    if (string[i] === string[i].toUpperCase()) {
+      return `${string.slice(0, 1).toUpperCase() + string.slice(1, i)} `
+        + `${string.slice(i)}`;
+    }
+  }
+
   return string.slice(0, 1).toUpperCase() + string.slice(1);
 }
 
@@ -13,7 +20,7 @@ inputs.forEach(input => {
 
   input.insertAdjacentHTML('beforebegin', `
     <label class="field-label" for=${input.getAttribute('id')}>
-      ${inputName.toUpperCase()}
+      ${capitalize(inputName).toUpperCase()}
     </label>
   `);
 });
