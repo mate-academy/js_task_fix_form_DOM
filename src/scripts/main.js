cf3f1c1fb3e89238c4612660fff4e8fc27060695
Oleksandr Naumchak <naumchak@ukr.net>
2022-09-26 14:17:22 +0300

    JS task fix form DOM (first solution)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputElements = document.querySelectorAll('input');
+
+function capitalizeText(currText) {
+  return currText.charAt(0).toUpperCase()
+    + currText.slice(1);
+}
+
+for (const inputElement of inputElements) {
+  inputElement.placeholder = capitalizeText(inputElement.name);
+
+  inputElement.insertAdjacentHTML('beforebegin', `
+  <label class="field-label" for="${inputElement.id}">
+  ${inputElement.name.toUpperCase()}
+  </label>
+`);
+}
