18f0d7dbab7e565076c2f1348470f8c98661867d
Zykhor Andrii <zuxoran@gmail.com>
2022-09-28 12:48:17 +0300

    task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // write code here
+
+const list = [...document.querySelectorAll('input')];
+
+list.forEach((item) => {
+  item.placeholder = item.name;
+
+  const label = document.createElement('label');
+
+  label.htmlFor = item.id;
+  label.textContent = item.name;
+  label.className = 'field-label';
+  item.parentElement.append(label);
+});
