0c5963e99e3da5e6ce9d8aeefd8df936efdf7ca7
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-09-28 10:24:48 +0300

    labels and placeholders was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+const item = document.createElement('label');
+
+item.className = 'field-label';
+
+inputs.forEach(input => {
+  let newName = input.name.toLowerCase();
+
+  if (newName.includes('name')) {
+    newName = newName
+      .slice(0, newName.indexOf('name'));
+    newName += ' name';
+  }
+
+  const newPlaceholder = newName
+    .replace(newName[0], newName[0].toUpperCase());
+
+  item.htmlFor = input.id;
+  item.innerText = newName.toUpperCase();
+  input.before(item.cloneNode(true));
+  input.placeholder = newPlaceholder;
+});
