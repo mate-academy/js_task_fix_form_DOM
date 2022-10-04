1f4022389eb7c40fca23315112aff61233c4908f
Zykhor Andrii <zuxoran@gmail.com>
2022-10-04 22:34:06 +0300

    some chenge

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,12 +5,24 @@
 const list = [...document.querySelectorAll('input')];
 
 list.forEach((item) => {
-  item.placeholder = item.name;
+  const upName = item.name.toUpperCase();
+
+  let nameInput = '';
+
+  for (let i = 0; i < upName.length; i++) {
+    if (item.name[i] === upName[i]) {
+      nameInput += ' ' + item.name[i];
+    } else {
+      nameInput += item.name[i];
+    }
+  }
+
+  item.placeholder = nameInput;
 
   const label = document.createElement('label');
 
   label.htmlFor = item.id;
-  label.textContent = item.name;
+  label.textContent = nameInput;
   label.className = 'field-label';
   item.parentElement.append(label);
 });
