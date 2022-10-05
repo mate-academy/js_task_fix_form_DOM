97d82c9924d00523546b6a75267925e5a9256bba
Oleksiy Uchaev <corob.dir@gmail.com>
2022-10-05 09:22:40 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+function addCaps(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+};
+
+const parents = document.getElementsByClassName('field');
+
+[...parents].map(div => {
+  const label = document.createElement('label');
+  const input = div.children[0];
+
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+  label.className = 'field-label';
+  input.placeholder = addCaps(input.name);
+
+  div.append(label);
+});
