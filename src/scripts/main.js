42b46dbeedfa7c1f4f791bc8c5cb12e1bfc5350a
Andriy Hladkevych <andriyglad@gmail.com>
2022-10-05 17:38:42 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const forms = document.querySelectorAll('form');
+
+for (const form of forms) {
+  const divInputs = form.querySelectorAll('div');
+
+  for (const div of divInputs) {
+    const input = div.querySelector('input');
+
+    if (input) {
+      const label = `<label for="${input.id ? input.id : null}"
+      class="field-label">
+      ${input.name}
+    </label>`;
+
+      input.placeholder = input.name[0].toUpperCase()
+        + input.name.slice(1).toLowerCase();
+
+      div.innerHTML = label + input.outerHTML;
+    }
+  }
+}
