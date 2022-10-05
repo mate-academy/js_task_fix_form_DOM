baed581d0c8ff3073da60aee08bddd33de3f6b4a
Danil <dasha8854567@gmail.com>
2022-10-05 16:09:34 +0300

    commit1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+const labels = inputs.map(x => `
+<label 
+  class="field-label"
+  for="${x.id}"
+>${x.name}
+</label>
+`);
+
+for (let i = 0; i < inputs.length; i++) {
+  inputs[i].insertAdjacentHTML('beforebegin', labels[i]);
+}
