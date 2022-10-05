531ea43cbc853335fd1dae2d4c6cb8398a7c0449
Danil <dasha8854567@gmail.com>
2022-10-05 20:28:16 +0300

    commit2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,13 +2,17 @@
 
 const inputs = [...document.querySelectorAll('input')];
 
-const labels = inputs.map(x => `
-<label 
-  class="field-label"
-  for="${x.id}"
->${x.name}
-</label>
-`);
+const labels = inputs.map(input => {
+  input.placeholder = input.name[0].toLocaleUpperCase() + input.name.slice(1);
+
+  return `<label 
+    class="field-label"
+    for="${input.id}"
+  >${input.name}
+  </label>
+  `;
+}
+);
 
 for (let i = 0; i < inputs.length; i++) {
   inputs[i].insertAdjacentHTML('beforebegin', labels[i]);
