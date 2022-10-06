6c524609da5711190ee16d4b10446c3a353f1dd5
Oleksiy Uchaev <corob.dir@gmail.com>
2022-10-06 14:43:04 +0300

    Fixed name displaying

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,11 +9,13 @@ const parents = document.getElementsByClassName('field');
 [...parents].map(div => {
   const label = document.createElement('label');
   const input = div.children[0];
+  const splitedName = input.name
+    .replace(/([a-z])([A-Z])/g, '$1 $2');
 
   label.htmlFor = input.id;
-  label.textContent = input.name;
+  label.textContent = splitedName;
   label.className = 'field-label';
-  input.placeholder = addCaps(input.name);
+  input.placeholder = addCaps(splitedName);
 
   div.append(label);
 });
