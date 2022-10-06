e29ffc1163f88343860c626da222891edc40ed7a
illia_kots <illyahennadievich@gmail.com>
2022-10-06 15:22:43 +0300

    Task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const collectionInput = [...document.querySelectorAll('input')];
+
+collectionInput.forEach((input) => {
+  const label = document.createElement('label');
+  const capitalizedName = input.name[0].toUpperCase() + input.name.slice(1);
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+  input.placeholder = capitalizedName;
+  input.parentElement.append(label);
+});
