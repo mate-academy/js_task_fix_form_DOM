25799d9376777c8d8736161f37ee6a1e66d6b817
Hlazman <glazman.b@gmail.com>
2022-10-06 13:47:02 +0300

    made fix form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const fields = document.querySelectorAll('.field');
+const input = document.querySelectorAll('input');
+let count = 0;
+
+fields.forEach(field => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', input[count].id);
+  label.innerText = input[count].name;
+  field.prepend(label);
+
+  input[count].placeholder
+    = input[count].name.charAt(0).toUpperCase() + input[count].name.slice(1);
+
+  count++;
+});
