3afc18cdea0029a7643216de4f2fc0f8609c8d57
Hlazman <glazman.b@gmail.com>
2022-10-09 12:38:32 +0300

    add split for words

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,11 +9,12 @@ fields.forEach(field => {
 
   label.classList.add('field-label');
   label.setAttribute('for', input[count].id);
-  label.innerText = input[count].name;
+  label.innerText = input[count].name.split(/(?=[A-Z])/).join(' ');
   field.prepend(label);
 
   input[count].placeholder
-    = input[count].name.charAt(0).toUpperCase() + input[count].name.slice(1);
+    = input[count].name.charAt(0).toUpperCase()
+    + input[count].name.slice(1).split(/(?=[A-Z])/).join(' ');
 
   count++;
 });
