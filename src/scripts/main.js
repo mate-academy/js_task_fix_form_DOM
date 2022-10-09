d342d3b376de779903a72379a292336ae387587e
Sergii Iatsenko <sergii.iatsenko@gmail.com>
2022-10-09 21:10:48 +0300

    Solution after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,9 +6,28 @@ for (const input of allInputs) {
   const inputLabel = document.createElement('label');
 
   inputLabel.className = 'field-label';
-  inputLabel.textContent = input.name;
+
+  let secondWordBegin = 0;
+
+  for (let i = 1; i < input.name.length; i++) {
+    if (input.name[i] === input.name[i].toUpperCase()) {
+      secondWordBegin = i;
+    }
+  };
+
+  inputLabel.textContent
+   = (secondWordBegin === 0)
+      ? input.name
+      : input.name.slice(0, secondWordBegin) + ' '
+       + input.name.slice(secondWordBegin);
+
   inputLabel.htmlFor = input.id;
 
   input.before(inputLabel);
-  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+
+  input.placeholder
+   = (secondWordBegin === 0)
+      ? input.name[0].toUpperCase() + input.name.slice(1)
+      : input.name[0].toUpperCase() + input.name.slice(1, secondWordBegin) + ' '
+    + input.name.slice(secondWordBegin);
 }
