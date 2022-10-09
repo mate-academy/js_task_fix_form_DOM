7d9571c727c626940e7d16322e27b0f4c66235db
Anton Martyniuk <antonmartyniuk@gmail.com>
2022-10-09 19:26:46 +0300

    Solution js_task_fix_form_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const forms = document.querySelectorAll('form');
+const inputs = [...forms].map(form =>
+  [...form.querySelectorAll('div')].map(el => el.querySelector('input')));
+
+const inputWithEmtyEl = [...inputs[0], ...inputs[1]];
+const input = inputWithEmtyEl.filter(el => el != null);
+
+for (let i = 0; i < input.length; i++) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.textContent = input[i].name;
+  label.htmlFor = input[i].id;
+
+  input[i].before(label);
+  input[i].placeholder = input[i].name;
+}
