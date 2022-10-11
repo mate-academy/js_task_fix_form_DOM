1237043afe57efc7e2f238fba15d36cfe2e18e4f
Anton Martyniuk <antonmartyniuk@gmail.com>
2022-10-11 19:12:05 +0300

    Solution with fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,5 +15,8 @@ for (let i = 0; i < input.length; i++) {
   label.htmlFor = input[i].id;
 
   input[i].before(label);
-  input[i].placeholder = input[i].name;
+
+  const placeholder = input[i].name[0].toUpperCase();
+
+  input[i].placeholder = placeholder + input[i].name.slice(1);
 }
