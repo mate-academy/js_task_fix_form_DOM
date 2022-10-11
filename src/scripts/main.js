d6df702424d91b16485a1b4c5b625a7ea0dc46bc
Vlad Kurachenko <x0674016244@gmail.com>
2022-10-11 11:36:15 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,7 @@ for (const input of [...inputs]) {
   const label = document.createElement('label');
 
   label.className = 'field-label';
-  label.innerText = input.name;
+  label.innerText = input.name.split(/(?=[A-Z])/g).join(' ');
   input.parentElement.append(label);
   input.placeholder = capitalize(input.name);
 };
