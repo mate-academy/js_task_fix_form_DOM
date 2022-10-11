b8227ee05b941c7bfec0b36da338890c696d4c62
Ruslan Nedaiborshch <rnedaiborshch@gmail.com>
2022-10-11 23:03:34 +0300

    cod for task fix from

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const list = [...document.querySelectorAll('input')];
+
+function devide(word) {
+  const stringWord = String(word);
+
+  let result = '';
+
+  for (let i = 0; i < stringWord.length; i++) {
+    if (stringWord[i] === stringWord[i].toUpperCase()) {
+      result += ' ';
+    }
+
+    i === 0 ? result += stringWord[i].toUpperCase() : result += stringWord[i];
+  }
+
+  return result;
+}
+
+for (const input of list) {
+  const item = document.createElement('label');
+
+  input.setAttribute('placeholder', devide(input.name));
+
+  item.htmlFor = input.id;
+  item.classList = 'field-label';
+
+  item.innerText = (devide(input.name)).toUpperCase();
+
+  input.parentElement.prepend(item);
+}
