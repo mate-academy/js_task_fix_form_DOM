1ec85d354b7ce7fff5e506d05681d0b5f064d890
Hlazman <glazman.b@gmail.com>
2022-10-12 13:38:23 +0300

    used index instead count

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,19 +2,16 @@
 
 const fields = document.querySelectorAll('.field');
 const input = document.querySelectorAll('input');
-let count = 0;
 
-fields.forEach(field => {
+fields.forEach((field, index) => {
   const label = document.createElement('label');
 
   label.classList.add('field-label');
-  label.setAttribute('for', input[count].id);
-  label.innerText = input[count].name.split(/(?=[A-Z])/).join(' ');
+  label.setAttribute('for', input[index].id);
+  label.innerText = input[index].name.split(/(?=[A-Z])/).join(' ');
   field.prepend(label);
 
-  input[count].placeholder
-    = input[count].name.charAt(0).toUpperCase()
-    + input[count].name.slice(1).split(/(?=[A-Z])/).join(' ');
-
-  count++;
+  input[index].placeholder
+    = input[index].name.charAt(0).toUpperCase()
+    + input[index].name.slice(1).split(/(?=[A-Z])/).join(' ');
 });
