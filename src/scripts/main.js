64a71c08333757dc2a66779b4f4d703fba5a1fb7
Yuri Gatskovskyi <yurimoreau@gmail.com>
2022-10-14 14:59:39 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+const labelItem = document.createElement('label');
+
+labelItem.className = 'field-label';
+
+inputs.forEach(input => {
+  const newName = input.name.split('');
+  let newWordIndex;
+
+  do {
+    newWordIndex = newName.findIndex(char => char.toLowerCase() !== char);
+
+    if (newWordIndex !== -1) {
+      newName.splice(newWordIndex, 1, ' ', newName[newWordIndex].toLowerCase());
+    }
+  } while (newWordIndex !== -1);
+
+  newName[0] = newName[0].toUpperCase();
+  labelItem.htmlFor = input.id;
+  labelItem.innerText = newName.join('');
+  input.before(labelItem.cloneNode(true));
+  input.placeholder = newName.join('');
+});
