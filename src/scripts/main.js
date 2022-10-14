319efbffd3872c57402491fc7015a5785cbe4e75
Anastasiia Tilikina <anastasiia.tilikina@gmail.com>
2022-10-14 20:37:38 +0300

    Fix form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const inputList = [...document.querySelectorAll('input')];
+
+for (const field of inputList) {
+  const label = document.createElement('label');
+
+  field.placeholder = field.name.toUpperCase();
+  field.insertAdjacentElement('beforebegin', label);
+
+  label.htmlFor = field.id;
+  label.className = 'field-label';
+  label.innerText = field.placeholder;
+}
