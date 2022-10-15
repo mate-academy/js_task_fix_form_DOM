551d3cc1716fc50479bd9d2b4f94ba8cbe9b34e3
Anastasiia Tilikina <anastasiia.tilikina@gmail.com>
2022-10-15 16:58:28 +0300

    Fix input Text

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,17 @@
 'use strict';
 
+const kebabCase = require('../../node_modules/lodash/kebabCase');
+
 const inputList = [...document.querySelectorAll('input')];
 
 for (const field of inputList) {
+  const inputText = kebabCase(field.name).split('-').join(' ').toUpperCase();
+
   const label = document.createElement('label');
 
-  field.placeholder = field.name.toUpperCase();
+  field.placeholder = inputText;
   field.insertAdjacentElement('beforebegin', label);
-
   label.htmlFor = field.id;
   label.className = 'field-label';
-  label.innerText = field.placeholder;
+  label.innerText = inputText;
 }
