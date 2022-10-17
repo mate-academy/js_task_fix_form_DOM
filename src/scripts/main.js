0e5fcf0e7abafbdbb702df62c69f20417770bbdf
John Rust <johnrust12@gmail.com>
2022-10-17 09:09:33 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+function splitUpperText(string) {
+  return string.name
+    .replace(/[A-Z]/, ' $&').toUpperCase();
+}
+
+for (const element of inputs) {
+  const label = document.createElement('label');
+
+  label.setAttribute('for', `${element.id}`);
+  element.setAttribute('placeholder', splitUpperText(element));
+  element.before(label);
+
+  label.textContent = `${splitUpperText(element)}`;
+}
