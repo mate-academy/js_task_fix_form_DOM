18df01d19f46ebbab59464d66abb545215aa8028
Kris Mur <budkevych.h@gmail.com>
2022-10-18 19:35:54 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
+/* eslint-disable no-console */
 'use strict';
 
-// write code here
+const allInputs = document.getElementsByClassName('field-text');
+
+[...allInputs].forEach((el) => {
+  const newLabel = document.createElement('lable');
+
+  newLabel.className = 'field-label';
+  newLabel.setAttribute('for', `${el.id}`);
+  newLabel.textContent = `${el.name}`;
+  el.before(newLabel);
+
+  const placeholderText = el.getAttribute('name');
+
+  el.setAttribute('placeholder',
+    `${placeholderText[0].toUpperCase() + placeholderText.slice(1)}`);
+});
