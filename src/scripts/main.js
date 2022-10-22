e22b2f12101222fd096387da440b18ee5c4a9f3b
Anastasia Babko <hr.anastasia.t@gmail.com>
2022-10-22 14:36:39 +0300

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,27 @@
 
 const inputs = document.querySelectorAll('input');
 
+const separatingWords = function(words) {
+  const splited = words.split('');
+
+  for (let i = 1; i < splited.length; i++) {
+    if (splited[i] === splited[i].toUpperCase()) {
+      splited[i] = ` ${splited[i].toLowerCase()}`;
+    }
+  }
+
+  return splited.join('');
+};
+
 inputs.forEach(input => {
-  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+  input.placeholder = input.name[0].toUpperCase()
+    + separatingWords(input.name.slice(1));
 
   const label = document.createElement('label');
 
   label.classList.add('field-label');
   label.setAttribute('for', input.id);
-  label.textContent = input.name;
+  label.textContent = separatingWords(input.name);
 
   input.insertAdjacentElement('beforebegin', label);
 });
