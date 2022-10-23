43d2303da777b053f660a179d7ce9242aded2386
Margarita Chorniak <chorniak.rita@gmail.com>
2022-10-23 10:06:32 +0300

    fix_form_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+for (const input of [...inputs]) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.getAttribute('id'));
+
+  let text = input.name[0].toUpperCase();
+
+  for (let i = 1; i < input.name.length; i++) {
+    if (input.name[i] === input.name[i].toUpperCase()) {
+      text += ' ';
+    }
+    text += input.name[i];
+  }
+
+  label.textContent = text.toUpperCase();
+  input.placeholder = text;
+  input.parentElement.insertAdjacentElement('afterbegin', label);
+}
