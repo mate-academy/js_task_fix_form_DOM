c265ecac882592c8821e19e36c9f4f100f900da4
Mykyta Krainik <nikinta2003@gmail.com>
2022-10-25 12:49:15 +0300

    Function fixForm was implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+function fixForm(form) {
+  const inputs = form.querySelectorAll('input');
+
+  for (const input of inputs) {
+    addLabel(input);
+    addPlaceholder(input);
+  }
+}
+
+function addLabel(input) {
+  input.insertAdjacentHTML('beforebegin', `
+    <label for=${input.id} class='field-label'>
+      ${input.name}
+    </label>
+  `);
+}
+
+function addPlaceholder(input) {
+  const placeholder = separateWords(input.name);
+
+  input.placeholder = placeholder[0].toUpperCase() + placeholder.slice(1);
+}
+
+function separateWords(words) {
+  return words
+    .split('')
+    .map(letter => {
+      return letter === letter.toUpperCase()
+        ? ' ' + letter.toLowerCase()
+        : letter;
+    })
+    .join('');
+}
+
+const forms = document.querySelectorAll('form');
+
+for (const form of forms) {
+  fixForm(form);
+}
