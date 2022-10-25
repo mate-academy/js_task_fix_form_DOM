ec1d594fd60e0508f5275a94e443eea762fc6a26
Anastasiya145 <asiva@ukr.net>
2022-10-25 20:04:16 +0200

    the task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const input = document.querySelectorAll('input');
+
+[...input].forEach(el => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.textContent = el.name;
+  label.setAttribute('for', el.id);
+  el.placeholder = el.name[0].toUpperCase() 
+  + el.name.substring(1).split(/(?=[A-Z])/).join(' ');
+  el.append(label);
+
+  el.parentElement.append(label);
+});
