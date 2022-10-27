2eb37502dda561753c581d0464e3f29a97f4cf9b
Yurii Teslia <teslya.yuriy@gmail.com>
2022-10-27 13:37:23 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const inputsList = document.getElementsByTagName('input');
+
+for (const el of inputsList) {
+  const newElement = document.createElement('label');
+
+  newElement.setAttribute('class', `field-label`);
+  newElement.setAttribute('for', `${el.id}`);
+  newElement.innerText = el.name;
+
+  const parentElement = el.parentElement;
+
+  newElement.append(el);
+  parentElement.append(newElement);
+}
