0f37ae4b122d04f24e35888a2cdc1ba1c94930a7
Yurii Teslia <teslya.yuriy@gmail.com>
2022-10-27 21:14:45 +0300

    update 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,20 @@
 'use strict';
 
 const inputsList = document.getElementsByTagName('input');
+const updatedNames = {
+  firstName: 'First Name',
+  lastName: 'Last Name',
+  email: 'Email',
+  password: 'Password',
+};
 
 for (const inputItem of inputsList) {
   const newElement = document.createElement('label');
 
   newElement.setAttribute('class', `field-label`);
   newElement.setAttribute('for', `${inputItem.id}`);
-  inputItem.setAttribute('placeholder', `${inputItem.name}`);
-  newElement.innerText = `${inputItem.name}`;
+  inputItem.setAttribute('placeholder', `${updatedNames[inputItem.name]}`);
+  newElement.innerText = `${updatedNames[inputItem.name]}`;
 
   const itemParent = inputItem.parentElement;
 
