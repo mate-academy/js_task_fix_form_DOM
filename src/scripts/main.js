ddf23ab753dbcdfc3f9c70781d5f0274f7443cc1
Dima Romanenko <demigod177712@gmail.com>
2022-10-29 19:45:34 +0300

    labels and placeholders was added by dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+inputs.forEach(inp => {
+  const label = document.createElement('label');
+
+  label.classList = 'field-label';
+  label.htmlFor = inp.id;
+
+  label.textContent = inp.name;
+
+  inp.parentElement.append(label);
+
+  inp.placeholder = label.textContent[0].toUpperCase()
+    + label.textContent.substring(1);
+
+  if (inp.name === 'firstName') {
+    inp.placeholder = inp.name[0].toUpperCase()
+    + inp.name.slice(1, 5)
+    + ' '
+    + inp.name.slice(5);
+
+    label.textContent = inp.name[0].toUpperCase()
+    + inp.name.slice(1, 5)
+    + ' '
+    + inp.name.slice(5);
+  }
+
+  if (inp.name === 'lastName') {
+    inp.placeholder = inp.name[0].toUpperCase()
+    + inp.name.slice(1, 4)
+    + ' '
+    + inp.name.slice(4);
+
+    label.textContent = inp.name[0].toUpperCase()
+    + inp.name.slice(1, 4)
+    + ' '
+    + inp.name.slice(4);
+  }
+});
