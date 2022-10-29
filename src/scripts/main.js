c426ed89f456fe877606e965a487898be28f4bf3
Oleksandr Lytvynenko <olek.lytvynenko@gmail.com>
2022-10-29 16:26:47 +0300

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const itemList = document.querySelectorAll('input');
+
+function addLabel(arg) {
+  arg.forEach((item, i) => {
+    const label = document.createElement('label');
+    const capitalize = arg[i].name.charAt(0).toUpperCase()
+    + arg[i].name.slice(1);
+
+    label.className = 'field-label';
+    label.setAttribute('for', arg[i].id);
+    label.textContent = arg[i].name;
+    item.insertAdjacentElement('beforebegin', label);
+    item.setAttribute('placeholder', capitalize);
+  });
+}
+
+addLabel(itemList);
