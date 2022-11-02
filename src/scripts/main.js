124291f8a05da6e3f067103d6fc4dba55f23c58c
Ruslan Miroshnychenko <rusikm87@gmail.com>
2022-11-02 09:15:18 +0200

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const input = [...document.querySelectorAll('input')];
+
+function addElements(elements) {
+  elements.forEach(el => {
+    const label = document.createElement('label');
+    const names = formatText(el.name);
+
+    label.htmlFor = el.id;
+    label.className = 'field-label';
+    label.innerText = names;
+    el.placeholder = names;
+
+    el.parentElement.prepend(label);
+  });
+}
+
+function formatText(text) {
+  return text.split('')
+    .map(el => {
+      if (el === el.toUpperCase()) {
+        return ' ' + el;
+      }
+
+      return el;
+    })
+    .join('')
+    .toUpperCase();
+}
+
+addElements(input);
