1d8c5bee06c5e92f142fb908c7b46ddd4f207ad2
ovsiannikov <ovsiannikov@ucu.edu.ua>
2022-11-03 14:29:42 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+const arrOfInputs = [...inputs];
+
+for (const el of arrOfInputs) {
+  el.insertAdjacentHTML('beforebegin', `
+    <label for='${el.id}' class='field-label'> 
+    ${upCase(el.name)}
+    </label>
+    `);
+
+  el.placeholder = upCase(el.name);
+};
+
+function upCase(str) {
+  let result = str;
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] === str[i].toUpperCase()) {
+      result = str.slice(0, i) + ' ' + str.slice(i);
+    }
+  }
+
+  return result.toUpperCase();
+};
