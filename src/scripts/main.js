7382243adc531ab09c9d10cf0cd37dfcdd4f1b1b
Denis Cherevaschenko <deniska.cherevaschenko@gmail.com>
2022-11-04 13:03:47 +0200

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+// const signUpForm = forms[0];
+// const signInForm = forms[1];
+
+// console.log(signUpForm);
+
+const label = document.createElement('label');
+
+label.className = 'field-label';
+
+for (const input of inputs) {
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+  input.closest('.field').prepend(label.cloneNode(true));
+}
