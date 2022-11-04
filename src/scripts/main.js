511206d2a867e6fd059b01b4e8003560f0cac838
Denis Cherevaschenko <deniska.cherevaschenko@gmail.com>
2022-11-04 16:41:28 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,16 @@
 'use strict';
 
 const inputs = document.querySelectorAll('form input');
-// const signUpForm = forms[0];
-// const signInForm = forms[1];
-
-// console.log(signUpForm);
-
 const label = document.createElement('label');
 
 label.className = 'field-label';
 
 for (const input of inputs) {
+  const placeholderText = [...input.name][0]
+    .toUpperCase() + input.name.slice(1);
+
   label.htmlFor = input.id;
   label.textContent = input.name;
+  input.setAttribute('placeholder', placeholderText);
   input.closest('.field').prepend(label.cloneNode(true));
 }
