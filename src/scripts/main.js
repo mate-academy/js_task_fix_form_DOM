1462b275470aac2ef3f28876b0d7defc367288b2
July Terekhova <sky206380@gmail.com>
2022-11-06 22:41:51 +0200

    Solution 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,7 +7,23 @@ for (const input of inputs) {
 
   label.className = 'field-label';
   label.htmlFor = input.id;
-  label.textContent = input.name;
+  label.textContent = splitByWord(input.name);
   input.closest('.field').prepend(label);
-  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+
+  input.placeholder
+  = input.name[0].toUpperCase()
+  + splitByWord(input.name.slice(1));
+}
+
+function splitByWord(string) {
+  let result = '';
+
+  for (const char of string) {
+    if (char.toUpperCase() === char) {
+      result += ' ';
+    }
+    result += char;
+  }
+
+  return result;
 }
