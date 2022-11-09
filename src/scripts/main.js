f4cd31ccbb612d950b0fbb49e75216861f819d94
Maryna Kucher <marina.kucher8@gmail.com>
2022-11-09 18:50:39 +0200

    Solution 19 is fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,8 +7,22 @@ fields.forEach(field => {
   const names = field.getAttribute('name');
 
   label.className = 'field-label';
+
   label.for = field.getAttribute('id');
-  label.textContent = names;
+  label.textContent = addSpace(names);
   field.placeholder = names[0].toUpperCase() + names.slice(1);
   field.parentElement.append(label);
 });
+
+function addSpace(str) {
+  let words = '';
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] && str[i] === str[i].toUpperCase()) {
+      words += ' ';
+    }
+    words += str[i];
+  }
+
+  return words;
+}
