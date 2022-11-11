442c057227356beb873a08d5ae0f738468e15b24
Taras Kovalchuk <taras.kovalcuk93@gmail.com>
2022-11-11 21:06:05 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const container = document.querySelectorAll('input');
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+for (const child of container) {
+  const childName = child.getAttribute('name');
+  const childId = child.getAttribute('id');
+
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.textContent = child.getAttribute('name');
+  label.setAttribute('for', childId);
+  child.setAttribute('placeholder', capitalizeFirstLetter(childName));
+
+  child.parentElement.prepend(label);
+}
