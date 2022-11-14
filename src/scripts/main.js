1be2c7466fa8f6c3ffdf44d21dfbe3fc8e56d3b7
Taras Kovalchuk <taras.kovalcuk93@gmail.com>
2022-11-15 00:49:44 +0200

    Fixed after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,7 +7,18 @@ function capitalizeFirstLetter(string) {
 }
 
 for (const child of container) {
-  const childName = child.getAttribute('name');
+  let childName = child.getAttribute('name');
+  const alfabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let sepatateName = '';
+
+  for (let i = 1; i < childName.length; i++) {
+    if (alfabet.includes(childName[i])) {
+      sepatateName = childName.slice(0, i) + ' ' + childName.slice(i);
+      childName = sepatateName;
+      break;
+    }
+  }
+
   const childId = child.getAttribute('id');
 
   const label = document.createElement('label');
