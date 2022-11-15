f92e00ca399ee8844f3baa10263bbe58796ba397
Bohdan Rudenko <RudBeee@gmail.com>
2022-11-15 14:38:28 +0200

    vol_1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const allInput = document.querySelectorAll('input');
+
+function correctName(text) {
+  return text[0].toUpperCase() + text.slice(1).toLowerCase();
+}
+
+for (const input of [...allInput]) {
+  const label = document.createElement('label');
+
+  label.className = `field-label`;
+  label.htmlFor = input.id;
+  label.textContent = input.name.toUpperCase();
+  input.placeholder = correctName(input.name);
+  input.parentElement.append(label);
+}
