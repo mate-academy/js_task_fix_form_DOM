472b92e9e6e71f55d8b56a7234ea89922c3ba5ab
Yevhen Petrovskyi <petrovskyi.yevhen@gmail.com>
2022-11-19 00:11:41 +0200

    Solution for fix_form_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+inputs.forEach(item => {
+  const label = document.createElement('label');
+
+  label.htmlFor = item.id;
+  label.classList.add('field-label');
+  label.innerText = item.name.toUpperCase();
+  item.parentElement.appendChild(label);
+
+  item.placeholder = item.name[0].toUpperCase()
+    + item.name.slice(1).toLowerCase();
+});
