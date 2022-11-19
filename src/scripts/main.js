031677c34a2ccb6f817f2001159c11e92571d4d9
Maria Bramnik <mariabramnik@gmail.com>
2022-11-19 21:59:57 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const formNodeList = document.querySelectorAll('form');
+const formArray = Array.from(formNodeList);
+
+for (let j = 0; j < formArray.length; j++) {
+  const formElement = formArray[j];
+  const inputNodeList = formElement.querySelectorAll('input');
+  const inputArray = Array.from(inputNodeList);
+
+  for (let i = 0; i < inputArray.length; i++) {
+    const inputElement = inputArray[i];
+    let strName = inputElement.name;
+    const placeholderText = strName[0].toUpperCase() + strName.slice(1);
+    inputElement.placeholder = placeholderText;
+    const myLabel = document.createElement('label');
+    myLabel.textContent = inputElement.name;
+    myLabel.setAttribute("for", inputElement.id);
+    myLabel.className = 'field-label';
+    const inputParentNode = inputElement.parentNode;
+    inputParentNode.appendChild(myLabel);
+
+  }
+
+}
