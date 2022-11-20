d153bf946d2e6e4bd116bac015489e398ee23e1d
Andrii Medintsev <medintsev.andrew@gmail.com>
2022-11-20 23:54:04 +0200

    Form was fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  input.parentElement.insertAdjacentHTML('beforeend', `
+    <label class="field-label" for="#${input.id}">
+      ${input.name}
+    </label>
+  `);
+
+  input.placeholder = capitalizeName(input.name);
+});
+
+function capitalizeName(str) {
+  const camelToTitle = str.replace(/([A-Z])/g, ' $1');
+  const result = camelToTitle[0].toUpperCase() + camelToTitle.slice(1);
+
+  return result;
+}
