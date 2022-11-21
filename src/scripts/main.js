735932386008c12e8a4debd2fa5efdf767793ac7
Sergey Mironenko <serg1prime@gmail.com>
2022-11-22 01:26:40 +0200

    Task was done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const items = document.querySelectorAll('.field-text');
+
+for (const node of [...items]) {
+  const item = document.createElement('label');
+
+  item.className = 'field-label';
+  item.textContent = `${node.name}`;
+  item.setAttribute('for', `${node.id}`);
+  node.parentElement.insertBefore(item, node);
+
+  switch (node.name) {
+    case 'firstName':
+      node.placeholder = 'firstName'.toUpperCase();
+      break;
+    case 'lastName':
+      node.placeholder = 'lastName'.toUpperCase();
+      break;
+    case 'email':
+      node.placeholder = 'email'.toUpperCase();
+      break;
+    case 'password':
+      node.placeholder = 'password'.toUpperCase();
+      break;
+  }
+}
