668dee4728fab23bebe70e3323017f9cd7ed2def
Maks <comanderius@gmail.com>
2022-11-22 14:20:09 +0100

    Fix form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('.field');
+const inputList = document.querySelectorAll('input');
+
+for (let i = 0; i < list.length; i++) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+
+  label.textContent = inputList[i].name;
+
+  inputList[i].placeholder = inputList[i].name[0].toUpperCase()
+    + inputList[i].name.slice(1);
+
+  label.htmlFor = inputList[i].id;
+  list[i].append(label);
+}
