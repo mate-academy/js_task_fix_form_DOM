b135644bb61ff0ceed5f6e0fb00e5d9d059eb3c2
Sergey Mironenko <serg1prime@gmail.com>
2022-11-24 22:28:59 +0200

    Fix form task third commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,31 +2,21 @@
 
 const formFields = document.querySelectorAll('.field-text');
 
+function separateName(word) {
+  let result = word;
+  result = result[0].toUpperCase()
+    + result.slice(1, result.length).split('N').join(' N');
+
+  return result;
+}
+
 for (const textField of [...formFields]) {
   const label = document.createElement('label');
 
-  if (textField.name === 'firstName' || `${textField.name}` === 'lastName') {
-    label.textContent = textField.name.split('N').join(' N');
-  } else {
-    label.textContent = `${textField.name}`;
-  }
-
   label.className = 'field-label';
-  label.setAttribute('for', `${textField.id}`);
+  label.textContent = separateName(textField.name);
+  label.setAttribute('for', textField.id);
   textField.parentElement.insertBefore(label, textField);
 
-  switch (textField.name) {
-    case 'firstName':
-      textField.placeholder = 'first Name'.toUpperCase();
-      break;
-    case 'lastName':
-      textField.placeholder = 'last Name'.toUpperCase();
-      break;
-    case 'email':
-      textField.placeholder = 'email'.toUpperCase();
-      break;
-    case 'password':
-      textField.placeholder = 'password'.toUpperCase();
-      break;
-  }
+  textField.placeholder = separateName(textField.name);
 }
