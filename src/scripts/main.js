b504fd611781da53c99c8704e4671ab7bf58face
«ifUride» <ifuride@gmail.com>
2022-11-27 14:30:59 +0100

    Word separation in labels and placeholders

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,25 @@
 'use strict';
 
 const inputs = document.querySelectorAll('input');
+const capitalLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 for (const input of [...inputs]) {
   const label = document.createElement('label');
 
   label.setAttribute('for', input.id);
   input.parentElement.append(label);
-  label.textContent = input.name;
   label.className = 'field-label';
 
-  const placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+  let fieldName = input.name[0].toUpperCase();
 
-  input.setAttribute('placeholder', placeholder);
+  for (let i = 1; i < input.name.length; i++) {
+    if (capitalLetters.includes(input.name[i])) {
+      fieldName += ' ' + input.name[i];
+    } else {
+      fieldName += input.name[i];
+    }
+  }
+
+  label.textContent = fieldName.toUpperCase();
+  input.setAttribute('placeholder', fieldName);
 }
