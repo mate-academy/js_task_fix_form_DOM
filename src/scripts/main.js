0db983adbc93b2d1384a80fab6a11d835e7e9f7a
Panianchuk M <panianchuk@gmail.com>
2022-11-29 01:08:05 +0200

    first

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const fields = [...document.querySelectorAll('input')];
+
+fixForm(fields);
+
+function fixForm(inputs) {
+  inputs.forEach(item => {
+    const labelEl = document.createElement('label');
+    const text = getText(item.name);
+
+    labelEl.htmlFor = item.id;
+    labelEl.className = 'field-label';
+    labelEl.innerText = text.toUpperCase();
+    item.placeholder = text;
+
+    item.before(labelEl);
+  });
+}
+
+function getText(word) {
+  let newStr = '';
+
+  for (const letter of word) {
+    if (letter === letter.toUpperCase()) {
+      newStr += ' ';
+      newStr += letter;
+    } else {
+      newStr += letter;
+    }
+  }
+
+  const result = newStr.charAt(0).toUpperCase() + newStr.slice(1);
+
+  return result;
+}
