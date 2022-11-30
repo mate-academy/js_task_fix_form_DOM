7ab43dc7ada1fe7d97443336ea8117564643c249
«ifUride» <ifuride@gmail.com>
2022-11-30 10:14:27 +0100

    capitalLetters variable removal

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const inputs = document.querySelectorAll('input');
-const capitalLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 for (const input of [...inputs]) {
   const label = document.createElement('label');
