2b7753869367e5ef6a2cab4c93d5cfd8b3c1b141
ShkarupaDenys <shkarupadenys@gmail.com>
2022-12-01 22:37:30 +0200

    Fixed after review.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,35 @@
 
 const inputs = document.querySelectorAll('.field-text');
 
+const transformWords = word => {
+  let transformedWord = '';
+  let index = null;
+
+  for (let i = 0; i < word.length; i++) {
+    if (word[i].toUpperCase() === word[i]) {
+      index = i;
+
+      break;
+    }
+
+    transformedWord += word[i];
+  }
+
+  if (index) {
+    transformedWord += ` ${word.slice(index)}`;
+  }
+
+  return transformedWord.toUpperCase();
+};
+
 inputs.forEach(input => {
   const { name: inputName, id } = input;
   const label = document.createElement('label');
 
-  label.textContent = inputName.toUpperCase();
+  label.textContent = transformWords(inputName);
   label.className = 'field-label';
   label.htmlFor = id;
 
-  input.placeholder = inputName.toUpperCase();
+  input.placeholder = transformWords(inputName);
   input.insertAdjacentElement('beforebegin', label);
 });
