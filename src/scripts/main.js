1f50752ed1e48efc1bac83c8108e2c63af861e02
Dmitriy-Kovalenko <dmitrijkovalenko@gmail.com>
2022-12-01 21:52:29 +0200

    lebel and placeholder added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const inputs = document.getElementsByTagName('input');
+const label = document.createElement('label');
+
+for (const input of inputs) {
+  input.parentElement.children[0].before(label.cloneNode());
+
+  const labelElement = input.parentElement.firstElementChild;
+
+  labelElement.className = 'field-label';
+  labelElement.htmlFor = input.id;
+  labelElement.textContent = input.name;
+
+  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+}
