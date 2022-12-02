592483e55bc92db19285a5e1764f6e3ec61ed123
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2022-12-02 19:55:40 +0200

    fixed form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+inputs.forEach(input => {
+  const names = input.name.split(/(?=[A-Z])/).join(' ');
+
+  input.insertAdjacentHTML('beforebegin',
+    `<label for="${input.id}" class="field-label">
+      ${names}
+    </label>
+  `);
+
+  input.placeholder = `${names[0].toUpperCase() + names.slice(1)}`;
+});
