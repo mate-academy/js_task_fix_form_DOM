e64232ede95c780a676b2a3f223220f95e44889b
dmitriy-kovalenko <kovalenkodmitrij@gmail.com>
2022-12-02 12:22:31 +0200

    add func splitCamelCase()

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,7 +10,13 @@ for (const input of inputs) {
 
   labelElement.className = 'field-label';
   labelElement.htmlFor = input.id;
-  labelElement.textContent = input.name;
+  labelElement.textContent = splitCamelCase(input.name);
 
-  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+  const inputName = input.name[0].toUpperCase() + input.name.slice(1);
+
+  input.placeholder = splitCamelCase(inputName);
+}
+
+function splitCamelCase(str) {
+  return str.replace(/[a-z](?=[A-Z])/g, '$& ');
 }
