8ffa375e019a98166b27ca0481340499e15acb6d
Sergey Kovyazin <mr.roland1989@gmail.com>
2022-12-13 06:53:31 +0200

    fix form solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const input = document.body.querySelectorAll('input');
+
+[...input].forEach(item => {
+  const label = document.createElement('label');
+
+  item.before(label);
+  label.classList.add('field-label');
+  label.setAttribute('for', `${item.id}`);
+  label.textContent = `${item.name}`;
+  item.setAttribute('placeholder', `${item.name}`);
+});
