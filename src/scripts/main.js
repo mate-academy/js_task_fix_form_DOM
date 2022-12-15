31fe4309d116c40de88d920629f1a31286de1da0
Oselskyi Oleksandr <oselskyi3@gmail.com>
2022-12-15 17:42:31 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write code here
+const fieldsForms = document.querySelectorAll('.field');
+
+fieldsForms.forEach(field => {
+  const list = [...field.children];
+
+  field.children[0]
+    .setAttribute('placeholder', `${list[0].attributes.name.value}`);
+
+  const label = document.createElement('label');
+
+  label.setAttribute('for', `${list[0].attributes.id.value}`);
+  label.textContent = `${list[0].attributes.name.value}`;
+  field.prepend(label);
+});
