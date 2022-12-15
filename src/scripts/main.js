5fb2d18ce3db7586d21ecb61acd7779d57a6ceaf
OksanaBaloh <obaloh1108@gmail.com>
2022-12-15 14:49:37 +0200

    Whitespaces were added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,12 +4,19 @@ const inputs = document.querySelectorAll('input');
 
 for (const input of inputs) {
   const label = document.createElement('label');
+  let nameWithSpace = input.name[0].toUpperCase();
+
+  for (let i = 1; i < input.name.length; i++) {
+    if (input.name[i] === input.name[i].toUpperCase()) {
+      nameWithSpace += ' ';
+    }
+    nameWithSpace += input.name[i];
+  }
 
   label.setAttribute('class', 'field-label');
-  label.setAttribute('for', `${input.id}`);
-  label.textContent = input.name;
+  label.setAttribute('for', input.id);
+  label.textContent = nameWithSpace;
   input.parentElement.prepend(label);
 
-  input.setAttribute('placeholder',
-    `${input.name.charAt(0).toUpperCase()}${input.name.slice(1)}`);
+  input.setAttribute('placeholder', nameWithSpace);
 }
