c146370e6d03078abb9194528d07369229f60950
Taras Vorobets <vorobetst777@gmail.com>
2022-12-15 18:44:19 +0200

    Sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const allInputs = document.querySelectorAll('input');
+
+for (let i = 0; i < allInputs.length; i++) {
+  const newLabel = document.createElement('label');
+
+  const inputName = allInputs[i].getAttribute('name');
+
+  newLabel.className = 'field-label';
+  newLabel.htmlFor = allInputs[i].id;
+  newLabel.textContent = inputName;
+  allInputs[i].parentElement.append(newLabel);
+
+  const toUpperLetter = inputName.charAt(0)
+    .toUpperCase() + inputName.slice(1);
+
+  allInputs[i].placeholder = toUpperLetter;
+}
