695830d017ee0761134011c921618c6f75281963
Oselskyi Oleksandr <oselskyi3@gmail.com>
2022-12-16 21:50:37 +0200

    fix remarks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,13 +5,16 @@ const fieldsForms = document.querySelectorAll('.field');
 
 fieldsForms.forEach(field => {
   const list = [...field.children];
+  const capitalize = str =>
+    str.slice(0, 1).toUpperCase() + str.slice(1);
 
-  field.children[0]
-    .setAttribute('placeholder', `${list[0].attributes.name.value}`);
+  field.children[0].setAttribute('placeholder',
+    `${capitalize(list[0].attributes.name.value)}`);
 
   const label = document.createElement('label');
 
   label.setAttribute('for', `${list[0].attributes.id.value}`);
-  label.textContent = `${list[0].attributes.name.value}`;
+  label.className = 'field-label';
+  label.textContent = `${capitalize(list[0].attributes.name.value)}`;
   field.prepend(label);
 });
