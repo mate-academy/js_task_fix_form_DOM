22e8fb9bbcba75fe7a8300772bd31ab7d223843a
Deniskakaka <krollik1000000@gmail.com>
2022-12-17 20:28:38 +0200

    add space

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,29 @@
 'use strict';
 
+const addSpace = (text, words, letters) => {
+  if (text.includes(words)) {
+    const separate = text.split('');
+
+    separate.splice(text.indexOf(letters), 0, ' ');
+
+    return separate.join('');
+  }
+
+  return text;
+};
+
 const inputs = document.querySelectorAll('input');
 const fields = document.querySelectorAll('.field');
 
 fields.forEach((el, index) => {
   el.insertAdjacentHTML('afterbegin',
     `<label for=${inputs[index].id} class=field-label>
-    ${inputs[index].name}
+    ${addSpace(inputs[index].name, 'Name', 'N')}
     </input>
   </label>`);
 });
 
 inputs.forEach(el => {
-  el.placeholder = el.name.charAt(0).toUpperCase() + el.name.slice(1);
+  el.placeholder = addSpace(el.name, 'Name', 'N')
+    .charAt(0).toUpperCase() + addSpace(el.name, 'Name', 'N').slice(1);
 });
