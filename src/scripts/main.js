0608492e2224bf5deee783b3c2cf24de3bf27b3c
Dmitriy Sporysh <dopmoneyd@gmail.com>
2022-12-19 20:05:39 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+const inputClass = 'field-label';
+const firstLatterUp = (word = '') => {
+  if (word.length > 0) {
+    return (
+      word.charAt(0).toUpperCase() + word.slice(1)
+    );
+  } else {
+    return word;
+  }
+};
+
+[...inputs].forEach(input => {
+  const inputId = input.id;
+  const inputName = input.name;
+
+  input.insertAdjacentHTML('beforebegin', `
+    <label
+      class="${inputClass}"
+      for="${inputId}"
+    >
+    ${inputName}
+    </label>
+  `);
+
+  input.placeholder = firstLatterUp(inputName);
+});
