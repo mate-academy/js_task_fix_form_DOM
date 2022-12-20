d7df5a09df3118e208c7dd26c75eee6e02fcc4e4
Roman <romanmalysh@romans-macbook-air.home>
2022-12-20 14:21:08 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const [...inputs] = document.querySelectorAll('input');
+
+const [...fields] = document.querySelectorAll('.field');
+
+function splitter(text) {
+  let res = text;
+  if (text !== text.toLowerCase()) {
+    const word = res.split(/(?=[A-Z])/).join(' ');
+
+    return word;
+  }
+
+  return text;
+}
+
+[...inputs].map((elem, index) => {
+  elem.placeholder = splitter(elem.name);
+});
+
+[...fields].map((elem, index) => {
+  elem.insertAdjacentHTML('afterbegin', `<label for="inputs[index].id" class="field-label">
+    ${splitter(inputs[index].name)}
+    </label>`);
+});
