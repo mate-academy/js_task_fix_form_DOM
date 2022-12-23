e0c0f2677ff6125e3ecf8237ae8ce996b65cd368
Kukharuk <its.up.to.you.dk@gmail.com>
2022-12-23 18:09:44 +0200

    addedLabel

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const div = document.querySelectorAll('.field');
+
+for (let i = 0; i < div.length; i++) {
+  const lab = document.createElement('label');
+
+  lab.setAttribute('for', div[i].firstElementChild.getAttribute('id'));
+  lab.className = 'field-label';
+  lab.textContent = div[i].firstElementChild.getAttribute('name').toUpperCase();
+
+  div[i].append(lab);
+
+  div[i].firstElementChild.placeholder
+  = div[i].firstElementChild.getAttribute('name').toUpperCase();
+};
