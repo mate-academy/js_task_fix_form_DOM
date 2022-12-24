bd02aef28e4d9a34b95b429310b39b18fcbf1065
Oleksii Mishchenko <alex.mishch@gmail.com>
2022-12-25 00:09:02 +0200

    Form is fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const fields = [...document.querySelectorAll('.field')];
+
+fields.forEach(field => {
+  const inputElement = field.querySelector('input');
+  const { id: inputID, name: inputName } = inputElement;
+
+  field.insertAdjacentHTML('afterbegin', `
+    <label class="field-label" for="${inputID}">
+      ${capitalized(inputName)}
+    </label>
+  `);
+
+  inputElement.placeholder = capitalized(inputName);
+});
+
+function capitalized(str) {
+  switch (str) {
+    case 'firstName': {
+      return 'First Name';
+    }
+
+    case 'lastName': {
+      return 'Last Name';
+    }
+  };
+
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
