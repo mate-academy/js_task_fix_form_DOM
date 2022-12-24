dee22c92dc374c52c522e1c511beefa002df9467
Kukharuk <its.up.to.you.dk@gmail.com>
2022-12-24 14:35:05 +0200

    spaceBetweenAdded

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,13 +8,15 @@ for (let i = 0; i < div.length; i++) {
   lab.setAttribute('for', div[i].firstElementChild.getAttribute('id'));
   lab.className = 'field-label';
 
-  lab.textContent = div[i].firstElementChild.name.toUpperCase();
+  lab.textContent = div[i].firstElementChild.name.replace(
+    /([a-z])([A-Z])/, '$1 $2').toUpperCase();
 
   div[i].append(lab);
 
   let nameCap = div[i].firstElementChild.name;
 
-  nameCap = nameCap.charAt(0).toUpperCase() + nameCap.slice(1);
+  nameCap = (nameCap.charAt(0).toUpperCase() + nameCap.slice(1)).replace(
+    /([a-z])([A-Z])/, '$1 $2');
 
   div[i].firstElementChild.placeholder = nameCap;
 };
