83f57c5aa1408325f4209307064a64f921830740
Vasiliy22 <hitretsukrnet@gmail.com>
2022-12-25 15:14:00 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const element = document.querySelector('form');
+const formElements = Array.from(element.elements);
+
+formElements.forEach(el => {
+  el.setAttribute('placeholder', `${el.name}`);
+
+  const label = document.createElement('label');
+
+  label.setAttribute('for', 'sign-up-name');
+  label.setAttribute('class', 'field-label');
+  label.textContent = `${el.name}`;
+  el.append(label);
+});
