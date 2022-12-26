bba2d50d3b018f19a247cdf5073ba26f412d74fd
Vasiliy22 <hitretsukrnet@gmail.com>
2022-12-26 18:56:25 +0200

    Solution2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,18 @@
 'use strict';
 
-const element = document.querySelector('form');
-const formElements = Array.from(element.elements);
+const onlyInputs = document.querySelectorAll('form input');
 
-formElements.forEach(el => {
-  el.setAttribute('placeholder', `${el.name}`);
+[...onlyInputs].map(item => {
+  const reg = /(?=[A-Z])/g;
+
+  item.setAttribute('placeholder',
+    item.name[0].toUpperCase()
+      + item.name.substring(1).replace(reg, ' '));
 
   const label = document.createElement('label');
 
-  label.setAttribute('for', 'sign-up-name');
+  label.setAttribute('for', item.id);
   label.setAttribute('class', 'field-label');
-  label.textContent = `${el.name}`;
-  el.append(label);
+  label.textContent = item.name.replace(reg, ' ');
+  item.parentElement.append(label);
 });
