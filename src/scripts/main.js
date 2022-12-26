591070ce579e24f4b516da0c1fafd3cc40963a25
Kristina Makarovska <kris.makarovska@gmail.com>
2022-12-26 17:47:20 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+function textmodifier(att) {
+  const capitilized = att.charAt(0).toUpperCase()
+    + att.slice(1);
+
+  return capitilized.split('t').join('t ');
+}
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.textContent = textmodifier(input.name);
+  input.parentElement.append(label);
+
+  input.setAttribute('placeholder', textmodifier(input.name));
+});
