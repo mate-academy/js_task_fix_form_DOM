56bfb2e2a76014bde66d8c311021b00d56c18219
Yaroslav Radchuk <105112047+Yaroslav-Radchuk@users.noreply.github.com>
2022-12-29 21:20:09 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const lists = document.querySelectorAll('input');
+
+for (const list of lists) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', list.id);
+
+  if (list.name === 'firstName') {
+    list.name = 'First Name';
+  };
+
+  if (list.name === 'lastName') {
+    list.name = 'Last Name';
+  };
+
+  label.textContent = list.name;
+  list.placeholder = list.name;
+  list.parentElement.append(label);
+};
