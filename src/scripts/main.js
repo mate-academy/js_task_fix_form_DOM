b1f6b8346878cdd0052f93ec6ef378c907a17977
EhorSaprykin <astralik1993@gmail.com>
2023-01-01 12:30:32 +0200

    Add placeholder's & label's

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+function addLabel(elements) {
+  elements.forEach(element => {
+    const inputLabel = document.createElement('label');
+
+    element.before(inputLabel);
+
+    inputLabel.classList = 'field-label';
+    inputLabel.setAttribute('for', element.id);
+    inputLabel.textContent = element.name;
+  });
+};
+
+function addPlaceholder(element) {
+  function capitalizeName(names) {
+    return names.charAt(0).toUpperCase() + names.slice(1);
+  };
+
+  return element.forEach((input) => {
+    input.placeholder = capitalizeName(input.name);
+  });
+};
+
+addLabel(inputs);
+addPlaceholder(inputs);
