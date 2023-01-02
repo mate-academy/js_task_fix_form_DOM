d8821bbfe0923402ce0ab1134d36bc9636551ea8
Mykola Chernysh <nanoteck7@gmail.com>
2023-01-02 13:30:10 +0200

    Task completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const item of inputs) {
+  const label = document.createElement('label');
+  const inputId = item.getAttribute('id');
+  const inputName = item.getAttribute('name');
+
+  label.className = 'field-label';
+  label.htmlFor = inputId;
+  label.textContent = inputName;
+
+  item.setAttribute('placeholder', inputName[0].toLocaleUpperCase()
+    + inputName.slice(1));
+
+  item.parentElement.append(label);
+}
