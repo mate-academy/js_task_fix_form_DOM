8a4a9d54c9239408e935de4219743b9e5ea67261
Nick Busarhin <busarginkolia@gmail.com>
2023-01-03 15:19:50 +0200

    fixedForm

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,12 @@
 'use strict';
 
+function convertToNewString(string) {
+  const newWord = `${string.slice(0, -4)}` + ' ' + 'Name';
+  const newName = newWord.toUpperCase();
+
+  return newName;
+}
+
 const inputs = document.querySelectorAll('input');
 const listOfInputs = [...inputs];
 
@@ -7,9 +14,17 @@ for (const input of listOfInputs) {
   const label = document.createElement('label');
 
   label.className = 'field-label';
+
   label.textContent = input.name;
+
   label.setAttribute('for', input.id);
 
   input.setAttribute('placeholder', input.name.toUpperCase());
+
+  if (input.name.includes('Name')) {
+    label.textContent = convertToNewString(input.name);
+    input.setAttribute('placeholder', convertToNewString(input.name));
+  }
+
   input.parentElement.append(label);
 }
