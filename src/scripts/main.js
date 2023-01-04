becf93631c1920f6d40cbdbedb9fd29721c91e59
Nick Busarhin <busarginkolia@gmail.com>
2023-01-04 15:44:36 +0200

    one more try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,16 @@
 'use strict';
 
 function convertToNewString(string) {
-  const newWord = `${string.slice(0, -4)}` + ' ' + 'Name';
-  const newName = newWord.toUpperCase();
+  const newWord = `${string.slice(0, -4)}` + ' ' + 'name';
+  const newName = newWord;
 
   return newName;
+}
+
+function capitalize(string) {
+  const letters = string.split('');
+
+  return letters[0].toUpperCase() + letters.slice(1).join('');
 }
 
 const inputs = document.querySelectorAll('input');
@@ -17,12 +23,17 @@ for (const input of listOfInputs) {
   label.textContent = input.name;
   label.setAttribute('for', input.id);
 
-  input.setAttribute('placeholder', input.name.toUpperCase());
+  input.setAttribute('placeholder', capitalize(input.name));
 
   if (input.name.includes('Name')) {
     label.textContent = convertToNewString(input.name);
-    input.setAttribute('placeholder', convertToNewString(input.name));
+
+    const capWord = capitalize(input.name);
+
+    input.setAttribute('placeholder', convertToNewString(capWord));
   }
 
   input.parentElement.append(label);
 }
+
+capitalize('surname');
