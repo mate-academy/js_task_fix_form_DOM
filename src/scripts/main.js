3c0f0a8cd2dec5567e2817ff458777dd52e63856
Nick Busarhin <busarginkolia@gmail.com>
2023-01-04 12:04:38 +0200

    formFix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,9 +14,7 @@ for (const input of listOfInputs) {
   const label = document.createElement('label');
 
   label.className = 'field-label';
-
   label.textContent = input.name;
-
   label.setAttribute('for', input.id);
 
   input.setAttribute('placeholder', input.name.toUpperCase());
