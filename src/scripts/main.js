b494741f4b3b96a4b36e4fb0206463e661df16ff
Avichez <starkavichi@gmail.com>
2023-01-07 18:10:49 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const allInputs = document.querySelectorAll('input');
+
+[...allInputs].forEach(input => {
+  const newLabel = document.createElement('label');
+  const rightName = divideWords(input.name);
+
+  newLabel.className = 'field-label';
+  newLabel.htmlFor = input.id;
+  newLabel.textContent = rightName;
+
+  input.placeholder = rightName[0].toUpperCase() + rightName.slice(1);
+  input.parentElement.append(newLabel);
+});
+
+function divideWords(words) {
+  return words.split('').reduce((result, letter, index) => {
+    if (index && letter === letter.toUpperCase()) {
+      return result + ` ${letter}`;
+    }
+
+    return result + letter;
+  }, '');
+}
