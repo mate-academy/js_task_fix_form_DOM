de84b110fc88e17eecfc8b439b6f7a49ec7eed8d
@nataliia.vyrsta <natali.vyrsta@gmail.com>
2023-01-08 23:50:34 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+// variables
+const inputs = document.querySelectorAll('.field-text');
+
+//  function for formating camelCase
+function fixCamelCase(str) {
+  const formatedStr = str.replace(/(\B[A-Z])/g, ' $1').toLowerCase();
+
+  return formatedStr[0].toUpperCase() + formatedStr.slice(1);
+}
+
+//  add placeholders and labels for inputs
+[...inputs].forEach(
+  (input) => {
+    const inputName = fixCamelCase(input.name);
+    const label = document.createElement('label');
+
+    input.placeholder = inputName;
+    label.className = 'field-label';
+    label.textContent = inputName;
+    label.htmlFor = `${input.id}`;
+    input.parentElement.prepend(label);
+  }
+);
