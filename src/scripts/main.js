ab09b1257f5bdc2690d5c58bba46a4de3ad1f28d
yana-kotsulym <yanak7170@gmail.com>
2023-01-08 22:00:47 +0100

    Fixes after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,8 +7,9 @@ for (const input of inputs) {
 
   label.className = 'field-label';
   label.htmlFor = input.id;
-  label.textContent = `${input.name}`;
+  label.textContent = `${input.name.replace(/([A-Z])/g, ' $1').trim()}`;
   input.parentElement.append(label);
 
-  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+  input.placeholder = input.name[0].toUpperCase()
+    + input.name.slice(1).replace(/([A-Z])/g, ' $1').trim();
 }
