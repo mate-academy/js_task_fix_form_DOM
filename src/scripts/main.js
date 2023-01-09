11ea9018d10370a17cbadbdabdb76a432677c682
Taras Baran <baranprivat22@gmail.com>
2023-01-10 00:42:43 +0200

    finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+inputs.forEach(input => {
+  const inputName = input.getAttribute('name').toUpperCase();
+  const inputId = input.getAttribute('id');
+  const label = `
+    <label for='${inputId}' class='field-label'>
+      ${inputName}
+    </label>
+  `;
+
+  input.insertAdjacentHTML('beforebegin', label);
+
+  input.setAttribute('placeholder',
+    inputName[0] + inputName.slice(1).toLowerCase());
+});
