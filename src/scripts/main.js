5dee04d9f03263e86a7f229cbd6e4992bb3071a9
Nick_Radchuk <mukola.radchuk@gmail.com>
2023-01-10 15:43:55 +0200

    changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,38 @@
 'use strict';
 
 const inputs = document.querySelectorAll('input');
-// console.log(inputs);
 
-[...inputs].forEach(input => input
-  .setAttribute('placeholder', input.name[0]
-    .toUpperCase() + input.name.substring(1).toLowerCase()));
+const divideWords = string => {
+  const words = [];
+  let index = 0;
 
-[...inputs].forEach(label => label.insertAdjacentHTML(
-  'beforebegin', `
-  <label for="${label.id}" class="field-label">
-    ${label.name}
-  </label>`
-));
+  for (let i = 0; i < string.length; i++) {
+    const char = string[i];
+
+    if (char === char.toUpperCase()) {
+      const word = string.slice(index, i);
+
+      words.push(word);
+      index = i;
+    }
+  }
+
+  words.push(string.slice(index));
+
+  return words.join(' ');
+};
+
+[...inputs].forEach(input => {
+  input.setAttribute('placeholder', divideWords(input.name)[0]
+    .toUpperCase() + divideWords(input.name).slice(1));
+});
+
+[...inputs].forEach(label => {
+  label.insertAdjacentHTML(
+    'beforebegin', `
+    <label for="${label.id}" class="field-label">
+      ${divideWords(label.name)[0]
+    .toUpperCase() + divideWords(label.name).slice(1)}
+    </label>`
+  );
+});
