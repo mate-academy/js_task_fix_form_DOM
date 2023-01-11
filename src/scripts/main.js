421f89db6ba561662774b09f644bffc3bb05f009
Vitalii Skomskiy <skomskiy@ukr.net>
2023-01-11 19:52:27 +0200

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field');
+const input = document.querySelectorAll('input');
+const idArray = [];
+const nameLabels = [
+  'First name', 'Email', 'Last name', 'Password', 'Email', 'Password',
+];
+
+for (const item of input) {
+  idArray.push(item.id);
+};
+
+for (let i = 0; i < inputs.length; i++) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', idArray[i]);
+  label.textContent = nameLabels[i];
+
+  input[i].placeholder = nameLabels[i];
+  inputs[i].append(label);
+};
