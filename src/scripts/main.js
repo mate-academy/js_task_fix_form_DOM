27a8244744d332249fe6bdd3f529cfd27c1ed5d5
Yaroslav Radchuk <105112047+Yaroslav-Radchuk@users.noreply.github.com>
2023-01-11 10:35:12 +0200

    new sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,12 +2,26 @@
 
 const inputs = document.querySelectorAll('input');
 
-[...inputs].forEach(input => input
-  .setAttribute('placeholder', input.name[0]
-    .toUpperCase() + input.name.substring(1).toLowerCase()));
+function splitWords(word) {
+  let result = '';
 
-[...inputs].forEach(input => input.insertAdjacentHTML('beforebegin', `
-  <label for="${input.id}" class="field-label">
-    ${input.name}
-  </label>
-`));
+  for (let i = 0; i < word.length; i++) {
+    if (word[i].toUpperCase() === word[i]) {
+      result += ' ';
+    }
+
+    result += word[i];
+  }
+
+  return result;
+}
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.htmlFor = input.id;
+  label.textContent = splitWords(input.name).toLowerCase();
+  input.placeholder = splitWords(input.name).toUpperCase();
+  input.parentElement.append(label);
+}
