345b9d0a59ec41ff1ff6ddbca7d78e0668ece47f
Kyrychenko Illia <i.kirichenko160804@gmail.com>
2023-01-11 23:32:33 +0200

    splitted words

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,14 +5,34 @@ const inputs = document.querySelectorAll('input');
 
 inputs.forEach((input) => {
   const label = document.createElement('label');
-  const capitalizedInputName
-    = input.name[0].toUpperCase() + input.name.slice(1).toLowerCase();
   const inputParentContainer = input.parentElement;
 
   label.classList.add('field-label');
   label.setAttribute('for', input.id);
-  label.textContent = input.name.toUpperCase();
-  input.placeholder = capitalizedInputName;
+  label.textContent = splitWords(input.name);
+  input.placeholder = splitWords(input.name);
 
   inputParentContainer.prepend(label);
 });
+
+function splitWords(word) {
+  let resultWord = formatWord(word);
+
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === word[i].toUpperCase()) {
+      const firstWord = word.slice(0, i).toLowerCase();
+      const secondWord = word.slice(i, word.length).toLowerCase();
+
+      const firstFormattedWord = formatWord(firstWord);
+      const secondFormattedWord = formatWord(secondWord);
+
+      resultWord = `${firstFormattedWord} ${secondFormattedWord}`;
+    }
+  }
+
+  return resultWord;
+}
+
+function formatWord(word) {
+  return word[0].toUpperCase() + word.slice(1).toLowerCase();
+}
