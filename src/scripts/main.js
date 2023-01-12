808fe99280a966eb088b1b380f5151e51669bb5d
Kate <ekaterina2002aleks@gmail.com>
2023-01-12 17:49:04 +0200

    fix form dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const listNodeInput = document.querySelectorAll('input');
+const inputs = [...listNodeInput];
+const listNodeField = document.querySelectorAll('.field');
+const fields = [...listNodeField];
+let count = 0;
+
+for (const input of inputs) {
+  const newLabel = document.createElement('label');
+  const atribClass = document.createAttribute('class');
+  const atribFor = document.createAttribute('for');
+
+  atribClass.value = 'field-label';
+  atribFor.value = input.getAttribute('id');
+
+  newLabel.setAttributeNode(atribClass);
+  newLabel.setAttributeNode(atribFor);
+  newLabel.textContent = input.getAttribute('name').toUpperCase();
+  fields[count].append(newLabel);
+  count++;
+
+  const atribPlaceholder = document.createAttribute('placeholder');
+
+  atribPlaceholder.value = input.getAttribute('name');
+  input.setAttributeNode(atribPlaceholder);
+}
