4e314c1fe14d4016a3da250bb9b895b9921589af
Taras Baran <baranprivat22@gmail.com>
2023-01-14 00:34:36 +0200

    changed placeholders and label's name

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,26 @@
 'use strict';
 
+function spaceBetweenWord(word) {
+  return word
+    .split('')
+    .map(letter => letter === letter.toUpperCase()
+      ? ' ' + letter
+      : letter)
+    .join('');
+}
+
 const inputs = document.querySelectorAll('.field-text');
 
 inputs.forEach(input => {
-  const inputName = input.getAttribute('name').toUpperCase();
-  const inputId = input.getAttribute('id');
+  const { id, name: inputName } = input;
   const label = `
-    <label for='${inputId}' class='field-label'>
-      ${inputName}
+    <label for='${id}' class='field-label'>
+      ${spaceBetweenWord(inputName)}
     </label>
   `;
+  const placeholder = inputName[0].toUpperCase()
+    + spaceBetweenWord(inputName).slice(1).toLowerCase();
 
   input.insertAdjacentHTML('beforebegin', label);
-
-  input.setAttribute('placeholder',
-    inputName[0] + inputName.slice(1).toLowerCase());
+  input.setAttribute('placeholder', placeholder);
 });
