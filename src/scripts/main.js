f135da28c7982ba85288d569b70c0a820bac9351
Yulia Borshovska <yulya.borshovska@gmail.com>
2023-01-12 21:00:55 -0800

    solution for a fix form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const labels = document.createElement('label');
+
+  labels.className = 'field-label';
+  labels.htmlFor = input.getAttribute('id');
+  labels.textContent = input.name;
+
+  input.parentElement.append(labels);
+
+  input.setAttribute(
+    'placeholder', `${input.name[0].toUpperCase()}${input.name}`
+  );
+}
