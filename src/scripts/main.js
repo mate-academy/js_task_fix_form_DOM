d60ab1f2cfa0e9dd9df375a9449424caaa0deacb
Kate <ekaterina2002aleks@gmail.com>
2023-01-13 16:15:07 +0200

    fix styling words

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,18 +10,31 @@ for (const input of inputs) {
   const newLabel = document.createElement('label');
   const atribClass = document.createAttribute('class');
   const atribFor = document.createAttribute('for');
+  let inputName = input.getAttribute('name');
+  let inputNameStyle = '';
 
   atribClass.value = 'field-label';
   atribFor.value = input.getAttribute('id');
 
   newLabel.setAttributeNode(atribClass);
   newLabel.setAttributeNode(atribFor);
-  newLabel.textContent = input.getAttribute('name').toUpperCase();
+
+  if (inputName === 'firstName') {
+    inputName = 'First Name';
+  } else if (inputName === 'lastName') {
+    inputName = 'Last Name';
+  }
+
+  newLabel.textContent = inputName.toUpperCase();
+
   fields[count].append(newLabel);
   count++;
 
   const atribPlaceholder = document.createAttribute('placeholder');
 
-  atribPlaceholder.value = input.getAttribute('name');
+  inputNameStyle = inputName.slice(0, 1).toUpperCase();
+  inputNameStyle += inputName.slice(1, inputName.length);
+
+  atribPlaceholder.value = inputNameStyle;
   input.setAttributeNode(atribPlaceholder);
 }
