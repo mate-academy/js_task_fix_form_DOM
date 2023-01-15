396ffcd39a5ecdc80780f4c00710cd3f6a9f950e
unknown <zhodko32@gmail.com>
2023-01-15 20:55:59 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const forms = [...document.querySelectorAll('form')];
+
+for (const form of forms) {
+  const formInputs = [...form.querySelectorAll('input')];
+
+  for (const formInput of formInputs) {
+    const formLabel = document.createElement('label');
+
+    formLabel.classList.add('field-label');
+    formLabel.setAttribute('for', formInput.getAttribute('id'));
+    formLabel.textContent = formInput.getAttribute('name');
+
+    formInput.before(formLabel);
+
+    formInput.placeholder = formInput.getAttribute('name')[0].toUpperCase()
+      + formInput.getAttribute('name').slice(1).toLowerCase();
+  }
+}
