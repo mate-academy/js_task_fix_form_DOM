2b8e3350919f023a093e829462e872d82820fb28
Yuliia Andrieieva <andrieieva.y@gmail.com>
2023-01-15 20:07:20 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const inputs = document.body.querySelectorAll('input');
+
+const createLabel = function(input) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.for = input.id;
+  label.textContent = cpitalizeIt(input.name);
+
+  input.parentElement.append(label);
+  input.placeholder = cpitalizeIt(input.name);
+};
+
+function cpitalizeIt(text) {
+  let result = text.slice(0, 1).toUpperCase();
+
+  for (let i = 1; i < text.length; i++) {
+    if (text[i] === text[i].toUpperCase()) {
+      result += ' ' + text[i];
+    } else {
+      result += text[i];
+    }
+  }
+
+  return result;
+}
+
+[...inputs].forEach(createLabel);
