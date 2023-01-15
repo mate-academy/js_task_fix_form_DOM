796574f7aee7ab985e5f9e6407d80b6373ff870c
Anastasia Radchenko <radchenckooo@gmail.com>
2023-01-15 12:42:08 +0200

    Label & Placeholder

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+for (const input of inputs) {
+  const inputName = input.getAttribute('name');
+
+  input.insertAdjacentHTML('afterend', `
+    <label class="field-label">
+      ${inputName}
+    </label>
+  `);
+
+  input.placeholder = inputName;
+}
