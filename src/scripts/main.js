a90d4d6b1c49de82307eee59d2a106d1345c2a1f
Taras Baran <baranprivat22@gmail.com>
2023-01-16 12:28:59 +0200

    move spaceBetweenWord(inputName) in const

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,13 +13,14 @@ const inputs = document.querySelectorAll('.field-text');
 
 inputs.forEach(input => {
   const { id, name: inputName } = input;
+  const spaceBetweenName = spaceBetweenWord(inputName);
   const label = `
     <label for='${id}' class='field-label'>
-      ${spaceBetweenWord(inputName)}
+      ${spaceBetweenName}
     </label>
   `;
   const placeholder = inputName[0].toUpperCase()
-    + spaceBetweenWord(inputName).slice(1).toLowerCase();
+    + spaceBetweenName.slice(1).toLowerCase();
 
   input.insertAdjacentHTML('beforebegin', label);
   input.setAttribute('placeholder', placeholder);
