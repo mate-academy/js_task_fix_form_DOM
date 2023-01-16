e4a7f8bdbb0c34de421bb2d3e826a0cc8a946660
Mykhailo Orlov <mihalikmihalik85@gmail.com>
2023-01-16 21:08:27 +0200

    tests good

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+
+  input.after(label);
+}
