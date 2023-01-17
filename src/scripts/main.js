44190966cc95915aaea2189e3ac3cbde078040d9
Mykhailo Orlov <mihalikmihalik85@gmail.com>
2023-01-17 17:50:47 +0200

    correct mentor

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,14 @@
 const inputs = document.querySelectorAll('input');
 
 for (const input of inputs) {
-  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+  input.placeholder = input.name[0].toUpperCase()
+  + input.name.slice(1).replace(/([A-Z])/g, ' $1');
 
   const label = document.createElement('label');
 
   label.className = 'field-label';
   label.setAttribute('for', input.id);
-  label.textContent = input.name;
+  label.textContent = input.name.replace(/([A-Z])/g, ' $1');
 
   input.after(label);
 }
