02a44b030a243ced5a3de64f19e8880c002b74fb
Anastasia Radchenko <radchenckooo@gmail.com>
2023-01-19 10:49:20 +0200

    Label & Placeholder

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,12 +4,14 @@ const inputs = [...document.querySelectorAll('input')];
 
 for (const input of inputs) {
   const inputName = input.getAttribute('name');
+  const splitInput = inputName.split('N');
+  const joinedInput = splitInput.join(' N');
 
   input.insertAdjacentHTML('afterend', `
     <label class="field-label">
-      ${inputName}
+      ${joinedInput}
     </label>
   `);
 
-  input.placeholder = inputName;
+  input.placeholder = (joinedInput[0].toUpperCase() + joinedInput.slice(1));
 }
