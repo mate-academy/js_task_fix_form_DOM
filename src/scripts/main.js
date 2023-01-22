88d83631e4109c37b73571a27cb07ba42d229004
Ivan Krynychnyi <sinus1313@gmail.com>
2023-01-22 19:59:51 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const inputs = [...document.querySelectorAll('input')];
+
+for (const input of inputs) {
+  const inputElem = document.createElement('label');
+
+  inputElem.setAttribute('class', 'field-label');
+  inputElem.setAttribute('for', input.getAttribute('id'));
+  inputElem.textContent = input.getAttribute('name');
+
+  input.setAttribute('placeholder',
+    `${capitalizeFirstLetter(inputElem.textContent.toLowerCase())}`);
+
+  input.parentNode.append(inputElem);
+}
