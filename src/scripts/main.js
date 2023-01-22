c15bd3efd4e6fbfe0ce39b29f709f7d8937e27da
haduigon <haduigon@gmail.com>
2023-01-22 23:07:16 +0200

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,54 +2,49 @@
 
 // write code here
 
-let inputArray = document.querySelectorAll('div.field')
+const docArray = document.querySelectorAll('div.field');
 
-let copy = [...inputArray]
+const copy = [...docArray];
 
-copy.map(a => makeFormGood(a))
+copy.map(a => makeFormGood(a));
 
 function makeFormGood(inputArray) {
+  const label = document.createElement('label');
 
-  let label = document.createElement('label')
+  inputArray.append(label);
 
-  inputArray.append(label)
+  const content
+  = capitilazeFirstLetter(splitPhraseByCapitalLetter(
+    inputArray.children[0].name));
 
-  let content = capitilazeFirstLetter(splitPhraseByCapitalLetter(inputArray.children[0].name));
-
-  label.className = 'field-label'
+  label.className = 'field-label';
 
   label.textContent = content;
 
   inputArray.children[0].placeholder = content;
 }
 
-
 function splitPhraseByCapitalLetter(phrase) {
-
   let total = phrase;
   let text2 = '';
   let text3 = '';
 
   for (let i = 0; i < phrase.length; i++) {
-
     if (phrase[i] === phrase[i].toUpperCase()) {
-      text2 = phrase.slice(i)
-      text3 = phrase.slice(0, i)
+      text2 = phrase.slice(i);
+      text3 = phrase.slice(0, i);
       total = text3 + ' ' + text2;
     }
   }
 
-    return total;
-
+  return total;
 }
-
 
 function capitilazeFirstLetter(word) {
-  let temporary = word.slice(0,1);
-  let temporary2 = temporary.toUpperCase();
-  let temp3 = word;
-  let temp4 = temp3.replace(temp3[0], temporary2)
+  const temporary = word.slice(0, 1);
+  const temporary2 = temporary.toUpperCase();
+  const temp3 = word;
+  const temp4 = temp3.replace(temp3[0], temporary2);
 
-  return temp4
+  return temp4;
 }
-
