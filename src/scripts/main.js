79c2ea43f43494ee2dac339bb726c1a25e8c050e
Roman Kvas <naruto.yzymaki4@gmail.com>
2023-01-22 15:19:44 +0100

    Well done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,14 +7,15 @@ for (const el of arrOfTag) {
   const label = document.createElement('label');
 
   label.classList.add('field-label');
-  label.textContent = el.getAttribute('name');
+  label.textContent = firstLetter(el.getAttribute('name'));
 
   el.setAttribute('placeholder', firstLetter(label.textContent));
   el.after(label);
 }
 
 function firstLetter(letter) {
-  const resLetter = letter.charAt(0).toUpperCase() + letter.slice(1);
+  const resLetter = letter.charAt(0).toUpperCase()
+  + letter.slice(1).replace(/([A-Z])/g, word => ` ${word}`);
 
   return resLetter;
 }
