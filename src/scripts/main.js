4f7521b2a7e1ab3593fc5b49751fa3b110b59ed8
ihorivna007 <emmakorovnik@gmail.com>
2023-01-25 17:19:48 +0200

    initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  input.insertAdjacentHTML('beforebegin',
+    `<label class="field-label" for="${input.id}">
+      ${input.name}
+    </label>`
+  );
+
+  input.setAttribute(
+    'placeholder',
+    input.name.substring(0, 1).toUpperCase() + input.name.substring(1)
+  );
+
+  input.parentNode.insertAdjacentHTML('beforebegin', `<label></label>`);
+}
