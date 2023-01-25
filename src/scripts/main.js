d6f4c5a0bbaf295f6b80c470906bb0196de372c8
Ukrainian_panda <94929618+Ukrainiane-panda@users.noreply.github.com>
2023-01-25 22:13:50 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.htmlFor = input.id;
+  label.className = 'field-label';
+
+  switch (input.name) {
+    case 'firstName':
+      input.placeholder = 'First name';
+      break;
+
+    case 'lastName':
+      input.placeholder = 'Last name';
+      break;
+
+    case 'email':
+      input.placeholder = 'Email';
+      break;
+
+    case 'password':
+      input.placeholder = 'Password';
+      break;
+  }
+
+  label.textContent = input.placeholder;
+
+  input.parentElement.append(label);
+}
