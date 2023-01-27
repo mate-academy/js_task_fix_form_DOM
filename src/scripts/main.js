392c00187e14fa593134ae6c4cc4ddf3e2de40d9
Vadym Tsyndra <Mr.Tsyndra@gmail.com>
2023-01-27 16:01:42 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,9 +11,24 @@ for (const input of inputs) {
 
   label.textContent = input.getAttribute('name');
 
-  input.setAttribute('placeholder',
-    input.getAttribute('name')[0].toUpperCase()
-    + input.getAttribute('name').slice(1));
+  let placeholder = input.getAttribute('name');
+
+  const indexLargeLetter = Array.from(placeholder).findIndex(
+    letter => letter === letter.toUpperCase());
+
+  if (indexLargeLetter !== -1) {
+    const partOfPlaceholder = placeholder.slice(0, indexLargeLetter);
+    const partOfPlaceholder2 = placeholder.slice(indexLargeLetter);
+
+    placeholder = `${partOfPlaceholder[0].toUpperCase()
+    + partOfPlaceholder.slice(1)}` + ' '
+    + `${partOfPlaceholder2[0].toUpperCase() + partOfPlaceholder2.slice(1)}`;
+  } else {
+    placeholder = input.getAttribute('name')[0].toUpperCase()
+    + input.getAttribute('name').slice(1);
+  }
+
+  input.setAttribute('placeholder', placeholder);
 
   const parentOfInput = input.parentElement;
 
