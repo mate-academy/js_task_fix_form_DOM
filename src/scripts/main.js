f9c4b6afb31431d8f04f9b0c4901b9f72f650c88
Ruslan Yarosh <ruslan.yarosh.96@gmail.com>
2023-02-02 01:15:52 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  input.parentElement.append(label);
+  label.textContent = input.name.toUpperCase();
+
+  label.setAttribute('for', input.id);
+  input.setAttribute('placeholder', getCapitalize(input.name));
+}
+
+function getCapitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
