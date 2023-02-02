80b20a8ca6f4fa8b5719b0521ba20e83e3ea5e20
Ruslan Yarosh <ruslan.yarosh.96@gmail.com>
2023-02-02 14:19:46 +0200

    Fixes after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,12 +7,24 @@ for (const input of inputs) {
 
   label.classList.add('field-label');
   input.parentElement.append(label);
-  label.textContent = input.name.toUpperCase();
+  label.textContent = camelCaseSeparate(input.name);
 
   label.setAttribute('for', input.id);
-  input.setAttribute('placeholder', getCapitalize(input.name));
+  input.setAttribute('placeholder', getCapitalize(label.textContent));
 }
 
 function getCapitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+function camelCaseSeparate(str) {
+  let result = str;
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === str[i].toUpperCase()) {
+      result = str.slice(0, i) + ' ' + str.slice(i);
+    }
+  }
+
+  return result.toUpperCase();
 }
