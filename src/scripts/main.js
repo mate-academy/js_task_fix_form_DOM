a944bd6da22db9030760b49913ca9f027296f617
Max Zelenskyi <rio1391@gmail.com>
2023-02-04 15:08:30 +0100

    Rio Solutions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+function fixCamelCase(str) {
+  const formatedStr = str.replace(/(\B[A-Z])/g, ' $1').toLowerCase();
+
+  return formatedStr[0].toUpperCase() + formatedStr.slice(1);
+}
+
+[...inputs].forEach(
+  (input) => {
+    const inputName = fixCamelCase(input.name);
+    const label = document.createElement('label');
+
+    input.placeholder = inputName;
+    label.className = 'field-label';
+    label.textContent = inputName;
+    label.htmlFor = `${input.id}`;
+    input.parentElement.prepend(label);
+  }
+);
