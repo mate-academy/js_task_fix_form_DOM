5bbb7a4af47b996a4c6f469a9e466b462aa9c80d
Kate <novak5683@gmail.com>
2023-02-06 17:36:17 +0100

    fixed UpperCase

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,23 @@
 'use strict';
 
+function normalize(n) {
+  let normalizedName = n[0].toUpperCase() + n.slice(1);
+
+  if (normalizedName.toLowerCase().indexOf('name') > -1) {
+    normalizedName = normalizedName.slice(0, -4) + ' name';
+  }
+
+  return normalizedName;
+}
+
 const input = document.getElementsByTagName('input');
 
 for (let i = 0; i < input.length; i++) {
   const elem = document.createElement('label');
 
   input[i].parentElement.append(elem);
-  input[i].placeholder = input[i].getAttribute('name').toLocaleUpperCase();
-  elem.textContent = input[i].getAttribute('name');
+  input[i].placeholder = normalize(input[i].getAttribute('name'));
+  elem.textContent = normalize(input[i].getAttribute('name'));
   elem.setAttribute('for', input[i].getAttribute('id'));
   elem.setAttribute('class', 'field-label');
 }
