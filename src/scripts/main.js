ebd9462c1ecc900526121d8235f92f4a8a60cf67
Kate <kate@Kates-MacBook-Pro-16.local>
2023-02-06 14:40:59 +0100

    result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const input = document.getElementsByTagName('input');
+
+for (let i = 0; i < input.length; i++) {
+  const elem = document.createElement('label');
+
+  input[i].parentElement.append(elem);
+  input[i].placeholder = input[i].getAttribute('name').toLocaleUpperCase();
+  elem.textContent = input[i].getAttribute('name');
+  elem.setAttribute('for', input[i].getAttribute('id'));
+  elem.setAttribute('class', 'field-label');
+}
