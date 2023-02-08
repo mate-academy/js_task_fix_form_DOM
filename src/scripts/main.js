f9ac491d59c1e411a02acbf10a35358abc7de3a2
kirill-vypirovskyi <kirill.vypirovsky@gmail.com>
2023-02-08 14:15:42 +0200

    form fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = input.getAttribute('name');
+
+  input.before(label);
+
+  const capName = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  input.placeholder = capName;
+}
