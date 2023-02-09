c79356e87f5d97e0c68e2e9995c98f6127d894e7
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-02-09 15:44:21 +0200

    task was solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const fields = [...document.querySelectorAll('.field')];
+
+const separateCamelCase = (text, caseType) => {
+  const separatedWords = text.split(/(?=[A-Z])/).join(' ');
+
+  switch (caseType) {
+    case 'uppercase':
+      return separatedWords.toUpperCase();
+    case 'capitalize':
+      const first = separatedWords.toLowerCase().slice(0, 1).toUpperCase();
+      const rest = separatedWords.toLowerCase().slice(1);
+
+      return first + rest;
+  }
+};
+
+for (let i = 0; i < fields.length; i++) {
+  const field = fields[i];
+  const label = document.createElement('label');
+  let input = field.children[0];
+  const textOfInput = input.getAttribute('name');
+  const idOfInput = input.getAttribute('id');
+
+  field.style.paddingTop = '20px';
+  label.setAttribute('for', idOfInput);
+  label.innerHTML = separateCamelCase(textOfInput, 'uppercase');
+  input.before(label);
+
+  input = fields[i].children[1];
+
+  input.setAttribute('placeholder',
+    separateCamelCase(textOfInput, 'capitalize'));
+}
