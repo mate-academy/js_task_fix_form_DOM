856b8be746ec00592f28b40cdc9e836df2c201cf
b.gavrilyk3 <bgavrilyk3@gmail.com>
2023-02-10 16:06:57 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
 // write code here
+const inputs = [...document.querySelectorAll('input')];
+
+for (const value of inputs) {
+  const label = `
+    <label class="field-label" for="${value.id}">
+      ${camelToString(value.name)}
+    </label>
+  `;
+
+  value.setAttribute('placeholder', camelToString(value.name));
+  value.insertAdjacentHTML('beforebegin', label);
+}
+
+function camelToString(str) {
+  let newStr = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] !== str[i].toLowerCase()) {
+      newStr += ' ';
+    }
+    newStr += str[i];
+  }
+
+  return newStr;
+}
