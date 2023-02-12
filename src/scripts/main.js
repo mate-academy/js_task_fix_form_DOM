01dbb26e9fa6d0d18ca36a799289dac5bf4e05c9
Petro Lozynskyi <petrolozynskyi01@gmail.com>
2023-02-12 15:29:49 +0200

    Task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const form = document.querySelector('form');
+const inputs = form.querySelectorAll('input');
+
+function capitalize(someText) {
+  someText[0].toUpperCase();
+
+  return someText;
+}
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.className = 'field-class';
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+  input.placeholder = capitalize(input.name);
+  input.parentElement.appendChild(label);
+}
