20aeb25e8686fc52673009d686230cfaed8353a0
Petro Lozynskyi <petrolozynskyi01@gmail.com>
2023-02-12 15:43:01 +0200

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const form = document.querySelector('form');
+const form = document.querySelectorAll('form');
 const inputs = form.querySelectorAll('input');
 
 function capitalize(someText) {
@@ -13,7 +13,9 @@ for (const input of inputs) {
   const label = document.createElement('label');
 
   label.className = 'field-class';
+
   label.setAttribute('for', input.id);
+
   label.textContent = input.name;
   input.placeholder = capitalize(input.name);
   input.parentElement.appendChild(label);
