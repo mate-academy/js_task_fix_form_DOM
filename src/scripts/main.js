894b26658120bb08a432d7ab098066a2a1118ee2
Nigga <yomzfk@gmail.com>
2023-02-12 13:40:56 +0200

    Fix Form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
+// debugger;
 
-// write code here
+const collection = document.querySelectorAll('input');
+
+collection.forEach(input => {
+  const names = input.name.split(/(?=[A-Z])/).join(' ');
+
+  input.insertAdjacentHTML('beforebegin', `
+    <label for="${input.id}" class="field-label">
+      ${names}
+    </label>
+  `);
+
+  input.placeholder = `${names[0].toUpperCase() + names.slice(1)}`;
+});
