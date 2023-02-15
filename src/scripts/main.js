162869f5d00c94e5c0cf2603005453801673137e
mykolachupyak <chupyak@gmail.com>
2023-02-15 15:11:23 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const words = ['Name', 'Last name', 'Email', 'Password', 'Email', 'Password'];
+
+const inputRows = document.querySelectorAll('input');
+
+for (let i = 0; i < inputRows.length; i++) {
+  inputRows[i].placeholder = words[i];
+
+  const elementInDiv = document.createElement('label');
+
+  elementInDiv.className = 'field-label';
+  elementInDiv.htmlFor = inputRows[i].id;
+  elementInDiv.textContent = words[i].toUpperCase();
+  inputRows[i].parentElement.append(elementInDiv);
+}
