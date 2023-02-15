c5b2b5c6cf5446f31fd9ed4d8d46c271bdbba136
Petro Lozynskyi <petrolozynskyi01@gmail.com>
2023-02-15 22:01:27 +0200

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,8 +11,9 @@ function capitalize(someText) {
 for (const input of inputs) {
   const label = document.createElement('label');
 
-  label.className = 'field-class';
+  label.className = 'field-label';
   label.setAttribute('for', input.id);
+  label.innerText = input.name;
   label.textContent = input.name;
   input.placeholder = capitalize(input.name);
   input.parentElement.prepend(label);
