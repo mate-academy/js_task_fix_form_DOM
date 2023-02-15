42b0a57a2e86b1659415df7a962b314b95d9f60a
mykolachupyak <chupyak@gmail.com>
2023-02-15 16:24:16 +0200

    improvement

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,22 @@
 'use strict';
 
-const words = ['Name', 'Last name', 'Email', 'Password', 'Email', 'Password'];
-let countWords = 0;
-
 const inputRows = document.querySelectorAll('input');
 
+function separateAndCapitalize(str) {
+  return str.split(/(?=[A-Z])/).join(' ').replace(/(^|\s)\S/g,
+    function(a) {
+      return a.toUpperCase();
+    }
+  );
+}
+
 for (const index of inputRows) {
-  index.placeholder = words[countWords];
+  index.placeholder = separateAndCapitalize(index.name);
 
   const elementInDiv = document.createElement('label');
 
   elementInDiv.className = 'field-label';
   elementInDiv.htmlFor = index.id;
-  elementInDiv.textContent = words[countWords].toUpperCase();
+  elementInDiv.textContent = (separateAndCapitalize(index.name).toUpperCase());
   index.parentElement.append(elementInDiv);
-  countWords++;
 }
