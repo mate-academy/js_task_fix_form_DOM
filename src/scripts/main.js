dd248f5bc4696f72f47f81c5c2fe4d1900e64939
Yevhenii Hura <yevheniihura@gmail.com>
2023-02-16 14:51:51 +0200

    the task was solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+for (let i = 0; i < inputs.length; i++) {
+  const label = document.createElement('label');
+
+  label.setAttribute('class', 'field-label');
+  label.setAttribute('for', inputs[i].id);
+
+  label.textContent = textFormat(inputs[i].name);
+
+  inputs[i].parentElement.prepend(label);
+  inputs[i].setAttribute('placeholder', textFormat(inputs[i].name));
+}
+
+function textFormat(string) {
+  let result = string[0].toUpperCase();
+
+  for (let i = 1; i < string.length; i++) {
+    if (string[i] === string[i].toUpperCase()) {
+      result += ` ${string[i]}`;
+    } else {
+      result += string[i];
+    }
+  }
+
+  return result;
+};
