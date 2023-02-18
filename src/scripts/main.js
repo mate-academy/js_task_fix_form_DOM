fba5e4c8fa13b26e4816b998b0acb1d9b064c292
Dmytro Hubych <dh.hubych@gmail.com>
2023-02-18 10:18:15 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('.field')];
+
+inputs.forEach(inputWraper => {
+  const input = inputWraper.children[0];
+
+  input.setAttribute('placeholder', input.name);
+
+  inputWraper.insertAdjacentHTML('afterbegin',
+    `<label for="${input.id}" class=field-label>
+      ${input.name}
+    </label>`
+  );
+});
