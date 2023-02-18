3e1200a11cd6ab18986de40cd7cabd446fcc8268
Dmytro Hubych <dh.hubych@gmail.com>
2023-02-18 10:29:57 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,9 @@ const inputs = [...document.querySelectorAll('.field')];
 
 inputs.forEach(inputWraper => {
   const input = inputWraper.children[0];
+  const placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
 
-  input.setAttribute('placeholder', input.name);
+  input.setAttribute('placeholder', placeholder);
 
   inputWraper.insertAdjacentHTML('afterbegin',
     `<label for="${input.id}" class=field-label>
