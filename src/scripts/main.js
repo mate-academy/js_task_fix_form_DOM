d914c4cb29ac36e0965337524ef15afb673163c3
Stanislav Vdovychenko <vdovychenkostas@gmail.com>
2023-02-19 20:31:17 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const inputList = document.querySelectorAll('input');
+
+for (const input of inputList) {
+  const label = document.createElement('label');
+
+  input.setAttribute('placeholder', (input.name).split('N').join(' N'));
+  input.style = 'text-transform: capitalize';
+
+  label.className = `field-label`;
+  label.textContent = input.placeholder;
+  label.setAttribute('for', input.id);
+
+  input.before(label);
+}
