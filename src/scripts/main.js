a6de0ffcc74a3b5833841c3f55a226264b783bc5
Mykola Butylkov <kokos.ibo.58@gmail.com>
2023-02-24 10:15:47 +0200

    Solution-1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
 // write code here
+
+const inputs = document.querySelectorAll('input');
+
+const textPlaceholders = [
+  'First Name', 'Last Name', 'Email', 'Password', 'Email', 'Password',
+];
+
+for (let i = 0; i < inputs.length; i++) {
+  const label = document.createElement('label');
+  const divField = document.querySelectorAll('.field');
+  const textContentLabel = inputs[i].getAttribute('name');
+
+  label.textContent = textContentLabel;
+
+  label.setAttribute(
+    'for', textPlaceholders[i].toLocaleLowerCase().replaceAll(' ', '')
+  );
+  label.setAttribute('class', 'field-label');
+
+  divField[i].prepend(label);
+
+  inputs[i].setAttribute('placeholder', textPlaceholders[i]);
+
+  inputs[i].setAttribute(
+    'id', textPlaceholders[i].toLocaleLowerCase().replaceAll(' ', '')
+  );
+}
