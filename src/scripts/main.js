745e45fbd4f2163e2db0e7e90aec37594c34a748
Kateryna Stemblivska <ekaterina_09@ukr.net>
2023-02-27 18:05:43 +0200

    Forms fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const listInput = [...document.querySelectorAll('input')];
+
+listInput.forEach(input => {
+  input.parentElement.insertAdjacentHTML('afterbegin', `
+    <label for="${input.id}" class="field-label">
+      ${input.name}
+    </label>
+  `);
+});
+
+listInput.forEach(input => {
+  const placeholderValue = `${input.name[0].toUpperCase()}`
+    + `${input.name.slice(1)}`;
+
+  input.setAttribute('placeholder', `${placeholderValue}`);
+});
