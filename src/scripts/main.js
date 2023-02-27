d38ba9116f0751ad393cd27dd5c3304db11bdea7
Kateryna Stemblivska <ekaterina_09@ukr.net>
2023-02-27 22:03:34 +0200

    Separate different words

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,16 +3,20 @@
 const listInput = [...document.querySelectorAll('input')];
 
 listInput.forEach(input => {
-  input.parentElement.insertAdjacentHTML('afterbegin', `
-    <label for="${input.id}" class="field-label">
-      ${input.name}
-    </label>
-  `);
+  const capLetter = input.name
+    .split('')
+    .find(ch => ch === ch.toUpperCase());
+
+  const placeholderValue = `${input.name[0].toUpperCase()}`
+    + `${input.name.slice(1).split(`${capLetter}`).join(` ${capLetter}`)}`;
+
+  input.setAttribute('placeholder', placeholderValue);
 });
 
 listInput.forEach(input => {
-  const placeholderValue = `${input.name[0].toUpperCase()}`
-    + `${input.name.slice(1)}`;
-
-  input.setAttribute('placeholder', `${placeholderValue}`);
+  input.parentElement.insertAdjacentHTML('afterbegin', `
+    <label for="${input.id}" class="field-label">
+      ${input.placeholder}
+    </label>
+  `);
 });
