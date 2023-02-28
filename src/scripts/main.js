4544a267641f605bda1bd1cc51ac26288e2a128b
Sergey Mikhalskiy <msa1991work@gmail.com>
2023-02-28 20:13:51 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const fields = document.querySelectorAll('.field');
+
+[...fields].map(field => {
+  const input = field.children[0];
+
+  const placeholder = input.name
+    .split('')
+    .map((el, i) => i === 0 ? el.toUpperCase() : el)
+    .join('');
+
+  input.placeholder = placeholder;
+
+  field.insertAdjacentHTML('beforeend', `
+    <label for=${input.id} class='field-label'>
+      ${input.name}
+    </label>
+  `);
+});
