8149e88f21af1f7dc5c1887c2c5672354a01e78f
Vadim Drobyazko <volandemortuwka@gmail.com>
2023-03-01 20:18:15 +0200

    JS Task Fix Form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const createInput = document.querySelectorAll('.field-text');
+
+for (const input of createInput) {
+  const createLabel = document.createElement('Label');
+  const placeholdeName = input.name[0].toUpperCase()
+  + input.name.slice(1).split('N').join(' N');
+
+  createLabel.className = 'field-label';
+  createLabel.setAttribute('for', input.id);
+  createLabel.textContent = input.placeholder;
+  input.setAttribute('placeholder', placeholdeName);
+  input.before(createLabel);
+}
