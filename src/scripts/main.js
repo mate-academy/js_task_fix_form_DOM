d6e88f0d6fe9d30e1f6238dc42916b2f3c69e06b
Sviatoslav Koziatynskyi <koziatynskyi2013@gmail.com>
2023-03-02 23:10:50 +0200

    Solution2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,15 @@
 const forms = document.querySelectorAll('.field');
 const inputs = document.querySelectorAll('input');
 
+function nameWithSpace(nameWithoutSpace) {
+  return nameWithoutSpace.replace('N', ' N');
+};
+
 for (let i = 0; i < forms.length; i++) {
   forms[i].insertAdjacentHTML('afterbegin', `
   <label class='field-label' for='${inputs[i].id}'>${inputs[i].name}</label>
   `);
 
-  inputs[i].placeholder = inputs[i]
-    .name[0].toLocaleUpperCase() + inputs[i].name.slice(1);
+  inputs[i].placeholder = nameWithSpace(inputs[i]
+    .name[0].toLocaleUpperCase() + inputs[i].name.slice(1));
 };
