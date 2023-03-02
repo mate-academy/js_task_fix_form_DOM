f6c2feaee68c194b9ddf8a888eb8c5edf8b93e41
Nazarii Piatnochka <nazar.piatnochka@gmail.com>
2023-03-02 16:50:28 +0200

    Task completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(function(input) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+  input.placeholder = input.name.toUpperCase();
+
+  input.after(label);
+});
