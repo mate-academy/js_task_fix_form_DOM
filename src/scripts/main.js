44b2889e65850c2344925e043274db9ac5e5a8b3
Vadim Drobyazko <volandemortuwka@gmail.com>
2023-03-04 17:01:19 +0200

    Fix v2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,10 +5,11 @@ const createInput = document.querySelectorAll('.field-text');
 for (const input of createInput) {
   const createLabel = document.createElement('Label');
 
-  createLabel.className = 'field-label';
-  createLabel.setAttribute('for', input.id);
-  createLabel.textContent = input.placeholder;
   input.style = 'text-transform: capitalize';
   input.setAttribute('placeholder', input.name.split('N').join(' N'));
   input.before(createLabel);
+
+  createLabel.className = 'field-label';
+  createLabel.setAttribute('for', input.id);
+  createLabel.textContent = input.placeholder;
 }
