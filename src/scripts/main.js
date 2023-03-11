a8e358fab69bc9ab441a253398c00aff8376b49e
MarynaTovtyn <marina6782@gmail.com>
2023-03-11 21:28:12 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+for (const input of [...inputs]) {
+  input.insertAdjacentHTML('afterend', `
+      <label
+        class="field-label"
+        for=${input.id}
+      >
+        ${changeString(input.name).toUpperCase()}
+      </label>
+    `);
+
+  input.placeholder = changeString(input.name);
+};
+
+function changeString(string) {
+  const sepatedString = string.split(/(?=[A-Z])/).join(' ');
+  const [fisrtLetter, ...rest] = sepatedString;
+
+  return `${fisrtLetter.toUpperCase()}${rest.join('')}`;
+};
