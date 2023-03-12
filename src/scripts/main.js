ab4bc1fe48e54f0a61b6d3d47502c0b70a37495e
Serhii Melnychenko <8melya8@gmail.com>
2023-03-12 16:39:35 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  input.insertAdjacentHTML('afterEnd', `
+      <label
+        class="field-label"
+        for=${input.id}
+      >
+        ${capitalize(input.name)}
+      </label>
+    `);
+
+  input.placeholder = capitalize(input.name);
+}
+
+function capitalize(named) {
+  return (named.charAt(0).toUpperCase()
+    + named.slice(1).replace(/([A-Z])/g, ' $1'));
+}
