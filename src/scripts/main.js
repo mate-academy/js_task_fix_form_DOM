e4088fd4efae5b70d841e1784a8a78abf593dda2
Nazar <nazar378378379379@gmail.com>
2023-03-13 14:55:19 +0200

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const divClassField = document.querySelectorAll('.field');
+
+divClassField.forEach(label => {
+  const labelTag = document.createElement('label');
+
+  labelTag.className = 'field-label';
+  label.prepend(labelTag);
+
+  const elementId = label.lastElementChild.id;
+
+  label.firstElementChild.setAttribute('for', elementId);
+
+  const elementName = label.lastElementChild.name;
+
+  label.firstElementChild.textContent = elementName;
+
+  const uppercasedElementName = toUppercase(elementName);
+
+  label.lastElementChild.setAttribute('placeholder', uppercasedElementName);
+});
+
+function toUppercase(elementName) {
+  const uppercased = elementName.charAt(0).toUpperCase() + elementName.slice(1);
+
+  return uppercased;
+}
