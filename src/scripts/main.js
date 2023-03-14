cf79e41911026f16ecb3895e02079cc92f324ca8
Vladyslav Kulybaba <106146104+VladGraund@users.noreply.github.com>
2023-03-14 13:23:44 +0100

    complete task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+// const allDiv = [...document.querySelectorAll('.field')];
+
+const allInput = [...document.querySelectorAll('input')];
+
+for (const element of allInput) {
+  const label = document.createElement('label');
+  const thisDiv = element.parentNode;
+
+  element.placeholder = element.name;
+  element.style.textTransform = `Capitalize`;
+  label.textContent = element.name;
+  label.htmlFor = element.id;
+  label.classList.add('field-label');
+
+  thisDiv.prepend(label);
+}
