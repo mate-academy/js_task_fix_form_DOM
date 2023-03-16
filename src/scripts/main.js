da19c4c7da9ba6af3b6f96461d78cbcba6204e79
Dmytro Zhuk <dave.beetle2311@gmail.com>
2023-03-16 17:40:29 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+  input.parentElement.append(label);
+
+  input.setAttribute('placeholder', (input.name).split('N').join(' N'));
+  input.style = 'text-transform: capitalize;';
+}
