e9c5be18d0ac77fd0e1afc8fc8cba27997a294c2
Oleksandr Nedobezhkin <nedobezhkin_ad@yahoo.com>
2023-03-16 15:28:37 +0200

    Fix form DOM v.0.1 - Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const allInputs = document.querySelectorAll('input');
+
+for (const input of allInputs) {
+  const label = document.createElement('label');
+  const reqTextFormat = input.name.toUpperCase().split('N').join(' N');
+
+  label.className = 'field-label';
+  label.textContent = reqTextFormat;
+  label.setAttribute('for', input.id);
+  input.setAttribute('placeholder', reqTextFormat);
+
+  input.before(label);
+}
