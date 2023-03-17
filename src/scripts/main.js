8c2b6f6fa068325cfc6246943527ef6a475c9695
Anastasiia Em <ema199625@gmail.com>
2023-03-17 16:33:35 +0200

    Added changes according to the comment

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,14 +3,18 @@
 const inputs = [...document.querySelectorAll('input')];
 
 for (const input of inputs) {
-  input.setAttribute('placeholder',
-    (input.name).split('N').join(' N').toUpperCase());
+  const inputName = input.name.split(/(?=[A-Z])/).join(' ');
+
+  const firstLetter = inputName.charAt(0).toUpperCase();
+  const remainingInput = inputName.slice(1);
+
+  input.setAttribute('placeholder', firstLetter + remainingInput);
 
   const label = document.createElement('label');
 
   label.className = 'field-label';
   label.setAttribute('for', input.id);
-  label.textContent = input.name.toUpperCase();
+  label.textContent = inputName.toUpperCase();
 
   input.before(label);
 }
