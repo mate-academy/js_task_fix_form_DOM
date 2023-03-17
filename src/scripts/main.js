22e39f8d0d502a237fc1cc1a7cca6329c421793d
stetsenko_viktoriia <stetsenko.viktoriia@gmail.com>
2023-03-17 20:48:45 +0200

    form fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  const fixedText = input.name[0].toUpperCase()
+    + input.name.slice(1).replace(/([A-Z])/g, ' $1');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.textContent = fixedText;
+  input.placeholder = fixedText;
+  input.parentElement.append(label);
+}
