c5a70e6308b8eae341da100516a8c44993227ea8
Denys <kolbasin1990@gmail.com>
2023-03-17 19:00:42 +0200

    js_task_fix_form_DOM_v_01

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const forms = document.querySelectorAll('form');
+const inputs = [...forms].map(form => form.querySelectorAll('input'));
+
+inputs.map(inputList => {
+  inputList.forEach(input => {
+    const label = document.createElement('label');
+
+    label.className = 'field-label';
+    label.setAttribute('for', input.id);
+    label.textContent = input.name;
+
+    input.setAttribute('placeholder',
+      input.name[0].toUpperCase()
+      + input.name.slice(1)
+    );
+
+    input.append(label);
+  });
+});
