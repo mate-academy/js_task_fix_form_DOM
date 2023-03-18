09832f48b9d53110e5884b0fa4208c2f73e65e90
Denys <kolbasin1990@gmail.com>
2023-03-18 15:06:00 +0200

    js_task_fix_form_DOM_v_06

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,22 +3,24 @@
 const inputs = document.querySelectorAll('input');
 
 inputs.forEach(input => {
-    const label = document.createElement('label');
+  const label = document.createElement('label');
+  const { name: inputName, id, parentElement, type } = input;
 
-    label.className = 'field-label';
-    label.setAttribute('for', input.id);
-    label.textContent = input.name;
+  label.className = 'field-label';
+  label.setAttribute('for', id);
+  label.textContent = name.replace(/([A-Z])/g, ' $1');
 
-    input.setAttribute('placeholder',
-      input.name[0].toUpperCase()
-      + input.name.slice(1)
-    );
+  input.setAttribute('placeholder',
+    inputName === 'firstName' ? 'First Name'
+      : inputName === 'lastName' ? 'Last Name'
+        : inputName[0].toUpperCase() + inputName.slice(1)
+  );
 
-    const inputParent = input.parentElement;
+  const inputParent = parentElement;
 
-    inputParent.insertBefore(label, input);
+  inputParent.insertBefore(label, input);
 
-    if (input.type === 'email') {
-      input.setAttribute('pattern', '[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$');
-    }
-  });
+  if (type === 'email') {
+    input.setAttribute('pattern', '[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$');
+  }
+});
