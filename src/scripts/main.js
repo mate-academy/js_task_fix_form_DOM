f1e02668d110154292830013a550cc992ca01e6e
IvanVaverchak <ivanvaverchak@2000gmail.com>
2023-03-18 12:46:43 +0200

    add split

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,26 @@
 'use strict';
 
+function splitter(str) {
+  let string = str;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toUpperCase()) {
+      string = str.slice(0, i) + ' ' + str.slice(i);
+    }
+  };
+
+  return string.toUpperCase();
+}
+
 const inputs = document.querySelectorAll('input');
 const arrOfInputs = [...inputs];
 
 for (const input of arrOfInputs) {
   input.insertAdjacentHTML('beforebegin', `
     <label for="${input.id}" class="field-label">
-    ${input.name}
+    ${splitter(input.name)}
     </label>
   `);
 
-  input.placeholder = input.name;
+  input.placeholder = splitter(input.name);
 }
