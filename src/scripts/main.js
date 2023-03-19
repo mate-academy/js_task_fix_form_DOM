4209f7acf019564b0587f699b225878bc55272ec
Sergey Lipikhin <football3@ukr.net>
2023-03-19 23:40:56 +0200

    Finish solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+for (const inputElement of document.querySelectorAll('input')) {
+  const labelElement = document.createElement('label');
+
+  labelElement.htmlFor = inputElement.id;
+  labelElement.className = 'field-label';
+
+  let inputNameUpperCase = inputElement.name.toUpperCase();
+
+  inputNameUpperCase = inputNameUpperCase.replace('NAME', ' NAME');
+
+  labelElement.textContent = inputNameUpperCase;
+  inputElement.before(labelElement);
+
+  inputElement.placeholder = inputNameUpperCase[0]
+    + inputNameUpperCase.slice(1).toLowerCase();
+}
