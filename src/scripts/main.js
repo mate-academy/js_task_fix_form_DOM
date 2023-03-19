1ef362d83912c2314e6d4936e660170e023ad0b7
Natalia Maryshko <n.maryshko@gmail.com>
2023-03-19 07:01:55 -0500

    Fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,15 +2,18 @@
 
 const inputs = [...document.querySelectorAll('input')];
 
-inputs.map(input => {
+inputs.forEach(input => {
   const label = document.createElement('label');
 
   label.classList.add('field-label');
 
   label.htmlFor = input.id;
-  label.textContent = input.name;
 
-  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+  const inputName = input.name[0].toUpperCase()
+  + input.name.slice(1).replace(/([A-Z])/g, ' $1');
+
+  label.textContent = inputName;
+  input.placeholder = inputName;
 
   input.parentElement.append(label);
 });
