7bf78072d29717aa081cf5b3df13ef56d273ad25
Maksym Avramenko <nonste@outlook.com>
2023-03-21 16:58:43 +0200

    fixed form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.className = `field-label`;
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+
+  input.placeholder = input.name
+    .replace(/([a-z])([A-Z])/g, '$1 $2').replace(/\b\w/g, c => c.toUpperCase());
+
+  input.append(label);
+}
