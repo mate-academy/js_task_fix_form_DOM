96c74e31e0c7cb053154ff299936aeb2eb4ac8e7
Nazar Kharkevych <nkharkevych@gmail.com>
2023-03-23 17:26:32 +0200

    Fixes after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,15 +4,26 @@ const inputs = document.querySelectorAll('form > div > input');
 
 for (const input of inputs) {
   const label = document.createElement('label');
-  const capitalized = input.name.charAt(0).toUpperCase() + input.name.slice(1);
 
   label.classList.add('field-label');
 
   label.setAttribute('for', input.id);
 
-  label.textContent = input.name;
+  label.textContent = input.name.includes('Name')
+    ? `${input.name.slice(0, -4)} ${input.name.slice(-4)}`
+    : input.name;
 
-  input.setAttribute('placeholder', capitalized);
+  input.setAttribute('placeholder', capitalize(input));
 
   input.parentElement.prepend(label);
+}
+
+function capitalize(input) {
+  const capitalized = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  if (input.name.includes('Name')) {
+    return `${capitalized.slice(0, -4)} ${capitalized.slice(-4)}`;
+  }
+
+  return capitalized;
 }
