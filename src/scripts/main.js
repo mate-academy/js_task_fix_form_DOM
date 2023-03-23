01f25e5056d95ce7c83ca5e1d1ae06bab1dfea63
Sergii Mishchenko <serge.mishchenko@gmail.com>
2023-03-23 17:49:44 +0300

    Separate words added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,16 @@ for (const item of inputs) {
   item.before(document.createElement('label'));
   item.previousSibling.className = 'field-label';
   item.previousSibling.htmlFor = item.id;
-  item.previousSibling.textContent = item.name;
-  item.placeholder = item.name.charAt(0).toUpperCase() + item.name.slice(1);
+
+  let newName = item.name.charAt(0).toUpperCase() + item.name.slice(1);
+
+  for (let i = 0; i < item.name.length; i++) {
+    if (item.name.charAt(i) === item.name.charAt(i).toUpperCase()) {
+      newName = item.name.charAt(0).toUpperCase()
+      + item.name.slice(1, i) + ' ' + item.name.slice(i);
+    }
+  }
+
+  item.previousSibling.textContent = newName;
+  item.placeholder = newName;
 }
