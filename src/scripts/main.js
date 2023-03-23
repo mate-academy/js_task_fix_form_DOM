67c7aed20dca6bd37671d6dd571e5119385efa07
Sergii Mishchenko <serge.mishchenko@gmail.com>
2023-03-23 16:04:43 +0300

    Form was fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const item of inputs) {
+  item.before(document.createElement('label'));
+  item.previousSibling.className = 'field-label';
+  item.previousSibling.htmlFor = item.id;
+  item.previousSibling.textContent = item.name;
+  item.placeholder = item.name.charAt(0).toUpperCase() + item.name.slice(1);
+}
