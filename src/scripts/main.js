8ccd222c20d35e3206c441a6780e5075d4ea5f09
Roman Hrytsiuk <vasylguk7@gmail.com>
2023-03-26 16:46:14 +0300

    Placeholders are fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,13 +2,29 @@
 
 const inputs = document.querySelectorAll('input');
 
+function placeholderText(text) {
+  let res = text[0].toUpperCase();
+
+  for (let i = 1; i < text.length; i++) {
+    if (text[i] === text[i].toUpperCase()) {
+      res += ' ';
+      res += text[i].toLowerCase();
+      continue;
+    }
+
+    res += text[i];
+  };
+
+  return res;
+}
+
 for (const input of inputs) {
   const label = document.createElement('label');
 
   label.className = 'field-label';
   label.setAttribute('for', input.id);
   label.textContent = input.name;
-  input.placeholder = input.name.toUpperCase();
+  input.placeholder = placeholderText(input.name);
 
   input.parentElement.append(label);
 }
