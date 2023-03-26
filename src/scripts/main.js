dca35bd6cd6adc7b4d042d515ebf23f024a21bab
Yaroslav Dudar <dudar.komod@gmail.com>
2023-03-26 02:45:59 +0200

    Label and placeholder was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const formImputs = [...document.querySelectorAll('input')];
+
+const createLabel = function(input) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.innerText = input.name;
+
+  return label;
+};
+
+const labels = formImputs.map(createLabel);
+
+for (let i = 0; i < formImputs.length; i++) {
+  formImputs[i].parentElement.append(labels[i]);
+
+  formImputs[i].placeholder
+    = formImputs[i].name[0].toUpperCase() + formImputs[i].name.slice(1);
+};
