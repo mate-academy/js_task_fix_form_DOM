fe2d1c50e3b4091583644273931f5603123b9562
Eduard <Eduard@eduards-macbook-pro.home>
2023-03-26 15:01:01 +0200

    formFixing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const textFormat = input.name.split('N').join(' N');
+
+  input.insertAdjacentHTML('afterend', `
+    <label
+      class = "field-label"
+      for = ${input.id}
+    >
+      ${textFormat}
+    </label>
+  `);
+
+  input.style = 'text-transform: capitalize;';
+  input.placeholder = textFormat;
+}
