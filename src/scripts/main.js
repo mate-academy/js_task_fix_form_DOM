65ef5caf95d9e10547b36008e8abf9ae941c3d01
Leonid <leonidbondarchuk6@gmail.com>
2023-03-26 13:11:16 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+const field = document.querySelectorAll('.field');
+
+for (let i = 0; i < inputs.length; i++) {
+  const atributeName = inputs[i].getAttribute('name');
+  const nameForPlaceholder
+    = atributeName.replace(atributeName[0], atributeName[0].toUpperCase());
+  const nameForLabel = atributeName;
+
+  inputs[i].placeholder = nameForPlaceholder;
+
+  const html
+  = `<label 
+      for = "${atributeName}"
+      class = "field-label">${nameForLabel}
+    </label>`;
+
+  field[i].insertAdjacentHTML('afterbegin', html);
+}
