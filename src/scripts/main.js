eda644405a838c500621ef49fb89edcb8c390921
Roman Hrytsiuk <vasylguk7@gmail.com>
2023-03-26 15:33:13 +0300

    Initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+  input.placeholder = input.name.toUpperCase();
+
+  input.parentElement.append(label);
+}
