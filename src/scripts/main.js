58a7e5bc1ce0c7f136fbe8c527f5097d49b61962
Diana Snihur <dianasnigur2@gmail.com>
2023-03-26 08:25:47 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+function convertCamelCase(string) {
+  const words = string.replace(/[A-Z]/g, (match) =>
+    ` ${match.toLowerCase()}`).trim().split(' ');
+  const titleCase = words.map(word =>
+    word.charAt(0).toUpperCase() + word.slice(1));
+
+  return titleCase.join(' ');
+}
+
+const inputsEl = document.querySelectorAll('input');
+
+inputsEl.forEach(input => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+  input.parentNode.insertBefore(label, input);
+  input.placeholder = convertCamelCase(input.name);
+});
