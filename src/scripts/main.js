e2c430b4453f3663e64cd641c02bc86482012dde
Mikhieiev Ivan <navigator79@gmail.com>
2023-03-30 10:05:01 +0300

    form DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  const createLabel = document.createElement('label');
+
+  createLabel.className = 'field-label';
+  createLabel.htmlFor = input.id;
+  createLabel.textContent = input.name.replace('N', ' N');
+
+  const nameWithBigLatter = input.name.charAt(0).toUpperCase()
+    + input.name.slice(1).replace('N', ' N');
+
+  input.placeholder = nameWithBigLatter;
+  // input.parentElement.append(createLabel);
+  input.parentElement.append(createLabel);
+});
