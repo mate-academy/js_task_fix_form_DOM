0eee58ce84cb29b70604958b7106b9bce121e227
Daryna Hnidash <melissa998877@gmail.com>
2023-03-31 16:34:05 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  const label = document.createElement('label');
+
+  label.className = `field-label`;
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+
+  input.parentElement.append(label);
+  input.placeholder = toOrdinaryText(input.name);
+});
+
+function toOrdinaryText(camelCase) {
+  const result = camelCase.replace(/([A-Z])/g, ' $1');
+
+  return result[0].toUpperCase() + result.substring(1).toLowerCase();
+}
