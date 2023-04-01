d9672a34138f5883292937229a73bafe71d8a3af
metja <mikolapenzanik@gmail.com>
2023-04-01 22:22:33 +0300

    Completed task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+
+  input.placeholder = capitalize(input.name);
+  input.parentElement.append(label);
+};
+
+function capitalize(str) {
+  const capitalizeStr
+    = str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+
+  return capitalizeStr;
+};
