af36ad2d0077a8f7f27ad9efbae386cafec4c23d
metja <mikolapenzanik@gmail.com>
2023-04-02 10:52:57 +0300

    Fixed separate differents words

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,5 +17,5 @@ function capitalize(str) {
   const capitalizeStr
     = str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 
-  return capitalizeStr;
+  return capitalizeStr.replace('n', ' n');
 };
