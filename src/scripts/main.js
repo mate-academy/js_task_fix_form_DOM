f6da811e7c69982dff467ad1423cc8ccc0be2fc8
Anna Peretiatko <tobossanna@gmail.com>
2023-04-03 18:52:01 +0200

    Fix form solution v.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write code here
+const divs = document.querySelectorAll('.field');
+
+for (const div of [...divs]) {
+  const input = div.querySelector('input');
+  const inputType = input.getAttribute('type');
+  const inputName = input.getAttribute('name');
+  const inputId = input.getAttribute('id');
+  const inputPlaceholder = inputName.toUpperCase();
+
+  div.innerHTML = `
+  <input
+    type="${inputType}"
+    name="${inputName}"
+    class="field-text"
+    autocomplete="off"
+    id="${inputId}"
+    placeholder="${inputPlaceholder}"
+    autofocus
+  >
+  <label class="field-label" for="${inputId}"> ${inputName} </label>
+  `;
+}
