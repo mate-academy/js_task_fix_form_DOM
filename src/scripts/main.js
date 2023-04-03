12f063ff302c962cd7797c27e6a88ca829e3b26f
Viktor <vasha2020@gmail.com>
2023-04-03 10:37:17 +0100

    Solution-2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,12 +7,28 @@ for (let i = 0; i < allInputs.length; i++) {
 
   label.className = 'field-label';
   label.htmlFor = allInputs[i].id;
-  label.textContent = allInputs[i].name.replace('N', ' N');
+  label.textContent = separator(allInputs[i].name);
 
   const parentInput = allInputs[i].parentElement;
 
   parentInput.append(label);
 
-  allInputs[i].placeholder = allInputs[i].name.charAt(0).toUpperCase()
-    + allInputs[i].name.slice(1).replace('N', ' N');
+  allInputs[i].placeholder = separator(allInputs[i].name);
+};
+
+function separator(str) {
+  let formatedString = '';
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toUpperCase()) {
+      formatedString += ' ' + str[i];
+    } else {
+      formatedString += str[i];
+    }
+  }
+
+  formatedString = formatedString.charAt(0).toUpperCase()
+    + formatedString.slice(1);
+
+  return formatedString;
 };
