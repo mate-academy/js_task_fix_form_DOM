c7df483b2c46212e03805daa20ce04e0fc31bd81
Anna Peretiatko <tobossanna@gmail.com>
2023-04-03 21:50:42 +0200

    Fix form solution v.2 (changed element naming, label value and input placeholder outputs)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,32 @@
 'use strict';
 
 // write code here
-const divs = document.querySelectorAll('.field');
+const fields = document.querySelectorAll('.field');
 
-for (const div of [...divs]) {
-  const input = div.querySelector('input');
+for (const field of [...fields]) {
+  const input = field.querySelector('input');
   const inputType = input.getAttribute('type');
-  const inputName = input.getAttribute('name');
   const inputId = input.getAttribute('id');
-  const inputPlaceholder = inputName.toUpperCase();
+  let inputName = input.getAttribute('name');
+  let inputPlaceholder = inputName.charAt(0).toUpperCase() + inputName.slice(1);
 
-  div.innerHTML = `
+  if (inputName.toLowerCase().includes('name')) {
+    inputName = inputName.toLowerCase().split('name').join(' name');
+  }
+
+  if (inputPlaceholder.toLowerCase().includes('name')) {
+    const placeholderText = inputPlaceholder
+      .toLowerCase()
+      .split('name')
+      .join(' Name');
+
+    inputPlaceholder = placeholderText
+      .charAt(0)
+      .toUpperCase()
+      + placeholderText.slice(1);
+  }
+
+  field.innerHTML = `
   <input
     type="${inputType}"
     name="${inputName}"
