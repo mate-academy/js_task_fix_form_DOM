1c08322b5979f68f885c351ce412a34eac010370
Pavlo Kostyshyn <kostishinpavlo24@gmail.com>
2023-04-04 17:55:16 +0300

    form was fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+inputs.forEach(input => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+  input.parentElement.append(label);
+  input.setAttribute('placeholder', capitalizeString(input.name));
+});
+
+function capitalizeString(str) {
+  return str[0].toUpperCase() + str.slice(1);
+}
