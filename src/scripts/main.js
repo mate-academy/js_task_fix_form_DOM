5aa0bd9190ea3af7be35dbbf0ce5e02eb8c2c6eb
Anna Grechana <grechanaanna@gmial.com>
2023-04-04 23:44:35 +0300

    1st version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const elements = document.querySelectorAll('input');
+
+for (const i of elements) {
+  i.placeholder = i.name.charAt(0).toUpperCase() + i.name.slice(1);
+
+  i.insertAdjacentHTML('beforebegin', `
+  <label class ="field-label" for="${i.id}">${i.name}</label>`);
+};
