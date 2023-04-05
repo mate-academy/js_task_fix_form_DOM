dce59432eb58cfdacf1f189bed7d6f7cc70f1f44
Anna Grechana <grechanaanna@gmial.com>
2023-04-05 18:06:54 +0300

    2nd version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,19 @@
 'use strict';
 
-const elements = document.querySelectorAll('input');
+const inputArr = document.querySelectorAll('input');
 
-for (const i of elements) {
-  i.placeholder = i.name.charAt(0).toUpperCase() + i.name.slice(1);
+// console.log(inputArr)
 
-  i.insertAdjacentHTML('beforebegin', `
-  <label class ="field-label" for="${i.id}">${i.name}</label>`);
+for (const input of inputArr) {
+  input.placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  for (let letter = 1; letter < input.name.length; letter++) {
+    if (input.name[letter] === (input.name[letter]).toUpperCase()) {
+      input.placeholder = input.placeholder.substring(0, letter)
+      + ' ' + input.name.substring(letter, input.name.length);
+    }
+  }
+
+  input.insertAdjacentHTML('beforebegin', `
+  <label class ="field-label" for="${input.id}">${input.name}</label>`);
 };
