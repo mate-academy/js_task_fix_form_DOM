dfa85834ac2b9eecef889597ed76dc3f23d82a19
Oksana Tytanych <oksanatytanych@gmail.com>
2023-04-05 20:40:28 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+function fixForm() {
+  const inputs = document.querySelectorAll('input');
+
+  for (let i = 0; i < inputs.length; i++) {
+    const label = document.createElement('label');
+
+    label.innerHTML = `
+      <label class="field-label" for="${inputs[i].id}">
+        ${inputs[i].name}
+      </label>
+    `;
+
+    inputs[i].before(label);
+
+    inputs[i].placeholder = inputs[i].name[0].toUpperCase()
+      + inputs[i].name.slice(1);
+  }
+}
+
+fixForm();
