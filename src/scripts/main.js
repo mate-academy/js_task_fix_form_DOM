7a4c62d1ee0796b73b00144188d8f5c42ce3e232
Oksana Tytanych <oksanatytanych@gmail.com>
2023-04-06 11:27:23 +0300

    layout_task-guideline

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,17 +5,23 @@ function fixForm() {
 
   for (let i = 0; i < inputs.length; i++) {
     const label = document.createElement('label');
+    let changedName = inputs[i].name[0].toUpperCase();
+
+    for (let y = 1; y < inputs[i].name.length; y++) {
+      if (inputs[i].name[y] !== inputs[i].name[y].toLowerCase()) {
+        changedName += ' ';
+      }
+      changedName += inputs[i].name[y];
+    }
 
     label.innerHTML = `
       <label class="field-label" for="${inputs[i].id}">
-        ${inputs[i].name}
+        ${changedName}
       </label>
     `;
 
     inputs[i].before(label);
-
-    inputs[i].placeholder = inputs[i].name[0].toUpperCase()
-      + inputs[i].name.slice(1);
+    inputs[i].placeholder = changedName;
   }
 }
 
