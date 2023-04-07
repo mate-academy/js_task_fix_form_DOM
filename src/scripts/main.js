b7a72fe27aab5fa6f48e3c559b40898fc14e6fb4
Ilia Kolesnik <koleshnik160799@gmail.com>
2023-04-07 17:08:24 +0300

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const blocks = document.querySelectorAll('.field');
+
+const inputs = document.querySelectorAll('input');
+
+
+
+[...blocks].forEach(block => {
+  const inputBlock = block.querySelector('input')
+  const label = document.createElement('label');
+  const firstWord = 
+    inputBlock.name.slice(0, 1).toUpperCase() +
+    inputBlock.name.slice(1).toLowerCase()
+  ;
+
+  label.className = 'field-label';
+  label.innerText = inputBlock.name.toUpperCase();
+  label.htmlFor = inputBlock.id;
+  inputBlock.setAttribute('placeholder', firstWord);
+
+  block.prepend(label);
+});
+
