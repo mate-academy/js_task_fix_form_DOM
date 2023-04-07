ca9ac93cf7c11afd69070f81dac5df81d7eae57d
popeye88 <aleksei.taran@gmail.com>
2023-04-07 19:06:58 +0300

    SOLUTION

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+  const text = input.name.replace(/name/gi, ' name');
+
+  input.placeholder = text;
+  input.style.textTransform = 'capitalize';
+  input.parentNode.prepend(label);
+  label.classList.add('field-label');
+  label.htmlFor = input.id;
+  label.style.textTransform = 'uppercase';
+  label.innerText = text;
+}
