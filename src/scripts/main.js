988a20c31cb627690fe18f184cfa62baf998ebc9
Ilia Kolesnik <koleshnik160799@gmail.com>
2023-04-08 15:08:21 +0300

    Add sep in placeholder

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,18 +6,25 @@ const blocks = document.querySelectorAll('.field');
   const inputBlock = block.querySelector('input');
   const label = document.createElement('label');
 
-  const firstWord
-    = inputBlock.name.slice(0, 1).toUpperCase()
-    + inputBlock.name.slice(1).toLowerCase();
+  function firstWord(word) {
+    return word.slice(0, 1).toUpperCase()
+    + word.slice(1);
+  }
+
+  function separName(word) {
+    return word.name.includes('Name')
+    ? word.name.split('Name').join(' ') + 'Name'
+    : word.name;
+  }
+
 
   label.className = 'field-label';
-  
-  label.innerText = inputBlock.name.includes('Name') ?
-    inputBlock.name.split('Name').join(' ') + 'Name':
-    inputBlock.name;
 
-  label.htmlFor = inputBlock.id;  
-  inputBlock.setAttribute('placeholder', firstWord);
+  label.innerText = separName(inputBlock);
+
+  label.htmlFor = inputBlock.id;
+  inputBlock.setAttribute('placeholder', 
+  firstWord(separName(inputBlock)));
 
   block.prepend(label);
 });
