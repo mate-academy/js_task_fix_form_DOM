d5a3f4d0167e330d4c7b833a601c9458111b9cd0
Sasha Green <aleksa146311@gmail.com>
2023-04-08 22:48:40 +0300

    add task fix form DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.textContent = input.name;
+  label.htmlFor = input.id;
+  label.style.textTransform = 'uppercase';
+
+  input.parentNode.prepend(label);
+  input.setAttribute('placeholder', breakToWords(input.name));
+  input.style.textTransform = 'capitalize';
+}
+
+function breakToWords(word) {
+  const wordArray = word.split('');
+
+  for (let i = 0; i < wordArray.length; i++) {
+    if (wordArray[i] === wordArray[i].toUpperCase()) {
+      wordArray.splice(i, 0, ' ');
+      i++;
+    }
+  }
+
+  return wordArray.join('');
+}
