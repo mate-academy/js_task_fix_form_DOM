7040c2e11c925962d693a0cae6a237d3ccb6ed8c
адмін <admin@Mac-Max.local>
2023-04-17 13:18:20 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+[...inputs].forEach(function(input) {
+  const label = document.createElement('label');
+
+  label.setAttribute('class', 'field-label');
+  label.setAttribute('for', `${input.id}`);
+
+  const labelText = label.textContent
+  = input.name.replace(/([a-z])([A-Z])/g, '$1 $2').toLowerCase().toUpperCase();
+
+  input.insertAdjacentElement('beforebegin', label);
+
+  const inputName = labelText[0].toUpperCase()
+  + labelText.slice(1).toLowerCase();
+
+  input.setAttribute('placeholder', `${inputName}`);
+});
