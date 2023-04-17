d337fe0e4daa3a8d6112155e840be7808b1bb1fb
Bogdan Parubchenko <bogdanparubchenko@gmail.com>
2023-04-17 09:56:31 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+for (const element of inputs) {
+  const label = document.createElement('label');
+  const thisDiv = element.parentNode;
+
+  element.placeholder = element.name;
+  element.style.textTransform = `Capitalize`;
+  label.textContent = element.name;
+  label.htmlFor = element.id;
+  label.classList.add('field-label');
+
+  if (element.name.includes('Name')) {
+    element.placeholder = element.placeholder.replace(/Name/g, ' Name');
+    label.textContent = label.textContent.replace(/Name/g, ' Name');
+  }
+
+  thisDiv.prepend(label);
+}
