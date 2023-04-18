a936a985031c04bf66c45c62bf758ec618367405
DanielSorokowski <sorokowskidaniel0@gmail.com>
2023-04-18 11:41:45 +0200

    seperate words and capitalize

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,12 +4,21 @@ const inputs = document.querySelectorAll('input');
 
 inputs.forEach((input, index) => {
   const label = document.createElement('label');
+  const inputName = input.getAttribute('name');
+
+  input.style.textTransform = 'capitalize';
 
   label.classList.add('field-label');
   label.setAttribute('for', index);
-  label.innerText = input.getAttribute('name');
+
+  if (inputName.includes('Name')) {
+    label.innerText = inputName.replace(/Name/g, ' Name');
+    input.setAttribute('placeholder', inputName.replace(/Name/g, ' Name'));
+  } else {
+    label.innerText = inputName;
+    input.setAttribute('placeholder', inputName);
+  }
 
   input.parentElement.appendChild(label);
-  input.setAttribute('placeholder', input.getAttribute('name'));
 });
 // write code here
