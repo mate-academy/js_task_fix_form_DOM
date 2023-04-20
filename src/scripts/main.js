2f5d6b30fb7920ae5394b17a67f653ca398e8a9b
Victor Buldenko <gookidoo@gmail.com>
2023-04-20 15:59:35 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const inputContainers = [...document.querySelectorAll('.field')];
+
+inputContainers.forEach((el) => {
+  const input = el.querySelector('input');
+  const label = document.createElement('LABEL');
+
+  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.innerText = input.name;
+  el.insertAdjacentElement('afterbegin', label);
+});
