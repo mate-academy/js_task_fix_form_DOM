20dd273764ef55f17233d2c47a484ee96e728a69
Alla Gutsul <allagutsul1990@gmail.com>
2023-04-20 14:12:09 +0300

    add labels & placeholders to form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+[...inputs].map(input => {
+  const label = document.createElement('label');
+
+  label.className = ['field-label'];
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+  input.before(label);
+
+  input.placeholder = capitalize(input.name);
+});
+
+function capitalize(word) {
+  return word.slice(0, 1).toUpperCase()
+    + word.slice(1);
+}
