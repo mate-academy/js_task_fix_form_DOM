28a7c1e0c4430a202b7fe058417969e9864116e7
Victor Buldenko <gookidoo@gmail.com>
2023-04-20 20:01:12 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,13 +2,22 @@
 
 const inputContainers = [...document.querySelectorAll('.field')];
 
-inputContainers.forEach((el) => {
+inputContainers.forEach((el, i) => {
   const input = el.querySelector('input');
   const label = document.createElement('LABEL');
+  let nameText = input.name[0];
 
-  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+  for (let j = 1; j < input.name.length; j++) {
+    if (input.name[j] === input.name[j].toUpperCase()) {
+      nameText += ` ${input.name[j]}`;
+    } else {
+      nameText += input.name[j];
+    }
+  }
+
+  input.placeholder = nameText[0].toUpperCase() + nameText.slice(1);
+  label.innerText = nameText;
   label.className = 'field-label';
   label.setAttribute('for', input.id);
-  label.innerText = input.name;
   el.insertAdjacentElement('afterbegin', label);
 });
