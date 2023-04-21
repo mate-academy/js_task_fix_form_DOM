c7b852ce4b24504d681b0e5aa2fcf55e03a55f4b
danulyk05 <ivan.danulyk2016@gmail.com>
2023-04-22 00:17:35 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const forma = document.querySelectorAll('form');
+const inputs = [...forma].map(element => element.querySelectorAll('input'));
+
+for (const inputElement of inputs) {
+  [...inputElement].map(input => {
+    input.insertAdjacentHTML('beforebegin', `
+    <label class= "field-label" for="${input.id}">
+      ${input.name}
+    </label>
+    `);
+
+    input.placeholder = input.name.toLocaleUpperCase();
+  });
+};
