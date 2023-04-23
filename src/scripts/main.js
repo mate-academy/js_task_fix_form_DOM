14155763efab4bfa0aefee74235b9e4ecf325a72
Danylo Pasko <danil.pasco@gmail.com>
2023-04-23 19:03:19 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,21 @@
 'use strict';
 
-const inputs = [...document.querySelectorAll('input')];
+const allInput = [...document.querySelectorAll('input')];
 
-for (const input of inputs) {
+for (const element of allInput) {
   const label = document.createElement('label');
-  const text = input.name.replace(/name/gi, ' name');
+  const thisDiv = element.parentNode;
 
-  input.placeholder = text;
-  input.style.textTransform = 'capitalize';
-  input.parentNode.prepend(label);
+  element.placeholder = element.name;
+  element.style.textTransform = `Capitalize`;
+  label.textContent = element.name;
+  label.htmlFor = element.id;
   label.classList.add('field-label');
-  label.htmlFor = input.id;
-  label.style.textTransform = 'uppercase';
-  label.innerText = text;
+
+  if (element.name.includes('Name')) {
+    element.placeholder = element.placeholder.replace(/Name/g, ' Name');
+    label.textContent = label.textContent.replace(/Name/g, ' Name');
+  }
+
+  thisDiv.prepend(label);
 }
