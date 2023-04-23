b16d8c749fac6d1b02d6c06271cffe5509850d14
Danylo Pasko <danil.pasco@gmail.com>
2023-04-23 18:37:03 +0300

    rework

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,38 +1,16 @@
 'use strict';
 
-const inputElements = document.querySelectorAll('input');
-const inputContainers = document.querySelectorAll('.field');
+const inputs = [...document.querySelectorAll('input')];
 
-const capitalize = (word) => (
-  word.charAt(0).toUpperCase() + word.slice(1));
+for (const input of inputs) {
+  const label = document.createElement('label');
+  const text = input.name.replace(/name/gi, ' name');
 
-function removeCamelCasing(word, separator = ' ') {
-  const camelParts = [];
-
-  for (let letter of word) {
-    if (letter === letter.toUpperCase()) {
-      camelParts.push(separator);
-      letter = letter.toLowerCase();
-    }
-    camelParts.push(letter);
-  }
-
-  return camelParts.join('');
+  input.placeholder = text;
+  input.style.textTransform = 'capitalize';
+  input.parentNode.prepend(label);
+  label.classList.add('field-label');
+  label.htmlFor = input.id;
+  label.style.textTransform = 'uppercase';
+  label.innerText = text;
 }
-
-function createFormLabels() {
-  inputElements.forEach((inputElement, currentIndex) => {
-    const label = document.createElement('label');
-    const removedCamelCasing = removeCamelCasing(inputElement.name);
-
-    label.textContent = removedCamelCasing.toUpperCase();
-    label.classList.add('field-label');
-    label.style.fontWeight = 'bold';
-    inputElement.placeholder = capitalize(removedCamelCasing);
-
-    label.setAttribute('for', inputElement.id);
-    inputContainers[currentIndex].prepend(label);
-  });
-}
-
-createFormLabels();
