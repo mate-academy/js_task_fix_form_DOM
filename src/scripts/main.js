67898e3b1ced421729bab69d7f97ca9b5b4d08e8
Kyryl Maslov <kyrylmaslov2007@gmail.com>
2023-04-24 18:16:43 +0200

    form fixer was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+const fields = document.querySelectorAll('.field');
+const fieldsArray = [ ...fields ];
+const inputsArray = [ ...inputs ];
+
+for (let i = 0; i < inputsArray.length; i++) {
+  for (let y = 0; y < inputsArray[i].name.length; y++) {
+    if (inputsArray[i].name[y] === inputsArray[i].name[y].toUpperCase()
+      && y !== 0) {
+      inputsArray[i].placeholder += ' ';
+    }
+
+    if (y === 0) {
+      inputsArray[i].placeholder += inputsArray[i].name[y].toUpperCase();
+    } else {
+      inputsArray[i].placeholder += inputsArray[i].name[y];
+    }
+  }
+
+  fieldsArray[i].insertAdjacentHTML('afterbegin', `
+    <label for=${inputsArray[i].id} class ='field-label'>${inputsArray[i].name}
+    </label>
+  `);
+}
