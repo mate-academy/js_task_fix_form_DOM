9381fe2b787db0dcb41254328cd7d4cfb183e662
Kyryl Maslov <kyrylmaslov2007@gmail.com>
2023-04-24 19:31:53 +0200

    form fixed was fixed 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,9 +6,18 @@ const fieldsArray = [ ...fields ];
 const inputsArray = [ ...inputs ];
 
 for (let i = 0; i < inputsArray.length; i++) {
+  fieldsArray[i].insertAdjacentHTML('afterbegin', `
+    <label for=${inputsArray[i].id} class ='field-label'></label>
+  `);
+}
+
+const labelsArr = [ ...document.querySelectorAll('.field-label') ];
+
+for (let i = 0; i < labelsArr.length; i++) {
   for (let y = 0; y < inputsArray[i].name.length; y++) {
     if (inputsArray[i].name[y] === inputsArray[i].name[y].toUpperCase()
       && y !== 0) {
+      labelsArr[i].textContent += ' ';
       inputsArray[i].placeholder += ' ';
     }
 
@@ -17,10 +26,7 @@ for (let i = 0; i < inputsArray.length; i++) {
     } else {
       inputsArray[i].placeholder += inputsArray[i].name[y];
     }
-  }
 
-  fieldsArray[i].insertAdjacentHTML('afterbegin', `
-    <label for=${inputsArray[i].id} class ='field-label'>${inputsArray[i].name}
-    </label>
-  `);
+    labelsArr[i].textContent += inputsArray[i].name[y];
+  }
 }
