d49f650585fcede1636de7af3b8b8c3a6ad6fc39
Kyryl Maslov <kyrylmaslov2007@gmail.com>
2023-04-25 09:26:15 +0200

    start index was added to form fixer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,7 @@ const inputs = document.querySelectorAll('.field-text');
 const fields = document.querySelectorAll('.field');
 const fieldsArray = [ ...fields ];
 const inputsArray = [ ...inputs ];
+const startIndex = 0;
 
 for (let i = 0; i < inputsArray.length; i++) {
   fieldsArray[i].insertAdjacentHTML('afterbegin', `
@@ -16,14 +17,14 @@ const labelsArr = [ ...document.querySelectorAll('.field-label') ];
 inputsArray.forEach((input, inputIndex) => {
   for (let letterIndx = 0; letterIndx < input.name.length; letterIndx++) {
     if (input.name[letterIndx] === input.name[letterIndx].toUpperCase()
-      && letterIndx !== 0) {
+      && letterIndx !== startIndex) {
       labelsArr[inputIndex].textContent += ' ';
       input.placeholder += ' ';
     }
 
     const inputName = input.name[letterIndx];
 
-    input.placeholder += letterIndx === 0
+    input.placeholder += letterIndx === startIndex
       ? inputName.toUpperCase() : inputName;
 
     labelsArr[inputIndex].textContent += input.name[letterIndx];
