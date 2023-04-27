e34f5ddd77e83257f6a2f900eecb8477ab2a3cf8
Maria Shmakova <masha.shmakova@gmail.com>
2023-04-27 21:41:03 +0300

    foarm were adjusted, names, were splited

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,7 +8,18 @@ const allInputs = document.querySelectorAll('input');
 
   label.className = 'field-label';
   label.setAttribute('for', input.id);
+
+  if (input.name === 'firstName' || input.name === 'lastName') {
+    const nIdx = input.name.indexOf('N');
+
+    input.name = input.name.substring(0, nIdx)
+    + ' '
+    + input.name.substring(nIdx);
+
+    input.name = input.name.toLowerCase();
+  }
   label.textContent = input.name;
+
   input.after(label);
   input.setAttribute('placeholder', input.name);
 });
