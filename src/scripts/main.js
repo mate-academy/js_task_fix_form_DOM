fd364db262394c7ac0cc5dd50ddd1cb9b668721f
Maria Shmakova <masha.shmakova@gmail.com>
2023-04-27 15:56:00 +0300

    foarm were adjusted

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write code here
+const allInputs = document.querySelectorAll('input');
+
+[...allInputs].forEach(input => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+  input.after(label);
+  input.setAttribute('placeholder', input.name);
+});
