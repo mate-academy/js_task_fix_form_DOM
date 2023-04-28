6319f60f7607ab64bec4761560eaaac5b7f00ea8
Valeria Oleynik <oleynik.valeria27@gmail.com>
2023-04-28 17:23:37 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const inputElements = document.querySelectorAll('.field-text');
+
+const valuePlaceholder = (value) => {
+  return value.slice(0, 1).toUpperCase() + value.slice(1);
+};
+
+inputElements.forEach(input => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+
+  label.textContent = input.name;
+  label.htmlFor = input.id;
+  input.placeholder = valuePlaceholder(input.name);
+
+  input.before(label);
+});
