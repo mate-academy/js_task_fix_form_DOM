29648482ae6fce90159076228842ef6f9bf922f2
mariuszzawierucha <mariusz.zawierucha@gmail.com>
2023-04-29 00:09:59 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+[...inputs].forEach(input => {
+  // adding placeholedr
+  input.placeholder = capitalizeIt(input.name);
+
+  // creating and appending label
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+  input.parentElement.prepend(label);
+});
+
+function capitalizeIt(text) {
+  return text[0].toUpperCase() + text.slice(1);
+}
