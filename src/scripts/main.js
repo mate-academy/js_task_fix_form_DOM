ad020b138e1897a8af1c2f51546a75beaa5364bc
OlegRak86 <rakolegit@gmail.com>
2023-04-29 17:02:48 +0300

    js_task_fix_form_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+  const inputName = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  input.placeholder = inputName;
+  label.className = `field-label`;
+  label.for = input.id;
+  label.textContent = input.name;
+  input.insertAdjacentElement('afterend', label);
+}
