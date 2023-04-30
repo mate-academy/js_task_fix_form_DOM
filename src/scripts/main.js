e7a2fafbd3edcfe86ad4168fd4ee157b627d8a6c
OlegRak86 <rakolegit@gmail.com>
2023-04-30 14:44:55 +0300

    js_task_fix_form_DOM_Fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,18 @@
 'use strict';
 
 const inputs = document.querySelectorAll('input');
+let inputName = '';
 
 for (const input of inputs) {
   const label = document.createElement('label');
-  const inputName = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  if (input.name.split('N').length >= 2) {
+    inputName = input.name.split('N')[0].charAt(0).toUpperCase()
+    + input.name.split('N')[0].slice(1)
+    + ' N' + input.name.split('N')[1];
+  } else {
+    inputName = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+  }
 
   input.placeholder = inputName;
   label.className = `field-label`;
