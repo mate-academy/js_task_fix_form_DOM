3441562af80ecffa3aafebf4608942575f037ced
Karyna Onyshchuk <onyshchukkarina@gmail.com>
2023-05-02 10:47:43 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+function capitalize(string) {
+  return string[0].toUpperCase()
+     + string.split(/(?=[A-Z])/).join(' ')
+       .slice(1).toLowerCase();
+}
+
+const inputs = document.querySelectorAll('input');
+
+[...inputs].forEach(input => {
+  const label = document.createElement('label');
+
+  label.className = `field-label`;
+  label.htmlFor = input.id;
+  label.textContent = capitalize(input.name);
+
+  input.placeholder = capitalize(input.name);
+  input.parentElement.append(label);
+});
