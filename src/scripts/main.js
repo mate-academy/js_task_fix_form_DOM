10f625d8e6e26336d44272258ce092e7733a3393
OlegRak86 <rakolegit@gmail.com>
2023-05-04 15:12:39 +0300

    js_task_fix_form_DOM_fix2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,16 @@
 'use strict';
 
 const inputs = document.querySelectorAll('input');
-let inputName = '';
 
 for (const input of inputs) {
   const label = document.createElement('label');
 
-  if (input.name.split('N').length >= 2) {
-    inputName = input.name.split('N')[0].charAt(0).toUpperCase()
-    + input.name.split('N')[0].slice(1)
-    + ' N' + input.name.split('N')[1];
-  } else {
-    inputName = input.name.charAt(0).toUpperCase() + input.name.slice(1);
-  }
-
-  input.placeholder = inputName;
+  input.placeholder = input.name[0].toUpperCase()
+  + input.name.slice(1).split('N').join(' N');
   label.className = `field-label`;
   label.for = input.id;
-  label.textContent = input.name;
+
+  label.textContent = input.name[0].toUpperCase()
+  + input.name.slice(1).split('N').join(' N'); ;
   input.insertAdjacentElement('afterend', label);
 }
