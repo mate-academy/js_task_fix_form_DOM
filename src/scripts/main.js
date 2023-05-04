e71a76e68ef8b0db14fa913a1b230122422aa505
OlegRak86 <rakolegit@gmail.com>
2023-05-04 15:36:01 +0300

    js_task_fix_form_DOM_fix3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,11 +6,11 @@ for (const input of inputs) {
   const label = document.createElement('label');
 
   input.placeholder = input.name[0].toUpperCase()
-  + input.name.slice(1).split('N').join(' N');
+    + input.name.slice(1).split('N').join(' N');
   label.className = `field-label`;
   label.for = input.id;
 
   label.textContent = input.name[0].toUpperCase()
-  + input.name.slice(1).split('N').join(' N'); ;
+    + input.name.slice(1).split('N').join(' N'); ;
   input.insertAdjacentElement('afterend', label);
 }
