33334509f60c486f7dccb9c977810463b223d1d4
Kate Matviiv <katyaya56@gmail.com>
2023-05-05 00:26:28 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
+/* eslint-disable max-len */
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.setAttribute('class', 'field-label');
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+
+  input.insertAdjacentElement('beforebegin', label);
+
+  const placeholderName = input.name.charAt(0).toUpperCase() + input.name.slice(1).split(/(?=[A-Z])/).join(' ');
+
+  input.placeholder = placeholderName;
+
+  input.before(label);
+});
