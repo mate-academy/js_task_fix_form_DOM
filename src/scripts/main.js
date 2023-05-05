eafae98f484e4f9a664fa3d4bf3fe708574976d0
vladimir tkachenko <vladimirscloud@icloud.com>
2023-05-05 17:34:34 +0300

    fix_form commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelector('form input');
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.setAttribute('class', 'field-label');
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+
+  input.insertAdjacentElemet('beforebegin', label);
+
+  const placeholderName = input.name.charAt(0).toUpperCase()
+  + input.name.slice(1).split(/(?=[A-Z])/).join('');
+
+  input.placeholde = placeholderName;
+
+  input.before(label);
+});
