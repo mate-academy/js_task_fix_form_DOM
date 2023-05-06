dab2a56210e21bf8ee8f27c147602b294a5cf096
Veronika Pastushkova <d9169393507@gmai.com>
2023-05-06 15:27:36 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const input = document.querySelectorAll('input');
+
+for (let i = 0; i < input.length; i++) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+
+  const inputId = input[i].getAttribute('id');
+
+  label.setAttribute('for', inputId);
+
+  const textLabel = document.createTextNode(input[i].name);
+
+  label.append(textLabel);
+
+  input.forEach(el => {
+    el.placeholder = `${el.name.charAt(0).toUpperCase() + el.name.slice(1)}`;
+  });
+
+  input[i].before(label);
+}
