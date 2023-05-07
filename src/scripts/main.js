0596c193633042494ce43b3803c0f8f294b1bec0
Oleksii Lavka <lavka.alexey37gmail.com>
2023-05-07 16:15:31 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const formsWrapper = document.querySelector('.login-wrap');
+const signUpForm = formsWrapper.firstElementChild;
+const signInForm = formsWrapper.lastElementChild;
+
+const fixForm = form => {
+  const formInputs = form.querySelectorAll('input');
+
+  for (const inputField of formInputs) {
+    const { id: inputId, name: inputName } = inputField;
+    const label = document.createElement('label');
+
+    inputField.setAttribute('placeholder', capitalizeWord(inputName));
+
+    label.className = 'field-label';
+    label.htmlFor = inputId;
+    label.textContent = inputName;
+
+    inputField.before(label);
+  }
+};
+
+const capitalizeWord = word => {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+};
+
+fixForm(signUpForm);
+fixForm(signInForm);
