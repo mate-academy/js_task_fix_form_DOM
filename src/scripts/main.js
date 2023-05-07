34697a9e4a2d7be9cc5c8bb7b6bbf64958a5583d
metin <metinbicaksiz@gmail.com>
2023-05-07 18:14:56 +0300

    completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const formInputs = document.querySelectorAll('form input');
+
+formInputs.forEach(input => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
+  label.textContent = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+  input.parentElement.appendChild(label);
+});
+
+formInputs.forEach(input => {
+  input.placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+});
