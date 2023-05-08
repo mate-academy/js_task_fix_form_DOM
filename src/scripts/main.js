28694b11d49b0a6e424beb02f35d6d0a6b74e12a
Yura Enttsel <equikmla@gmail.com>
2023-05-08 14:10:28 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const formInputs = document.querySelectorAll('input');
+
+[...formInputs].forEach(input => {
+  const formLable = document.createElement('label');
+
+  input.placeholder = toCapitalized(input.name);
+  formLable.className = 'field-label';
+  formLable.htmlFor = input.id;
+  formLable.textContent = input.name;
+  input.parentElement.append(formLable);
+});
+
+function toCapitalized(text) {
+  return text[0].toUpperCase() + text.slice(1);
+}
