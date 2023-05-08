e7fb9dfb96132b23e9a33a95a38df0b5dd94b58d
Halyna Rydasova <halyna.rydasova@gmail.com>
2023-05-08 18:58:08 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,13 +4,15 @@ const inputs = document.querySelectorAll('input');
 
 for (const input of inputs) {
   const label = document.createElement('label');
-  const inputName = input.getAttribute('name');
+  let inputName = input.getAttribute('name');
+
+  inputName = inputName.split(/(?=[A-Z])/).map(word => (
+    word.charAt(0).toUpperCase() + word.slice(1))).join(' ');
 
   label.classList.add('field-label');
   label.setAttribute('for', input.getAttribute('id'));
-  label.textContent = inputName.toUpperCase();
+  label.textContent = inputName;
 
-  input.setAttribute('placeholder',
-    inputName.slice(0, 1).toUpperCase() + inputName.slice(1));
+  input.setAttribute('placeholder', inputName);
   input.parentElement.append(label);
 }
