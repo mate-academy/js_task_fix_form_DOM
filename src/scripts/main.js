8b913fc6b84c8154511db16ffac272f5d13b25ab
Yura Enttsel <equikmla@gmail.com>
2023-05-08 16:50:18 +0300

    Fixes after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,13 +5,17 @@ const formInputs = document.querySelectorAll('input');
 [...formInputs].forEach(input => {
   const formLable = document.createElement('label');
 
-  input.placeholder = toCapitalized(input.name);
+  input.placeholder = toFormatText(input.name);
   formLable.className = 'field-label';
   formLable.htmlFor = input.id;
-  formLable.textContent = input.name;
+  formLable.textContent = toFormatText(input.name);
   input.parentElement.append(formLable);
 });
 
-function toCapitalized(text) {
-  return text[0].toUpperCase() + text.slice(1);
+function toFormatText(text) {
+  return text[0].toUpperCase()
+    + text
+      .slice(1)
+      .split('N')
+      .join(' N');
 }
