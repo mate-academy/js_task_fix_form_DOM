3919591de67d85ac894872a004b228a56e582892
Denys Didenko <ddidenko441@gmail.com>
2023-05-12 19:14:03 +0300

    add fix_form solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+inputs.forEach(input => {
+  const label = document.createElement('label');
+
+  input.parentElement.append(label);
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = input.name.toUpperCase();
+
+  input.placeholder = input.name
+    .slice(0, 1).toUpperCase()
+    + input.name.slice(1);
+});
