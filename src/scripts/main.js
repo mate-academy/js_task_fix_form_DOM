12118cc692f96c684f27d066fe03d18f7f8d7346
Vasyl Zhyliakov <snak871@gmail.com>
2023-05-12 18:12:11 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
+const inputs = document.querySelectorAll('input')
+console.log(inputs)
+inputs.forEach(inp => {
+  const label = document.createElement('label');
+  label.setAttribute('for', inp.getAttribute('id'));
+  label.className = 'field-label';
+  label.textContent = getName(inp.getAttribute('name'));
 
-// write code here
+  inp.parentElement.append(label);
+
+  inp.placeholder = getName(inp.getAttribute('name'));
+
+})
+
+function getName(value) {
+  let name = value[0].toUpperCase();
+  for (let i = 1; i < value.length; i++) {
+    if (value[i] === value[i].toLowerCase()) {
+      name += value[i];
+    } else {
+      name += ' ' + value[i].toLowerCase();
+    }
+  }
+
+  return name;
+}
