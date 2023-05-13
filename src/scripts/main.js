a11d966e0261ab423257f8a4549f84b6d34ae08b
Constantin <Constantin.Andreevich.Zhevzhik@gmail.com>
2023-05-13 16:39:10 +0300

    made changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,46 +1,15 @@
 'use strict';
 
-const firstName = document.getElementById('sign-up-name');
-const lastName = document.getElementById('sign-up-last-name');
-const email = document.querySelectorAll('input[name="email"]');
-const password = document.querySelectorAll('input[name="password"]');
+const inputs = document.querySelectorAll('.field-text');
 
-// added first name label
-const firstNameLabel = document.createElement('label');
+inputs.forEach(input => {
+  const label = document.createElement('label');
 
-firstNameLabel.innerHTML = 'First Name';
-firstNameLabel.classList.add('field-label');
-firstName.placeholder = 'First Name';
-firstNameLabel.setAttribute('for', 'sign-up-name');
-firstName.parentNode.insertBefore(firstNameLabel, firstName);
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = input.name.toUpperCase();
 
-// added last name label
-const lastNameLabel = document.createElement('label');
-
-lastNameLabel.innerHTML = 'Last Name';
-lastNameLabel.classList.add('field-label');
-lastName.placeholder = 'Last Name';
-lastNameLabel.setAttribute('for', 'sign-up-last-name');
-lastName.parentNode.insertBefore(lastNameLabel, lastName);
-
-// added email label
-email.forEach((input) => {
-  const emailLabel = document.createElement('label');
-
-  emailLabel.innerHTML = 'Email';
-  emailLabel.classList.add('field-label');
-  input.placeholder = 'Email';
-  emailLabel.setAttribute('for', input.id);
-  input.parentNode.insertBefore(emailLabel, input);
-});
-
-// added password label
-password.forEach((input) => {
-  const passwordLabel = document.createElement('label');
-
-  passwordLabel.innerHTML = 'Password';
-  passwordLabel.classList.add('field-label');
-  input.placeholder = 'Password';
-  passwordLabel.setAttribute('for', input.id);
-  input.parentNode.insertBefore(passwordLabel, input);
+  input.placeholder = input.name.charAt(0).toUpperCase()
+  + input.name.slice(1).toLowerCase();
+  input.parentNode.insertBefore(label, input);
 });
