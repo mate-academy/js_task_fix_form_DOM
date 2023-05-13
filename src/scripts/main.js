c780e2fedee119f6288280558797a7b7a001c65a
Yaroslav Liutyi <slavko5481@gmail.com>
2023-05-13 15:03:03 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const field = document.querySelectorAll('form');
+const inputs = document.querySelectorAll('input');
+
+for (const i of field) {
+  for (const input of inputs) {
+    const label = document.createElement('label');
+
+    label.className = 'field-label';
+    label.setAttribute('for', input.id);
+    label.textContent = input.name;
+
+    input.placeholder = input.name.toUpperCase();
+
+    label.append('field');
+  }
+}
