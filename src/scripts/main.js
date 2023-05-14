d26130f08f46f915d4b1c5beed2e964494463b83
Yaroslav Liutyi <slavko5481@gmail.com>
2023-05-14 14:42:10 +0300

    split inputs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,18 @@
 'use strict';
 
-const field = document.querySelectorAll('form');
 const inputs = document.querySelectorAll('input');
+const textFormat = (value) => {
+  return value[0].toUpperCase() + value.slice(1).split('N').join(' N');
+};
 
-for (const i of field) {
-  for (const input of inputs) {
-    const label = document.createElement('label');
+for (const input of inputs) {
+  const label = document.createElement('label');
 
-    label.className = 'field-label';
-    label.setAttribute('for', input.id);
-    label.textContent = input.name;
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
 
-    input.placeholder = input.name.toUpperCase();
+  input.placeholder = textFormat(input.name);
 
-    label.append('field');
-  }
+  label.append('field');
 }
