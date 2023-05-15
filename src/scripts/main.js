3fb883467ba4ae270ecc0f588b5e2f7e29a790ce
Denys Didenko <ddidenko441@gmail.com>
2023-05-15 12:06:22 +0300

    fix mistakes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,11 +5,11 @@ const inputs = document.querySelectorAll('.field-text');
 inputs.forEach(input => {
   const label = document.createElement('label');
 
-  input.parentElement.append(label);
-
   label.className = 'field-label';
   label.htmlFor = input.id;
   label.textContent = input.name.toUpperCase();
+
+  input.parentElement.append(label);
 
   input.placeholder = input.name
     .slice(0, 1).toUpperCase()
