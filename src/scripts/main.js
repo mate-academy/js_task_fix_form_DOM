7762eddcfe7e21e8958f8a19458f1bf9949d4e0e
Yelyzaveta <molochiza@gmail.com>
2023-05-15 19:44:54 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+function nameFormat(word) {
+  let result = word.slice(0, 1).toUpperCase();
+
+  for (let i = 1; i < word.length; i++) {
+    if ((word[i] !== word[i].toLowerCase())) {
+      result += ' ' + word[i].toLowerCase();
+    } else {
+      result += word[i];
+    }
+  }
+
+  return result;
+}
+
+const inputs = document.querySelectorAll('input');
+
+[...inputs].forEach(input => {
+  const label = document.createElement('label');
+
+  label.className = ['field-label'];
+  label.htmlFor = input.id;
+  label.textContent = nameFormat(input.name).toUpperCase();
+  input.before(label);
+
+  input.placeholder = nameFormat(input.name);
+});
