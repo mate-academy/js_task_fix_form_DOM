1d8248740f4bdd31a93f96fa9af09cc82c6ac41c
Vitalii Drabynko <vpdrabynko@gmail.com>
2023-05-15 19:25:15 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,12 +4,16 @@ const inputs = document.querySelectorAll('.field-text');
 
 inputs.forEach(input => {
   const label = document.createElement('label');
+  const placeholder = input.name.replace(/([a-z])([A-Z])/g, '$1 $2');
 
   label.setAttribute('for', input.id);
   label.classList.add('field-label');
 
-  label.textContent = input.name;
-  input.placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+  label.textContent
+  = input.name.replace(/([a-z])([A-Z])/g, '$1 $2').toUpperCase();
+
+  input.placeholder
+  = placeholder.charAt(0).toUpperCase() + placeholder.slice(1);
 
   input.parentNode.insertBefore(label, input);
 });
