f993e83b4976914527c87a479f0877b3eb465fe7
Artem Kurinniy <artem.kyrinniy@gmail.com>
2023-05-16 20:40:59 +0300

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,17 +3,17 @@
 const fieldsHTML = document.querySelectorAll('.field');
 
 for (const field of fieldsHTML) {
-  const fieldName = field.querySelector('input').name;
+  const inputField = field.querySelector('input');
 
-  field.querySelector('input').setAttribute('placeholder',
-    fieldName.charAt(0).toUpperCase()
+  inputField.setAttribute('placeholder',
+    inputField.name.charAt(0).toUpperCase()
     + field.querySelector('input').name.slice(1).toLowerCase());
 
   const label = document.createElement('label');
 
   label.setAttribute('for', field.querySelector('input').id);
 
-  label.innerText = fieldName.charAt(0).toUpperCase()
-    + fieldName.slice(1).replace(/([A-Z])/g, ' $1').toLowerCase();
+  label.innerText = inputField.name.charAt(0).toUpperCase()
+    + inputField.name.slice(1).replace(/([A-Z])/g, ' $1').toLowerCase();
   field.prepend(label);
 }
