3f1a1cd9e9e12faec58422849c2f883c0d2bd6e2
Artem Kurinniy <artem.kyrinniy@gmail.com>
2023-05-16 17:45:51 +0300

    added labeles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const fieldsHTML = document.querySelectorAll('.field');
+
+for (const field of fieldsHTML) {
+  const label = document.createElement('label');
+
+  label.setAttribute('for', field.querySelector('input').id);
+
+  label.innerText = field.querySelector('input').name.charAt(0).toUpperCase()
+    + field.querySelector('input').name.slice(1).toLowerCase();
+  field.prepend(label);
+}
