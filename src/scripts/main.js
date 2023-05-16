cf11dc7876f8de66af83bbf35a1ef5dce7411ba0
Bohdan Shumylo <injobs.kharkiv@gmail.com>
2023-05-16 21:59:05 +0300

    New Fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,5 @@
 'use strict';
 
-// const inputs = document.querySelectorAll('input.field-text');
-
-// inputs.forEach((input) => {
-//   const label = document.createElement('label');
-
-//   label.className = 'field-label';
-//   label.setAttribute('for', input.id);
-//   label.textContent = input.getAttribute('name');
-
-//   const inputName = input.getAttribute('name');
-
-//   const capitalizedName
-//   = inputName.charAt(0).toUpperCase() + inputName.slice(1);
-
-//   input.setAttribute('placeholder', capitalizedName);
-
-//   input.parentElement.appendChild(label);
-// });
-
 const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 
 const inputs = document.querySelectorAll('input.field-text');
