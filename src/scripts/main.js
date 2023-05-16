da24f4adfbbe6a3e18677f2e0a6138e2bedbeca1
Artem Kurinniy <artem.kyrinniy@gmail.com>
2023-05-16 20:13:30 +0300

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,17 @@
 const fieldsHTML = document.querySelectorAll('.field');
 
 for (const field of fieldsHTML) {
+  const fieldName = field.querySelector('input').name;
+
   field.querySelector('input').setAttribute('placeholder',
-    field.querySelector('input').name.charAt(0).toUpperCase()
+    fieldName.charAt(0).toUpperCase()
     + field.querySelector('input').name.slice(1).toLowerCase());
 
   const label = document.createElement('label');
 
   label.setAttribute('for', field.querySelector('input').id);
-  label.innerText = field.querySelector('input').name.toUpperCase();
+
+  label.innerText = fieldName.charAt(0).toUpperCase()
+    + fieldName.slice(1).replace(/([A-Z])/g, ' $1').toLowerCase();
   field.prepend(label);
 }
