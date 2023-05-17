4ba419139ec76f43bf7fb6b828334674d927e882
Oleksandr Chernikov <pjatochkin2013@gmail.com>
2023-05-17 10:50:13 +0300

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputElements = document.querySelectorAll('input');
+
+inputElements.forEach(function(inputElement) {
+  const labelElement = document.createElement('label');
+
+  labelElement.setAttribute('for', inputElement.id);
+  labelElement.textContent = inputElement.name;
+  inputElement.parentNode.insertBefore(labelElement, inputElement);
+  labelElement.classList.add('field-label');
+
+  const newStr = labelElement.textContent[0].toUpperCase()
+    + labelElement.textContent.slice(1);
+
+  inputElement.placeholder = newStr;
+});
