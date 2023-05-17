7b2a4c141fa23ad98331d03e6ecea1f16a58733b
Oleksandr Chernikov <pjatochkin2013@gmail.com>
2023-05-17 15:23:56 +0300

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,18 @@ inputElements.forEach(function(inputElement) {
   const labelElement = document.createElement('label');
 
   labelElement.setAttribute('for', inputElement.id);
-  labelElement.textContent = inputElement.name;
+
+  let text = '';
+
+  for (let i = 0; i < inputElement.name.length; i++) {
+    if (inputElement.name[i] === inputElement.name[i].toUpperCase()
+      && i !== 0) {
+      text += ' ';
+    }
+    text += inputElement.name[i];
+  }
+
+  labelElement.textContent = text;
   inputElement.parentNode.insertBefore(labelElement, inputElement);
   labelElement.classList.add('field-label');
 
