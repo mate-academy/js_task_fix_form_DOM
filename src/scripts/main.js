c5f527eb0e8a029abca29c18384d6a14f3bac99d
Bogdan Parubchenko <bogdanparubchenko@gmail.com>
2023-05-18 17:28:15 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const value = document.querySelectorAll('input');
+
+function goodPlaceholder(arr) {
+  return `${arr[0]}`.toLocaleUpperCase() + `${arr.slice(1).join('')}`;
+}
+
+function conversion(elements) {
+  for (const element of elements) {
+    const Parent = element.parentElement;
+    const newElement = document.createElement('label');
+
+    newElement.textContent = `${element.getAttribute('name')}`;
+    newElement.className = 'field-label';
+
+    const word = element.getAttribute('name').split('');
+
+    element
+      .setAttribute('placeholder', `${goodPlaceholder(word)}`);
+
+    Parent.prepend(newElement);
+  }
+}
+
+conversion(value);
