2feab5b087c51526cca71d64e98196a1e9ed3e6f
Antonina Klishch <tonya91t@gmail.com>
2023-05-19 16:48:00 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  let placeholder = input.name;
+
+  for (let i = 0; i < placeholder.length; i++) {
+    const lowerName = placeholder.toLocaleLowerCase();
+
+    if (placeholder[i] !== lowerName[i]) {
+      const adaptedPhrase = placeholder.substring(0, i) + ' '
+        + placeholder.slice(i);
+
+      placeholder = adaptedPhrase;
+      i = i + 1;
+    }
+  }
+
+  input.placeholder = placeholder[0].toLocaleUpperCase() + placeholder.slice(1);
+
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+  input.before(label);
+});
