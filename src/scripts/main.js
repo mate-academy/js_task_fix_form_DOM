def502cd0293d0d6fd633dfc909af64af8292ec6
pasternakv19@gmail.com <pasternakv19@gmail.com>
2023-05-22 00:27:22 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const allInput = document.querySelectorAll('input');
+
+for (const input of allInput) {
+  const value = input.name;
+  const firLetter = value.charAt(0).toUpperCase();
+  const res = firLetter + value.slice(1);
+
+  const lab = document.createElement('label');
+
+  lab.for = value;
+  lab.className = 'field-label';
+  lab.textContent = value;
+
+  input.before(lab);
+  input.setAttribute('placeholder', res);
+}
