19a76d24f189f675d2f23881502ef767ad82d444
Elijah Vinkov <illia.vinkov@gmail.com>
2023-05-21 11:11:28 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,10 +6,12 @@ inputs.forEach(input => {
   const label = document.createElement('label');
 
   label.setAttribute('for', input.id);
-  label.textContent = input.name;
+  label.textContent = input.name.replace(/([A-Z])/g, ' $1').trim();
   label.classList.add('field-label');
 
-  input.placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+  input.placeholder
+  = input.name.replace(/([A-Z])/g, ' $1').trim().charAt(0).toUpperCase()
+  + input.name.replace(/([A-Z])/g, ' $1').trim().slice(1);
 
   input.parentElement.appendChild(label);
 });
