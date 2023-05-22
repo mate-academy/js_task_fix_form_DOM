51ba286b0bf2ca20d472889c96412b86ce8686f6
Alessandra Nitti <alessandrannitti@gmail.com>
2023-05-22 22:46:07 +0300

    task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  const labelName = document.createElement('label');
+
+  labelName.className = `field-label`;
+  labelName.setAttribute('for', input.getAttribute('id'));
+  labelName.textContent = input.getAttribute('name');
+
+  input.parentElement.append(labelName);
+  input.placeholder = capitalizeName(input.getAttribute('name'));
+});
+
+function capitalizeName(str) {
+  const separated = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+  const lettersArray = separated.toLowerCase().split('');
+
+  const first = lettersArray[0].toUpperCase();
+  const others = lettersArray.slice(1);
+
+  return first + others.join('');
+}
