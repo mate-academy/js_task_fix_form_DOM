79f1a62d3aa7f0499d03c4618cc8b973bc6ac48e
Jane Shavrukova <eva.shavrukova@gmail.com>
2023-05-25 14:54:19 -0700

    js_task_fix_form_DOM solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+inputs.forEach(input => {
+  input.insertAdjacentHTML('afterend',
+    `<label class="field-label" for="${input.id}">
+       ${input.name}
+     </label>
+  `);
+
+  input.placeholder = capitalize(input.name);
+});
