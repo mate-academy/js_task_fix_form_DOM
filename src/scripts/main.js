5213ceab5c09e6b1ab1c0c8583481adeec07906f
Hanna Balabukha <anna.aniskovets1992@ukr.net>
2023-05-25 17:59:54 +0300

    'fixed'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,15 +17,20 @@ for (const char of inputs) {
 }
 
 function capitalLetter(str) {
-  let x = '';
+  const x = str.split(' ').map((word) =>
+    `${word[0].toUpperCase()}${word.slice(1).toLowerCase()}`);
 
-  for (let i = 0; i < str.length; i++) {
-    if (str[i - 1] === ' ' || i === 0) {
-      x += str[i].toUpperCase();
+  const y = x.join().split('');
+
+  let result = '';
+
+  for (let i = 0; i < y.length; i++) {
+    if (y[i + 1] === 'n') {
+      result += (y[i] + `${' '}`);
     } else {
-      x += str[i];
+      result += y[i];
     }
   }
 
-  return x;
+  return result;
 }
