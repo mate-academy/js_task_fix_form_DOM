aa4ffcb4acdad4f6d269737ee8690c201a212772
Hanna Balabukha <anna.aniskovets1992@ukr.net>
2023-05-25 15:51:16 +0300

    'done'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const char of inputs) {
+  const newlabel = document.createElement('label');
+
+  newlabel.setAttribute('for', char.id);
+
+  newlabel.classList.add('field-label');
+
+  const box = document.querySelector('input').parentNode;
+
+  box.appendChild(newlabel);
+
+  char.setAttribute('placeholder', capitalLetter(char.name));
+}
+
+function capitalLetter(str) {
+  let x = '';
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i - 1] === ' ' || i === 0) {
+      x += str[i].toUpperCase();
+    } else {
+      x += str[i];
+    }
+  }
+
+  return x;
+}
