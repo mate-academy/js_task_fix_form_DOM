ea3203809b3cfceb43204368c92eee3a9839973f
Roma Tsvindin <tsvindin1995@gmail.com>
2023-05-28 15:35:35 +0300

    js_task_fix_form_DOM solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const inputs = document.getElementsByClassName('field-text');
+
+function normalizeString(string) {
+  const result = string.replace(/([A-Z])/g, ' $1');
+  const finalResult = result.charAt(0).toUpperCase() + result.slice(1);
+
+  return finalResult;
+}
+
+function createLabel(input) {
+  const label = document.createElement('label');
+
+  label.textContent = input.getAttribute('name');
+  label.setAttribute('for', input.getAttribute('id'));
+  label.setAttribute('class', 'field-label');
+  input.parentNode.insertBefore(label, input);
+}
+
+for (const input of inputs) {
+  const nameOfInput = input.getAttribute('name');
+
+  createLabel(input);
+  input.setAttribute('placeholder', `${normalizeString(nameOfInput)}`);
+}
