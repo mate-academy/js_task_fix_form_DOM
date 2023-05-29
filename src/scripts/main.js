91c6bcafba8badc2a7d975cf12a7a32ac5873121
Igor Galkin <igor_galkin@ukr.net>
2023-05-29 17:40:58 +0300

    1st commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(elem => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', elem.id);
+  label.textContent = elem.name;
+  elem.setAttribute('placeholder', elem.name.toUpperCase());
+
+  elem.before(label);
+});
