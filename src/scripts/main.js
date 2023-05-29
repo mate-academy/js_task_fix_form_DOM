3aa16e67151edaee8745b3a1a1ffe4b197d45607
Yuri Shkrobut <georgebest2409@gmail.com>
2023-05-29 14:26:26 +0300

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,15 +2,29 @@
 
 const inputs = document.querySelectorAll('input');
 
+function correctLabel(word) {
+  let string = word[0].toUpperCase();
+
+  for (let i = 1; i < word.length; i++) {
+    if (word[i] === word[i].toUpperCase()) {
+      string += ' ' + word[i].toLowerCase();
+      continue;
+    }
+
+    string += word[i];
+  }
+
+  return string;
+}
+
 inputs.forEach(input => {
   const label = document.createElement('label');
   const field = input.parentElement;
-  const firstLetter = input.name[0].toUpperCase();
 
   label.className = 'field-label';
   label.htmlFor = input.id;
-  label.textContent = input.name;
-  input.placeholder = firstLetter + input.name.slice(1);
+  input.placeholder = correctLabel(input.name);
+  label.textContent = input.placeholder.toUpperCase();
 
   field.append(label);
 });
