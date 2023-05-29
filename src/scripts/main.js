a3cd46ff8a7043249f6dc5a8ce76cde5ddbdab48
Nadiia Romanskaya <97957968+NadiiaR@users.noreply.github.com>
2023-05-29 20:05:55 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const formInputs = document.querySelectorAll('form input');
+
+  formInputs.forEach(input => {
+    input.parentElement.insertAdjacentHTML('afterbegin',
+      `
+        <label
+          class="field-label"
+          for="${input.id}"
+        >
+          ${input.name}
+        </label>
+      `
+    );
+
+    input.setAttribute(
+      'placeholder',
+      `${input.name[0].toUpperCase() + input.name.slice(1)}`
+    );
+  });
+});
