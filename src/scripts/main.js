d5d64870c2a95e46d7f9d6b9b1a81807682125a6
Igor Galkin <igor_galkin@ukr.net>
2023-05-29 18:22:10 +0300

    2nd commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,21 @@
 'use strict';
 
+function camelCaseToNormal(str) {
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+    const currentChar = str[i];
+
+    if (currentChar.toUpperCase() === currentChar) {
+      result += ' ';
+    }
+
+    result += currentChar.toUpperCase();
+  }
+
+  return result;
+}
+
 const inputs = document.querySelectorAll('input');
 
 inputs.forEach(elem => {
@@ -7,8 +23,11 @@ inputs.forEach(elem => {
 
   label.className = 'field-label';
   label.setAttribute('for', elem.id);
-  label.textContent = elem.name;
-  elem.setAttribute('placeholder', elem.name.toUpperCase());
+  label.textContent = camelCaseToNormal(elem.name);
+
+  const placeholder = camelCaseToNormal(elem.name);
+
+  elem.setAttribute('placeholder', placeholder);
 
   elem.before(label);
 });
