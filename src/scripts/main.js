33f7b960ad281ca94d367bbf973d30e20c721548
AllaNebozhenko <nebozhenkoalla@gmail.com>
2023-06-03 15:20:19 +0200

    Solution fix form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const allInputs = document.querySelectorAll('.field-text');
+
+for (const input of allInputs) {
+  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+
+  input.insertAdjacentHTML('beforebegin', `
+    <label class='field-label' for='${input.id}'>
+      ${input.name}
+    </label>
+  `);
+}
