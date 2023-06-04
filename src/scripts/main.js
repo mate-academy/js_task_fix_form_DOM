93bba2b004b6b33baf6d6190748a94c55037f3cc
Vasyl Kozak <sasdaiv@gmail.com>
2023-06-04 10:14:57 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,10 +7,17 @@ inputs.forEach(input => {
 
   label.className = 'field-label';
   label.setAttribute('for', input.id);
-  label.textContent = input.name;
 
-  const placeholderText = input.name.charAt(0).toUpperCase()
-    + input.name.slice(1);
+  const indexName = input.name.toLowerCase().indexOf('name');
+
+  label.textContent = indexName !== -1
+    ? input.name.slice(0, indexName)
+      + ' ' + input.name.slice(indexName)
+    : input.name;
+
+  const placeholderText
+  = label.textContent[0].toUpperCase()
+  + label.textContent.slice(1);
 
   input.setAttribute('placeholder', placeholderText);
 
