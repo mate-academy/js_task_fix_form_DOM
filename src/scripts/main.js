2a85c0c81d917f4d5b65f47141946813d0ffd93e
Rufat Mammadov <rufat.mammadov.m@gmail.com>
2023-06-05 23:31:17 +0300

    js_task_fix_form_DOM - first try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  const inputLabel = document.createElement('label');
+  const inputId = input.getAttribute('id');
+  const inputName = input.getAttribute('name');
+
+  inputLabel.classList.add('field-label');
+  inputLabel.setAttribute('for', inputId);
+  inputLabel.innerHTML = inputName;
+  input.setAttribute('placeholder', inputName.toUpperCase());
+
+  input.parentElement.prepend(inputLabel);
+});
