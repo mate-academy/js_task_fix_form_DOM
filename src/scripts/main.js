c1ac4eed97b663f6988ed5d3a46874f3a8275962
Hishchak Tetiana <tanaiosered@ukr.net>
2023-06-05 22:53:01 +0300

    form2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,33 @@
 'use strict';
 
+function camelCase(str) {
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+    const currentChar = str[i];
+
+    if (currentChar.toUpperCase() === currentChar) {
+      result += ' ';
+    }
+
+    result += currentChar.toUpperCase();
+  }
+
+  return result;
+}
+
 const inputs = document.querySelectorAll('input');
 
 for (const input of inputs) {
   input.setAttribute('placeholder', input.name[0].toLocaleUpperCase()
-   + input.name.slice(1));
+   + camelCase(input.name.slice(1)).toLocaleLowerCase());
 
   const item = document.createElement('label');
 
   item.className = 'field-label';
   item.htmlFor = input.id;
-  item.textContent = input.name.toLocaleUpperCase();
+
+  item.textContent = camelCase(input.name);
 
   input.before(item);
 }
