72ade21fe6903f47761f0b4607a3a012517186ac
Volodymyr Kustikov <kustforever@gmail.com>
2023-06-05 11:44:33 +0300

    l

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const myInputs = document.querySelectorAll('input');
+
+function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+for (let input of myInputs) {
+  const touchpad = document.createElement('label');
+  touchpad.className = "field-label";
+  touchpad.htmlFor = `${input.id}`;
+
+  
+  let capitalized = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+  touchpad.textContent = capitalized;
+  input.placeholder = capitalized;
+  insertAfter(input, touchpad);
+}
+
+
+// const isUpperCase = (string) => /^[A-Z]*$/.test(string)
+
+// function capitalize(input) {
+//     let capitalized = '';
+    
+//     for (let i = 0; i < input.name.length; i++) {
+//         if (isUpperCase(input.name[i])) {
+//             capitalized = input.name.slice(0, i-1) + ' ' + input.name.slice(i);
+//         }
+//     }
+    
+//     capitalized = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+    
+//     return capitalized;
+// }
