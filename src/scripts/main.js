727685e9d7e355a523b5c75c4e20fbd040ed3b04
sheva <vladislavshevchenko10@gmail.com>
2023-06-05 21:59:20 +0300

    fixed mistakes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,15 +2,29 @@
 
 const inputsArr = [...document.querySelectorAll('input')];
 
-inputsArr.map((input) => {
+inputsArr.forEach((input) => {
   const label = document.createElement('label');
 
   label.className = 'field-label';
   label.htmlFor = input.id;
 
-  label.textContent = input.name;
+  label.textContent = capitalizeString(input.name);
 
-  input.placeholder = input.name.toUpperCase();
+  input.placeholder = capitalizeString(input.name);
 
   input.parentElement.prepend(label);
 });
+
+function capitalizeString(str) {
+  let string = str;
+  const regex = /[A-Z]/;
+  const match = string.match(regex);
+
+  if (match) {
+    const index = match.index;
+
+    string = string.slice(0, index) + ' ' + string.slice(index);
+  }
+
+  return string.toUpperCase();
+}
