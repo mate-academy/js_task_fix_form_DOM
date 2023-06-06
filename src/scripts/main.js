f22296b247aa4eb59d39016d3982438efc6c013f
Volodymyr Kuzmin <vlkzmn@gmail.com>
2023-06-06 15:11:37 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+const label = document.createElement('label');
+
+for (const item of inputs) {
+  item.placeholder = item.name[0].toUpperCase() + item.name.slice(1);
+
+  label.htmlFor = item.id;
+  label.textContent = item.name.toUpperCase();
+  label.className = 'field-label';
+
+  item.before(label.cloneNode(true));
+}
