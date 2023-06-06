3b590a57eab44916037168eb8ab7d9787e0593b8
Volodymyr Kustikov <kustforever@gmail.com>
2023-06-06 22:12:08 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,34 +3,24 @@
 const myInputs = document.querySelectorAll('input');
 
 function insertAfter(referenceNode, newNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
-for (let input of myInputs) {
+for (const input of myInputs) {
   const touchpad = document.createElement('label');
-  touchpad.className = "field-label";
+
+  touchpad.className = 'field-label';
   touchpad.htmlFor = `${input.id}`;
 
-  
-  let capitalized = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+  console.log(touchpad.htmlFor);
+  console.log(touchpad.className);
+
+  const capitalized = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
   touchpad.textContent = capitalized;
   input.placeholder = capitalized;
+
   insertAfter(input, touchpad);
 }
 
 
-// const isUpperCase = (string) => /^[A-Z]*$/.test(string)
-
-// function capitalize(input) {
-//     let capitalized = '';
-    
-//     for (let i = 0; i < input.name.length; i++) {
-//         if (isUpperCase(input.name[i])) {
-//             capitalized = input.name.slice(0, i-1) + ' ' + input.name.slice(i);
-//         }
-//     }
-    
-//     capitalized = input.name.charAt(0).toUpperCase() + input.name.slice(1);
-    
-//     return capitalized;
-// }
