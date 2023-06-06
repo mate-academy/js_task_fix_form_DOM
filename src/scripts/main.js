63dbee24d7715fec486ab6a4e9113fd0c3cb1a72
Volodymyr Kuzmin <vlkzmn@gmail.com>
2023-06-06 17:41:52 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,11 +5,24 @@ const inputs = document.querySelectorAll('input');
 const label = document.createElement('label');
 
 for (const item of inputs) {
-  item.placeholder = item.name[0].toUpperCase() + item.name.slice(1);
+  item.placeholder = separate(item.name)[0].toUpperCase()
+  + separate(item.name).slice(1);
 
   label.htmlFor = item.id;
-  label.textContent = item.name.toUpperCase();
+  label.textContent = separate(item.name).toUpperCase();
   label.className = 'field-label';
 
   item.before(label.cloneNode(true));
+}
+
+function separate(nameAtribute) {
+  let result = nameAtribute;
+
+  for (const char of result) {
+    if (char.toUpperCase() === char) {
+      result = result.replace(char, ` ${char}`);
+    }
+  }
+
+  return result;
 }
