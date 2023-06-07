64efb56b39cbbf9a009298ea67ad8fa4322e6feb
Mykola Plekan <zikzar@gmail.com>
2023-06-07 16:09:40 +0300

    Fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,7 @@ const fieldLabel = document.createElement('label');
 const input = document.querySelectorAll('input');
 
 [...input].forEach(elem => {
-  elem.placeholder = elem.name.toUpperCase();
+  elem.placeholder = separateWord(elem.name).toUpperCase();
 });
 
 fieldLabel.className = 'field-label';
@@ -16,5 +16,17 @@ fieldLabel.className = 'field-label';
 
 [...parentElem].forEach(elem => {
   elem.lastElementChild.htmlFor = elem.firstElementChild.id;
-  elem.lastElementChild.textContent = elem.firstElementChild.name;
+  elem.lastElementChild.textContent = separateWord(elem.firstElementChild.name);
 });
+
+function separateWord(word) {
+  const upper = word.slice(0, 1).toUpperCase();
+
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === word[i].toUpperCase()) {
+      return upper + word.slice(1, [i]) + ' ' + word.slice(i, word.length);
+    }
+  }
+
+  return upper + word.slice(1, word.length);
+}
