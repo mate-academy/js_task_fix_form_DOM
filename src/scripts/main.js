9f62a12047ce325e9748bc907d9c7433442ef07a
Mykola Plekan <zikzar@gmail.com>
2023-06-07 14:40:12 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const parentElem = document.querySelectorAll('.field');
+const fieldLabel = document.createElement('label');
+const input = document.querySelectorAll('input');
+
+[...input].forEach(elem => {
+  elem.placeholder = elem.name.toUpperCase();
+});
+
+fieldLabel.className = 'field-label';
+
+[...parentElem].forEach(elem => {
+  elem.append(fieldLabel.cloneNode(true));
+});
+
+[...parentElem].forEach(elem => {
+  elem.lastElementChild.htmlFor = elem.firstElementChild.id;
+  elem.lastElementChild.textContent = elem.firstElementChild.name;
+});
