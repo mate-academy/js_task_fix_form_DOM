b885c085362ee54881f9116d047b58cce6518b4a
Vo7kov <jegor.volkov.a@gmail.com>
2023-06-07 17:51:25 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+inputs.forEach(input => {
+  input.insertAdjacentHTML('beforebegin', `
+    <label
+      class="field-label"
+      for="${input.id}"
+    >
+      ${input.name}
+    </label>
+  `);
+
+  input.placeholder = input.name;
+  input.style.cssText = 'text-transform: capitalize;';
+});
