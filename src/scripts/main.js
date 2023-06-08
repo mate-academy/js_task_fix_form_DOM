7b14cc8eaea58f955e9f9155114d20fac889f4bd
Liza Strykharchuk <ol220681vol@gmail.com>
2023-06-08 17:18:58 +0300

    code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const newInput = document.querySelectorAll(`.field-text`);
+
+for (const input of newInput) {
+  const label = document.createElement('label');
+
+  label.setAttribute('class', `field-label`);
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+
+  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+
+  input.parentNode.insertBefore(label, input.nextSibling);
+}
