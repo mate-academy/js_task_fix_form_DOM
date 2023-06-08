d61dfe22493c6c3ffb443c9c1bf05baf4025e78e
Nataliia Koreniuk <koreniuknataliia@gmail.com>
2023-06-08 20:10:06 +0300

    fixed after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,12 +3,28 @@
 const inputs = document.querySelectorAll('input');
 const inputsArray = [...inputs];
 
+function createString(str) {
+  let newStr = '';
+
+  if (/[A-Z]/.test(str)) {
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === str[i].toUpperCase()) {
+        newStr += `${str.slice(0, i)} ${str.slice(i)}`;
+      }
+    }
+  } else {
+    newStr += str;
+  }
+
+  return newStr.toUpperCase();
+}
+
 for (const input of inputsArray) {
   const myLabel = document.createElement('label');
 
   myLabel.className = 'field-label';
   myLabel.setAttribute('for', input.id);
-  myLabel.innerText = input.name;
+  myLabel.innerText = createString(input.name);
   input.before(myLabel);
-  input.setAttribute('placeholder', input.name.toUpperCase());
+  input.setAttribute('placeholder', createString(input.name));
 }
