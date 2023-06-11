c5cc39eda1e82e27f7eeb3c9d64e1791b94dbab2
Rufat Mammadov <rufat.mammadov.m@gmail.com>
2023-06-11 22:25:46 +0300

    requested changes were made

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,13 +5,28 @@ const inputs = document.querySelectorAll('input');
 
 inputs.forEach(input => {
   const inputLabel = document.createElement('label');
-  const inputId = input.getAttribute('id');
-  const inputName = input.getAttribute('name');
+  const inputId = input.id;
+  const inputName = input.name;
 
   inputLabel.classList.add('field-label');
   inputLabel.setAttribute('for', inputId);
-  inputLabel.innerHTML = inputName;
-  input.setAttribute('placeholder', inputName.toUpperCase());
+  inputLabel.textContent = inputName;
+
+  if (inputName.includes('Name')) {
+    let index = 0;
+
+    for (let i = 1; i < inputName.length; i++) {
+      if (inputName[i] === inputName[i].toUpperCase()) {
+        index = i;
+        break;
+      }
+    }
+
+    input.setAttribute('placeholder', inputName.slice(0, index).toUpperCase()
+    + ' ' + inputName.slice(index).toUpperCase());
+  } else {
+    input.setAttribute('placeholder', inputName.toUpperCase());
+  }
 
   input.parentElement.prepend(inputLabel);
 });
