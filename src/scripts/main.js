f046f082118164b3c4a3df7ebd4e1a27ddc8c229
Rufat Mammadov <rufat.mammadov.m@gmail.com>
2023-06-12 23:11:11 +0300

    words separated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,27 +6,25 @@ const inputs = document.querySelectorAll('input');
 inputs.forEach(input => {
   const inputLabel = document.createElement('label');
   const inputId = input.id;
-  const inputName = input.name;
+  let index = 0;
+
+  for (let i = 1; i < input.name.length; i++) {
+    if (input.name[i] === input.name[i].toUpperCase()) {
+      index = i;
+      break;
+    }
+  }
+
+  const inputName = input.name.includes('Name')
+    ? input.name.slice(0, index).toUpperCase()
+    + ' ' + input.name.slice(index).toUpperCase()
+    : input.name.toUpperCase();
 
   inputLabel.classList.add('field-label');
   inputLabel.setAttribute('for', inputId);
   inputLabel.textContent = inputName;
 
-  if (inputName.includes('Name')) {
-    let index = 0;
-
-    for (let i = 1; i < inputName.length; i++) {
-      if (inputName[i] === inputName[i].toUpperCase()) {
-        index = i;
-        break;
-      }
-    }
-
-    input.setAttribute('placeholder', inputName.slice(0, index).toUpperCase()
-    + ' ' + inputName.slice(index).toUpperCase());
-  } else {
-    input.setAttribute('placeholder', inputName.toUpperCase());
-  }
+  input.setAttribute('placeholder', inputName);
 
   input.parentElement.prepend(inputLabel);
 });
