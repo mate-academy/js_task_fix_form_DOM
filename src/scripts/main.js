401a52abbebbc35bc356f39af93dd0b614d60e5a
Hanna Anishyna <ann.986@gmail.com>
2023-06-19 14:18:17 -0700

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,26 +2,15 @@
 
 const inputAll = document.querySelectorAll('input');
 
-const addLabel = [...inputAll].forEach(input => {
+return [...inputAll].forEach(input => {
   const label = document.createElement('label');
   const idAttribute = input.getAttribute('id');
   const inputName = input.getAttribute('name');
   const parentContainer = input.parentElement;
 
-  label.classList.add('field-label');
-  label.setAttribute('for', idAttribute);
-  label.textContent = inputName;
-
-  parentContainer.append(label);
-
-  return addLabel;
-});
-
-const addPlaceholder = [...inputAll].forEach(input => {
-  const inputName = input.getAttribute('name');
   let modifiedName = '';
 
-  modifiedName += inputName.charAt(0).toUpperCase();
+  modifiedName += inputName[0].toUpperCase();
 
   for (let i = 1; i < inputName.length; i++) {
     const char = inputName[i];
@@ -34,7 +23,10 @@ const addPlaceholder = [...inputAll].forEach(input => {
     }
   }
 
-  input.setAttribute('placeholder', modifiedName);
+  label.classList.add('field-label');
+  label.setAttribute('for', idAttribute);
+  label.textContent = modifiedName;
 
-  return addPlaceholder;
+  parentContainer.append(label);
+  input.setAttribute('placeholder', modifiedName);
 });
