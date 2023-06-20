38abe20137ddf24d38cedc7bcaaf0c9a60670ebb
Hanna Anishyna <ann.986@gmail.com>
2023-06-20 15:02:13 -0700

    fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,25 +2,29 @@
 
 const inputAll = document.querySelectorAll('input');
 
-return [...inputAll].forEach(input => {
-  const label = document.createElement('label');
-  const idAttribute = input.getAttribute('id');
-  const inputName = input.getAttribute('name');
-  const parentContainer = input.parentElement;
-  let modifiedName = inputName[0].toUpperCase();
+function addAttributes() {
+  return [...inputAll].forEach(input => {
+    const label = document.createElement('label');
+    const idAttribute = input.getAttribute('id');
+    const inputName = input.getAttribute('name');
+    const parentContainer = input.parentElement;
+    let modifiedName = inputName[0].toUpperCase();
 
-  for (let i = 1; i < inputName.length; i++) {
-    const char = inputName[i];
+    for (let i = 1; i < inputName.length; i++) {
+      const char = inputName[i];
 
-    char === char.toUpperCase()
-      ? modifiedName += ' ' + char.toLowerCase()
-      : modifiedName += char;
-  }
+      modifiedName += char === char.toUpperCase()
+        ? ' ' + char.toLowerCase()
+        : char;
+    }
 
-  label.classList.add('field-label');
-  label.setAttribute('for', idAttribute);
-  label.textContent = modifiedName;
+    label.classList.add('field-label');
+    label.setAttribute('for', idAttribute);
+    label.textContent = modifiedName;
 
-  parentContainer.append(label);
-  input.setAttribute('placeholder', modifiedName);
-});
+    parentContainer.append(label);
+    input.setAttribute('placeholder', modifiedName);
+  });
+}
+
+addAttributes();
