7cc3fae23a1d76866fe73274c8148b5c6701726b
Hanna Anishyna <ann.986@gmail.com>
2023-06-20 14:28:01 -0700

    fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,19 +7,14 @@ return [...inputAll].forEach(input => {
   const idAttribute = input.getAttribute('id');
   const inputName = input.getAttribute('name');
   const parentContainer = input.parentElement;
-  let modifiedName = '';
-
-  modifiedName += inputName[0].toUpperCase();
+  let modifiedName = inputName[0].toUpperCase();
 
   for (let i = 1; i < inputName.length; i++) {
     const char = inputName[i];
 
-    if (char === char.toUpperCase()) {
-      modifiedName += ' ';
-      modifiedName += char.toLowerCase();
-    } else {
-      modifiedName += char;
-    }
+    char === char.toUpperCase()
+      ? modifiedName += ' ' + char.toLowerCase()
+      : modifiedName += char;
   }
 
   label.classList.add('field-label');
