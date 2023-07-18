a233d636028021eba84d72937dc2c8f59feed919
Lukasz Warda <lukaszwarda1989@gmail.com>
2023-07-18 15:52:24 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (let i = 0; i < inputs.length; i++) {
+  document.createElement('label');
+
+  const input = inputs[i];
+  const parentInput = input.parentNode;
+  const label = document.createElement('label');
+
+  label.setAttribute('class', 'field-label');
+  label.setAttribute('for', input.getAttribute('id'));
+  label.textContent = input.getAttribute('name');
+
+  let placeholder = label.textContent.charAt(0).toUpperCase();
+
+  placeholder += label.textContent.slice(1);
+
+  input.setAttribute('placeholder', placeholder);
+
+  parentInput.insertBefore(label, parentInput.firstChild);
+}
