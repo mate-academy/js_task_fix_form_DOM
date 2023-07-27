33546a3e7d661f56fabb8de61a762fbe6a82b159
Karolina <karolinabednarek888@gmail.com>
2023-07-27 12:51:28 +0200

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,19 @@
 'use strict';
 
-const forms = document.querySelectorAll('form');
+const inputs = document.querySelectorAll('input');
 
 function capitalize(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
 
-forms.forEach(form => {
-  const inputs = form.querySelectorAll('input');
+inputs.forEach(input => {
+  const label = document.createElement('label');
 
-  inputs.forEach(input => {
-    const label = document.createElement('label');
+  label.setAttribute('for', input.id);
+  label.classList.add('field-label');
+  label.textContent = capitalize(input.name);
 
-    label.setAttribute('for', input.id);
-    label.classList.add('field-label');
-    label.textContent = capitalize(input.name);
+  input.setAttribute('placeholder', capitalize(input.name));
 
-    input.setAttribute('placeholder', capitalize(input.name));
-
-    input.parentNode.insertBefore(label, input);
-  });
+  input.parentNode.insertBefore(label, input);
 });
