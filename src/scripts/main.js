5d9a538472ee91039cda5638337f622af933f007
GGsmou <svyatoslav.golovin.2005@gmail.com>
2023-08-01 16:37:08 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  const label = `
+    <label
+      for="${input.id}"
+      class="field-label"
+    >
+      ${input.name}
+    </label>
+  `;
+
+  input.insertAdjacentHTML('beforebegin', label);
+
+  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+});
