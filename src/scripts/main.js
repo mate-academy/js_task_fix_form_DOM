71e0136e4fe4f53e9f93f50d6e7fd99ef360d468
GGsmou <svyatoslav.golovin.2005@gmail.com>
2023-08-13 20:24:43 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,16 +3,32 @@
 const inputs = document.querySelectorAll('input');
 
 inputs.forEach(input => {
+  const nameArray = input.name.split('');
+  let newName = '';
+
+  for (const char of nameArray) {
+    if (newName.length === 0) {
+      newName += char.toUpperCase();
+      continue;
+    }
+
+    if (char === char.toUpperCase()) {
+      newName += ' ';
+    }
+
+    newName += char;
+  }
+
   const label = `
     <label
       for="${input.id}"
       class="field-label"
     >
-      ${input.name}
+      ${newName}
     </label>
   `;
 
   input.insertAdjacentHTML('beforebegin', label);
 
-  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+  input.placeholder = newName;
 });
