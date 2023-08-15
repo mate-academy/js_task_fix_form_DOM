ebed59fa496bb7d39eaa3cd34b91a7e821e91828
Edgar Kocuba <edgarkocuba@gmail.com>
2023-08-15 17:42:03 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,21 +3,15 @@
 // write code here
 const inputs = document.querySelectorAll('input');
 
-for (let i = 0; i < inputs.length; i++) {
-  const parentDiv = inputs[i].parentNode;
+inputs.forEach(item => {
+  const parentDiv = item.parentNode;
   const label = document.createElement('label');
-  const id = inputs[i].getAttribute('id');
-  const placeholder = inputs[i].getAttribute('name')
-    .split('')
-    .map(a => a === a.toUpperCase()
-      ? `-${a}`
-      : a)
-    .join('')
-    .replace('-', ' ');
+  const id = item.getAttribute('id');
+  const placeholder = item.getAttribute('name').replace(/(?=[A-Z])/, ' ');
 
-  label.innerHTML = placeholder;
+  label.textContent = placeholder;
   label.setAttribute('class', 'field-label');
   label.setAttribute('for', id);
-  inputs[i].setAttribute('placeholder', placeholder.toUpperCase());
+  item.setAttribute('placeholder', placeholder.toUpperCase());
   parentDiv.prepend(label);
-}
+});
