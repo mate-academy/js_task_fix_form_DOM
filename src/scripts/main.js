ff98bc3c9ca5a6c738a888be820f372612940789
Dmytro Loboda <dmytro.loboda@gmail.com>
2023-08-24 02:53:02 +0800

    Auth form fix solition

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', function() {
+  const forms = document.querySelectorAll('form');
+
+  forms.forEach(form => {
+    const inputs = form.querySelectorAll('input');
+
+    inputs.forEach(input => {
+      const inputName = input.getAttribute('name');
+      const label = document.createElement('label');
+
+      label.setAttribute('class', 'field-label');
+      label.setAttribute('for', input.id);
+
+      label.textContent
+        = inputName.charAt(0).toUpperCase() + inputName.slice(1);
+
+      input.setAttribute('placeholder',
+        inputName
+          .charAt(0)
+          .toUpperCase() + inputName.slice(1));
+
+      input.parentNode.insertBefore(label, input);
+    });
+  });
+});
