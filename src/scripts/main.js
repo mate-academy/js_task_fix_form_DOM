8abdc18efc163ec69614b64696e89d078eef0ba1
Volodymyr Zadorozhnyi <Torsida@gmail.com>
2023-08-28 20:59:33 +0300

    first version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write code here
+
+const inputs = document.querySelectorAll('form input');
+
+inputs.forEach(input => {
+  const label = document.createElement('label');
+
+  label.setAttribute('for', input.id);
+
+  label.classList.add('field-label');
+
+  label.textContent = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  input.placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  input.parentElement.appendChild(label);
+});
