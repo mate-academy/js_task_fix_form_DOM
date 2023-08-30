6eaa870871ffb8c78ffc79b2537dc0dcb268ee07
Dmytro Lazarenko <lazarenkodmytro@gmail.com>
2023-08-30 21:57:50 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,9 +8,11 @@ inputs.forEach(input => {
   label.classList.add('field-label');
   label.setAttribute('for', input.id);
 
-  label.textContent = input.name;
+  const formattedName = input.name.charAt(0).toUpperCase()
+  + input.name.slice(1).replace(/([A-Z])/g, ' $1').trim();
 
-  input.placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+  label.textContent = formattedName;
+  input.setAttribute('placeholder', formattedName);
 
-  input.parentNode.appendChild(label);
+  input.parentNode.insertBefore(label, input);
 });
