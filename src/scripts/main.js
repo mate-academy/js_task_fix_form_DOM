8900dae4017eea141b247512b5fc98f66a53fd1f
Ivan <your@mail.com>
2023-09-20 00:48:02 +0300

    Rework Capitalize words

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,14 +6,38 @@ const field = document.querySelectorAll('.field');
 
 [...field].forEach((subject, index) => {
   const createLabel = document.createElement('label');
+  let correctName = [...input][`${index}`].name;
+
+  if ([...input][`${index}`].name.includes('Name')) {
+    const nameIndex = [...input][`${index}`].name.indexOf('N');
+    const firstPart = correctName.slice(0, `${nameIndex}`);
+    const secondPart = correctName.slice(`${nameIndex}`);
+
+    correctName = `${firstPart.toUpperCase()} ${secondPart.toUpperCase()}`;
+  }
 
   createLabel.className = 'field-label';
   createLabel.htmlFor = [...input][`${index}`].id;
-  createLabel.textContent = [...input][`${index}`].name.toUpperCase();
+  createLabel.textContent = correctName;
 
   subject.appendChild(createLabel);
 });
 
-[...input].forEach(item => {
-  item.placeholder = item.name;
+[...input].forEach((item, index) => {
+  let correctPlaceholder = [...input][`${index}`].name;
+
+  if ([...input][`${index}`].name.includes('Name')) {
+    const nameIndex = [...input][`${index}`].name.indexOf('N');
+    const firstPart = correctPlaceholder.slice(0, `${nameIndex}`);
+    const secondPart = correctPlaceholder.slice(`${nameIndex}`);
+
+    correctPlaceholder = `${firstPart} ${secondPart.toLowerCase()}`;
+  }
+
+  const firstLetter = correctPlaceholder.slice(0, 1);
+  const goodPart = correctPlaceholder.slice(1);
+
+  correctPlaceholder = firstLetter.toUpperCase() + goodPart;
+
+  item.placeholder = correctPlaceholder;
 });
