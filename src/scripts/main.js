c0e113a42c672f29ee0a2c8fb341a1bd734b7f2c
Ivan <your@mail.com>
2023-09-19 17:35:23 +0300

    Rework form ready!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write code here
+const input = document.querySelectorAll('input');
+const field = document.querySelectorAll('.field');
+
+[...field].forEach((subject, index) => {
+  const createLabel = document.createElement('label');
+
+  createLabel.className = 'field-label';
+  createLabel.htmlFor = [...input][`${index}`].id;
+  createLabel.textContent = [...input][`${index}`].name.toUpperCase();
+
+  subject.appendChild(createLabel);
+});
+
+[...input].forEach(item => {
+  item.placeholder = item.name;
+});
