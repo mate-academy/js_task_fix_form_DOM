68db1f0721cadef14ec7162b5f53274025d2c380
AndruhaMan <andreysut12@gmail.com>
2023-09-24 16:00:21 +0300

    The task is completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.htmlFor = input.id;
+  label.textContent = input.name.toUpperCase();
+  label.className = 'field-label';
+
+  input.placeholder = capitalize(input.name);
+  input.insertAdjacentElement('beforebegin', label);
+}
+
+function capitalize(text) {
+  return text[0].toUpperCase() + text.slice(1).toLowerCase();
+}
