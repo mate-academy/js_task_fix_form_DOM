d3cd0d719c35d01c8b9b87975607ec662dcfa2ec
AndriiYelieva <andrew.yelieva@gmail.com>
2023-10-12 19:02:50 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputsInsideForm = document.querySelectorAll('form input');
+
+inputsInsideForm.forEach(element => {
+  const label = document.createElement('label');
+  const elementPlaceholder = element.name[0].toUpperCase()
+    + element.name.slice(1);
+
+  label.htmlFor = element.id;
+  label.classList.add('field-label');
+  label.innerText = replaceToCamelCase(element.name);
+
+  element.placeholder = replaceToCamelCase(elementPlaceholder);
+  element.parentElement.prepend(label);
+});
+
+function replaceToCamelCase(inputName) {
+  return inputName.replace(/([a-z])([A-Z])/g, '$1 $2');
+}
