8332e9c068e2ce57ad7eb601ffbe59d319f3b57e
Andrii Rykal <andreuka79@gmail.com>
2023-10-13 19:47:07 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+function firstUpper(str) {
+  return !str ? str : str[0].toUpperCase() + str.slice(1);
+}
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = input.name.toUpperCase();
+  input.placeholder = firstUpper(input.name);
+  input.before(label);
+}
