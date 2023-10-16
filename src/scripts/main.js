e603edd3a48e630f471af3f7dc3c8cfeb9d7d6cf
Kristina Bekher <bekherkr@gmail.com>
2023-10-16 21:11:04 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 // write code here
+function capitalize(string) {
+  return string.slice(0, 1).toUpperCase() + string.slice(1);
+}
+
+const inputElements = document.querySelectorAll('input');
+
+inputElements.forEach(element => {
+  const labelElement = document.createElement('label');
+
+  labelElement.classList.add('field-label');
+  labelElement.htmlFor = element.id;
+  labelElement.textContent = element.name;
+
+  element.parentNode.appendChild(labelElement);
+
+  element.setAttribute('placeholder', capitalize(element.name));
+});
