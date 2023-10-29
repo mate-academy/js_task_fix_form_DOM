b5a63ee12c4584f64f2b1fc6c9035ff26ef431df
alex <cebanoleksandr@gmail.com>
2023-10-29 09:31:48 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+function fixForm(inputs) {
+  inputs.forEach(input => {
+    const label = document.createElement('label');
+
+    label.textContent = input.name;
+    label.setAttribute('for', input.id);
+    input.placeholder = input.name;
+
+    input.insertAdjacentElement('beforebegin', label);
+  });
+}
+
+fixForm(inputs);
