c945846a6055593ad90690a495bc8c8219da38dc
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2023-11-03 18:12:03 +0100

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,7 +11,7 @@ for (const input of inputs) {
   const labelElement = document.createElement('label');
 
   input.before(labelElement);
-  input.setAttribute('placeholder', `${input.name}`);
+  input.setAttribute('placeholder', capitalize(input.name));
   input.setAttribute('autocapitalize', 'words');
   labelElement.classList.add('field-label');
   labelElement.setAttribute('for', `${input.id}`);
