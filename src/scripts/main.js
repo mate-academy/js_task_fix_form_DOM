5304cacf98b37e6d4cb2a43a9debc37cb712dc73
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2023-11-03 17:51:21 +0100

    correction

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,11 @@
 
 const inputs = document.querySelectorAll('input');
 
+function capitalize(string) {
+  return (string.slice(0, 1).toUpperCase() + string.slice(1))
+    .replace(/([A-Z])/g, ' $1').trim();
+}
+
 for (const input of inputs) {
   const labelElement = document.createElement('label');
 
@@ -10,5 +15,5 @@ for (const input of inputs) {
   input.setAttribute('autocapitalize', 'words');
   labelElement.classList.add('field-label');
   labelElement.setAttribute('for', `${input.id}`);
-  labelElement.textContent = `${input.name}`;
+  labelElement.textContent = capitalize(input.name);
 }
