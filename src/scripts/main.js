de411394664f5c1928c9f1b9b111bd25fc231d38
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2023-11-03 17:19:18 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const labelElement = document.createElement('label');
+
+  input.before(labelElement);
+  input.setAttribute('placeholder', `${input.name}`);
+  input.setAttribute('autocapitalize', 'words');
+  labelElement.classList.add('field-label');
+  labelElement.setAttribute('for', `${input.id}`);
+  labelElement.textContent = `${input.name}`;
+}
