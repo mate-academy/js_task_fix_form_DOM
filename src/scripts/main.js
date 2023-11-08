8713a1b0154ce2c9d311eb53987339a1af560e31
Damian <damian.adamczewski92@gmail.com>
2023-11-08 12:45:29 +0100

    add capitalize and separate, label/placeholder

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,12 +2,28 @@
 
 const inputs = document.querySelectorAll('.field-text');
 
+function firstUp(str) {
+  if (!str) {
+    return str;
+  }
+
+  let result = str[0].toUpperCase() + str.slice(1);
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] === str[i].toUpperCase()) {
+      result = result.slice(0, i) + ' ' + result.slice(i);
+    }
+  }
+
+  return result;
+}
+
 for (const input of inputs) {
   const label = document.createElement('label');
 
   label.className = 'field-label';
-  label.textContent = input.name;
+  label.textContent = firstUp(input.name);
   label.htmlFor = input.id;
   input.after(label);
-  input.placeholder = input.name.toUpperCase();
+  input.placeholder = firstUp(input.name);
 }
