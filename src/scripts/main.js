08a8e0a760c3f4bf267e0a00ae9f79e37b059674
Liliya Kalinichenko <fedotova.liliya90@gmail.com>
2023-11-13 17:34:23 +0200

    label fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,16 +3,16 @@
 const inputElements = document.querySelectorAll('input');
 
 [...inputElements].forEach(input => {
-  input.placeholder = capitalize(input.name);
+  input.placeholder = normalizeCamelCase(input.name);
 
   input.insertAdjacentHTML('beforebegin',
     `<label class="field-label" for="${input.id}">
-      ${input.name}
+      ${normalizeCamelCase(input.name)}
     </label>`
   );
 });
 
-function capitalize(str) {
+function normalizeCamelCase(str) {
   const result = str.replace(/([A-Z])/g, ' $1');
 
   return result.charAt(0).toUpperCase() + result.slice(1);
