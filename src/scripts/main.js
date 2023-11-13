f7892473ca6cecd43e3a651ceb0c1a1664ff6d8d
Marek Maciejewski <marekmaciejewski1982@gmail.com>
2023-11-13 10:10:37 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+const capitalizeCamelCase = (string) => {
+  if (!string) {
+    return;
+  }
+
+  return string[0].toUpperCase()
+        + string.slice(1).replace(/([a-z])([A-Z])/g, '$1 $2');
+};
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.htmlFor = input.id;
+  label.innerText = capitalizeCamelCase(input.name);
+  input.before(label);
+  input.placeholder = capitalizeCamelCase(input.name);
+}
