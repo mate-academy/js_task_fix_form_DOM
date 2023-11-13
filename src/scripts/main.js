f83a06da8c999605b0f8955a4d1b213bb578201e
Liliya Kalinichenko <fedotova.liliya90@gmail.com>
2023-11-13 17:03:00 +0200

    task solution was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const inputElements = document.querySelectorAll('input');
+
+[...inputElements].forEach(input => {
+  input.placeholder = capitalize(input.name);
+
+  input.insertAdjacentHTML('beforebegin',
+    `<label class="field-label" for="${input.id}">
+      ${input.name}
+    </label>`
+  );
+});
+
+function capitalize(str) {
+  const result = str.replace(/([A-Z])/g, ' $1');
+
+  return result.charAt(0).toUpperCase() + result.slice(1);
+}
