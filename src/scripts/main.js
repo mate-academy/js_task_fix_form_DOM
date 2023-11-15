8093305c53cb6df91e71ea648fdeb68120e24a46
spencerspe <spencerspe@gmail.com>
2023-11-15 09:15:03 +0000

    task finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write code here
+const inputs = document.querySelectorAll('input');
+
+const inputsAsArray = Array.from(inputs);
+
+inputsAsArray.forEach(entry => {
+  const label = document.createElement('label');
+
+  const placeHolder = entry.name.charAt(0).toLocaleUpperCase()
+  + entry.name.toLocaleLowerCase().slice(1);
+
+  entry.placeholder = placeHolder;
+
+  label.classList.add(`field-label`);
+
+  label.setAttribute('for', entry.id);
+
+  label.insertAdjacentText('afterbegin', entry.name);
+
+  entry.before(label);
+});
