59a752353065dc38ce09f2e03fc0af816483ead9
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2023-11-24 13:51:18 +0100

    small placeholder update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,20 @@
 'use strict';
 
-// creating list of inputs
 const listOfInputs = document.querySelectorAll('input');
-
-// creating list of parents conteiners
 const listOfParents = document.getElementsByClassName('field');
 
 listOfInputs.forEach((el, i) => {
-  // capitalize name from input
-  const fieldName = el.name.charAt(0).toUpperCase() + el.name.slice(1);
+  let fieldName = el.name.replace(/([A-Z])/g, ' $1');
 
-  // adding placeholder to input
+  fieldName = fieldName.charAt(0).toUpperCase() + fieldName.slice(1);
+
   el.placeholder = fieldName;
 
-  // creating label
   const label = document.createElement('label');
 
-  // setting attributs and conntent for label
   label.className = 'field-label';
   label.textContent = el.name.replace(/([A-Z])/g, ' $1').toUpperCase();
   label.setAttribute('for', el.id);
 
-  // adding label to parent element of input
   listOfParents[i].insertBefore(label, el);
 });
