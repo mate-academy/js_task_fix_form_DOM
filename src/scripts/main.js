888b7bb3389372e1944a4aaeddf7675ddc9409dd
Vladyslav Kolisnyk <vadyslavkolisnyk20@gmail.com>
2023-11-27 16:11:51 +0200

    add task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+function capitalize(string) {
+  return (string.slice(0, 1).toUpperCase() + string.slice(1))
+    .replace(/([A-Z])/g, ' $1').trim();
+}
+
+const inputElements = document.querySelectorAll('input');
+
+inputElements.forEach(element => {
+  const labelElement = document.createElement('label');
+
+  labelElement.classList.add('field-label');
+  labelElement.htmlFor = element.id;
+  labelElement.textContent = capitalize(element.name);
+
+  element.parentNode.appendChild(labelElement);
+
+  element.setAttribute('placeholder', capitalize(element.name));
+});
