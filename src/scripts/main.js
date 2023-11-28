9c75a20d7e073b18a5a973fd0062985945458c99
Dominika Karmel <dkarmel@op.pl>
2023-11-28 13:33:53 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const forms = document.querySelectorAll('form');
+
+forms.forEach(form => {
+  const inputs = form.querySelectorAll('input');
+
+  inputs.forEach(input => {
+    const label = document.createElement('label');
+
+    label.classList.add('field-label');
+    label.setAttribute('for', input.id);
+
+    label.textContent = input.name.charAt(0)
+      .toUpperCase() + input.name.slice(1);
+
+    input.placeholder = input.name.charAt(0)
+      .toUpperCase() + input.name.slice(1);
+
+    input.parentElement.appendChild(label);
+  });
+});
