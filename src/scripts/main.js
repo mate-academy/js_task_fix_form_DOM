fd6798e8dee02ce2aee6b365e2c1121dce5adc41
donizer <bogdan.ua45@gmail.com>
2023-12-03 19:52:33 +0200

    split words

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,26 @@
 'use strict';
 
+function camelCaseToArray(inputString = '') {
+  const wordsArray = inputString.split(/(?=[A-Z])/);
+  const lowercaseArray = wordsArray.map((word) => word.toLowerCase());
+
+  return lowercaseArray;
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   const inputs = document.querySelectorAll('form input');
 
   inputs.forEach(function(input) {
     const label = document.createElement('label');
+    const newTextContent = camelCaseToArray(input.name)
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
 
     label.className = 'field-label';
     label.setAttribute('for', input.id);
 
-    label.textContent
-      = input.name.charAt(0).toUpperCase() + input.name.slice(1);
-
-    input.placeholder
-      = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+    label.textContent = newTextContent;
+    input.placeholder = newTextContent;
 
     input.parentNode.appendChild(label);
   });
