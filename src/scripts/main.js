930fc4a50763836eef4784cca5235402e9ebb522
donizer <bogdan.ua45@gmail.com>
2023-12-03 15:45:13 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', function() {
+  const inputs = document.querySelectorAll('form input');
+
+  inputs.forEach(function(input) {
+    const label = document.createElement('label');
+
+    label.className = 'field-label';
+    label.setAttribute('for', input.id);
+
+    label.textContent
+      = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+    input.placeholder
+      = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+    input.parentNode.appendChild(label);
+  });
+});
