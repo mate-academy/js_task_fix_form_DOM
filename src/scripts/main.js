65020162075d3d2824f805e9da8aad86248091fe
Nataliia Henda <nataliehenda1296@gmail.com>
2023-12-11 19:11:11 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+inputs.forEach(node => {
+  const labelName = node.getAttribute('name');
+  const inputLabel = document.createElement('label');
+  const labelFor = node.getAttribute('id');
+
+  inputLabel.className = 'field-label';
+  inputLabel.setAttribute('for', labelFor);
+  inputLabel.textContent = labelName;
+
+  node.after(inputLabel);
+
+  node.placeholder = labelName
+    .slice(0, 1)
+    .toUpperCase() + labelName.slice(1);
+});
