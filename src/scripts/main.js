0c67e89d2212ebcf4e614fbaca79c478da70f402
Wojtek <wojciechsadocha@gmail.com>
2023-12-15 19:47:56 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,15 +5,28 @@ function capitalize(string) {
   return string[0].toUpperCase() + string.slice(1);
 }
 
+function makeRealWords(string) {
+  if (string.toLowerCase() === string) {
+    return string;
+  }
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i].toLowerCase() !== string[i]) {
+      return string.split(`${string[i]}`).join(` ${string[i]}`);
+    }
+  }
+}
+
 const inputs = document.querySelectorAll('input');
 
 for (const input of inputs) {
   const labelElement = document.createElement('label');
+  const inputName = makeRealWords(input.name);
 
-  labelElement.textContent = input.name;
+  labelElement.textContent = inputName;
   labelElement.setAttribute('for', input.id);
   labelElement.setAttribute('class', 'field-label');
   input.before(labelElement);
 
-  input.setAttribute('placeholder', capitalize(input.name));
+  input.setAttribute('placeholder', capitalize(inputName));
 }
