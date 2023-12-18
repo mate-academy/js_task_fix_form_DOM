41ff086de592b8c0297c34446333aec51fae1a05
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-12-18 21:35:04 +0200

    Revert "fixed form"
    This reverts commit 592483e55bc92db19285a5e1764f6e3ec61ed123.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,3 @@
 'use strict';
 
-const inputs = document.querySelectorAll('.field-text');
-
-inputs.forEach(input => {
-  const names = input.name.split(/(?=[A-Z])/).join(' ');
-
-  input.insertAdjacentHTML('beforebegin',
-    `<label for="${input.id}" class="field-label">
-      ${names}
-    </label>
-  `);
-
-  input.placeholder = `${names[0].toUpperCase() + names.slice(1)}`;
-});
+// write code here
