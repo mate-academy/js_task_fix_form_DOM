6bc76eca6d2bb7f90cf9afc1eacd2fcbbec131c6
Rodionsav <rodionsav@mate.academy>
2023-12-19 19:25:32 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  input.placeholder = input.name
+    .split(/(?=[A-Z])/)
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
+
+  const labelElement = document.createElement('label');
+
+  labelElement.textContent = input.placeholder;
+  labelElement.className = 'field-label';
+
+  input.parentNode.insertBefore(labelElement, input);
+});
