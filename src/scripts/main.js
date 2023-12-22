411c4f7d2c751a42d929a0c2d94d703d6c18b23a
Oksana Vasyliuk <oksana.vasyliuk13@gmail.com>
2023-12-22 14:30:16 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+inputs.forEach(input => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.textContent = splitCamelCase(input.name);
+
+  input.setAttribute('placeholder', splitCamelCase(input.name));
+  input.parentElement.append(label);
+});
+
+function splitCamelCase(camelWords) {
+  const splited = camelWords.replace(/([a-z])([A-Z])/g, '$1 $2');
+
+  return splited[0].toUpperCase() + splited.slice(1);
+}
