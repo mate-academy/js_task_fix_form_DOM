3d6f9472d04ee5fbfd67977fb672ae8587ba3941
Dmytro Fefelov <reseneweb@gmail.com>
2023-12-30 15:14:16 +0200

    Develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const fields = document.querySelectorAll('.field');
+
+fields.forEach(field => {
+  const label = document.createElement('label');
+
+  field.children[0].setAttribute('placeholder',
+    field.children[0].name.charAt(0).toUpperCase()
+    + field.children[0].name.slice(1));
+
+  label.className = 'field-label';
+  label.htmlFor = field.children[0].id;
+  label.textContent = field.children[0].name;
+  field.append(label);
+});
