7ceef337eaef233b87de24b9181a72a7c54dfef6
UlianaChorna <110450537+UlianaChorna@users.noreply.github.com>
2024-01-02 15:30:42 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const labelElement = document.createElement('label');
+
+  labelElement.className = 'field-label';
+  labelElement.textContent = getFirstLetterToUpper(input.name);
+  labelElement.setAttribute('for', input.id);
+
+  input.setAttribute('placeholder', getFirstLetterToUpper(input.name));
+
+  input.parentElement.append(labelElement);
+}
+
+function getFirstLetterToUpper(word) {
+  return word.charAt(0).toUpperCase()
+  + word.slice(1);
+}
