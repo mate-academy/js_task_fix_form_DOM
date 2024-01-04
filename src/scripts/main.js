995138b50644b5860cb708dd1a3991676182a97e
Kseniia Chepur <kseniia.chepur@gmail.com>
2024-01-04 13:16:40 +0200

    add logic for separating words in placeholder and textContent

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,10 +4,19 @@ const inputElements = document.querySelectorAll('input');
 
 for (const input of inputElements) {
   const label = document.createElement('label');
+
   label.classList = 'field-label';
   label.setAttribute('for', `${input.id}`);
   label.textContent = `${input.name}`;
+
+  if (label.textContent.includes('Name')) {
+    label.textContent = label.textContent.replace('Name', ' name');
+  }
   input.parentNode.append(label);
 
   input.setAttribute('placeholder', `${input.name.toUpperCase()}`);
+
+  if (input.placeholder.includes('NAME')) {
+    input.placeholder = input.placeholder.replace('NAME', ' NAME');
+  }
 };
