396d2b6d0746dec692d8e9c32b1eb4e106e1c7d9
Vovchuk <kulivackijvoloda@gmail.com>
2024-01-04 17:18:45 +0200

    add

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+inputs.forEach(input => {
+  const labelName = input.getAttribute('name');
+  const labelFor = input.getAttribute('id');
+
+  const label = document.createElement('lable');
+
+  label.className = 'field-label';
+  label.textContent = labelName;
+  label.setAttribute('for', labelFor);
+
+  input.after(label);
+
+  input.placeholder = labelName
+    .slice(0, 1)
+    .toUpperCase() + labelName.slice(1);
+});
