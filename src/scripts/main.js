92355343a365e1e34a5483ddd2a832c35e9e098e
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-01-07 18:28:01 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+
+  // Adding the label to the parent container of the input
+  const parentContainer = input.parentNode;
+
+  parentContainer.appendChild(label);
+});
