01f174fe873716c31d2541d8e80a8bd284da1511
purplefade <tarovykanton@gmail.com>
2024-01-13 14:03:17 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write code here
+const inputElements = document.getElementsByTagName('input');
+const inputElementsArray = Array.from(inputElements);
+
+inputElementsArray.forEach(function(input) {
+  const label = document.createElement('label');
+
+  label.setAttribute('for', input.id);
+
+  label.classList.add('field-label');
+
+  label.textContent = input.name;
+
+  input.parentNode.appendChild(label);
+
+  input.placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+});
