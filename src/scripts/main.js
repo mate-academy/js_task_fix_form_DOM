ce79b78990aa337f829e3901a27c7e732ee3d6dd
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2024-01-15 20:53:17 +0200

    split text

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,16 +2,24 @@
 
 const inputElements = document.getElementsByTagName('input');
 
+function getCorrectStr(str) {
+  const newStr = str.split(/(?=[A-Z])/).join(' ');
+
+  return newStr;
+}
+
 [...inputElements].forEach((input) => {
   const { name: str } = input;
 
-  input.placeholder = str.charAt(0).toUpperCase() + str.slice(1);
+  const validStr = getCorrectStr(str);
+
+  input.placeholder = validStr.charAt(0).toUpperCase() + validStr.slice(1);
 
   const label = document.createElement('label');
 
   label.className = 'field-label';
   label.htmlFor = input.id;
-  label.textContent = str.toUpperCase();
+  label.textContent = validStr.toUpperCase();
 
   input.parentElement.append(label);
 });
