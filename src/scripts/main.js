c0d4db25e80c2ac82c40453b24017f889a0e25c3
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2024-01-19 19:53:05 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const inputsList = document.querySelectorAll('.field-text');
+
+inputsList.forEach(element => {
+  element.insertAdjacentHTML('beforebegin',
+    `<label class="field-label" for=${element.id}>${element.name}</label>`);
+});
+
+inputsList.forEach(element => {
+  element.setAttribute('placeholder',
+    `${element.name[0].toUpperCase() + element.name.slice(1)}`);
+});
