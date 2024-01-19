a637eb99d4c6b0119cb69835245034ff3fe6f27f
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2024-01-19 21:01:09 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,12 +2,24 @@
 
 const inputsList = document.querySelectorAll('.field-text');
 
-inputsList.forEach(element => {
-  element.insertAdjacentHTML('beforebegin',
-    `<label class="field-label" for=${element.id}>${element.name}</label>`);
-});
+function divideWords(word) {
+  let result;
+
+  if (word.includes('Name')) {
+    result = word.slice(0, -4) + ' Name';
+
+    return result;
+  }
+
+  return word;
+}
 
 inputsList.forEach(element => {
+  element.insertAdjacentHTML('beforebegin',
+    `<label class="field-label" for=${element.id}>
+      ${divideWords(element.name)}
+    </label>`);
+
   element.setAttribute('placeholder',
-    `${element.name[0].toUpperCase() + element.name.slice(1)}`);
+    `${divideWords(element.name[0].toUpperCase() + element.name.slice(1))}`);
 });
