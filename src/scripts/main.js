87719ee9bdea1eff39d0b7467c938c7d4c88e788
AgnieszkaWw <agnieszkawie82@onet.pl>
2024-01-23 09:34:44 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((input) => {
+  const idElement = input.getAttribute('id');
+  const nameElement = input.getAttribute('name');
+  const inputLabel = document.ctreateElement('label');
+
+  inputLabel.textContent = nameElement;
+  inputLabel.className = 'field-label';
+  inputLabel.setAttribute('for', idElement);
+  input.after(inputLabel);
+
+  input.placeholder
+  = nameElement.charAt(0).toUpperCase() + nameElement.slice(1);
+});
