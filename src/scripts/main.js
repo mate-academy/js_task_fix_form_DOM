d633bc6ae24620204615d4fa99084778af58ff93
AgnieszkaWw <agnieszkawie82@onet.pl>
2024-01-23 16:25:56 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,33 @@
 'use strict';
 
+const normalizeName = (currentName) => {
+  let newName = '';
+
+  for (const ch of currentName) {
+    if (ch === ch.toLocaleUpperCase()) {
+      newName += ' ' + ch;
+    } else {
+      newName += ch;
+    }
+  }
+
+  newName = newName[0].toLocaleUpperCase() + newName.slice(1);
+
+  return newName;
+};
+
 const inputs = document.querySelectorAll('input');
 
-inputs.forEach((input) => {
-  const idElement = input.getAttribute('id');
-  const nameElement = input.getAttribute('name');
-  const inputInLabel = document.createElement('label');
+inputs.forEach(input => {
+  const label = document.createElement('label');
+  const newName = normalizeName(input.name);
 
-  inputInLabel.textContent = nameElement;
-  inputInLabel.className = 'field-label';
-  inputInLabel.setAttribute('for', idElement);
-  input.after(inputInLabel);
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
+  label.textContent = newName;
+  input.setAttribute('placeholder', newName);
 
-  input.placeholder
-= nameElement.slice(0, 1).toUpperCase() + nameElement.slice(1);
+  const parentContainer = input.parentNode;
+
+  parentContainer.appendChild(label);
 });
