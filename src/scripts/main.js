29aeaf8b2ee5c843d8f877befe483bef95d365da
AgnieszkaWw <agnieszkawie82@onet.pl>
2024-01-23 14:44:57 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,12 +5,12 @@ const inputs = document.querySelectorAll('input');
 inputs.forEach((input) => {
   const idElement = input.getAttribute('id');
   const nameElement = input.getAttribute('name');
-  const inputLabel = document.ctreateElement('label');
+  const inputLabel = document.createElement('label');
 
   inputLabel.textContent = nameElement;
   inputLabel.className = 'field-label';
   inputLabel.setAttribute('for', idElement);
-  input.after(inputLabel);
+  input.insertAdjacentElement('afterend', inputLabel);
 
   input.placeholder
   = nameElement.charAt(0).toUpperCase() + nameElement.slice(1);
