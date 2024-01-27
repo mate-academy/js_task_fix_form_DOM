ede6a1c5d98cda54a41ce50d1fd217a13e68a5c8
Lubomur Chuchvara <lubomircucvara@gmail.com>
2024-01-27 18:05:20 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const inputsList = document.querySelectorAll('.field-text');
+
+function divideWords(word) {
+  let result;
+
+  if (word.includes('Name')) {
+    result = word.slice(0, -4) + ' Name';
+
+    return result;
+  }
+
+  return word;
+}
+
+inputsList.forEach(element => {
+  element.insertAdjacentHTML('beforebegin',
+    `<label class="field-label" for=${element.id}>
+      ${divideWords(element.name)}
+    </label>`);
+
+  element.setAttribute('placeholder',
+    `${divideWords(element.name[0].toUpperCase() + element.name.slice(1))}`);
+});
