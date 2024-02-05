dabb3c0486f9a319b37ecf7059909f64c26dfd34
Maryna Kyrychynska <m_kirichok@ukr.net>
2024-02-05 14:55:39 +0200

    solutiuon

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+function capitalizeName(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const inputs = [...document.querySelectorAll('input')];
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+
+  input.placeholder = capitalizeName(input.name);
+  input.parentNode.appendChild(label);
+}
