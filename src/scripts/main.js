96b4e82f9eb153317f351c84120f49130e351a00
Wita-Shchurko <vitaliya.mememe@gmail.com>
2024-02-09 14:10:35 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const inputsList = document.querySelectorAll('input');
+
+function fixInputs(list) {
+  list.forEach(input => {
+    const inputName = input.getAttribute('name');
+    const inputId = input.getAttribute('id');
+    const newName = inputName.slice(0, 1).toUpperCase() + inputName.slice(1);
+
+    input.setAttribute('placeholder', newName);
+
+    const label = document.createElement('label');
+
+    label.setAttribute('for', inputId);
+    label.textContent = inputName.toUpperCase();
+
+    input.before(label);
+  });
+}
+
+fixInputs(inputsList);
