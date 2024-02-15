1d9a0999e19c35797fddc13d97ab68fc7004a90e
Dmitriy Matvichuk <dmytromatviichuk1979@gmail.com>
2024-02-15 12:05:16 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+[...inputs].forEach(input => {
+  const label = document.createElement('label');
+  const capitalizedInputName = input.name.charAt(0).toUpperCase()
+    + input.name.slice(1);
+
+  label.textContent = capitalizedInputName;
+  label.classList.add('field-label');
+  label.htmlFor = input.id;
+  input.parentNode.insertBefore(label, input.nextSibling);
+  input.placeholder = capitalizedInputName;
+});
