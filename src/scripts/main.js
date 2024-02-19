6b34949aca3d43c41695eb95583a6166cf8c89e5
Horizon-git <apbreloaded947@gmail.com>
2024-02-19 17:16:32 +0200

    completed task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const inputElements = document.getElementsByTagName('input');
+
+[...inputElements].forEach((input) => {
+  const inputName = camelCaseToWords(input.name);
+
+  input.placeholder = inputName.at(0).toUpperCase() + inputName.slice(1);
+
+  const labelInput = document.createElement('label');
+  labelInput.htmlFor = input.id;
+  labelInput.textContent = inputName.toUpperCase();
+
+  input.insertAdjacentElement('beforebegin', labelInput);
+});
+
+function camelCaseToWords(inputStr) {
+  return inputStr.replace(/([a-z])([A-Z])/g, '$1 $2').toLowerCase();
+}
