32de2bc4d38d01d92bc5d04be27fe94bc60f4d80
Justin <thedarkvibe1@gmail.com.com>
2024-03-11 21:02:55 +0100

    task complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('form').forEach((form) => {
+  form.querySelectorAll('div.field > input').forEach((input) => {
+    const fieldName = input.getAttribute('name');
+    const fieldId = input.getAttribute('id');
+
+    input.setAttribute(
+      'placeholder',
+      fieldName.slice(0, 1).toUpperCase() + fieldName.slice(1),
+    );
+
+    const label = document.createElement('label');
+
+    label.classList = 'field-label';
+    label.setAttribute('for', fieldId);
+    label.textContent = fieldName.toUpperCase();
+
+    input.parentNode.insertBefore(label, input);
+  });
+});
