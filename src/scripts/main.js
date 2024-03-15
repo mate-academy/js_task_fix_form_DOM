8c7f31c414a740963773da2972a5485559f82d8e
Denys Lytvyn <ddl@lytvyn.com>
2024-03-15 23:36:48 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((item) => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = item.id;
+  label.textContent = item.name.charAt(0).toUpperCase() + item.name.slice(1);
+
+  item.insertAdjacentElement('beforebegin', label);
+
+  item.placeholder = item.name.charAt(0).toUpperCase() + item.name.slice(1);
+});
