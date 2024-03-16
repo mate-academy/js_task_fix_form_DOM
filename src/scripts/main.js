936a689e77b1396f5d438d05d169788050472f17
Denys Lytvyn <ddl@lytvyn.com>
2024-03-16 12:58:44 +0200

    problem fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,11 +6,17 @@ const inputs = document.querySelectorAll('input');
 inputs.forEach((item) => {
   const label = document.createElement('label');
 
+  const words = item.name.split(/(?=[A-Z])/);
+
+  const labelText = words
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+
   label.className = 'field-label';
   label.htmlFor = item.id;
-  label.textContent = item.name.charAt(0).toUpperCase() + item.name.slice(1);
+  label.textContent = labelText;
 
   item.insertAdjacentElement('beforebegin', label);
 
-  item.placeholder = item.name.charAt(0).toUpperCase() + item.name.slice(1);
+  item.placeholder = labelText;
 });
