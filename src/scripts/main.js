2c63270b512c070de36098202643f8aee83c3818
Semen Sokolovskyi <anori.fjord@gmail.com>
2024-03-26 14:07:05 +0200

    fixed form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((inp) => {
+  const label = document.createElement('label');
+  let nameElement = inp.name;
+
+  nameElement = nameElement[0].toUpperCase() + nameElement.slice(1);
+
+  if (nameElement.includes('Name')) {
+    nameElement = nameElement.replace('Name', ' Name');
+  }
+
+  label.htmlFor = inp.id;
+  label.innerHTML = nameElement.toUpperCase();
+  label.classList = 'field-label';
+
+  inp.placeholder = nameElement;
+  inp.insertAdjacentElement('beforebegin', label);
+});
