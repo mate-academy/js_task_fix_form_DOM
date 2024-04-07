a5c1cd1bfe2947c0016073e6bdf3a72177b31c42
vadymboichenkoAcupower <vadym.boichenko@acupower.co.uk>
2024-04-07 18:12:49 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const froms = document.querySelectorAll('form');
+
+froms.forEach((element) => {
+  const inputs = element.querySelectorAll('input');
+
+  inputs.forEach((input) => {
+    const label = document.createElement('label');
+
+    label.setAttribute('class', 'field-label');
+    label.setAttribute('for', input);
+    label.textContent = input.name;
+
+    let inputName = input.name;
+
+    inputName =
+      inputName.charAt(0).toUpperCase() + inputName.slice(1).toLowerCase();
+
+    input.setAttribute('placeholder', inputName);
+
+    input.parentElement.appendChild(label);
+  });
+});
