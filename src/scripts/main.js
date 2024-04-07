3e6e0d2081593488e0c634ee76c6dbf6d66602aa
vadymboichenkoAcupower <vadym.boichenko@acupower.co.uk>
2024-04-07 20:56:13 +0300

    update firstName and lastName

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 'use strict';
 
 const froms = document.querySelectorAll('form');
@@ -10,12 +11,33 @@ froms.forEach((element) => {
 
     label.setAttribute('class', 'field-label');
     label.setAttribute('for', input);
-    label.textContent = input.name;
+
+    if (input.name === 'firstName') {
+      label.textContent = input.name.slice(0, 4) + ' ' + input.name.slice(5);
+    } else if (input.name === 'lastName') {
+      label.textContent = input.name.slice(0, 3) + ' ' + input.name.slice(4);
+    } else {
+      label.textContent = input.name;
+    }
 
     let inputName = input.name;
 
-    inputName =
-      inputName.charAt(0).toUpperCase() + inputName.slice(1).toLowerCase();
+    if (inputName === 'firstName') {
+      inputName =
+        inputName.charAt(0).toUpperCase() +
+        inputName.slice(1, 5).toLowerCase() +
+        ' ' +
+        inputName.slice(5).toLowerCase();
+    } else if (inputName === 'lastName') {
+      inputName =
+        inputName.charAt(0).toUpperCase() +
+        inputName.slice(1, 4).toLowerCase() +
+        ' ' +
+        inputName.slice(4).toLowerCase();
+    } else {
+      inputName =
+        inputName.charAt(0).toUpperCase() + inputName.slice(1).toLowerCase();
+    }
 
     input.setAttribute('placeholder', inputName);
 
