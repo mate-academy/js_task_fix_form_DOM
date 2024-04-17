3f149cbce03dca3d64038d85f0654607a35240d5
Bohdan Liutyi <liutyibohdan13@gmail.com>
2024-04-17 21:09:53 +0300

    fixed form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((element) => {
+  const label = document.createElement('label');
+  let nameElement = element.name;
+
+  nameElement = nameElement[0].toUpperCase() + nameElement.slice(1);
+
+  if (nameElement.includes('Name')) {
+    nameElement = nameElement.replace('Name', ' Name');
+  }
+
+  label.htmlFor = element.id;
+  label.innerHTML = nameElement.toUpperCase();
+  label.classList = 'field-label';
+
+  element.placeholder = nameElement;
+  element.insertAdjacentElement('beforebegin', label);
+});
