1ad7ffbf0a38800af92f98856a5e275f36ecd856
Marta Chobaniuk <marta.chobaniuk@gmail.com>
2024-04-18 16:19:30 +0300

    updated inputs name

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,16 +3,20 @@
 const inputs = document.querySelectorAll('input');
 
 inputs.forEach((input) => {
+  const inputName = input.getAttribute('name');
+  let capitalizedInputName =
+    inputName.charAt(0).toUpperCase() + inputName.slice(1);
+
+  if (capitalizedInputName.includes('Name')) {
+    capitalizedInputName = capitalizedInputName.replace('Name', ' Name');
+  }
+
   const label = document.createElement('label');
 
   label.setAttribute('class', 'field-label');
-  label.setAttribute('for', input.getAttribute('name'));
-
-  const inputName = input.getAttribute('name');
-  const capitalizedInputName =
-    inputName.charAt(0).toUpperCase() + inputName.slice(1);
-
+  label.setAttribute('for', inputName);
   label.textContent = capitalizedInputName;
+
   input.setAttribute('placeholder', capitalizedInputName);
   input.parentNode.appendChild(label);
 });
