d8859504a64a2c34f94f0d100b7224e04234b828
Marta Chobaniuk <marta.chobaniuk@gmail.com>
2024-04-18 14:46:07 +0300

    fixed form DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.setAttribute('class', 'field-label');
+  label.setAttribute('for', input.getAttribute('name'));
+
+  const inputName = input.getAttribute('name');
+  const capitalizedInputName =
+    inputName.charAt(0).toUpperCase() + inputName.slice(1);
+
+  label.textContent = capitalizedInputName;
+  input.setAttribute('placeholder', capitalizedInputName);
+  input.parentNode.appendChild(label);
+});
