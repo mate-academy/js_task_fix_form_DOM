af4a64f864638d5f2ee25683779d27a93e8e5304
Nazar Pastuschak <kns19pna@gmail.com>
2024-04-23 15:51:21 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+
+  label.setAttribute('for', input.id);
+
+  input.insertAdjacentElement('afterend', label);
+
+  const placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  input.setAttribute('placeholder', placeholder);
+
+  label.textContent = placeholder;
+
+  input.parentElement.appendChild(label);
+});
