ffacb66a6a94b3ea3e68b7b7e96838f7f6b6896d
Volodymyr Shepel <web.shepel@gmail.com>
2024-04-27 15:59:43 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+function capitalize(word = '') {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+inputs.forEach((el) => {
+  const id = el.id;
+  // eslint-disable-next-line no-shadow
+  const name = el.name;
+  const parentNode = el.parentNode;
+  const label = document.createElement('label');
+
+  label.htmlFor = id;
+  label.textContent = name;
+  label.className = 'field-label';
+
+  parentNode.append(label);
+  el.placeholder = capitalize(name);
+});
