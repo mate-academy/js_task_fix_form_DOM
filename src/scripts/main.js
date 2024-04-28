a878daf5a51b21fa3706ffb9386bba6171dec755
Orest Kozak <orestkozak17@gmail.com>
2024-04-28 16:27:30 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const forms = document.querySelectorAll('form');
+
+  forms.forEach((form) => {
+    const inputs = form.querySelectorAll('input');
+
+    inputs.forEach((input) => {
+      const label = document.createElement('label');
+
+      label.setAttribute('class', 'field-label');
+      label.setAttribute('for', input.id);
+      label.textContent = capitalize(input.getAttribute('name'));
+
+      input.setAttribute('placeholder', capitalize(input.getAttribute('name')));
+
+      const parentInput = input.parentNode;
+
+      parentInput.insertBefore(label, input.nextSibling);
+    });
+  });
+});
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
