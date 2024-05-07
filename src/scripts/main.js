904b93f5de3b31122b3201c9744d7cad61ea3b3f
Denis Hurskiy <denis.gurskiy@gmail.com>
2024-05-07 11:23:15 +0200

    divide

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,12 +4,13 @@ const inputs = document.querySelectorAll('input');
 
 inputs.forEach((input) => {
   const inputName = input.getAttribute('name');
+  const placeholder = inputName.charAt(0).toUpperCase() + inputName.slice(1);
   const label = document.createElement('label');
 
   label.classList.add('field-label');
   label.setAttribute('for', input.id);
   label.textContent = inputName.toUpperCase();
-  input.placeholder = inputName.charAt(0).toUpperCase() + inputName.slice(1);
+  input.placeholder = placeholder.replace(/([a-z])([A-Z])/g, '$1 $2');
 
   input.before(label);
 });
