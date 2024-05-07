2322e1e557a427f38d3833a142f9db3ad26c52f7
Denis Hurskiy <denis.gurskiy@gmail.com>
2024-05-07 11:08:47 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((input) => {
+  const inputName = input.getAttribute('name');
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
+  label.textContent = inputName.toUpperCase();
+  input.placeholder = inputName.charAt(0).toUpperCase() + inputName.slice(1);
+
+  input.before(label);
+});
