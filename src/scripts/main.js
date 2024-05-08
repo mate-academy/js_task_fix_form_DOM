94e3a27ef4b211fadc253066924ac23e7569dbde
Oleksandr Podvoiskyi <podvoyskiy01@gmail.com>
2024-05-08 19:02:07 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+  input.placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+  input.parentNode.insertBefore(label, input);
+});
