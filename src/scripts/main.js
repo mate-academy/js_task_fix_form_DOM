1bc7feff8a44bc3dcceab87b07182bf29fc3086a
Oleksandr Podvoiskyi <podvoyskiy01@gmail.com>
2024-05-09 23:40:04 +0300

    fix requested changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,7 +7,10 @@ inputs.forEach((input) => {
 
   label.className = 'field-label';
   label.htmlFor = input.id;
-  label.textContent = input.name;
-  input.placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+  label.textContent = input.name.replace(/([a-z0-9])([A-Z])/g, '$1 $2');
+
+  input.placeholder = (
+    input.name.charAt(0).toUpperCase() + input.name.slice(1)
+  ).replace(/([a-z0-9])([A-Z])/g, '$1 $2');
   input.parentNode.insertBefore(label, input);
 });
