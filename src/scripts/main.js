651c54a59019f7e623ded172fdff4c78c13eba8e
SylwiaNG <iwon9@poczta.onet.pl>
2024-05-19 23:02:32 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const form = document.querySelector('form');
+const inputs = form.querySelectorAll('input');
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.setAttribute('class', 'field-label');
+  label.setAttribute('for', input.getAttribute('name'));
+
+  label.textContent = input.getAttribute('name');
+
+  const capitalizedInputName =
+    input.getAttribute('name').charAt(0).toUpperCase() +
+    input.getAttribute('name').slice(1);
+
+  input.setAttribute('placeholder', capitalizedInputName);
+
+  input.parentNode.appendChild(label);
+});
