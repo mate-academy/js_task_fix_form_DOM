f056ddade2d7db5550959d636bb50a50506c5097
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-05-21 10:38:46 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputData = document.querySelectorAll('input');
+
+inputData.forEach((input) => {
+  const label = document.createElement('label');
+
+  const labelName = input.name;
+
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
+  label.textContent = labelName;
+
+  const namePlaceholder =
+    labelName.charAt(0).toUpperCase() + labelName.slice(1).toLowerCase();
+
+  input.placeholder = namePlaceholder;
+
+  input.parentNode.insertBefore(label, input);
+});
