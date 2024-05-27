273080fba94f302026d19f8b03177b1d63bc8dbd
Dmytro Levchenko <levchenkod91@gmail.com>
2024-05-27 10:46:16 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+const textTransform = (text) => {
+  let result = text.slice(0, 1).toUpperCase() + text.slice(1);
+
+  for (let i = 1; i < text.length; i++) {
+    if (text[i] !== text[i].toLowerCase()) {
+      result = result.replace(text[i], ` ${text[i]}`);
+    }
+  }
+
+  return result;
+};
+
+const createLables = (items) => {
+  items.forEach((item) => {
+    const lable = document.createElement('label');
+    const itemName = item.getAttribute('name');
+
+    lable.innerText = itemName;
+    lable.setAttribute('for', item.id);
+    lable.classList.add('field-label');
+    item.setAttribute('placeholder', textTransform(itemName));
+    item.before(lable);
+  });
+};
+
+createLables(inputs);
