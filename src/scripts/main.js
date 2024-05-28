fe084323267d46b90274a37560826151b144fd7d
Vlad Kach <kachevl@gmail.com>
2024-05-28 12:24:36 +0300

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const inputsCollection = document.querySelectorAll('input');
+
+inputsCollection.forEach((item) => {
+  const newLabel = document.createElement('label');
+
+  item.insertAdjacentElement('beforebegin', newLabel);
+  newLabel.setAttribute('class', 'field-label');
+  newLabel.setAttribute('for', item.id);
+  newLabel.setAttribute('textContent', item.name);
+  item.setAttribute('placeholder', item.name.toUpperCase());
+});
