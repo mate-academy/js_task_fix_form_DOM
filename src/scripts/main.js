fffd814be1f3aaec59fd4168a119a9e0322d40ce
zozulia.kirill <kirillzozulya95@gmail.com>
2024-05-28 14:31:02 +0300

    implement task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const inputElements = [...document.querySelectorAll('.field-text')];
+
+inputElements.forEach((el) => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', el.id);
+  label.textContent = el.name.toUpperCase();
+
+  el.before(label);
+
+  el.setAttribute('placeholder', el.name);
+});
