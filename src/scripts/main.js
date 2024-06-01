e81f6016fdca11cc566e37c1533a0a2bca7e4362
Krzysztof <krzysiek.gugala@gmail.com>
2024-06-01 15:30:48 +0200

    solutionV3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const inputs = Array.from(
+  document.querySelector('.login-wrap').querySelectorAll('input'),
+);
+
+inputs.forEach((input) => {
+  const labelElement = document.createElement('label');
+
+  labelElement.setAttribute('for', input.id);
+
+  const inputName =
+    input.name.charAt(0).toUpperCase() + input.name.slice(1).toLowerCase();
+
+  labelElement.textContent = inputName;
+  labelElement.classList.add('field-label');
+
+  input.parentNode.insertBefore(labelElement, input);
+
+  input.setAttribute('placeholder', inputName);
+});
