7506e79b1e625ee9beff51b16a04dd52971484fa
Yevhenii <polotnyak2004@gmail.com>
2024-06-04 16:51:54 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,24 @@
 'use strict';
 
+const separate = (param) => {
+  switch (param.toLowerCase()) {
+    case 'firstname':
+    case 'lastname':
+      return param.split('Name').join('') + ' Name';
+    default:
+      return param;
+  }
+};
+
 [...document.querySelectorAll('input')].forEach((input) => {
-  input.placeholder =
-    input.name.charAt(0).toUpperCase() + input.name.slice(1).toLowerCase();
+  input.placeholder = separate(
+    input.name.charAt(0).toUpperCase() + input.name.slice(1),
+  );
 
   const label = document.createElement('label');
 
   label.classList.add('field-label');
-  label.textContent = input.getAttribute('name');
+  label.textContent = separate(input.getAttribute('name'));
   label.htmlFor = input.getAttribute('id');
 
   input.insertAdjacentElement('beforebegin', label);
