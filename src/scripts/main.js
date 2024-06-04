2661f01cdd7ca057de75adecf277356b97a10bd7
Olexandr Rapin <alexrapin04@gmail.com>
2024-06-04 15:53:26 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write code here
+const inputs = [...document.querySelectorAll('input')];
+
+// const labels = inputs.map((inp) => inp.dataset.name);
+
+inputs.forEach((inp) => {
+  inp.setAttribute(
+    'placeholder',
+    inp.getAttribute('name').slice(0, 1).toUpperCase() +
+      inp.getAttribute('name').slice(1),
+  );
+
+  const elem = document.createElement('label');
+
+  elem.textContent = inp.getAttribute('name');
+  elem.classList.add('field-label');
+  elem.htmlFor = inp.getAttribute('id');
+
+  inp.insertAdjacentElement('beforebegin', elem);
+});
