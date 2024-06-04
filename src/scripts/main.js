a14bf9947f9b9419ac81fb6705954dde63dfccca
Olexandr Rapin <alexrapin04@gmail.com>
2024-06-04 16:53:18 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,28 @@
 'use strict';
 
 // write code here
-const inputs = [...document.querySelectorAll('input')];
+const separate = (n) => {
+  if (n.toLowerCase() === 'firstname' || n.toLowerCase() === 'lastname') {
+    return n.split('Name').join('') + ' Name';
+  } else {
+    return n;
+  }
+};
 
-// const labels = inputs.map((inp) => inp.dataset.name);
+const inputs = [...document.querySelectorAll('input')];
 
 inputs.forEach((inp) => {
   inp.setAttribute(
     'placeholder',
-    inp.getAttribute('name').slice(0, 1).toUpperCase() +
-      inp.getAttribute('name').slice(1),
+    separate(
+      inp.getAttribute('name').slice(0, 1).toUpperCase() +
+        inp.getAttribute('name').slice(1),
+    ),
   );
 
   const elem = document.createElement('label');
 
-  elem.textContent = inp.getAttribute('name');
+  elem.textContent = separate(inp.getAttribute('name'));
   elem.classList.add('field-label');
   elem.htmlFor = inp.getAttribute('id');
 
