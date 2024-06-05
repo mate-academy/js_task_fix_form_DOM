78766bdff7c6c52889eb379665eecdbb14ceb72d
deezhizyu <kalenskuja@gmail.com>
2024-06-05 18:46:35 +0200

    make build

---
+++ dist/index.ba4fd875.js
@@ -0,0 +1,2 @@
+var e=document.querySelectorAll("input"),l=!0,r=!1,t=void 0;try{for(var a,n=e[Symbol.iterator]();!(l=(a=n.next()).done);l=!0){var o=a.value,i=document.createElement("label");i.classList.add("field-label"),i.setAttribute("for",o.id),o.before(i),o.placeholder=o.name}}catch(e){r=!0,t=e}finally{try{l||null==n.return||n.return()}finally{if(r)throw t}}
+//# sourceMappingURL=index.ba4fd875.js.map
