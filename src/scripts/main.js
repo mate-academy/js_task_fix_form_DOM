d611aca59ab2a9d10ddcece76ac9d85aabb0099c
deezhizyu <kalenskuja@gmail.com>
2024-06-05 18:37:42 +0200

    Implement task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
+
+  input.before(label);
+  input.placeholder = input.name;
+}
