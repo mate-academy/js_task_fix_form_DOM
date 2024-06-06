625a57d06ed5ba1ac806c1365de1704c88a75dd4
Stanislav Romanyshyn <romanyshyn6696@gmail.com>
2024-06-06 18:36:11 +0300

    add task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const tablet = document.querySelectorAll('.field');
+
+Array.from(tablet).forEach((table) => {
+  const input = table.querySelector('input');
+  const nameCell = document.createElement('label');
+  const inputId = input.id;
+  const inputPlaceholder = input.name;
+
+  nameCell.textContent = input.name;
+
+  nameCell.setAttribute('for', inputId);
+  input.setAttribute('placeholder', inputPlaceholder);
+  nameCell.classList.add('field-label');
+
+  input.insertAdjacentElement('beforebegin', nameCell);
+});
