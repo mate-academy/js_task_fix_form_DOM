25d98684d12e9ef1c834036f94953305933b7828
Anton Polishchuk <hardixxxxx@gmail.com>
2024-06-06 20:11:32 +0200

    added label and placeholders to inputs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('form input')];
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+  const capitalizedName =
+    input.name[0].toUpperCase() + input.name.slice(1).toLowerCase();
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+
+  input.before(label);
+  input.setAttribute('placeholder', capitalizedName);
+});
