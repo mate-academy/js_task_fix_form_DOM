d2d7fd9cc5764dd978c64f30feb2488846f7d826
Anton Polishchuk <hardixxxxx@gmail.com>
2024-06-06 22:22:29 +0200

    split input names

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,13 +4,23 @@ const inputs = [...document.querySelectorAll('form input')];
 
 inputs.forEach((input) => {
   const label = document.createElement('label');
-  const capitalizedName =
-    input.name[0].toUpperCase() + input.name.slice(1).toLowerCase();
 
   label.className = 'field-label';
   label.htmlFor = input.id;
-  label.textContent = input.name;
+  label.textContent = capitalizedName(input.name);
 
   input.before(label);
-  input.setAttribute('placeholder', capitalizedName);
+  input.setAttribute('placeholder', capitalizedName(input.name));
 });
+
+function capitalizedName(inputName) {
+  const capitalizeName = inputName.split('');
+
+  for (let i = 0; i < capitalizeName.length; i++) {
+    if (capitalizeName[i].toUpperCase() === capitalizeName[i]) {
+      capitalizeName[i] = ' ' + capitalizeName[i];
+    }
+  }
+
+  return capitalizeName[0].toUpperCase() + capitalizeName.join('').slice(1);
+}
