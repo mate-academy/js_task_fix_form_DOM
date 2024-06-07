3a0a92c674d39409c9ad9e2dc87ce2264651088b
Stanislav Romanyshyn <romanyshyn6696@gmail.com>
2024-06-07 10:38:10 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,22 @@
 'use strict';
-
 const tablet = document.querySelectorAll('.field');
 
 Array.from(tablet).forEach((table) => {
   const input = table.querySelector('input');
   const nameCell = document.createElement('label');
   const inputId = input.id;
-  const inputPlaceholder = input.name;
+  const inputName = input.name;
 
-  nameCell.textContent = input.name;
+  const labelText =
+    inputName === 'firstName'
+      ? 'First Name'
+      : inputName === 'lastName'
+        ? 'Last Name'
+        : inputName.charAt(0).toUpperCase() + inputName.slice(1);
 
+  nameCell.textContent = labelText;
   nameCell.setAttribute('for', inputId);
-  input.setAttribute('placeholder', inputPlaceholder);
+  input.setAttribute('placeholder', labelText);
   nameCell.classList.add('field-label');
 
   input.insertAdjacentElement('beforebegin', nameCell);
