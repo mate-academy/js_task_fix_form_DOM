9381e8e423d3e096f7492802c98c5ca660c71eaa
mikezhylka <115790814+mikezhylka@users.noreply.github.com>
2024-06-07 23:25:10 +0200

    js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = [...document.getElementsByTagName('input')];
+
+inputs.forEach((input) => {
+  const tag = document.createElement('label');
+  const currentID = input.getAttribute('id');
+
+  tag.className = 'field-label';
+  tag.setAttribute('for', currentID);
+  tag.textContent = input.name.toUpperCase();
+
+  input.setAttribute(
+    'placeholder',
+    `${input.name[0].toUpperCase()}${input.name.slice(1)}`,
+  );
+  input.parentNode.insertBefore(tag, input);
+});
