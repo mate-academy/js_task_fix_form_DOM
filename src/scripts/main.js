a7b476d752e8da033c78ded2798f1e95ef4e629c
NataliaTopornitska <natalia.topornitska3@gmail.com>
2024-06-08 15:10:28 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const inputs = document.querySelectorAll('form input');
+
+  inputs.forEach((input) => {
+    const label = document.createElement('label');
+
+    label.className = 'field-label';
+    label.setAttribute('for', input.id);
+
+    label.textContent =
+      input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+    input.setAttribute(
+      'placeholder',
+      input.name.charAt(0).toUpperCase() + input.name.slice(1),
+    );
+
+    input.parentElement.insertBefore(label, input);
+  });
+});
