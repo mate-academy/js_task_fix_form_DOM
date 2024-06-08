86ee7279ce6af6d610b1b034c12ee5a8097c3992
SerjDSA <serjdsa7@gmail.com>
2024-06-08 08:14:14 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const signUpName = document.getElementById('sign-up-name');
+const signUpLastName = document.getElementById('sign-up-last-name');
+const signUpEmail = document.getElementById('sign-up-email');
+const signUpPassword = document.getElementById('sign-up-password');
+const signInEmail = document.getElementById('sign-in-email');
+const signInPassword = document.getElementById('sign-in-password');
+
+const inputs = [...document.querySelectorAll('form input')];
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
+  input.parentNode.insertBefore(label, input);
+  label.textContent = input.getAttribute('name');
+});
+
+signUpName.setAttribute('placeholder', 'FIRST NAME');
+signUpLastName.setAttribute('placeholder', 'LAST NAME');
+signUpEmail.setAttribute('placeholder', 'EMAIL');
+signUpPassword.setAttribute('placeholder', 'PASSWORD');
+signInEmail.setAttribute('placeholder', 'EMAIL');
+signInPassword.setAttribute('placeholder', 'PASSWORD');
