fc6bf70b43f47c30e0172fb77e255f703bf70bff
Dmytro Bashlai <imaxs2016@gmail.com>
2024-06-09 21:16:54 +0300

    The form has been fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('.field-text')];
+
+inputs.forEach((input) => {
+  input.placeholder =
+    input.name.charAt(0).toUpperCase() + input.name.slice(1).toLowerCase();
+
+  input.insertAdjacentHTML(
+    'afterend',
+    `<label
+      class='field-label'
+      for=#${input.id}
+    >
+      ${input.name}
+    </label>`,
+  );
+});
