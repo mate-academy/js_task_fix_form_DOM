f61c7155b06ef4dd999d48653c589ce36354ef65
Vadym Bakuma <bakuma.2001@gmail.com>
2024-06-09 23:03:52 +0200

    SOLUTION

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,10 +8,16 @@ inputs.forEach((input) => {
   label.classList.add('field-label');
   label.setAttribute('for', input.id);
 
-  const capName = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+  const capName = formatName(input.name);
 
   label.textContent = capName;
   input.placeholder = capName;
 
   input.parentNode.insertBefore(label, input);
 });
+
+function formatName(title) {
+  const withSpaces = title.replace(/([a-z])([A-Z])/g, '$1 $2');
+
+  return withSpaces.charAt(0).toUpperCase() + withSpaces.slice(1);
+}
