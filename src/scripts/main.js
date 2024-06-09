fea4f562032fced5a7bcf5cce4dafd0d36231581
Roman Romanchuk <roma.roman4uk.48@gmail.com>
2024-06-09 15:12:31 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const allInputs = document.querySelectorAll('form input');
+
+allInputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.setAttribute('for', input.id);
+  label.setAttribute('class', 'field-label');
+  label.textContent = input.name;
+  input.parentNode.insertBefore(label, input);
+
+  const placeholderWord = input.name.split('');
+
+  placeholderWord[0] = placeholderWord[0].toUpperCase();
+
+  const upperCaseWord = placeholderWord.join('');
+
+  input.setAttribute('placeholder', upperCaseWord);
+});
