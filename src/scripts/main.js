f5b0b065624aee3067fb5834b755158fada6ccc9
Volodymyr Isaiev <isaiev.v.o@gmail.com>
2024-06-10 15:43:37 +0300

    Fix for first and last name labels and placeholders.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,18 @@
 
 document.querySelectorAll('input').forEach((input) => {
   const label = document.createElement('label');
-  const inputName = input.getAttribute('name');
+  let inputName = input.getAttribute('name');
+
+  switch (inputName) {
+    case 'firstName':
+      inputName = inputName.slice(0, 5) + ' ' + inputName.slice(5);
+      break;
+    case 'lastName':
+      inputName = inputName.slice(0, 4) + ' ' + inputName.slice(4);
+      break;
+    default:
+      break;
+  }
 
   label.classList.add('field-label');
   label.textContent = inputName;
