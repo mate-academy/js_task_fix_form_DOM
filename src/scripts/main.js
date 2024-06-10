4ef246237bd2169c28fa1ac3c181d2059e55b61b
Dmytro Bashlai <imaxs2016@gmail.com>
2024-06-10 20:44:51 +0300

    The Labels have been fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,11 @@
 const inputs = [...document.querySelectorAll('.field-text')];
 
 inputs.forEach((input) => {
-  input.placeholder = input.name
+  const normalizedInputName = input.name
     .replace(/([a-z])([A-Z])/g, '$1 $2')
     .toUpperCase();
+
+  input.placeholder = normalizedInputName;
 
   input.insertAdjacentHTML(
     'afterend',
@@ -13,7 +15,7 @@ inputs.forEach((input) => {
       class='field-label'
       for=#${input.id}
     >
-      ${input.name}
+      ${normalizedInputName}
     </label>`,
   );
 });
