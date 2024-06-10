f684a880150c747e0efd40d41bbf5b8bd38c50c8
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2024-06-10 14:15:08 +0300

    fix labels

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,29 @@
 'use strict';
 
+const transformName = (attrName) => {
+  const firstLetter = attrName[0].toUpperCase();
+  const match = attrName.match(/[A-Z]/);
+
+  if (match) {
+    const index = attrName.indexOf(match[0]);
+
+    return `${firstLetter}${attrName.slice(1, index)} ${attrName.slice(index)}`;
+  } else {
+    return `${firstLetter}${attrName.slice(1)}`;
+  }
+};
+
 const fields = [...document.querySelectorAll('.field')];
 
 fields.forEach((field) => {
   const input = field.querySelector('.field-text');
   const label = document.createElement('label');
-
-  const attrName = input.getAttribute('name');
-  const labelName = `${attrName[0].toUpperCase()}${attrName.slice(1)}`;
+  const labelName = transformName(input.getAttribute('name'));
 
   input.setAttribute('placeholder', labelName);
   label.setAttribute('for', input.getAttribute('id'));
   label.classList.add('field-label');
-  label.innerText = labelName;
+  label.textContent = labelName;
 
   field.prepend(label);
 });
