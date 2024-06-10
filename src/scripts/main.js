0ebe4ee898d79147942dce411318772f066fe373
Volodymyr Isaiev <isaiev.v.o@gmail.com>
2024-06-10 14:04:44 +0300

    Fix form label + input placeholders

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('input').forEach((input) => {
+  const label = document.createElement('label');
+  const inputName = input.getAttribute('name');
+
+  label.classList.add('field-label');
+  label.textContent = inputName;
+  label.setAttribute('for', input.getAttribute('id'));
+
+  input.setAttribute(
+    'placeholder',
+    inputName.charAt(0).toUpperCase() + inputName.slice(1),
+  );
+  input.insertAdjacentElement('beforebegin', label);
+});
