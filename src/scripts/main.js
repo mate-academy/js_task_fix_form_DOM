06ba005ec57823727e4e295faa418011a2a33e61
Nora Audonnet <noraaudonnet@gmail.com>
2024-06-11 20:02:34 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+// eslint-disable-next-line no-shadow
+function normaliseName(name) {
+  let result = '';
+
+  for (let i = 0; i < name.length; i++) {
+    const char = name[i];
+
+    if (i === 0) {
+      result += char.toUpperCase();
+    } else {
+      if (char === char.toUpperCase()) {
+        result += ' ' + char;
+      } else {
+        result += char;
+      }
+    }
+  }
+
+  return result;
+}
+
+for (let i = 0; i < inputs.length; i++) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', inputs[i].id);
+  inputs[i].parentElement.append(label);
+
+  inputs[i].placeholder = normaliseName(inputs[i].name);
+}
