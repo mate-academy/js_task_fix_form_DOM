4a8803ac5de3985e338666abcab9b6dc70312fbe
Yurii <urabelivr@gmail.com>
2024-06-11 19:41:24 +0200

    added labels and placeholders

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+inputs.forEach((input) => {
+  const capitalizedInputName =
+    input.name[0].toUpperCase() + input.name.slice(1);
+
+  input.placeholder = capitalizedInputName;
+
+  const label = document.createElement('label');
+
+  label.htmlFor = input.id;
+  label.className = 'field-label';
+  label.textContent = input.name;
+  input.insertAdjacentElement('beforebegin', label);
+});
