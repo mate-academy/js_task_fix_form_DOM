1af2f5ce96902b8f326a2401305c8b59858e6111
Alina Ovod <alina.ipatova98@gmail.com>
2024-06-12 15:12:06 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+const updateInputs = (input) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('htmlFor', input.id);
+  label.textContent = input.name;
+
+  input.before(label);
+  input.setAttribute('placeholder', input.name.toUpperCase());
+};
+
+inputs.forEach((item) => updateInputs(item));
