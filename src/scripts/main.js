198d9a43574b5e19a394a8e90cb9cde67dc3db53
Dmytro Boiko <iamboikodmytro@gmail.com>
2024-06-13 10:36:13 +0300

    task complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', function () {
+  const forms = document.querySelectorAll('form');
+
+  forms.forEach((form) => {
+    const inputs = form.querySelectorAll('input');
+
+    inputs.forEach((input) => {
+      const label = document.createElement('label');
+
+      label.classList.add('field-label');
+
+      label.textContent = input.name;
+
+      label.setAttribute('for', input.id);
+
+      input.placeholder = input.name.toUpperCase();
+
+      input.parentNode.insertBefore(label, input);
+    });
+  });
+});
