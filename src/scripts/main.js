06387aa63dddcbe3248a57c489283eff3c33cd62
Marina Avramenko <marina.avramenko123@gmail.com>
2024-06-13 18:13:16 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,17 +3,14 @@
 const inputs = document.querySelectorAll('form input');
 
 inputs.forEach((input) => {
+  const inputName = input.getAttribute('name');
+  const placeholder = inputName.charAt(0).toUpperCase() + inputName.slice(1);
   const label = document.createElement('label');
 
   label.classList.add('field-label');
-
   label.setAttribute('for', input.id);
-
-  label.textContent = input.name.charAt(0).toUpperCase() + input.name.slice(1);
-
-  const placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
-
-  input.setAttribute('placeholder', placeholder);
+  label.textContent = inputName.toUpperCase();
+  input.placeholder = placeholder.replace(/([a-z])([A-Z])/g, '$1 $2');
 
   input.parentNode.appendChild(label);
 });
