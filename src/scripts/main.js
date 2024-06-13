6a50e29a998ff669d14d6fe1ca6fe4176927cd45
Tatiana <tanya_potapenko@bigmir.net>
2024-06-13 13:26:41 +0200

    js_task-fix-form_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const forms = document.querySelectorAll('form');
+
+forms.forEach((form) => {
+  const inputs = form.querySelectorAll('input');
+
+  inputs.forEach((input) => {
+    const inputId = input.id;
+    const inputName = input.name;
+
+    const label = document.createElement('label');
+
+    label.className = 'field-label';
+    label.htmlFor = inputId;
+    label.textContent = inputName.charAt(0).toUpperCase() + inputName.slice(1);
+
+    input.parentNode.insertBefore(label, input);
+    input.placeholder = inputName.charAt(0).toUpperCase() + inputName.slice(1);
+  });
+});
