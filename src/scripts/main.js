6dc66f321c3f5cab4725e10afb8e982cee224df8
Anastasiia <vasilisandreeva00@gmail.com>
2024-06-14 17:33:09 +0300

    create solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+[...inputs].map((element) => {
+  const { id } = element;
+
+  const label = document.createElement('label');
+
+  label.setAttribute('class', 'field-label');
+  label.setAttribute('for', id);
+  label.textContent = element.name;
+
+  const value = element.name[0].toUpperCase() + element.name.slice(1);
+
+  element.setAttribute('placeholder', value);
+
+  element.parentNode.insertBefore(label, element.nextSibling);
+
+  return label;
+});
