c937196e061ae275c86f3c066f8470c589f2a334
Vasil Liakhivskyi <vazilx@gmail.com>
2024-06-14 08:26:21 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const fields = [...document.querySelectorAll('.field-text')];
+
+fields.forEach((field) => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = field.id;
+  label.textContent = field.name;
+
+  field.placeholder = field.name;
+
+  field.insertAdjacentElement('afterend', label);
+});
