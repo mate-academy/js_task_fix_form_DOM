16f69f1ddef7729ab232500bf0f9de17bbd5f75c
Ivan Lavrinenko <lavrinenkoivan51@gmail.com>
2024-06-15 22:53:18 +0200

    created label for each input with DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const inputs = document.querySelectorAll('form input');
 
-// write code here
+  inputs.forEach((input) => {
+    const label = document.createElement('label');
+
+    label.className = 'field-label';
+    label.htmlFor = input.id;
+
+    const inputName = input.name;
+
+    const capitalizedInputName =
+      inputName.charAt(0).toUpperCase() + inputName.slice(1);
+
+    label.textContent = inputName.toUpperCase();
+    input.placeholder = capitalizedInputName;
+
+    input.insertAdjacentElement('beforebegin', label);
+  });
+});
