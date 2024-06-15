16bf04a7b9c43750ff50eb502bc403d557d4a8f3
Vasil Liakhivskyi <vazilx@gmail.com>
2024-06-15 07:47:19 +0200

    fix case

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,9 +7,20 @@ fields.forEach((field) => {
 
   label.className = 'field-label';
   label.htmlFor = field.id;
-  label.textContent = field.name;
+  label.textContent = transformCamelCase(field.name);
 
-  field.placeholder = field.name;
+  field.placeholder = transformCamelCase(field.name);
 
   field.insertAdjacentElement('afterend', label);
 });
+
+function transformCamelCase(strCamelCase) {
+  const normalizedStr = strCamelCase.replace(/([A-Z])/g, ' $1');
+
+  const result = normalizedStr
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
+
+  return result;
+}
