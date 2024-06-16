d412f07659a509029c5088e1a528de69c5e06c1b
Rusłan Karaniewski <karane88vka@gmail.com>
2024-06-16 21:25:41 +0200

    Fixes after PR

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,7 @@
 const inputs = document.querySelectorAll('input');
 
 inputs.forEach((elem) => {
-  const elName = elem.id.slice(8).replace('-', ' ');
+  const elName = elem.id.slice(8).replace(/-/g, ' ');
   const inputName = elName[0].toUpperCase() + elName.slice(1);
   const formEl = document.createElement('label');
 
