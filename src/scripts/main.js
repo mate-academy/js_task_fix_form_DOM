014b2c6e6675f693c2ecb885228ee1b58a3fabdd
ZakZh <38009936+ZakZh@users.noreply.github.com>
2024-06-16 20:11:36 +0300

    split first name and last name in form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,13 +4,19 @@ const formInputs = document.querySelectorAll('input');
 
 formInputs.forEach((el) => {
   const labelForInput = document.createElement('label');
+  const elementName =
+    el.name === 'firstName' || el.name === 'lastName'
+      ? el.name.charAt(0).toUpperCase() +
+        el.name.slice(1, -4) +
+        ' ' +
+        el.name.slice(-4).toLowerCase()
+      : el.name.charAt(0).toUpperCase() + el.name.slice(1).toLowerCase();
 
   labelForInput.className = 'field-label';
   labelForInput.htmlFor = el.id;
-  labelForInput.innerText = el.name.toUpperCase();
+  labelForInput.innerText = elementName.toUpperCase();
 
   el.parentElement.insertBefore(labelForInput, el);
 
-  el.placeholder =
-    el.name.charAt(0).toUpperCase() + el.name.slice(1).toLowerCase();
+  el.placeholder = elementName;
 });
