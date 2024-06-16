993312aff3e0889c543e80729fed4c84a3080c3d
Voloshynd <Voloshynd13@gmail.com>
2024-06-16 18:27:40 +0200

    add solution of task js_task_fix_form_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.setAttribute('for', `${input.getAttribute('id')}`);
+  label.classList.add('field-label');
+  label.textContent = input.getAttribute('name');
+  input.placeholder = `${input.getAttribute('name').toUpperCase()}`;
+  input.parentNode.prepend(label);
+}
