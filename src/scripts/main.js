b25f0a4779bdb70b99df1a96650322d690445ccf
Arthur Tolkachov <babanjerelo@gmail.com>
2024-06-17 14:00:42 +0300

    [Feature] added auth form improvements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const capitalize = (value) => {
+  return value[0].toUpperCase() + value.slice(1);
+};
+
+const inputsSelectors = [...document.querySelectorAll('input')];
+
+inputsSelectors.map((input) => {
+  const labelText = input.getAttribute('name');
+
+  const labelId = input.getAttribute('id');
+  const labelElement = document.createElement('label');
+
+  labelElement.textContent = labelText;
+  labelElement.classList.add('field-label');
+
+  labelElement.setAttribute('for', labelId);
+  input.setAttribute('placeholder', capitalize(labelText));
+
+  input.parentElement.append(labelElement);
+});
