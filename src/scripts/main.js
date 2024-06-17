c7ee22411922d8edbbdfa7349b24c724d2f0e30e
Vyacheslav Kokayko <vkokayko@gmail.com>
2024-06-17 11:21:41 +0300

    task-fix-form-dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
-'use strict';
+function camelCaseToText(str) {
+  return str
+    .replace(/([a-z])([A-Z])/g, '$1 $2')
+    .replace(/^\w/, (c) => c.toUpperCase());
+}
 
-// write code here
+const forms = document.querySelectorAll('form');
+
+forms.forEach((form) => {
+  const inputs = form.querySelectorAll('input');
+
+  inputs.forEach((input) => {
+    const label = document.createElement('label');
+
+    label.classList.add('field-label');
+    label.setAttribute('for', input.id);
+    label.textContent = camelCaseToText(input.name);
+
+    input.setAttribute('placeholder', camelCaseToText(input.name));
+    input.parentNode.insertBefore(label, input);
+  });
+});
