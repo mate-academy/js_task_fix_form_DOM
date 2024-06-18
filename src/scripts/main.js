1eea8e11b9bcb94fe8c2c235bf043c529133ec6a
Misha Vynnyk <vinnikmisha94@gmail.com>
2024-06-18 18:27:41 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const inputElement = document.querySelectorAll('input');
+
+const correctPlaceholder = (textInInput) => {
+  let correctText = '';
+
+  for (let i = 0; i < textInInput.length; i++) {
+    if (i === 0) {
+      correctText += textInInput[0].toUpperCase();
+    } else if (textInInput[i].match(/[A-Z]/g)) {
+      correctText += ' ' + textInInput[i];
+    } else {
+      correctText += textInInput[i];
+    }
+  }
+
+  return correctText;
+};
+
+const addLabel = (formInputs) => {
+  formInputs.forEach((input) => {
+    input.placeholder = correctPlaceholder(input.name);
+
+    input.insertAdjacentHTML(
+      'beforebegin',
+      `<label for="${input.id}" class="field-label">${input.name}</label>`,
+    );
+  });
+};
+
+addLabel(inputElement);
