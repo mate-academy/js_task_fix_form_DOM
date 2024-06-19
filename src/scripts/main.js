b0fb9f0d6a53ca18982e7a61a8ca0f2fd9db0915
Igor Novikov <novikov.igor.20@gmail.com>
2024-06-19 18:34:51 +0300

    task fix form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const allInput = [...document.querySelectorAll('form input')];
+
+allInput.forEach((el) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+
+  if (el.id) {
+    label.setAttribute('for', el.id);
+  }
+
+  const fieldName = formatFiledName(el.name);
+
+  label.textContent = fieldName;
+  el.placeholder = fieldName;
+
+  el.parentNode.insertBefore(label, el);
+});
+
+function formatFiledName(str) {
+  const withSpaces = str.replace(/([a-z])(A-Z)/g, '$1 $2');
+
+  return withSpaces.charAt(0).toUpperCase() + withSpaces.slice(1);
+}
