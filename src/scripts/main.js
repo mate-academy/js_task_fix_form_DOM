2a48150d68c8a9d4b09e2d783ae739f63772cd47
Radymyr <loboda.radymyr@gmail.com>
2024-06-20 17:29:42 +0300

    added fuction getWithCapitalLetter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,13 +2,50 @@
 
 const inputs = [...document.querySelectorAll('form input')];
 
-inputs.forEach((input) => {
-  const labelElement = document.createElement('label');
+/**
+ * @function getUpperCaseWord
+ * @param {string} word;
+ * @returns {string}
+ */
+const getUpperCaseWord = (word) => word[0].toUpperCase() + word.slice(1);
 
-  labelElement.htmlFor = input.id;
-  labelElement.classList.add('field-label');
-  labelElement.innerText = input.name;
+/**
+ * @function getStringFormat
+ * @param {string} str
+ * @returns {string}
+ */
+function getWithCapitalLetter(str) {
+  for (const ch of str) {
+    const isUpperCaseLetter = ch !== ch.toLowerCase();
 
-  input.placeholder = input.name;
-  input.parentElement.prepend(labelElement);
-});
+    if (isUpperCaseLetter) {
+      return str
+        .replace(ch, ` ${ch}`)
+        .split(' ')
+        .map(getUpperCaseWord)
+        .join(' ');
+    }
+  }
+
+  return getUpperCaseWord(str);
+}
+
+/**
+ * @function addedLabelElement
+ * @param {Element[]} arrayOfInputs;
+ * @returns {void}
+ */
+const addedLabelElement = (arrayOfInputs) => {
+  arrayOfInputs.forEach((input) => {
+    const labelElement = document.createElement('label');
+
+    labelElement.htmlFor = input.id;
+    labelElement.classList.add('field-label');
+    labelElement.innerText = getWithCapitalLetter(input.name);
+
+    input.placeholder = getWithCapitalLetter(input.name);
+    input.parentElement.prepend(labelElement);
+  });
+};
+
+addedLabelElement(inputs);
