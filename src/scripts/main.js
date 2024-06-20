de5e379563ea336ed68f31cce6ce57f3b8802932
Oleh Hanenko <hello.isaev@gmail.com>
2024-06-20 13:35:39 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+inputs.forEach((item) => {
+  const label = `<label for="${item.id}" class="field-label">${item.name}</label>`;
+
+  item.insertAdjacentHTML('beforebegin', label);
+
+  const capitalizedLabel = capitalizeFirstLetter(item.name);
+
+  item.placeholder = capitalizedLabel;
+});
