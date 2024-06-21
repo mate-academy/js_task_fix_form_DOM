94ef5ab505c13d621e21f36805ee5da881a47a48
Vadym Bryvka <vadym.bryvkaa@gmail.com>
2024-06-20 22:16:52 -0500

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const fieldList = [...document.getElementsByClassName('field-text')];
+
+fieldList.forEach((input) => {
+  let heading = input.getAttribute('name');
+
+  const label = document.createElement('label');
+
+  input.parentNode.insertBefore(label, input);
+
+  label.setAttribute('class', 'field-label');
+
+  label.id = heading;
+
+  label.textContent = `${heading.toUpperCase()}`;
+
+  heading = heading.charAt(0).toUpperCase() + heading.slice(1);
+
+  input.setAttribute('placeholder', heading);
+});
