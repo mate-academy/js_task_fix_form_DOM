a1d495dc4f3181ea2183d14c389733120a5ca98b
Denis Reznik <reznik.d.o12@gmail.com>
2024-06-21 05:44:37 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('.field-text').forEach((elem) => {
+  const nameAttr = elem.getAttribute('name');
+  const idAttr = elem.getAttribute('id');
+  const lableElement = document.createElement('label');
+
+  lableElement.setAttribute('for', idAttr);
+  lableElement.textContent = nameAttr.toUpperCase();
+  lableElement.classList.add('field-label');
+
+  elem.setAttribute('placeholder', firstLetterCap(nameAttr));
+  elem.before(lableElement);
+});
+
+function firstLetterCap(word) {
+  return word.slice(0, 1).toUpperCase() + word.slice(1);
+}
