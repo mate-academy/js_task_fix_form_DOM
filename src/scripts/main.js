4390b2d1071e246f380477aa5b05a036b5b27654
Denis Reznik <reznik.d.o12@gmail.com>
2024-06-22 03:13:12 +0300

    fix solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,10 +9,21 @@ document.querySelectorAll('.field-text').forEach((elem) => {
   lableElement.textContent = nameAttr.toUpperCase();
   lableElement.classList.add('field-label');
 
-  elem.setAttribute('placeholder', firstLetterCap(nameAttr));
+  elem.setAttribute(
+    'placeholder',
+    firstLetterCap(convertToSeparatedWords(nameAttr)),
+  );
   elem.before(lableElement);
 });
 
 function firstLetterCap(word) {
   return word.slice(0, 1).toUpperCase() + word.slice(1);
+}
+
+function convertToSeparatedWords(str) {
+  let result = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+
+  result = result.replace(/ ([A-Z])/g, (match) => match.toLowerCase());
+
+  return result;
 }
