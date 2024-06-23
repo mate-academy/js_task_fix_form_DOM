a65896fb4e0fa26159f205f91c0c9826473e8b3e
Melnyk Anna <anetamelnyk25@gmail.com>
2024-06-23 16:20:19 +0300

    fix_form_dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+function labelName(str) {
+  return str.replace(/([A-Z])/g, ' $1').trim();
+}
+
+function placeholderName(str) {
+  const result = str.replace(/([A-Z])/g, ' $1').trim();
+
+  return result.charAt(0).toUpperCase() + result.slice(1);
+}
+
+inputs.forEach((input) => {
+  if (!input.name) {
+    return;
+  }
+
+  const label = document.createElement('label');
+
+  input.before(label);
+
+  label.className = 'field-label';
+
+  label.textContent = labelName(input.name);
+
+  input.setAttribute('placeholder', placeholderName(input.name));
+});
