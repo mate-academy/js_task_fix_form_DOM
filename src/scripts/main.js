9eeef4bd57f561cdcc72240f301925f9172ce820
MTS13 <mtstaras@gmail.com>
2024-06-23 18:43:01 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const input = [...document.querySelectorAll('input')];
+
+for (const el of input) {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', el.id);
+
+  let value = el.name[0].toUpperCase() + el.name.slice(1);
+
+  if (value === 'LastName' || value === 'FirstName') {
+    value = value.slice(0, -4) + ' ' + value.slice(-4);
+  }
+
+  label.textContent = value;
+  el.setAttribute('placeholder', value);
+  el.before(label);
+}
