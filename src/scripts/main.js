055272bcb9e0311e27289f539cd3ba5f53e47e37
Oleksii Kozlov <oleksiikozlov@gmail.com>
2024-06-24 17:04:30 +0300

    fixed form DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+const divs = document.querySelectorAll('.field');
+
+for (let index = 0; index < inputs.length; index++) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', inputs[index].id);
+
+  const labelName = inputs[index].name;
+
+  inputs[index].style.textTransform = 'capitalize';
+  label.textContent = labelName;
+
+  inputs[index].setAttribute('placeholder', inputs[index].name);
+  divs[index].append(label);
+}
