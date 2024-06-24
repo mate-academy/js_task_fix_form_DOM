c2576452f813e68a08da4cc6a51a56cf22b4b9e7
luter <newgglutt@gmail.com>
2024-06-24 18:58:13 +0300

    new

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,15 +2,21 @@
 
 const inputs = document.querySelectorAll('form input');
 
+const toTitleCase = (str) => {
+  return str
+    .replace(/([A-Z])/g, ' $1')
+    .replace(/^./, (char) => char.toUpperCase());
+};
+
 inputs.forEach((input) => {
   const label = document.createElement('label');
 
   label.classList.add('field-label');
   label.setAttribute('for', input.id);
 
-  label.textContent = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+  label.textContent = toTitleCase(input.name);
 
-  input.placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+  input.placeholder = toTitleCase(input.name);
 
   input.parentNode.insertBefore(label, input);
 });
