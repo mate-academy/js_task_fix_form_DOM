47a161be8ca13587d3dacf490c16e5f59cbb40ac
Daniil <piecyberpie@gmail.com>
2024-06-25 22:52:29 +0300

    fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,18 +2,40 @@
 
 const fields = document.getElementsByClassName('field');
 
+const stringHumanizer = (inputString) => {
+  const sizing = [0];
+  const result = [];
+
+  for (let i = 1; i < inputString.length; i++) {
+    if (inputString[i] === inputString[i].toUpperCase()) {
+      sizing.push(i);
+    }
+  }
+
+  for (let i = 0; i < sizing.length; i++) {
+    result.push(inputString.slice(sizing[i], sizing[i + 1]));
+  }
+
+  return result.join(' ');
+};
+
 for (const field of fields) {
   const label = document.createElement('label');
 
   const input = field.querySelector('input');
 
-  label.textContent = input.getAttribute('name').toUpperCase();
+  let attributeName = input.getAttribute('name');
+
+  if (attributeName !== attributeName.toLowerCase()) {
+    attributeName = stringHumanizer(attributeName);
+  }
+  label.textContent = attributeName.toUpperCase();
   label.setAttribute('for', input.id);
   label.classList.add('field-label');
 
   input.setAttribute(
     'placeholder',
-    `${input.getAttribute('name').slice(0, 1).toUpperCase()}${input.getAttribute('name').slice(1)}`,
+    `${attributeName.slice(0, 1).toUpperCase()}${attributeName.slice(1)}`,
   );
 
   field.insertBefore(label, input);
