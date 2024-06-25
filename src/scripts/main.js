b58c152a876073b7c1e0c99f89c917ba9138355b
Daniil <piecyberpie@gmail.com>
2024-06-25 21:44:31 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const fields = document.getElementsByClassName('field');
+
+for (const field of fields) {
+  const label = document.createElement('label');
+
+  const input = field.querySelector('input');
+
+  label.textContent = input.getAttribute('name').toUpperCase();
+  label.setAttribute('for', input.id);
+  label.classList.add('field-label');
+
+  input.setAttribute(
+    'placeholder',
+    `${input.getAttribute('name').slice(0, 1).toUpperCase()}${input.getAttribute('name').slice(1)}`,
+  );
+
+  field.insertBefore(label, input);
+}
