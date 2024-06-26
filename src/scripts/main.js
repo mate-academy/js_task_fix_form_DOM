87fa1b38f8e319d4cc3e75ed16473d9a6d05dc44
MelnykVlad <21vladmel15@gmail.com>
2024-06-26 21:55:32 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const forms = document.querySelectorAll('form');
+
+forms.forEach((form) => {
+  const inputs = form.querySelectorAll('input');
+
+  inputs.forEach((input) => {
+    const label = document.createElement('label');
+
+    label.className = 'field-label';
+    label.htmlFor = input.id;
+    label.textContent = capitalize(input.name);
+
+    input.placeholder = capitalize(input.name);
+
+    input.parentElement.insertBefore(label, input);
+  });
+});
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
