2ed0410749479546134fdd4c9de9a7827e2b71ee
MacBook Pro 13 <macbookpro13@MacBook-Pro-MacBook.local>
2024-06-27 15:02:32 +0300

    add test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const forms = document.getElementsByTagName('form');
+const separation = (placeholder) => {
+  const firstCharCut = placeholder.slice(1);
+  let finalParse = '';
+
+  Array.from(firstCharCut).forEach((ch) => {
+    if (ch === ch.toUpperCase()) {
+      finalParse += ' ';
+    }
+
+    finalParse += ch;
+  });
+
+  return placeholder.slice(0, 1).toUpperCase() + finalParse;
+};
+
+for (let i = 0; i < forms.length; i++) {
+  Array.from(forms[i].getElementsByTagName('input')).map((input) => {
+    const labelElement = document.createElement('label');
+
+    labelElement.classList.add('field-label');
+    labelElement.setAttribute('for', input.id);
+    labelElement.textContent = input.name;
+
+    input.parentNode.insertBefore(labelElement, input);
+
+    input.setAttribute('placeholder', separation(input.name));
+  });
+}
