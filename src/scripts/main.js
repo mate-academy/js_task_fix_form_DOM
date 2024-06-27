dbd8c00534992484cc98e8a3d237e8e696671357
MacBook Pro 13 <macbookpro13@MacBook-Pro-MacBook.local>
2024-06-27 13:22:06 +0300

    add test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const forms = document.getElementsByTagName('form');
+
+for (let i = 0; i < forms.length; i++) {
+  Array.from(forms[i].getElementsByTagName('input')).map((input) => {
+    const labelElement = document.createElement('label');
+
+    labelElement.classList.add('field-label');
+    labelElement.setAttribute('for', input.id);
+    labelElement.textContent = input.name;
+
+    input.parentNode.insertBefore(labelElement, input);
+
+    input.setAttribute(
+      'placeholder',
+      input.name.slice(0, 1).toUpperCase() + input.name.slice(1),
+    );
+  });
+}
