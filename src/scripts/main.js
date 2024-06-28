5d86d53f671e862dc44269f8a5e5677e358da669
Daria Sydorenko <dashasidorenko123@gmail.com>
2024-06-28 12:16:08 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  input.insertAdjacentHTML(
+    'beforebegin',
+    `<label class="field-label" for=${input.id}>${input.name}</label>`,
+  );
+  input.setAttribute('placeholder', capitalizeFirstLetter(input.name));
+}
