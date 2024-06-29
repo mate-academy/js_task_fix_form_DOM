939e4ea37d79b954c55a9d8c575caef4ac118166
4xmplme <andrii_kondratenko@icloud.com>
2024-06-29 18:41:17 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+inputs.forEach((input) => {
+  const inputLabel = document.createElement('label');
+  const inputName = input.name.toLowerCase().replace(/name/gi, ' Name');
+
+  inputLabel.className = 'field-label';
+  inputLabel.htmlFor = input.id;
+  inputLabel.textContent = inputName;
+
+  input.placeholder = inputName[0].toUpperCase() + inputName.slice(1);
+  input.parentElement.append(inputLabel);
+});
