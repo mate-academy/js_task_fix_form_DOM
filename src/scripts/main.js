578bb3945cd2f6a5bea406b99977a4dcfe84be38
4xmplme <andrii_kondratenko@icloud.com>
2024-06-30 12:04:40 +0300

    add more flexible solution for dividing 'input.name'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,23 @@
 'use strict';
 
+function capitalize(string) {
+  return string[0].toUpperCase() + string.slice(1);
+}
+
+function separate(string) {
+  return string.replace(/([A-Z])/g, ' $1').trim();
+}
+
 const inputs = document.querySelectorAll('form input');
 
 inputs.forEach((input) => {
   const inputLabel = document.createElement('label');
-  const inputName = input.name.toLowerCase().replace(/name/gi, ' Name');
+  const inputName = capitalize(separate(input.name));
 
   inputLabel.className = 'field-label';
   inputLabel.htmlFor = input.id;
   inputLabel.textContent = inputName;
 
-  input.placeholder = inputName[0].toUpperCase() + inputName.slice(1);
+  input.placeholder = inputName;
   input.parentElement.append(inputLabel);
 });
