137a649443af77fd571447b4f7ff992087a95f95
Galers <galosaandrew@gmail.com>
2024-07-01 16:17:38 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
+/* eslint-disable function-paren-newline */
 'use strict';
 
-// write code here
+const reg = /N/g;
+
+const divFields = [...document.querySelectorAll('.field')];
+const inputs = divFields.flatMap((elem) =>
+  Array.from(elem.querySelectorAll('input')),
+);
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
+
+  const inputName = input.name.replace(reg, ' N');
+
+  label.textContent = inputName;
+
+  const placeholderText = `${inputName.charAt(0).toUpperCase()}${inputName.slice(1)}`;
+
+  input.setAttribute('placeholder', placeholderText);
+
+  input.parentNode.insertBefore(label, input);
+});
