d189d6957d914462d87c90b8c842e7dd1b6fbb20
Heorhii Savostikov <georgesavostikov@gmail.com>
2024-07-01 21:10:11 +0300

    add some changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,11 +2,27 @@
 
 const allInputElement = [...document.querySelectorAll('input.field-text')];
 
+function getCorrectName(string) {
+  const splitedString = string.split('');
+
+  const index = splitedString.findIndex((e) => e === e.toUpperCase());
+
+  if (index !== -1) {
+    const correctString = string.slice(0, index) + ' ' + string.slice(index);
+
+    return correctString;
+  }
+
+  return string;
+}
+
 allInputElement.forEach((element) => {
   const labelElement = document.createElement('label');
 
-  const inputElementName =
-    element.name[0].toUpperCase() + element.name.slice(1);
+  let inputElementName = getCorrectName(element.name);
+
+  inputElementName =
+    inputElementName[0].toUpperCase() + inputElementName.slice(1);
 
   labelElement.setAttribute('class', 'field-label');
   labelElement.setAttribute('for', `${element.id}`);
