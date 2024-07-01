669f9b6e8c30d479b761197b7cadf1f5f16f65da
Iryna Popovych <popovicirina40@gmail.com>
2024-07-02 00:08:20 +0100

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write code here
+const allInputs = document.getElementsByTagName('input');
+
+[...allInputs].forEach((input) => {
+  const label = document.createElement('label');
+
+  input.setAttribute('placeholder', input.name);
+
+  label.setAttribute('for', input.id);
+  label.className = 'field-label';
+  label.textContent = input.name;
+  input.parentNode.insertBefore(label, input);
+});
