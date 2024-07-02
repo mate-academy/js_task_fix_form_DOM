9a1d5fc8105a928994f026745ba9d38c76123827
Svitlana-Vitryk <vitriksvitlana@gmail.com>
2024-07-02 12:41:44 +0300

    js_task_fix_form_DOM2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,17 +3,21 @@
 document.addEventListener('DOMContentLoaded', function () {
   const inputs = document.querySelectorAll('form .field input');
 
+  function splitWords(inputName) {
+    return inputName.replace(/([A-Z])/g, ' $1').trim();
+  }
+
   inputs.forEach((input) => {
     const label = document.createElement('label');
 
     label.className = 'field-label';
     label.setAttribute('for', input.id);
 
-    label.textContent =
-      input.name.charAt(0).toUpperCase() + input.name.slice(1);
+    const labelText = splitWords(input.name);
 
-    input.placeholder =
-      input.name.charAt(0).toUpperCase() + input.name.slice(1);
+    label.textContent = input.name.charAt(0).toUpperCase() + labelText.slice(1);
+
+    input.placeholder = input.name.charAt(0).toUpperCase() + labelText.slice(1);
 
     input.parentNode.insertBefore(label, input);
   });
