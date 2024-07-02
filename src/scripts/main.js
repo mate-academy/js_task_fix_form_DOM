04fa8bcd2c9975d467b12c89e6d726b589a51e20
HunchakAndrii <andreygunchak1@gmail.com>
2024-07-02 17:25:57 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+inputs.forEach((input, index) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+
+  label.setAttribute('for', input.id);
+
+  label.textContent = input.name;
+
+  input.setAttribute('placeholder', input.name);
+
+  input.parentNode.insertAdjacentElement('afterbegin', label);
+});
