63b2b50a851631a43cab0df5a891da0daa85aefa
Svitlana-Vitryk <vitriksvitlana@gmail.com>
2024-07-02 06:51:26 +0300

    js_task_fix_form_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', function () {
+  const inputs = document.querySelectorAll('form .field input');
+
+  inputs.forEach((input) => {
+    const label = document.createElement('label');
+
+    label.className = 'field-label';
+    label.setAttribute('for', input.id);
+
+    label.textContent =
+      input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+    input.placeholder =
+      input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+    input.parentNode.insertBefore(label, input);
+  });
+});
