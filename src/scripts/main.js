3af33354e1895fc7b3b268a5177a016e398290fa
Iryna Stefaniuk <irinagoyan@gmail.com>
2024-07-02 20:36:05 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const containerForm = document.querySelector('.login-wrap');
+
+const list = [...containerForm.querySelectorAll('input')];
+
+function addLableInput(obj) {
+  const lable = document.createElement('label', { for: `${obj.id}` });
+  const lableContainer = document.createElement('label');
+  const element = obj.parentElement;
+
+  lable.textContent = obj.name;
+
+  lable.classList.add('field-label');
+
+  obj.placeholder = `${obj.name}`;
+
+  return (
+    obj.insertAdjacentElement('beforebegin', lable),
+    element.insertAdjacentElement('beforebegin', lableContainer)
+  );
+}
+
+list.forEach((elem) => {
+  addLableInput(elem);
+});
