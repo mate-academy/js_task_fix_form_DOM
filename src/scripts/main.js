bf5237a5848ce3961834df40c41c91ce1423d8c0
valentyn19939 <valik.hai1993@gmail.com>
2024-07-02 19:29:55 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const allInput = document.querySelectorAll('input');
+
+for (const input of allInput) {
+  const creatLabel = document.createElement('label');
+
+  creatLabel.className = 'field-label';
+  creatLabel.htmlFor = input.id;
+  creatLabel.textContent = input.name;
+  input.before(creatLabel);
+
+  input.placeholder = input.name;
+}
