82aa59909d7d9cd42d33d56483229519817336a0
smachyloooo <smachyloooo@gmail.com>
2024-07-02 10:43:50 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write code here
+
+function splitByUpperCase(str) {
+    return str.replace(/([A-Z])/g, ' $1').replace(/^./, function(str){ return str.toUpperCase(); }).trim();
+  }
+
+const allInputs = [...document.querySelectorAll('input')];
+const inputLabelsArr = [];
+const field = [...document.querySelectorAll('.field')];
+
+allInputs.forEach((el) => {
+    el.placeholder = splitByUpperCase(el.name);
+    inputLabelsArr.push(splitByUpperCase(el.name));
+})
+
+field.forEach((el, index) => {
+    let label = document.createElement('label');
+    label.innerText = inputLabelsArr[index].toUpperCase();
+    label.className = 'field-label'
+    el.prepend(label);
+})
