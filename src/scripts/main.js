aac311c25d65dcecea6a4e6303447232c0d0d89c
Iryna Popovych <popovicirina40@gmail.com>
2024-07-03 17:05:32 +0100

    fix bug

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,10 +6,10 @@ const allInputs = document.getElementsByTagName('input');
 [...allInputs].forEach((input) => {
   const label = document.createElement('label');
 
-  input.setAttribute('placeholder', input.name);
+  input.setAttribute('placeholder', input.name.split(/(?=[A-Z])/).join(' '));
 
   label.setAttribute('for', input.id);
   label.className = 'field-label';
-  label.textContent = input.name;
+  label.textContent = input.name.split(/(?=[A-Z])/).join(' ');
   input.parentNode.insertBefore(label, input);
 });
