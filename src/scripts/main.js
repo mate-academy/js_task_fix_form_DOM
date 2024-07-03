0b78fe06986d590e7fcae92d3cdee1ff586df7b0
Liudmyla Hnydyn <gnydynluda@gmail.com>
2024-07-03 00:28:16 -0400

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = document.getElementsByTagName('input');
+
+for (const input of inputs) {
+  input.setAttribute('placeholder', input.name);
+
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+
+  label.setAttribute('for', input.id);
+
+  label.innerHTML = input.name.toUpperCase();
+
+  input.before(label);
+}
