fda5d58ea82644c74ecc5879dafd198fef0162e3
Seva Podolskiy <s_podolskiy@live.com>
2024-07-03 10:23:32 -0400

    fixed Capitalizion

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,16 +3,21 @@
 const inputs = document.querySelectorAll('input');
 
 inputs.forEach((input) => {
-  input.setAttribute(
-    'placeholder',
-    input.getAttribute('name')[0].toUpperCase() +
-      input.getAttribute('name').slice(1).toLowerCase(),
-  );
+  let txt;
+  if (input.getAttribute('name') === 'firstName') {
+    txt = 'First name';
+  } else if (input.getAttribute('name') === 'lastName') {
+    txt = 'Last name';
+  } else {
+    txt = input.getAttribute('name');
+  }
+
+  input.setAttribute('placeholder', txt);
 
   const lableElem = document.createElement('label');
 
   lableElem.setAttribute('class', 'field-label');
   lableElem.setAttribute('for', input.getAttribute('id'));
-  lableElem.textContent = input.getAttribute('name');
+  lableElem.textContent = txt;
   input.insertAdjacentElement('beforebegin', lableElem);
 });
