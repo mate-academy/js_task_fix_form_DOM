9d853895c035dfa9d3d3bca9c159657d5cae745c
Kirill <ovoo1339@gmail.com>
2024-07-04 11:59:59 +0300

    work

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 // write code here
+const inputs = document.querySelectorAll('form input');
+
+inputs.forEach((input) => {
+  input.insertAdjacentHTML(
+    'beforebegin',
+    `<label class="field-label" for="${input.getAttribute('id')}">${input.getAttribute('name')}</label>`,
+  );
+
+  const attribute = input.getAttribute('name').split('');
+  const firstLetter = attribute[0].toUpperCase();
+  const rest = [...attribute];
+
+  rest.splice(0, 1);
+
+  const result = [firstLetter, ...rest];
+
+  input.placeholder = result.join('');
+});
