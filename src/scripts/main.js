a8e3c03169b13fa7b314ef94747c6cc93834d71c
TetianaBukhenko <tania04bukhenko@gmail.com>
2024-07-06 15:10:40 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const getPlaceholderName = (className) => {
+  return className
+    .replace(/([a-z])([A-Z])/g, '$1 $2')
+    .split(' ')
+    .map((el) => el[0].toUpperCase() + el.slice(1))
+    .join(' ');
+};
+
+[...document.querySelectorAll('input')].forEach((element) => {
+  const placeholder = getPlaceholderName(element.getAttribute('name'));
+  const id = element.getAttribute('id');
+
+  element.setAttribute('placeholder', `${placeholder}`);
+
+  element.insertAdjacentHTML(
+    'beforebegin',
+    `<label class=field-label htmlFor=${id}><strong>${placeholder}</strong></label>`,
+  );
+});
