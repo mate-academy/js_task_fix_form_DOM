2d59545ed95274db86c78037a5ac4a10772a3d81
roman-logos-frontend <romanst235@gmail.com>
2024-07-09 15:21:28 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // write code here
+const inputs = document.querySelectorAll('form input');
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+  const nameAtribute = input.getAttribute('name').toLowerCase();
+  const nameText = nameAtribute.charAt(0).toUpperCase() + nameAtribute.slice(1);
+
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
+  label.textContent = `${nameText}`;
+  input.setAttribute('placeholder', `${nameText}`);
+  input.parentNode.insertBefore(label, input);
+});
