e5c7091b461bcd031e3eb8db6461ccc432eaeebe
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-07-09 12:49:56 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const fields = document.querySelectorAll('.field');
+const inputs = [...document.querySelectorAll('.field-text')];
+
+const inputIds = inputs.map((element) => element.id);
+
+function formatInputName(inputName) {
+  const formattedName = inputName.split(/(?=[A-Z])/);
+
+  return formattedName.join(' ').toUpperCase();
+}
+
+fields.forEach((fieldElement, index) => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', inputIds[index]);
+  fieldElement.insertAdjacentElement('afterbegin', label);
+});
+
+inputs.forEach((element) => {
+  const placeholderValue = formatInputName(element.name);
+
+  element.setAttribute('placeholder', placeholderValue);
+});
