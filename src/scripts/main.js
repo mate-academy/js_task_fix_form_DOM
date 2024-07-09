aa5413b684584721021c95c2c4f300ac5ed4f1e5
roman-logos-frontend <romanst235@gmail.com>
2024-07-09 17:26:05 +0300

    Made corrections of the mentor

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,8 +5,16 @@ const inputs = document.querySelectorAll('form input');
 
 inputs.forEach((input) => {
   const label = document.createElement('label');
-  const nameAtribute = input.getAttribute('name').toLowerCase();
-  const nameText = nameAtribute.charAt(0).toUpperCase() + nameAtribute.slice(1);
+  const nameAttribute = input.getAttribute('name').toLowerCase();
+  let nameText = '';
+
+  if (nameAttribute.endsWith('name')) {
+    nameText = nameAttribute.substring(0, nameAttribute.length - 4);
+    nameText = nameText.charAt(0).toUpperCase() + nameText.slice(1);
+    nameText += ' name';
+  } else {
+    nameText = nameAttribute.charAt(0).toUpperCase() + nameAttribute.slice(1);
+  }
 
   label.classList.add('field-label');
   label.setAttribute('for', input.id);
