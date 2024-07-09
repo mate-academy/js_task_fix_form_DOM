e974df070c196721a2cee5eacab85b43ed8c96de
Dmytro Moshkivskyi <dmosh@ukr.net>
2024-07-09 15:58:56 +0300

    js_task_fix_form_DOM by Dimoshua

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+function inputName(className) {
+  let result = className.replace(/([A-Z])/g, ' $1');
+
+  return result.charAt(0).toUpperCase() + result.slice(1).toLowerCase();
+}
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.textContent = inputName(input.name);
+
+  input.placeholder = inputName(input.name);
+  input.parentElement.append(label);
+});
