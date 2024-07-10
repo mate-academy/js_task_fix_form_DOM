3a8764116bcdb0bb03e16eaf12d081977826967b
Viktoriia Melnyk <melnychka1993@gmail.com>
2024-07-10 08:37:38 +0100

    added function for placeholders

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,32 @@
 'use strict';
 
+function formatPlaceholder(inputName) {
+  let formattedName = '';
+
+  for (let i = 0; i < name.length; i++) {
+    const char = name[i];
+
+    if (char === char.toUpperCase() && i !== 0) {
+      formattedName += ' ';
+    }
+    formattedName += char;
+  }
+
+  return formattedName.charAt(0).toUpperCase() + formattedName.slice(1);
+}
+
 const inputs = [...document.querySelectorAll('input')];
 
 inputs.forEach((input) => {
-  const InputName = input.getAttribute('name');
+  const inputName = input.getAttribute('name');
   const id = input.getAttribute('id');
 
   const label = document.createElement('label');
 
   label.classList.add('field-label');
-  label.textContent = InputName;
+  label.textContent = inputName;
   label.setAttribute('for', id);
   input.before(label);
 
-  input.setAttribute(
-    'placeholder',
-    InputName.charAt(0).toUpperCase() + InputName.slice(1),
-  );
+  input.setAttribute('placeholder', formatPlaceholder(inputName));
 });
