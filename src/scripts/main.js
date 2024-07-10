8a4e1b2a4ff05fceffcf3d10e5c0927f01011c9f
Viktoriia <vkkishko@gmail.com>
2024-07-10 16:02:50 +0300

    fixed problems

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,17 +3,24 @@
 const inputs = [...document.querySelectorAll('input')];
 
 inputs.forEach((el) => {
-  el.setAttribute('placeholder', el.name);
+  el.setAttribute('placeholder', prepareName(el.name));
 
   const label = document.createElement('label');
 
   label.className = 'field-label';
   label.setAttribute('for', el.id);
-  label.textContent = el.name;
+  label.textContent = prepareName(el.name);
 
   const inputParent = el.closest('div');
 
   inputParent.append(label);
 });
 
-// write code here
+function prepareName(str) {
+  const formattedName = str.split(/(?=[A-Z])/);
+  const sentence = formattedName.join(' ').toLowerCase();
+  const capitalizedSentence =
+    sentence.charAt(0).toUpperCase() + sentence.slice(1);
+
+  return capitalizedSentence;
+}
