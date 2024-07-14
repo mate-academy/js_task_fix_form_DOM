27f15aa85aa985b535313e21fcde92746335e166
valost <valerieostrovska@yahoo.com>
2024-07-14 12:22:50 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+function preparePlaceholderOrLabel(inputId) {
+  if (inputId === 'sign-up-name') {
+    return 'First Name';
+  }
+
+  const strToArr = inputId.split('-');
+
+  strToArr.splice(0, 2);
+
+  const capitalisedArr = strToArr.map((element) => {
+    return element.charAt(0).toUpperCase() + element.slice(1);
+  });
+
+  return capitalisedArr.join(' ');
+}
+
+const inputArr = document.querySelectorAll('input');
+
+inputArr.forEach((inputElement) => {
+  inputElement.placeholder = preparePlaceholderOrLabel(inputElement.id);
+});
+
+const inputDiv = document.querySelectorAll('.field');
+
+inputDiv.forEach((divElement) => {
+  const input = divElement.querySelector('input');
+  const label = document.createElement('label');
+
+  label.textContent = preparePlaceholderOrLabel(input.id);
+  label.classList.add('field-label');
+
+  divElement.prepend(label);
+});
