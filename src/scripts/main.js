dd4846c9595dd2057664f013ab74e049135c7879
Oleksii Shpak <willwildred@gmail.com>
2024-07-13 19:38:55 -0600

    implemented solution, finaly!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+function strFormat(str) {
+  let capitalize = str.charAt(0).toUpperCase() + str.slice(1);
+
+  capitalize = capitalize.replace(/([A-Z])/g, ' $1').trim();
+
+  return capitalize;
+}
+
+function createLabel() {
+  inputs.forEach((input) => {
+    const label = document.createElement('label');
+    const inputId = input.getAttribute('id');
+    const inputName = strFormat(input.getAttribute('name'));
+
+    label.setAttribute('class', 'field-label');
+    label.setAttribute(`for`, `${inputId}`);
+    label.textContent = `${inputName}`;
+
+    input.setAttribute(
+      'placeholder',
+      `${strFormat(input.getAttribute('name'))}`,
+    );
+
+    input.before(label);
+  });
+}
+
+createLabel();
