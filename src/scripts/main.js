8dd8c4a3c1f8c5fcec7766b6d73e2a5becd999a4
Yehor Gordiienko <egordienko319@gmail.com>
2024-07-14 19:29:46 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
-'use strict';
+// 'use strict';
 
-// write code here
+const allFields = document.querySelectorAll('input');
+
+allFields.forEach((inputs, index) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+
+  inputs.setAttribute('for', inputs.id);
+
+  label.textContent =
+    inputs.name.charAt(0).toUpperCase() + inputs.name.slice(1);
+
+  inputs.placeholder =
+    inputs.name.charAt(0).toUpperCase() + inputs.name.slice(1);
+
+  inputs.parentNode.insertBefore(label, inputs);
+});
