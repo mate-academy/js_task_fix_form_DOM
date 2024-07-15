ca9679304005356f7e5280d595a11e030a49af75
Ivan <tixehack@gmail.com>
2024-07-15 11:29:34 +0300

    feat: add logic to complete task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const allInputs = document.querySelectorAll('input');
+
+function addLableForInputs(inputs) {
+  inputs.forEach((input) => {
+    const label = document.createElement('label');
+
+    label.className = 'field-label';
+    label.htmlFor = input.id;
+    label.textContent = input.name;
+
+    input.placeholder = `${input.name[0].toUpperCase()}${input.name.slice(1)}`;
+    input.parentNode.insertBefore(label, input);
+  });
+}
+
+addLableForInputs(allInputs);
