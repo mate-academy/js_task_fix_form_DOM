113c0e947cc10e89180a2390a1f38c2d6b5ad2e5
Yehor Gordiienko <egordienko319@gmail.com>
2024-07-15 16:58:59 +0300

    add new fucntion

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,18 +2,22 @@
 
 const allFields = document.querySelectorAll('input');
 
+function makeSpace(text) {
+  const result = text.charAt(0).toUpperCase() + text.slice(1);
+
+  return result.replace(/([A-Z])/g, ' $1').trim();
+}
+
 allFields.forEach((inputs, index) => {
   const label = document.createElement('label');
 
   label.classList.add('field-label');
 
-  inputs.setAttribute('for', inputs.id);
+  label.setAttribute('for', inputs.id);
 
-  label.textContent =
-    inputs.name.charAt(0).toUpperCase() + inputs.name.slice(1);
+  inputs.placeholder = makeSpace(inputs.name);
 
-  inputs.placeholder =
-    inputs.name.charAt(0).toUpperCase() + inputs.name.slice(1);
+  label.textContent = makeSpace(inputs.name).toUpperCase();
 
   inputs.parentNode.insertBefore(label, inputs);
 });
