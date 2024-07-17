53e6a925e00d0aad8b74d36c65f0ecf14e553fd3
Olya <olyavidzi97@gmail.com>
2024-07-17 17:17:44 +0300

    added changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,9 +7,14 @@ inputs.forEach((input) => {
 
   label.className = 'field-label';
   label.htmlFor = input.id;
-  label.textContent = input.name.charAt(0).toUpperCase() + input.name.slice(1);
 
-  input.placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+  label.textContent =
+    input.name.charAt(0).toUpperCase() +
+    input.name.slice(1).replace(/([A-Z])/g, ' $1');
+
+  input.placeholder =
+    input.name.charAt(0).toUpperCase() +
+    input.name.slice(1).replace(/([A-Z])/g, ' $1');
 
   input.before(label);
 });
