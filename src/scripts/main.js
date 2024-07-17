3e8477978cf1130ca8ac7b90c509e676eb96c729
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-07-17 06:48:15 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('input').forEach((el) => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', el.id);
+  label.textContent = el.name;
+
+  el.before(label);
+  el.placeholder = el.name;
+});
