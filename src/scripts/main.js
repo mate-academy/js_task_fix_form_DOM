03cd8d71d059939fefc367c2e597ccf6a0b67691
Bohdan Ivankov <bodanich600@gmail.com>
2024-07-17 08:05:44 +0100

    fix label name

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,33 @@
 
 const inputs = document.querySelectorAll('.field-text');
 
+function getName(oldName) {
+  const parts = [];
+
+  if (Array.from(oldName).some((l) => l === l.toUpperCase())) {
+    for (let i = 1; i < oldName.length; i++) {
+      if (oldName[i] === oldName[i].toUpperCase()) {
+        parts.push(oldName.slice(0, i));
+        parts.push(oldName.slice(i));
+      }
+    }
+  } else {
+    return oldName;
+  }
+
+  return parts.join(' ');
+}
+
 inputs.forEach((input) => {
-  input.setAttribute('placeholder', input.name.toUpperCase());
+  const newName = getName(input.name);
+
+  input.setAttribute('placeholder', newName);
 
   const label = document.createElement('label');
 
   label.className = 'field-label';
   label.setAttribute('for', input.id);
-  label.textContent = input.name;
+  label.textContent = newName;
 
   input.before(label);
 });
