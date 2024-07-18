120e3c87ea187d3aed488de7a612a50fd2b3e527
my-git-username <118355685+my-git-username@users.noreply.github.com>
2024-07-18 17:32:19 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+
+const input = document.querySelectorAll('input');
+
+input.forEach((item) => {
+  const label = document.createElement('label');
+
+  const separatedName = item.name.split(/(?=[A-Z])/).join(' ');
+
+  label.className = 'field-label';
+  label.htmlFor = item.id;
+  label.textContent = separatedName;
+
+  item.placeholder =
+    separatedName.charAt(0).toUpperCase() + separatedName.slice(1);
+
+  item.before(label);
+});
