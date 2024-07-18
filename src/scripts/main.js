00d22d0de65782f7ece12dd8d094309dd2c644b1
Julia Churkina <curkinau980@gmail.com>
2024-07-18 16:40:15 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const input = [...document.querySelectorAll('input')];
+
+for (const el of input) {
+  const lebel = document.createElement('lebel');
+
+  el.setAttribute('placeholder', el.name.toUpperCase());
+  lebel.textContent = el.name;
+  lebel.className = 'field-label';
+  lebel.setAttribute('for', el.id);
+  el.before(lebel);
+}
