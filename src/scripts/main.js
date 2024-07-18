f141fd3d69c954e5f4a2c3fe288234e4ec265325
Oleksandra Kuziura <radioday2014@gmail.com>
2024-07-18 12:57:50 +0300

    add placeholder and label with DOM manipulation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+// inputs
+const inputs = document.querySelectorAll('input');
+
+// add placeholder,label
+
+inputs.forEach(getPlaceholder);
+inputs.forEach(addLabel);
+
+function getPlaceholder(el) {
+  el.placeholder = el.getAttribute('name');
+}
+
+function addLabel(el) {
+  const label = document.createElement('label');
+
+  label.setAttribute('for', el.getAttribute('id'));
+  label.setAttribute('class', 'field-label');
+  label.innerText = el.getAttribute('name');
+  el.before(label);
+}
+
