74fe6ac9ecef2a2d6189b896612f0d8f566be0c8
Katya <evil.666.buny@gmail.com>
2024-07-19 18:39:34 +0300

    script added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const forms = document.querySelectorAll('form');
+
+  forms.forEach((form) => {
+    const inputs = form.querySelectorAll('input');
+
+    inputs.forEach((input) => {
+      const label = document.createElement('label');
+
+      label.className = 'field-label';
+      label.htmlFor = input.id;
+      label.textContent = capitalize(input.name);
+
+      input.placeholder = capitalize(input.name);
+
+      input.parentNode.insertBefore(label, input);
+    });
+  });
+
+  function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+});
