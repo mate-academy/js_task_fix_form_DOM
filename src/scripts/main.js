0455901bf6c5a09753298e0b557ea88768473fd4
Nadinnnka <tymoshyk02@gmail.com>
2024-07-21 15:37:44 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
+/* eslint-disable prettier/prettier */
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+inputs.forEach(input => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+
+  label.setAttribute('for', input.id);
+
+  const placeholderText = input.name
+    .replace(/([A-Z])/g, ' $1')
+    .replace(/^./, str => str.toUpperCase());
+
+  label.textContent = placeholderText;
+  input.setAttribute('placeholder', placeholderText);
+  input.parentElement.insertBefore(label, input);
+});
