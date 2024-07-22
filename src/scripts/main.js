c47aa1a9aa4916e70bc6f535365a4563a97a80ab
Serhii Babskyi <sergbald99@gmail.com>
2024-07-22 22:16:51 +0300

    word separation function has been implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,12 +9,19 @@ inputs.forEach((input) => {
   input.insertAdjacentHTML(
     'beforebegin',
     `<label class='field-label' for=${inputId}>
-      ${inputName}
+      ${formatString(inputName)}
     </label>`,
   );
 
-  input.setAttribute(
-    'placeholder',
-    `${inputName[0].toUpperCase() + inputName.slice(1)}`,
-  );
+  input.setAttribute('placeholder', formatString(inputName));
 });
+
+function formatString(str) {
+  if (!str) {
+    return '';
+  }
+
+  const spacedString = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+
+  return spacedString.charAt(0).toUpperCase() + spacedString.slice(1);
+}
