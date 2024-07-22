edda4709a392b96a4172b55667a9d6af2293635a
Your Name <ruslankurman13@gmail.com>
2024-07-22 17:22:58 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  const forms = document.querySelectorAll('form');
+
+  forms.forEach((form) => {
+    const inputs = form.querySelectorAll('input');
+
+    inputs.forEach((input) => {
+      const label = document.createElement('label');
+
+      label.className = 'field-label';
+      label.setAttribute('for', input.id);
+
+      const labelText = capitalize(
+        input.name.replace(/([A-Z])/g, ' $1').trim(),
+      );
+
+      label.textContent = labelText;
+
+      input.placeholder = capitalize(input.name);
+      input.parentNode.insertBefore(label, input);
+    });
+  });
+});
