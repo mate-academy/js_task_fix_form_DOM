264f4d642a244a8289eff2f176c5a8e370bbde70
Valentyn Radobenko <valikradobenko@gmail.com>
2024-07-23 17:12:56 +0300

    add camelToSeparate function and use it

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,10 @@ const inputs = document.querySelectorAll('.field-text');
 
 for (let i = 0; i < inputs.length; i++) {
   const label = document.createElement('label');
-  const inputName = inputs[i].getAttribute('name');
-  const placeholder = inputName[0].toUpperCase() + inputName.substring(1);
+  const inputName = camelToSeparate(inputs[i].getAttribute('name'));
+  let placeholder = inputName[0].toUpperCase() + inputName.substring(1);
+
+
 
   label.setAttribute('for', inputs[i].getAttribute('id'));
   label.className = 'field-label';
@@ -13,4 +15,21 @@ for (let i = 0; i < inputs.length; i++) {
   inputs[i].after(label);
 
   inputs[i].setAttribute('placeholder', placeholder);
+}
+
+function camelToSeparate(word) {
+  if (word.length === 0) {
+    return '';
+  }
+
+  let wordArr = word.split('');
+
+  for (let i = 0; i < wordArr.length; i++) {
+    if (wordArr[i].toUpperCase() === wordArr[i]) {
+      wordArr.splice(i, 0, ' ');
+      i++;
+    }
+  }
+
+  return wordArr.join('');
 }
