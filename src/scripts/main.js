dcccbcb0a3cbbf292d474401e1bd2d7d1aa13b7f
svyatoslav <svyatoslavik9@gmail.com>
2024-07-23 15:16:31 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
 // write code here
+const forms = document.querySelectorAll('form');
+const tagName = 'input';
+
+const capitalize = (text) => {
+  return text[0].toUpperCase() + text.slice(1);
+};
+
+const FieldLabel = ({ id, name }) => {
+  const label = document.createElement('label');
+  label.classList.add('field-label');
+  label.textContent = name;
+  label.setAttribute('for', id);
+
+  return label;
+};
+
+const createLabels = () => {
+  for (const form of forms) {
+    [...form.elements].forEach((input) => {
+      const { id, name } = input;
+
+      if (input.tagName === tagName.toUpperCase()) {
+        const fieldLabel = FieldLabel({ id, name });
+        input.placeholder = capitalize(name);
+
+        input.parentElement.prepend(fieldLabel);
+      }
+    });
+  }
+};
+
+createLabels();
