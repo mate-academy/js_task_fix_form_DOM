ee017a81920d883106cb1362504ec82c10bcad21
MaksymKolesnyk <maksymka@icloud.com>
2024-07-25 18:26:37 -0400

    add method to format name

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,10 +5,24 @@ const ar = document.querySelectorAll('.field');
 ar.forEach((el) => {
   const label = document.createElement('label');
   const input = el.lastElementChild;
+  const correctLabelName = getCorrectText(input.getAttribute('name'));
 
   label.className = 'field-label';
-  input.setAttribute('placeholder', input.getAttribute('name'));
+  input.setAttribute('placeholder', correctLabelName);
   label.setAttribute('for', input.getAttribute('id'));
-  label.textContent = input.getAttribute('name');
+  label.textContent = correctLabelName;
   el.prepend(label);
 });
+
+function getCorrectText(value) {
+  const separator = value.match(/[A-Z]/);
+  const splitedWord = value.split(separator);
+
+  if (splitedWord.length > 1) {
+    splitedWord[1] = separator + splitedWord[1];
+  }
+
+  const word = splitedWord.join(' ');
+
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
