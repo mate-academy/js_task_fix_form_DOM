e9344b4c96402532f2d25456c28983cc4b63006a
Vasyl Chukhrai <wasik1995@mail.com>
2024-07-25 14:00:41 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+inputs.forEach((input) => {
+  const inputName = `${input.getAttribute('name')}`;
+  const placeholder = inputName.replace(
+    inputName[0],
+    inputName[0].toUpperCase(),
+  );
+
+  const creatLable = document.createElement('label');
+
+  creatLable.classList.add('field-label');
+  creatLable.setAttribute('for', `${input.getAttribute('id')}`);
+  creatLable.textContent = `${input.getAttribute('name').toUpperCase()}`;
+
+  input.setAttribute('placeholder', placeholder);
+  input.before(creatLable);
+});
