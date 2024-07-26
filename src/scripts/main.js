a8a3d987a8c7f946bfe417c3a2c25f7df535b46e
Denys209818 <deniskravcuk63@gmail.com>
2024-07-27 01:22:26 +0300

    Load solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 // write code here
+const fields = document.getElementsByClassName('field-text');
+
+function goToUpperCase(str) {
+  return str.slice(0, 1).toUpperCase() + str.slice(1);
+}
+
+for (let i = 0; i < fields.length; i++) {
+  const label = document.createElement('label');
+  const nameField = fields[i].getAttribute('name');
+  const id = fields[i].getAttribute('id');
+
+  label.setAttribute('for', id);
+  label.setAttribute('class', 'field-label');
+  fields[i].setAttribute('placeholder', goToUpperCase(nameField));
+  label.textContent = nameField.toUpperCase();
+
+  fields[i].insertAdjacentElement('beforebegin', label);
+}
