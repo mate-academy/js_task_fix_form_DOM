69db7a395517646cba0896416f636c046a40700b
olshum <olshum8@gmail.com>
2024-07-28 19:38:09 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+function capitalize(str) {
+  return str[0].toUpperCase() + str.slice(1);
+}
+
+const forms = document.querySelectorAll('form');
+
+forms.forEach((form) => {
+  const inputs = form.querySelectorAll('input');
+
+  inputs.forEach((input) => {
+    const label = document.createElement('label');
+
+    label.classList.add('field-label');
+    label.htmlFor = input.id;
+    label.textContent = capitalize(input.name);
+
+    input.placeholder = capitalize(input.name);
+
+    const inputParent = input.parentElement;
+
+    inputParent.insertBefore(label, input);
+  });
+});
