51f3db5da8223d670b2b99b0a8f4995d21168244
Max <max@energyhill.com>
2024-07-31 11:12:22 -0400

    v 1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  input.setAttribute(
+    'placeholder',
+    input.name[0].toUpperCase() + input.name.slice(1),
+  );
+
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+
+  input.insertAdjacentElement('beforebegin', label);
+}
