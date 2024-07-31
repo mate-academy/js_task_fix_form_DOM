876a6fee34597c369c893a0600e88acb14f889da
Serhii Khobotov <hobbitura@gmail.com>
2024-07-31 12:44:01 +0300

    My solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+inputs.forEach((input) => {
+  const inputName = `${input.getAttribute('name')}`.replace('N', ' N');
+  const placeholder = inputName.replace(inputName[0], inputName[0].toUpperCase);
+
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', `${input.getAttribute('id')}`);
+  label.textContent = `${inputName.toUpperCase()}`;
+
+  input.setAttribute('placeholder', placeholder);
+  input.before(label);
+});
