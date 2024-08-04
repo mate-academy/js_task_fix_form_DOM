0a58eb1057546d4052277d40d6974ac4b9bcb640
Maksim Atanasov <atanasovmaksim56@gmail.com>
2024-08-04 17:23:22 +0300

    code improve

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,5 +4,10 @@ const inputs = document.querySelectorAll('input');
 
 inputs.forEach((input) => {
   input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
-  input.outerHTML = `<label for="${input.id}" class="field-label">${input.name}</label>${input.outerHTML}`;
+
+  const field = input.parentElement;
+  const label = document.createElement('label');
+
+  label.innerHTML = `<label for="${input.id}" class="field-label">${input.name}</label>`;
+  field.appendChild(label);
 });
