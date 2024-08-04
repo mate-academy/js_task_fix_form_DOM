a91daca1dedc45cb89ff919e2f2646340b421685
Maksim Atanasov <atanasovmaksim56@gmail.com>
2024-08-04 17:08:46 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((input) => {
+  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+  input.outerHTML = `<label for="${input.id}" class="field-label">${input.name}</label>${input.outerHTML}`;
+});
