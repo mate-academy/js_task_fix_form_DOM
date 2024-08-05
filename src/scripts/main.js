a69421993087bb3b7c8a4f18e8ca9fd7f80dfb1c
Oleksii Symonets <alex.simonets@gmail.com>
2024-08-05 19:53:59 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const inputElements = document.getElementsByTagName('input');
+
+for (let i = 0; i < inputElements.length; i++) {
+  const input = inputElements[i];
+  const inputId = input.getAttribute('id');
+  const inputName = input.getAttribute('name');
+  const label = document.createElement('label');
+
+  label.setAttribute('class', 'field-label');
+  label.setAttribute('for', inputId);
+  label.textContent = inputName;
+  input.setAttribute('placeholder', capitalize(inputName));
+
+  input.before(label);
+}
+
+function capitalize(text) {
+  return text[0].toUpperCase() + text.slice(1, text.length);
+}
