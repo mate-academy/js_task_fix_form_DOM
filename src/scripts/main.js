e19f24ce1ccf76b13183bf039168ce874d680a40
Maksim Atanasov <atanasovmaksim56@gmail.com>
2024-08-05 09:38:22 +0300

    placeholder changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,9 @@
 const inputs = document.querySelectorAll('input');
 
 inputs.forEach((input) => {
-  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+  input.placeholder =
+    input.name[0].toUpperCase() +
+    input.name.slice(1).replace(/([a-z])([A-Z])/g, '$1 $2');
 
   const field = input.parentElement;
   const label = document.createElement('label');
