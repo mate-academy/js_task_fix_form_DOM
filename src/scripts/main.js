f5d2270b6309aa98b25cb91f5bee6ade1fe11f78
Ostap Hedz <lordanvil18@gmail.com>
2024-08-05 12:14:56 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((input) => {
+  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+
+  const label = document.createElement('label');
+  const parentEl = input.parentElement;
+
+  label.className = 'field-label';
+  label.textContent = input.name;
+  label.setAttribute('for', input.id);
+  parentEl.append(label);
+});
