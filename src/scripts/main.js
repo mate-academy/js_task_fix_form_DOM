693b02d381627eb9c63189f949ba35202f67dd17
Gennadiy Boreyko <gennadiyboreyko0@gmail.com>
2024-08-05 14:12:01 +0300

    add fix form DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+  const inputName = input.getAttribute('name');
+  const inputId = input.getAttribute('id');
+
+  const putText = convertCamel(inputName);
+
+  label.textContent = putText;
+  label.htmlFor = inputId;
+  label.classList.add('field-label');
+  input.setAttribute('placeholder', putText);
+  input.before(label);
+}
+
+function capitalizeWord(word) {
+  return word ? word[0].toUpperCase() + word.slice(1) : '';
+}
+
+function convertCamel(camelWord) {
+  const lowerWord = camelWord.toLowerCase();
+  const arrayIndexs = [0];
+  let result = '';
+
+  for (let i = 0; i < camelWord.length; i++) {
+    if (camelWord[i] !== lowerWord[i]) {
+      arrayIndexs.push(i);
+    }
+  }
+
+  arrayIndexs.push(camelWord.length);
+
+  for (let k = 0; k < arrayIndexs.length - 1; k++) {
+    result += lowerWord.slice(arrayIndexs[k], arrayIndexs[k + 1]) + ' ';
+  }
+
+  const finalResult = result.trim();
+
+  return capitalizeWord(finalResult);
+}
