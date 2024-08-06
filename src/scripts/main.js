99ab35b3ac9de32b1d63b20008ca35c1479b8367
Victor Komara <vitioklss@gmail.com>
2024-08-06 15:19:50 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const arrInputs = document.querySelectorAll('input');
+
+for (let i = 0; i < arrInputs.length; i++) {
+  const item = document.createElement('label');
+
+  item.textContent = arrInputs[i].name;
+  item.setAttribute('for', `${arrInputs[i].id}`);
+  item.setAttribute('class', 'field-label');
+  arrInputs[i].parentElement.append(item);
+
+  arrInputs[i].setAttribute(
+    'placeholder',
+    `${arrInputs[i].name[0].toUpperCase()}${arrInputs[i].name.substring(1)}`,
+  );
+}
