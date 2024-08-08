177cd6a013d0b18f26487903533d2e1873c13111
Mainaiev <farid.mainaiev@proton.me>
2024-08-08 20:33:51 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write code here
+
+const inputs = document.querySelectorAll('input');
+
+document.addEventListener('DOMContentLoaded', () => {
+  inputs.forEach((input) => {
+    const label = document.createElement('label');
+
+    label.classList.add('field-label');
+    label.setAttribute('for', input.id);
+    label.textContent = input.name;
+
+    input.placeholder =
+      input.name.charAt(0).toUpperCase() + input.name.slice(1);
+    input.parentNode.insertBefore(label, input);
+  });
+});
