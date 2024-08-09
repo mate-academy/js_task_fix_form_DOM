28e8f35ae477aba2895933700ec4f6597682e4c0
Yevhenii Haidukov <jeniagaidukov@gmail.com>
2024-08-09 13:35:24 +0300

    fix_form Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 // write code here
+const fields = document.querySelectorAll('.field');
+const inputs = document.querySelectorAll('input');
+
+for (let i = 0; i < fields.length; i++) {
+  const labelElement = document.createElement('label');
+
+  labelElement.classList.add('field-label');
+  labelElement.textContent = `${inputs[i].name}`;
+  labelElement.setAttribute('for', `${inputs[i].id}`);
+  fields[i].appendChild(labelElement);
+
+  const placeholderName = inputs[i].name;
+  const placeholderText =
+    placeholderName.charAt(0).toUpperCase() + placeholderName.slice(1);
+
+  inputs[i].setAttribute('placeholder', placeholderText);
+}
