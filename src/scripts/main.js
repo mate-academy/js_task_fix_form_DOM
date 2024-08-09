54ab4b2babf77fd2e97f12f0927be49436d504b6
Yevhenii Haidukov <jeniagaidukov@gmail.com>
2024-08-09 14:27:21 +0300

    add space to label and placeholder

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,11 +8,14 @@ for (let i = 0; i < fields.length; i++) {
   const labelElement = document.createElement('label');
 
   labelElement.classList.add('field-label');
-  labelElement.textContent = `${inputs[i].name}`;
+
+  const addSpace = inputs[i].name.replace(/([A-Z])/g, ' $1').trim();
+
+  labelElement.textContent = addSpace;
   labelElement.setAttribute('for', `${inputs[i].id}`);
   fields[i].appendChild(labelElement);
 
-  const placeholderName = inputs[i].name;
+  const placeholderName = addSpace;
   const placeholderText =
     placeholderName.charAt(0).toUpperCase() + placeholderName.slice(1);
 
