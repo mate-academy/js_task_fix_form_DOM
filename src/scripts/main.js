0f53355e1076473285acc2dbe816ce92b533fa57
iryna lysko <goiralysko@gmail.com>
2024-08-12 16:40:20 +0300

    fix labels for inputs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form > * > input');
+
+for (const item of inputs) {
+  const label = document.createElement('label');
+  const formattedName =
+    item.name.charAt(0).toUpperCase() +
+    item.name
+      .replace(/([a-z])([A-Z])/g, '$1 $2')
+      .toLowerCase()
+      .slice(1);
+
+  label.className = 'field-label';
+  label.htmlFor = item.id;
+  label.innerText = item.name;
+  item.placeholder = formattedName;
+
+  item.before(label);
+}
