58df430806d76f25dda209126f4e65abe4480e14
iryna lysko <goiralysko@gmail.com>
2024-08-12 16:48:59 +0300

    fix solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,17 +4,22 @@ const inputs = document.querySelectorAll('form > * > input');
 
 for (const item of inputs) {
   const label = document.createElement('label');
-  const formattedName =
-    item.name.charAt(0).toUpperCase() +
-    item.name
-      .replace(/([a-z])([A-Z])/g, '$1 $2')
-      .toLowerCase()
-      .slice(1);
+  const formattedName = camelToNormal(item.name);
 
   label.className = 'field-label';
   label.htmlFor = item.id;
-  label.innerText = item.name;
+  label.innerText = formattedName;
   item.placeholder = formattedName;
 
   item.before(label);
+}
+
+function camelToNormal(text) {
+  return (
+    text.charAt(0).toUpperCase() +
+    text
+      .replace(/([a-z])([A-Z])/g, '$1 $2')
+      .toLowerCase()
+      .slice(1)
+  );
 }
