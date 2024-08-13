6f519b4ea2ea2c1b0d7d3aad88d4ebc5742506e2
Hlib Vakhrameiev <g.vakhrameev@icloud.com>
2024-08-13 11:24:18 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const formArr = document.querySelectorAll('input');
+
+setPlaceholder(formArr);
+createElement(formArr);
+
+function setPlaceholder(inputs) {
+  inputs.forEach((input) => {
+    const nameHolder =
+      input.getAttribute('name').charAt(0).toUpperCase() +
+      input.getAttribute('name').slice(1);
+
+    input.setAttribute('placeholder', nameHolder);
+  });
+}
+
+function createElement(elements) {
+  elements.forEach((el) => {
+    const label = document.createElement('label');
+    const textLabel = `${el.getAttribute('name')}`.toUpperCase();
+
+    label.setAttribute('class', 'field-label');
+
+    el.before(label, textLabel);
+  });
+}
