9777c4249bc008067ba390fa77cb80a3850998f0
Rostyslav <rostislavkpi@gmail.com>
2024-08-14 20:15:58 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+inputs.forEach((input) => {
+  /*
+    RegExp ?=[A-Z] mean all characters from A to Z with positive loockahead
+    matches a group after the main expression without including it in the result
+
+    https://regexr.com/
+  */
+  const inputName = input.name.split(/(?=[A-Z])/).join(' ');
+
+  input.insertAdjacentHTML(
+    'beforebegin',
+    `<label for="${input.id}" class="field-label">
+      ${inputName}
+    </label>`,
+  );
+
+  input.placeholder = `${inputName[0].toLocaleUpperCase() + inputName.slice(1).toLocaleLowerCase()}`;
+});
