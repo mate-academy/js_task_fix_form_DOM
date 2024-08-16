c1121ade45dd9849174ce839a838fede695565e3
Kateryna <moskkat40@gmail.com>
2024-08-16 13:58:03 +0200

    add task fix form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+function addLabel() {
+  inputs.forEach((input) => {
+    const label = document.createElement('label');
+
+    label.classList.add('field-label');
+    label.setAttribute('for', input.id);
+    label.textContent = input.name;
+    input.before(label);
+  });
+}
+
+function addPlaceholder() {
+  inputs.forEach((input) => {
+    input.placeholder = input.name.replace(
+      input.name[0],
+      input.name[0].toUpperCase(),
+    );
+  });
+}
+
+addLabel();
+addPlaceholder();
