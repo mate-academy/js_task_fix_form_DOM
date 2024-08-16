799b8a22b8dcb5098f0589cb48a318b17cae774d
Vlad Zahorovskyi <zagorovskii12@gmail.com>
2024-08-16 19:35:05 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+function capitalize(text) {
+  return text[0].toUpperCase() + text.slice(1);
+}
+
+inputs.forEach((el) => {
+  const label = document.createElement('label');
+
+  label.textContent = el.getAttribute('name').toLowerCase();
+
+  label.className = 'field-label';
+
+  label.setAttribute('for', el.getAttribute('id'));
+
+  el.setAttribute('placeholder', capitalize(label.textContent));
+
+  el.parentNode.prepend(label);
+});
