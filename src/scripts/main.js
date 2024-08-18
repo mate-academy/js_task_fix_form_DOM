56c95a143c23bc34579e5f911220a218a61c8cd2
Vlad Tkachuk <vlad.tkachuk.04@gmail.com>
2024-08-18 12:40:11 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((input) => {
+  const inputName = input.getAttribute('name');
+  const id = input.getAttribute('id');
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', id);
+  label.innerText = inputName;
+
+  input.setAttribute('placeholder', capitalize(inputName));
+  input.parentElement.append(label);
+});
