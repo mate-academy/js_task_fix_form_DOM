72ddd2e522853cb61f22cb8db7ef5f4596dc3cba
unknown <maksoleksyuk@gmail.com>
2024-08-20 14:32:02 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+function addSpacesBeforeUpperLetters(input) {
+  let result = '';
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === input[i].toUpperCase()) {
+      result += ' ';
+    }
+
+    result += input[i].toUpperCase();
+  }
+
+  return result;
+}
+
+const inputs = Array.from(document.querySelectorAll('input'));
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.textContent = addSpacesBeforeUpperLetters(input.name);
+  label.style.fontWeight = 'bold';
+  label.setAttribute('for', input.id);
+
+  input.setAttribute('placeholder', addSpacesBeforeUpperLetters(input.name));
+
+  input.parentElement.insertBefore(label, input);
+});
