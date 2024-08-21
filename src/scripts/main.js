20c00035d3f38bdd5fd20d9fdeae031bd2457e04
Stas <staskapica93@gmail.com>
2024-08-21 19:02:10 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+//  <label for="cheese">Do you like cheese?</label>
+
+// debugger;
+
+const inputs = [...document.querySelectorAll('input')];
+
+inputs.forEach((input) => {
+  if (input.name.includes('Name')) {
+    input.name = input.name.replace('t', 't ');
+  }
+  // well... FirstName or First Name ?
+
+  input.setAttribute('placeholder', `${input.name}`);
+
+  const LABEL = `<label class="field-label" for= ${input.id} > ${input.name} </label>`;
+
+  input.insertAdjacentHTML('beforebegin', LABEL);
+});
