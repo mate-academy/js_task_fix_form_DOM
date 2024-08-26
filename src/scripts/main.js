47f52bcba57c0cc070ac8acca449c6e030f43969
Yevhenii Lutsenko <evgenlutscenko@icloud.com>
2024-08-26 20:27:24 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const forms = document.querySelectorAll('form');
+
+forms.forEach((form) => {
+  const fields = form.querySelectorAll('.field');
+
+  fields.forEach((field) => {
+    const input = field.firstElementChild;
+
+    input.placeholder = input.name.toUpperCase();
+
+    field.innerHTML = `
+      <label for="${input.id}" class="field-label">${input.name}</label>
+    `;
+
+    field.append(input);
+  });
+});
