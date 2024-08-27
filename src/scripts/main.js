55e146562987b1fad7cd3f9f40cfca42ff133989
Дикун Дмитрий Николаевич <dikun.dmitriy@ankor.org>
2024-08-27 13:18:45 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const inputsElem = Array.from(document.querySelectorAll('input'));
+
+function initializ() {
+  for (const input of inputsElem) {
+    const atributeName =
+      input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+    const label = document.createElement('label');
+
+    label.classList.add('field-label');
+    label.setAttribute('for', input.id);
+    label.textContent = atributeName;
+
+    input.placeholder = atributeName;
+
+    input.insertAdjacentElement('beforebegin', label);
+  }
+}
+
+initializ();
