638bf54e8e34c3d6fca307f2c5b7ba638e4756e3
Maksym Mohyla <greenparksquad@gmail.com>
2024-08-27 15:27:51 +0300

    fix form solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const inpunts = Array.from(document.querySelectorAll('input'));
+
+inpunts.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+
+  input.parentNode.insertBefore(label, input);
+
+  input.placeholder =
+    input.name.charAt(0).toUpperCase() + input.name.slice(1).toLowerCase();
+});
