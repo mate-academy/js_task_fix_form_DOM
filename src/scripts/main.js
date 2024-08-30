68cadadcc3d72c82bd17e417d5f6fd296f4edeb1
Dmytro Ivasenko <dmtivasenko@gmail.com>
2024-08-31 01:42:29 +0700

    add labels and placeholders

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const fieldElements = document.querySelectorAll('.field');
+const inputElements = document.querySelectorAll('input');
+
+fieldElements.forEach((field) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', field.firstElementChild.id);
+  label.textContent = field.firstElementChild.name;
+  field.prepend(label);
+});
+
+inputElements.forEach((input) => {
+  input.placeholder = input.name.toUpperCase();
+});
