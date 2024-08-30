905dab6712d4a0a06df45c2531c35b13876ddbfd
Dmytro Ivasenko <dmtivasenko@gmail.com>
2024-08-31 03:42:23 +0700

    fix naming of labels and placeholders

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,42 +3,35 @@
 const fieldElements = document.querySelectorAll('.field');
 const inputElements = document.querySelectorAll('input');
 
+function setName(element) {
+  let itemName = '';
+
+  for (let i = 0; i < element.name.length; i++) {
+    if (i === 0) {
+      itemName += element.name[i].toUpperCase();
+      continue;
+    }
+
+    if (element.name[i] !== element.name[i].toLowerCase()) {
+      itemName += ' ' + element.name[i];
+    } else {
+      itemName += element.name[i];
+    }
+  }
+
+  return itemName;
+}
+
 fieldElements.forEach((field) => {
   const label = document.createElement('label');
 
   label.classList.add('field-label');
   label.setAttribute('for', field.firstElementChild.id);
 
-  let labelName = '';
-
-  for (let i = 0; i < field.firstElementChild.name.length; i++) {
-    if (
-      field.firstElementChild.name[i] !==
-      field.firstElementChild.name[i].toLowerCase()
-    ) {
-      labelName += ' ' + field.firstElementChild.name[i];
-    } else {
-      labelName += field.firstElementChild.name[i];
-    }
-  }
-  label.textContent = labelName;
+  label.textContent = setName(field.firstElementChild);
   field.prepend(label);
 });
 
 inputElements.forEach((input) => {
-  let placeholderName = '';
-
-  for (let i = 0; i < input.name.length; i++) {
-    if (i === 0) {
-      placeholderName += input.name[i].toUpperCase();
-      continue;
-    }
-
-    if (input.name[i] !== input.name[i].toLowerCase()) {
-      placeholderName += ' ' + input.name[i];
-    } else {
-      placeholderName += input.name[i];
-    }
-  }
-  input.placeholder = placeholderName;
+  input.placeholder = setName(input);
 });
