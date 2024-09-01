83dba82e1e22bd5e75db1fdc95fb3573a3025f9e
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2024-09-01 20:40:37 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+  input.placeholder = input.name;
+
+  input.after(label);
+});
