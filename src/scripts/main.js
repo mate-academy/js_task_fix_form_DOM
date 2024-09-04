5c243291614d90d1843a57a800f8253b01687ab2
Anna Agerone <olesnevych12@gmail.com>
2024-09-04 11:14:43 -0500

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
-'use strict';
+const inputs = document.querySelectorAll('input');
 
-// write code here
+inputs.forEach((input) => {
+  const nameofInput = `${input.getAttribute('name')}`.replace('N', ' N');
+
+  const placeholder = nameofInput.replace(
+    nameofInput[0],
+    nameofInput[0].toUpperCase(),
+  );
+
+  const newLable = document.createElement('label');
+
+  newLable.classList.add('field-label');
+  newLable.setAttribute('for', `${input.getAttribute('id')}`);
+
+  newLable.textContent = `${nameofInput.toUpperCase()}`;
+
+  input.setAttribute('placeholder', placeholder);
+  input.before(newLable);
+});
