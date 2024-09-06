43611f17eb13bc33920db1aa4d55cfc29f1b4098
Iryna Zahorodnia <irishas25@ukr.net>
2024-09-06 11:49:26 +0300

    fix form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+function styleFormat(word) {
+  function upperToGapLower(match) {
+    return ' ' + match.toLowerCase();
+  }
+
+  const formatedWord = word.replace(/[A-Z]/g, upperToGapLower);
+
+  return formatedWord[0].toUpperCase() + formatedWord.slice(1);
+}
+
+const inputs = [...document.querySelectorAll('input')];
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.setAttribute('class', 'field-label');
+  label.setAttribute('for', input.id);
+  label.textContent = styleFormat(input.name);
+  input.before(label);
+
+  input.setAttribute('placeholder', styleFormat(input.name));
+});
