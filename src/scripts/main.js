1b1c0e3a7bb136e285e36dd20eafbe01203e7c26
Stepan Bahrynivskyi <bagrunivskiystepan@gmail.com>
2024-09-06 08:42:59 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const inputs = [
+  ...document.querySelectorAll(
+    'input:not([type="submit"]):not([type="button"])',
+  ),
+];
+
+inputs.forEach((input) => {
+  if (!input.id || !input.name) {
+    return;
+  }
+
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+
+  const labelName = input.name
+    .replace(/([A-Z])/g, ' $1')
+    .replace(/^./, (str) => str.toUpperCase());
+
+  label.textContent = labelName;
+  input.placeholder = labelName;
+
+  input.before(label);
+});
