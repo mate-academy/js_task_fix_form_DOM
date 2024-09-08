8c38879d266f62a328694d1a9173b9da942bad08
Tetyana Chuhai <chugaytatyana@gmail.&git config --global user.email chugaytatyana@gmail.>
2024-09-08 11:57:12 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const allInputs = [...document.querySelectorAll('input')];
+
+allInputs.forEach((input) => {
+  input.placeholder = input.name[0].toLocaleUpperCase() + input.name.slice(1);
+
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+  input.parentElement.prepend(label);
+});
