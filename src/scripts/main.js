5a8011f67cdd2a76b513a55e6dd6a4b765246860
Artem-Brui <artembryj@gmail.com>
2024-09-08 14:59:59 +0200

    update input names

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,32 @@
 // write code here
 const inputs = document.querySelectorAll('input');
 
+const wordPrettier = (string) => {
+  return string
+    .split('')
+    .map((sym, i, arr) => {
+      if (i === 0) {
+        return sym.toUpperCase();
+      }
+
+      if (i !== arr.length - 1 && arr[i + 1] === arr[i + 1].toUpperCase()) {
+        return sym + ' ';
+      }
+
+      return sym;
+    })
+    .join('');
+};
+
 for (const input of inputs) {
   const label = document.createElement('label');
 
+  const inputName = wordPrettier(input.getAttribute('name'));
+
   label.className = 'field-label';
   label.setAttribute('for', input.id);
-  label.textContent = input.getAttribute('name');
+  label.textContent = inputName;
 
-  input.setAttribute('placeholder', input.getAttribute('name'));
+  input.setAttribute('placeholder', inputName);
   input.parentNode.prepend(label);
 }
