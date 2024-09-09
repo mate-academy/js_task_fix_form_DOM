0133213e3b4447717a549c61ca3eafe61957215d
Maksym Fedorchuk <maxeffect.apollo@gmail.com>
2024-09-09 19:54:53 +0300

    done, finally

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
 // write code here
+function camelCaseToReadable(str) {
+  return str
+    .replace(/([a-z])([A-Z])/g, '$1 $2')
+    .replace(/^./, function (match) {
+      return match.toUpperCase();
+    });
+}
+
+const pageForms = document.querySelectorAll('form');
+
+function addLabels(forms) {
+  forms.forEach((form) => {
+    const inputs = form.querySelectorAll('input');
+
+    inputs.forEach((formInput) => {
+      formInput.insertAdjacentHTML(
+        'beforebegin',
+        '<label class="field-label" for="' +
+          formInput.id +
+          '">' +
+          formInput.name +
+          '</label>',
+      );
+
+      formInput.placeholder = camelCaseToReadable(formInput.name);
+    });
+  });
+}
+
+addLabels(pageForms);
