d9124f32042008b80e63bcbfca84ce9fa9e5bf8c
Roman Martseniuk <roman.martseniuk.developer@gmail.com>
2024-09-12 19:01:06 +0300

    complete task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write code here
+const INPUTS = Array.from(document.querySelectorAll('.field-text'));
+
+INPUTS.forEach((el) => {
+  el.placeholder = el.name[0].toUpperCase() + el.name.slice(1);
+
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.innerText = el.name;
+
+  el.parentNode.appendChild(label);
+});
