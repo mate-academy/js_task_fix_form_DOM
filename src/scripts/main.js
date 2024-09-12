abacc8475aa283587ebabf9bd4c02b8d83056aaa
Roman Martseniuk <roman.martseniuk.developer@gmail.com>
2024-09-12 22:52:37 +0300

    fix names

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,12 +4,17 @@
 const INPUTS = Array.from(document.querySelectorAll('.field-text'));
 
 INPUTS.forEach((el) => {
-  el.placeholder = el.name[0].toUpperCase() + el.name.slice(1);
-
   const label = document.createElement('label');
 
   label.classList.add('field-label');
-  label.innerText = el.name;
+
+  const str = (el.name[0].toUpperCase() + el.name.slice(1)).replace(
+    /([a-z])([A-Z])/g,
+    '$1 $2',
+  );
+
+  label.innerText = str;
+  el.placeholder = str;
 
   el.parentNode.appendChild(label);
 });
