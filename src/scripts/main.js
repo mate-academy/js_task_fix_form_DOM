f86d43aa6cdfd133fb00378f0288ec3844d9269c
Anastasiia Tatarchenko <neveykina.nastya@gmail.com>
2024-09-16 12:53:05 +0200

    task_done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const signUpName = document.getElementById('sign-up-name');
+const signUpLastName = document.getElementById('sign-up-last-name');
+const signUpEmail = document.getElementById('sign-up-email');
+const signUpPassword = document.getElementById('sign-up-password');
+const signInEmail = document.getElementById('sign-in-email');
+const signInPassword = document.getElementById('sign-in-password');
+
+const inputs = [...document.querySelectorAll('form input')];
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
+  input.parentNode.insertBefore(label, input);
+  label.textContent = input.getAttribute('name');
+});
+
+signUpName.setAttribute('placeholder', 'First name');
+signUpLastName.setAttribute('placeholder', 'Last name');
+signUpEmail.setAttribute('placeholder', 'Email');
+signUpPassword.setAttribute('placeholder', 'Password');
+signInEmail.setAttribute('placeholder', 'Email');
+signInPassword.setAttribute('placeholder', 'Password');
