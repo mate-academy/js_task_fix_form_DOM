f5869e05c9bfb4d48eb8c1dd927e69e9f6ca9dac
Anastasiia Tatarchenko <neveykina.nastya@gmail.com>
2024-09-17 12:56:01 +0200

    revorked_the_task_corrected_the_error

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,28 @@
 'use strict';
 
-const signUpName = document.getElementById('sign-up-name');
-const signUpLastName = document.getElementById('sign-up-last-name');
-const signUpEmail = document.getElementById('sign-up-email');
-const signUpPassword = document.getElementById('sign-up-password');
-const signInEmail = document.getElementById('sign-in-email');
-const signInPassword = document.getElementById('sign-in-password');
+const inputs = document.querySelectorAll('form input');
+const splitAndCapitalize = (text) => {
+  let result = '';
 
-const inputs = [...document.querySelectorAll('form input')];
+  for (let i = 0; i < text.length; i++) {
+    if (i > 0 && text[i] === text[i].toUpperCase() && text[i - 1] !== ' ') {
+      result += ' ';
+    }
+    result += text[i];
+  }
+
+  return result
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
+};
 
 inputs.forEach((input) => {
-  const label = document.createElement('label');
+  const createElement = document.createElement('label');
 
-  label.classList.add('field-label');
-  label.setAttribute('for', input.id);
-  input.parentNode.insertBefore(label, input);
-  label.textContent = input.getAttribute('name');
+  createElement.classList.add('field-label');
+  createElement.setAttribute('for', input.name);
+  createElement.textContent = splitAndCapitalize(input.name);
+  input.setAttribute('placeholder', splitAndCapitalize(input.name));
+  input.parentNode.insertBefore(createElement, input);
 });
-
-signUpName.setAttribute('placeholder', 'First name');
-signUpLastName.setAttribute('placeholder', 'Last name');
-signUpEmail.setAttribute('placeholder', 'Email');
-signUpPassword.setAttribute('placeholder', 'Password');
-signInEmail.setAttribute('placeholder', 'Email');
-signInPassword.setAttribute('placeholder', 'Password');
