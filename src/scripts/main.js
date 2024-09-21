ad3e48dd737358c43491a20e8862d3fa321100f7
Viktoriia Dereza <viktoriiadereza7@gmail.com>
2024-09-21 16:08:09 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const inputEmail = document.getElementById('sign-in-email');
+const inputPassword = document.getElementById('sign-in-password');
+
+function createLabelAndPlaceholder(input) {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.textContent = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+  label.setAttribute('for', input.id);
+
+  input.placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  input.parentNode.insertBefore(label, input);
+}
+
+createLabelAndPlaceholder(inputEmail);
+createLabelAndPlaceholder(inputPassword);
