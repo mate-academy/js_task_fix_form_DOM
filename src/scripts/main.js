469dc8ce8b60a31fd9684c688d4775b3b07c482f
Kseniia Karpenko <ksushakarpenko635@gmail.com>
2024-09-21 17:24:47 +0200

    forms task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write code here
+
+const forms = document.querySelectorAll('form');
+
+forms.forEach((form) => {
+  const inputs = form.querySelectorAll('input');
+
+  inputs.forEach((input) => {
+    const label = document.createElement('label');
+
+    label.setAttribute('class', 'field-label');
+    label.setAttribute('for', input.id);
+
+    input.setAttribute(
+      'placeholder',
+      input.name.charAt(0).toUpperCase() + input.name.slice(1),
+    );
+
+    label.textContent = input.name;
+
+    input.insertAdjacentElement('beforebegin', label);
+  });
+});
