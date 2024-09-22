46ebaee4ded7f41c01b81af407bb3407be0dac84
Korshun <dmitriy.kor91@gmail.com>
2024-09-22 12:33:52 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,9 +7,19 @@ allInputs.forEach((input) => {
 
   label.setAttribute('for', input.id);
 
-  label.textContent = input.name.toUpperCase();
+  if (input.name === 'firstName') {
+    label.textContent = 'First Name';
+  } else if (input.name === 'lastName') {
+    label.textContent = 'Last Name';
+  } else {
+    label.textContent =
+      input.name.charAt(0).toUpperCase() + input.name.slice(1);
+  }
 
-  input.placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+  input.placeholder = input.name
+    .split(/(?=[A-Z])/)
+    .join(' ')
+    .replace(/\b\w/g, (char) => char.toUpperCase());
 
   input.parentNode.insertBefore(label, input);
 });
