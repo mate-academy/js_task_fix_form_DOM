ea1fcdf6008988b131855bef74130d1647df2a46
Korshun <dmitriy.kor91@gmail.com>
2024-09-22 10:38:04 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const allInputs = document.querySelectorAll('input');
+
+allInputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.setAttribute('for', input.id);
+
+  label.textContent = input.name.toUpperCase();
+
+  input.placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  input.parentNode.insertBefore(label, input);
+});
