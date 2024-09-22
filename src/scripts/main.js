83310172c3f70dad3f22db5264adef3a242f9139
Romanov <freeromanov@gmail.com>
2024-09-22 22:05:00 +0300

    fix form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const forms = document.querySelectorAll('form');
+
+forms.forEach((form) => {
+  const inputs = form.querySelectorAll('input');
+
+  inputs.forEach((input) => {
+    const label = document.createElement('label');
+
+    label.classList.add('field-label');
+    label.setAttribute('for', input.id);
+
+    label.textContent = capitalizeFirstLetter(input.name);
+
+    input.placeholder = capitalizeFirstLetter(input.name);
+
+    input.parentElement.insertBefore(label, input);
+  });
+});
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
