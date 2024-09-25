9e072b077a67351bca3a30d2eaec722792a430f8
Oleksandr Kovalchuk <kov280396@gmail.com>
2024-09-25 09:23:19 +0300

    fixed code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,8 +7,26 @@ inputs.forEach((input) => {
 
   label.className = 'field-label';
   label.htmlFor = input.id;
-  label.textContent = input.name;
-  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+
+  const separatedWords = separateWords(input.name);
+
+  label.textContent = separatedWords;
+
+  input.placeholder =
+    separatedWords.charAt(0).toUpperCase() + separatedWords.slice(1);
 
   input.before(label);
 });
+
+function separateWords(word) {
+  let result = '';
+
+  for (let i = 0; i < word.length; i++) {
+    if (i > 0 && word[i] === word[i].toUpperCase()) {
+      result += ' ';
+    }
+    result += word[i];
+  }
+
+  return result;
+}
