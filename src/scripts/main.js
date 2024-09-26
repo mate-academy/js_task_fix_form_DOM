a54ef4e69c172da9ce8d7676e0c48c704a02d9f9
Stas Borychevskyi <sborichevskiy74@gmail.com>
2024-09-26 18:22:10 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const forms = document.querySelectorAll('form');
+const inputs = [];
+
+forms.forEach((form) => {
+  const currentInputs = form.querySelectorAll('input');
+
+  inputs.push(...currentInputs);
+});
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+
+  if (input.name) {
+    const pHolder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+    input.placeholder = pHolder;
+  }
+
+  input.parentNode.appendChild(label);
+});
