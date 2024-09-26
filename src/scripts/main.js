c46c6b84b91f9651e32d1656e0fc1e36893f52a1
missk <rostock.vasiljev@gmail.com>
2024-09-26 23:01:28 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write code here
+
+function firstUpperChar(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+const forms = document.querySelectorAll('form');
+
+forms.forEach((form) => {
+  const inputs = form.querySelectorAll('input');
+
+  inputs.forEach((input) => {
+    const label = document.createElement('label');
+
+    label.setAttribute('for', input.id);
+
+    label.classList.add('field-label');
+    label.textContent = input.name.toUpperCase();
+
+    input.placeholder = firstUpperChar(input.name);
+    input.parentNode.insertBefore(label, input);
+  });
+});
