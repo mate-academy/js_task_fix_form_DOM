4bd00de3015c87de4ad48f629d8ae42be9a68e5f
Stas Borychevskyi <sborichevskiy74@gmail.com>
2024-09-27 14:07:28 +0300

    Corrected

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,12 +14,12 @@ inputs.forEach((input) => {
 
   label.className = 'field-label';
   label.setAttribute('for', input.id);
-  label.textContent = input.name;
+  label.textContent = input.name.replace(/([a-z])([A-Z])/g, '$1 $2');
 
   if (input.name) {
     const pHolder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
 
-    input.placeholder = pHolder;
+    input.placeholder = pHolder.replace(/([a-z])([A-Z])/g, '$1 $2');
   }
 
   input.parentNode.appendChild(label);
