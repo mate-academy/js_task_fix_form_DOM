36c0542ef73cf7d5298b52cbb237ed37cf908f99
kovaltar <kovaltar@gmail.com>
2024-09-30 15:07:32 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,12 +10,16 @@ function capitalize(str) {
 }
 
 inputs.forEach((item, ind, arr) => {
-  const itemName = item.getAttribute('name');
+  let itemName = capitalize(item.getAttribute('name'));
   const itemLabel = document.createElement('label');
+
+  if (itemName.toLowerCase().includes('name')) {
+    itemName = itemName.slice(0, -4) + ' ' + itemName.slice(-4);
+  }
 
   itemLabel.classList.add('field-label');
   itemLabel.setAttribute('for', item.id);
   itemLabel.textContent = itemName.toUpperCase();
-  item.setAttribute('placeholder', capitalize(itemName));
+  item.setAttribute('placeholder', itemName);
   item.before(itemLabel);
 });
