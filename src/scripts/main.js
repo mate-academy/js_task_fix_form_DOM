f4dcb923a1de60a7bd56e39bc0249aecbc3b2f53
Tetiana Skuratovska <tanya.skuratovskaya@gmail.com>
2024-09-30 14:37:44 +0200

    added new formatName for placeholder and textContent

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,18 +3,20 @@
 const allInput = document.querySelectorAll('form input');
 
 allInput.forEach((input) => {
-  const formatPlaceholder = (inputName) => {
+  const formatName = (inputName) => {
     return inputName.replace(/([A-Z])/g, ' $1').trim();
   };
 
-  input.placeholder = formatPlaceholder(
+  const formattedName = formatName(
     input.name.charAt(0).toUpperCase() + input.name.slice(1),
   );
+
+  input.placeholder = formattedName;
 
   const newLabel = document.createElement('label');
 
   newLabel.classList.add('field-label');
   newLabel.setAttribute('for', input.id);
-  newLabel.textContent = input.name;
+  newLabel.textContent = formattedName;
   input.parentElement.insertBefore(newLabel, input);
 });
