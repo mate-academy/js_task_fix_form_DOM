6dbaf48a31d47acd5040e03a2298d70ffefdc272
SerhiiSemennikov <semennik@hotmail.com>
2024-10-02 00:38:51 +0200

    v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.map((elementOfInput) => {
+  const labels = document.createElement('label');
+
+  labels.className = 'field-label';
+  labels.htmlFor = elementOfInput.id;
+  labels.textContent = elementOfInput.name;
+
+  elementOfInput.placeholder =
+    elementOfInput.name.charAt(0).toUpperCase() + elementOfInput.name.slice(1);
+  elementOfInput.parentElement.appendChild(labels);
+});
