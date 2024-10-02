4d1cb9d8aac04ebb9b50507b9a7cc658a287923e
Oleksii Karpov <alneon1@gmail.com>
2024-10-02 17:38:18 +0300

    added spaces between words in labels & placeholders v.2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,17 +4,30 @@ const inputFields = document.getElementsByClassName('field-text');
 const inputsArr = [...inputFields];
 
 inputsArr.forEach((input) => {
+  const text = addSpaces(input.name[0].toUpperCase() + input.name.slice(1));
+
+  input.setAttribute('placeholder', `${text}`);
+
   const label = document.createElement('label');
 
   label.className = 'field-label';
   label.setAttribute('for', input.id);
-  label.textContent = input.name;
+  label.textContent = text;
 
   const container = input.parentNode;
 
   container.insertBefore(label, input);
-
-  const placeholderText = input.name[0].toUpperCase() + input.name.slice(1);
-
-  input.setAttribute('placeholder', `${placeholderText}`);
 });
+
+function addSpaces(str) {
+  let result = str[0];
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] === str[i].toUpperCase()) {
+      result += ' ';
+    }
+    result += str[i];
+  }
+
+  return result;
+}
