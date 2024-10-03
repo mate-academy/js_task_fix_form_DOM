8da937ce211f4dc9d86016fe8a88f219b61a6b10
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2024-10-03 13:27:30 +0300

    Fixed

--- src/scripts/helpers.js
+++ src/scripts/helpers.js
@@ -4,15 +4,28 @@ export function setLabel(input) {
   const label = document.createElement('label');
   const { id, name } = input;
 
+  const normalName = camelToUpper(name);
+
   label.setAttribute('class', 'field-label');
   label.setAttribute('for', `${id}`);
-  label.textContent = name.toUpperCase();
+  label.textContent = normalName;
   parent.appendChild(label);
 }
 
 export function setPlaceholder(input) {
   const { name } = input;
-  const capitalizedName = name[0].toUpperCase() + name.slice(1).toLowerCase();
+  const capitalizedName = camelToNormal(name);
 
   input.setAttribute('placeholder', `${capitalizedName}`);
+}
+
+function camelToNormal(word) {
+  return word
+    .replace(/([A-Z])/g, ' $1')
+    .toLowerCase()
+    .replace(/^./, (str) => str.toUpperCase());
+}
+
+function camelToUpper(word) {
+  return word.replace(/([A-Z])/g, ' $1').toUpperCase();
 }
