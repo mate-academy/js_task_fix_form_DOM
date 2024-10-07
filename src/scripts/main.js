769e4c604d8d7a84e566dc6a875bd4e9966b0bc4
Ivan Paiareli <ipayareli@gmail.com>
2024-10-07 18:58:46 +0300

    add extra task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,11 +8,18 @@ allFields.forEach((field) => field.insertAdjacentHTML('afterbegin', '<label>'));
 const allLabels = document.querySelectorAll('label');
 
 for (let i = 0; i < allInputs.length; i++) {
-  allInputs[i].setAttribute(
-    'placeholder',
-    `${allInputs[i].name.toUpperCase()}`,
-  );
+  allInputs[i].setAttribute('placeholder', `${textFormat(allInputs[i].name)}`);
   allLabels[i].textContent = allInputs[i].name;
   allLabels[i].setAttribute('class', 'field-label');
   allLabels[i].setAttribute('for', `${allInputs[i].id}`);
+}
+
+function textFormat(word) {
+  const splitedWord = word.split(/(?=[A-Z])/);
+
+  for (let i = 0; i < splitedWord.length; i++) {
+    splitedWord[i] = splitedWord[i][0].toUpperCase() + splitedWord[i].slice(1);
+  }
+
+  return splitedWord.join(' ');
 }
