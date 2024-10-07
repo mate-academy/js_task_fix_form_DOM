9c090e4511a9e1c50fb22176fbde5c7a61babfbc
Ivan Paiareli <ipayareli@gmail.com>
2024-10-07 17:46:52 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const allInputs = document.querySelectorAll('input');
+const allFields = document.querySelectorAll('.field');
+
+allFields.forEach((field) => field.insertAdjacentHTML('afterbegin', '<label>'));
+
+const allLabels = document.querySelectorAll('label');
+
+for (let i = 0; i < allInputs.length; i++) {
+  allInputs[i].setAttribute(
+    'placeholder',
+    `${allInputs[i].name.toUpperCase()}`,
+  );
+  allLabels[i].textContent = allInputs[i].name;
+  allLabels[i].setAttribute('class', 'field-label');
+  allLabels[i].setAttribute('for', `${allInputs[i].id}`);
+}
