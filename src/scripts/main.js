6d3c995ad152b519f3fb5afb71d107526a8f64ba
Halyna <minimal005@gmail.com>
2024-10-08 23:44:49 +0300

    my solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const fieldTexts = document.querySelectorAll('.field-text');
+
+for (const fieldText of fieldTexts) {
+  const idFromField = fieldText.getAttribute('id');
+  const nameFromField = fieldText.getAttribute('name');
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', idFromField);
+  label.textContent = nameFromField;
+
+  fieldText.insertAdjacentElement('beforebegin', label);
+  fieldText.setAttribute('placeholder', nameFromField);
+}
