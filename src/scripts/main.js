66a829a58d34a80a6c23376ace0f3603448d7fd5
Andrii Zakharenko <andriizakharenko@gmail.com>
2024-10-10 11:45:37 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const fields = [...document.querySelectorAll('input.field-text')];
+
+fields.forEach((field) => {
+  const label = document.createElement('label');
+  const fieldName = field.name;
+
+  const index = fieldName.split('').findIndex((ch) => ch === ch.toUpperCase());
+  const indexEnd = index;
+
+  const placeholderText =
+    index > 0
+      ? field.name[0].toUpperCase() +
+        field.name.slice(1, indexEnd) +
+        ' ' +
+        field.name.slice(indexEnd)
+      : field.name[0].toUpperCase() + field.name.slice(1);
+
+  label.classList.add('field-label');
+  label.setAttribute('for', field.id);
+  label.textContent = placeholderText;
+  field.setAttribute('placeholder', placeholderText);
+  field.before(label);
+});
