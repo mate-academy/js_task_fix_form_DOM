5f0f61d5f2fbb32e1bda6814e1acbba0a71ddafa
Mateusz Cieplak <m.cieplak97@gmail.com>
2025-01-10 18:12:56 +0100

    Task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
+/* eslint-disable no-console */
+
 'use strict';
 
 // write code here
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach(input => {
+  input.setAttribute("placeholder", capitalize(input.name));
+
+  const label = document.createElement("label");
+
+  label.textContent = input.name;
+  label.classList.add("field-label");
+  label.setAttribute("for", input.id);
+
+  const parentElement = input.parentElement;
+
+  parentElement.appendChild(label);
+});
+
+function capitalize (namePlaceholder) {
+  const firstLetter = namePlaceholder.charAt(0).toUpperCase();
+  const restOfName = namePlaceholder.slice(1);
+
+  const newName = firstLetter + restOfName;
+
+  return newName;
+}
