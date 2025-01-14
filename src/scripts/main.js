303d87c1ab60a4c89345ed191cea95b0a67a0af9
Serhii <sergeyunguryan322@gmail.com>
2025-01-14 09:25:50 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('form .field input')];
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+
+  label.textContent = input.name
+    .replace(/([A-Z])/g, ' $1')
+    .replace(/^./, (match) => match.toUpperCase());
+
+  const placeholderText = input.name
+    .replace(/([A-Z])/g, ' $1')
+    .replace(/^./, (match) => match.toUpperCase());
+
+  // console.log(placeholderText);
+
+  input.setAttribute('placeholder', placeholderText);
+
+  const inputParent = input.closest('.field');
+
+  inputParent.prepend(label);
+});
