f8b8e97021d40ac62e7ac39fff39d4d465c6de37
Anton Tur <ant.tur85@gmail.com>
2025-01-14 15:30:28 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputFields = document.querySelectorAll('.field-text');
+
+function capitalize(str) {
+  return str[0].toUpperCase() + str.slice(1);
+}
+
+inputFields.forEach((item) => {
+  const labelField = document.createElement('label');
+
+  labelField.classList.add('field-label');
+  labelField.setAttribute('for', item.id);
+  labelField.textContent = item.name;
+
+  // item.parentNode.insertBefore(labelField, item);
+  item.insertAdjacentElement('beforebegin', labelField);
+
+  item.setAttribute('placeholder', capitalize(item.name));
+});
