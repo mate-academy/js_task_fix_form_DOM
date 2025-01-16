9c0c751e7ab65112fbbe4e012885ae0f79796c5e
Viktoriia <vikapazyuk@gmail.com>
2025-01-16 18:47:19 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const forms = document.querySelectorAll('form');
+
+for (const form of forms) {
+  const inputs = form.querySelectorAll('input');
+
+  for (const input of inputs) {
+    const label = document.createElement('label');
+
+    label.setAttribute('for', input.id);
+
+    const nameContent =
+      input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+    label.textContent = nameContent;
+
+    input.insertAdjacentElement('beforebegin', label);
+
+    input.setAttribute('placeholder', nameContent);
+  }
+}
