064347974e73ef52b6876a6692208af747620b3d
maksimka <maks.markevych@gmail.com>
2025-01-17 16:53:57 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+function formInputs() {
+  const forms = document.querySelectorAll('form');
+
+  forms.forEach((form) => {
+    const inputs = form.querySelectorAll('input');
+
+    inputs.forEach((input) => {
+      const label = document.createElement('label');
+
+      label.className = 'field-label';
+      label.setAttribute('for', input.id);
+      label.textContent = input.name;
+
+      const inputName = input.name.split('').map((el) => {
+        return el.toLowerCase();
+      });
+
+      inputName[0] = inputName[0].toUpperCase();
+      input.placeholder = inputName.join('');
+      input.parentElement.insertBefore(label, input);
+    });
+  });
+}
+formInputs();
