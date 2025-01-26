9f621c470e26077b560c410319f239025752021d
Bilokon Yaroslav <uvill.github@gmail.com>
2025-01-26 15:36:57 +0200

    fix form label and placeholder

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
 // write code here
+const inputs = [...document.querySelectorAll('input')];
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+  const labelContent = splitCamellCase(input.name);
+  const inputPlaceholder = upperCaseFirstLetter(labelContent);
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = labelContent;
+  input.placeholder = inputPlaceholder;
+  input.before(label);
+}
+
+function upperCaseFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function splitCamellCase(string) {
+  let result = string;
+
+  for (let i = 0; i < string.length; i++) {
+    if (result.charAt(i) === result.charAt(i).toUpperCase()) {
+      result = result.slice(0, i) + ' ' + result.slice(i, result.length);
+    }
+  }
+
+  return result.toLowerCase();
+}
