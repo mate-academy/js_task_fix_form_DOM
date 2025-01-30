37a7c77d73493b63caf74a862a472419c3ed5d12
Mykhailo <kurochkinmy@gmail.com>
2025-01-30 16:34:25 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const collection = document.querySelectorAll('.field');
+
+for (const field of collection) {
+  const child = document.createElement('label');
+
+  child.textContent = field.children[0].name.toUpperCase();
+  child.class = 'field-label';
+  child.for = field.children[0].id;
+
+  field.children[0].placeholder =
+    field.children[0].name[0].toUpperCase() + field.children[0].name.slice(1);
+  field.prepend(child);
+}
