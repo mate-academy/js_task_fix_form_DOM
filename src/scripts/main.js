31fe43667a9ad19fb7789d5d6477677889ce1f67
Ivan Lavruk <ilavr8725@gmail.com>
2025-01-31 15:42:05 +0100

    task fix form dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('form input').forEach((input) => {
+  const inputName = input.name;
+
+  if (!inputName) {
+    return;
+  }
+
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
+  label.textContent = inputName.charAt(0).toUpperCase() + inputName.slice(1);
+
+  input.parentNode.insertBefore(label, input);
+
+  input.placeholder = label.textContent;
+});
