61a557b61e18d82b5b1fea35582715587a176539
Nadiia Bulmak <nadgaval@gmail.com>
2025-02-04 13:06:01 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,15 +5,16 @@ const inputs = [...document.querySelectorAll('input')];
 
 inputs.forEach((element, i, arr) => {
   const label = document.createElement('label');
-
   label.className = 'field-label';
   label.setAttribute('for', arr[i].id);
-  label.textContent = element.name || 'Label';
+
+  const inputName = element.getAttribute('name');
+  label.textContent = inputName ? inputName : 'Label';
 
   element.parentNode.appendChild(label);
 
-  const capitalizedPlaceholder =
-    element.name.charAt(0).toUpperCase() + element.name.slice(1);
-
-  element.setAttribute('placeholder', capitalizedPlaceholder);
+  if (inputName) {
+    const capitalizedPlaceholder = inputName.charAt(0).toUpperCase() + inputName.slice(1);
+    element.setAttribute('placeholder', capitalizedPlaceholder);
+  }
 });
