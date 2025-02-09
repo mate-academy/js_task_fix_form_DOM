5927115257c9010d6168eefde9abeaf5c10614f7
Tomas <tomas7777tomas@gmail.com>
2025-02-09 20:03:09 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const form = [...document.querySelectorAll('input')];
+
+  form.map((input) => {
+    const inputName = input.getAttribute('name');
+    const capitalizedName =
+      inputName.charAt(0).toUpperCase() + inputName.slice(1);
+
+    input.setAttribute('placeholder', capitalizedName);
+
+    const id = input.getAttribute('id');
+    const label = document.createElement('label');
+
+    label.setAttribute('for', id);
+    label.setAttribute('class', 'field-label');
+    label.textContent = inputName;
+
+    input.insertAdjacentElement('beforebegin', label);
+  });
+});
