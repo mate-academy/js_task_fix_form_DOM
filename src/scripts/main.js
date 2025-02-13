727ebab8b5316bc1f2e571c493e0a9bdc402b952
Grzegorz <grze.pra@gmail.com>
2025-02-13 09:26:51 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const emptyInputs = document.querySelectorAll('form input');
+
+for (let i = 0; i < emptyInputs.length; i++) {
+  const input = emptyInputs[i];
+  const label = document.createElement('label');
+
+  label.setAttribute('for', input.getAttribute('id'));
+
+  label.textContent = input
+    .getAttribute('name')
+    .replace(/([A-Z])/g, ' $1')
+    .trim();
+
+  label.classList.add('field-label');
+
+  const placeholderText = input
+    .getAttribute('name')
+    .replace(/([A-Z])/g, ' $1')
+    .trim();
+
+  input.setAttribute('placeholder', placeholderText);
+
+  input.parentNode.insertBefore(label, input);
+}
