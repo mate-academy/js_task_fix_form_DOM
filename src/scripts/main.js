52c8bef497d7a3d2f9af149d4de35514ab6a43d9
Dariya-Goncharovskaya <dariyagoncharovskaya@gmail.com>
2025-02-17 11:00:31 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const forms = document.querySelectorAll('form');
+
+  forms.forEach((form) => {
+    const inputs = form.querySelectorAll('input');
+
+    inputs.forEach((input) => {
+      if (!input.id) {
+        return;
+      }
+
+      const label = document.createElement('label');
+
+      label.classList.add('field-label');
+      label.setAttribute('for', input.id);
+
+      const labelText =
+        input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+      label.textContent = labelText;
+
+      input.placeholder = labelText;
+
+      const parentElement = input.parentElement;
+
+      parentElement.insertBefore(label, input);
+    });
+  });
+});
