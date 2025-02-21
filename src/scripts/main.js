63878bec5f1d5cc3fd9266731956b2a3ef28754b
Ivan Ryabukha <ryabukhaivan98@gmail.com>
2025-02-21 16:47:55 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const div = document.querySelectorAll('.field');
+const input = document.querySelectorAll('input');
+
+for (let i = 0; i < div.length; i++) {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', input[i].getAttribute('id'));
+  label.textContent = input[i].getAttribute('name');
+
+  div[i].appendChild(label);
+
+  const splitted = input[i].getAttribute('name').split('');
+
+  const firstLetter = splitted[0].toUpperCase();
+
+  splitted.splice(0, 1);
+
+  const result = [firstLetter, ...splitted].join('');
+
+  input[i].placeholder = result;
+}
