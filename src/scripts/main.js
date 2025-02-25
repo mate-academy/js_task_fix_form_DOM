fca5cb38a051e31e368fab3cd9343f11c7eebc22
Samarash Eugene <samarasha95@gmail.com>
2025-02-25 23:09:44 +0200

    tasks are implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.innerHTML = input.name.toUpperCase();
+  label.setAttribute('for', input.id);
+  input.parentNode.prepend(label);
+
+  input.setAttribute('placeholder', input.name.toUpperCase());
+}
