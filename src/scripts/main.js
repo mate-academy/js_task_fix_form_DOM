b783c6f9365101659ec02bf92d9e4ff08591ba38
Anna Poplavska <annapoplavska10@gmail.com>
2025-02-25 12:12:47 +0100

    Task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const inputElements = [...document.querySelectorAll('input')];
+
+function camelToRegular(text) {
+  const result = text.replace(/([A-Z])/g, ' $1');
+
+  return result.charAt(0).toUpperCase() + result.slice(1);
+}
+
+for (let i = 0; i < inputElements.length; i++) {
+  const label = document.createElement('label');
+
+  label.setAttribute('class', 'field-label');
+  label.setAttribute('for', inputElements[i].getAttribute('id'));
+  label.textContent = camelToRegular(inputElements[i].getAttribute('name'));
+
+  inputElements[i].setAttribute(
+    'placeholder',
+    camelToRegular(inputElements[i].getAttribute('name')),
+  );
+  inputElements[i].before(label);
+}
