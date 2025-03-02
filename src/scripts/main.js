39a99514b2ccdb1e9e64069575ca72d215779e46
Anton Polishchuk <hardixxxxx@gmail.com>
2025-03-02 18:45:39 +0100

    complete task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+for (const input of [...inputs]) {
+  const label = document.createElement('label');
+  const inputContainer = input.parentElement;
+
+  label.classList.add('field-label');
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+
+  input.placeholder = capitalize(input.name);
+
+  inputContainer.append(label);
+}
+
+function capitalize(text) {
+  return text[0].toUpperCase() + text.slice(1);
+}
