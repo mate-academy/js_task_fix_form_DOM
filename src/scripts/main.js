d0c352a1ccc66fdbe6a1834d5196ad32a868ef1b
Stelmashchuk Pavlo <stelmaschukpavlo3@gmail.com>
2025-03-13 16:28:08 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+function addLabel(inputs) {
+  inputs.forEach((input) => {
+    const label = document.createElement('Label');
+
+    label.textContent = input.name;
+    label.class = 'field-label';
+    label.htmlFor = input.id;
+
+    input.parentNode.insertBefore(label, input);
+  });
+}
+
+function addPlaceholder(inputs) {
+  inputs.forEach((input) => {
+    const placeHolder = input.name[0].toUpperCase() + input.name.slice(1);
+
+    input.placeholder = placeHolder;
+  });
+}
+
+addLabel(document.querySelectorAll('input'));
+addPlaceholder(document.querySelectorAll('input'));
