02fb6e4d70a00772af0c879b7de3331ecc818331
AnoriFrell <anori.fjord@gmail.com>
2025-04-08 20:16:06 +0300

    form complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write code here
+const inputs = [...document.querySelectorAll('input')];
+const fields = [...document.querySelectorAll('.field')];
+// const eachInput = inputs[index];
+
+fields.forEach((field, index) => {
+  const eachInput = inputs[index];
+  const newLabel = document.createElement('label');
+  const placeholderText = eachInput.getAttribute('name');
+
+  newLabel.className = 'field-label';
+  newLabel.setAttribute('for', eachInput.getAttribute('id'));
+  newLabel.textContent = `${eachInput.getAttribute('name')}`;
+
+  eachInput.setAttribute(
+    'placeholder',
+    placeholderText.charAt(0).toUpperCase() + placeholderText.slice(1),
+  );
+  field.insertBefore(newLabel, eachInput);
+});
