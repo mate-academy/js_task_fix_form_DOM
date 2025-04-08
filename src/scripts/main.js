2be3060f621a19adc4151cedec98a5e4d5effe15
AVyhaa <artemvyhovanec1@gmail.com>
2025-04-08 11:14:09 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write code here
+function setLabelAndPlaceholderByName(node) {
+  // document.getElementsByName('firstName')[0]
+  const label = document.createElement('span');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', node.id);
+  label.textContent = capitalizedString(node.name);
+
+  node.parentNode.prepend(label);
+  node.placeholder = capitalizedString(node.name);
+}
+
+function capitalizedString(string) {
+  let modifiedString = '';
+
+  for (const i of string) {
+    if (i === i.toUpperCase()) {
+      modifiedString += ' ';
+    }
+
+    modifiedString += i;
+  }
+
+  modifiedString = modifiedString.trim();
+
+  return (
+    modifiedString.charAt(0).toUpperCase() +
+    modifiedString.slice(1).toLowerCase()
+  );
+}
+
+function proccessAllInputs() {
+  const inputElements = document.getElementsByTagName('input');
+
+  for (const element of inputElements) {
+    setLabelAndPlaceholderByName(element);
+  }
+}
+
+proccessAllInputs();
