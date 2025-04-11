c9f25d99e778a91592203b7435d3d6d7eb0c54ab
Oksana Sidanych <sasha3sashkova@gmail.com>
2025-04-11 17:03:45 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const inputs = document.querySelectorAll('input');
+
+  inputs.forEach((input) => {
+    const label = document.createElement('label');
+
+    label.setAttribute('for', input.id);
+
+    label.textContent =
+      input.name.charAt(0).toUpperCase() + input.name.slice(1);
+    label.classList.add('field-label');
+    input.parentNode.insertBefore(label, input);
+
+    input.setAttribute(
+      'placeholder',
+      input.name.charAt(0).toUpperCase() + input.name.slice(1),
+    );
+  });
+});
