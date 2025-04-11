7994f2923010be58955518a935b9e5486eb145a9
Nikola57 <nikola.sribniy@gmail.com>
2025-04-11 19:46:07 +0200

    complit js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const inputs = document.querySelectorAll('input');
+
+  inputs.forEach((input) => {
+    const namee = input.getAttribute('name');
+
+    const label = document.createElement('label');
+
+    label.className = 'field-label';
+    label.setAttribute('for', input.id);
+    label.textContent = namee.charAt(0).toUpperCase() + namee.slice(1);
+
+    input.setAttribute(
+      'placeholder',
+      namee.charAt(0).toUpperCase() + namee.slice(1),
+    );
+
+    input.parentNode.insertBefore(label, input);
+  });
+});
