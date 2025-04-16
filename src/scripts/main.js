c101412425f29da18b78b34d774c2dc20eb8f9c0
Aleksandr-front-end-developer <oleksandr.danyliuk.88@gmail.com>
2025-04-16 13:58:26 +0300

    have done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputAll = document.querySelectorAll('.field-text');
+
+inputAll.forEach((item) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', `${item.id}`);
+
+  item.setAttribute(
+    'placeholder',
+    `${item.name.charAt(0).toUpperCase() + item.name.slice(1)}`,
+  );
+  label.innerText = item.name;
+  item.parentNode.insertBefore(label, item);
+});
