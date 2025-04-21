9ecaf335619b78af397164f26a4f0ccdb34313ef
Denys Piddubnyi <iforgotmypincode@gmail.com>
2025-04-21 21:12:29 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const findSelector = document.querySelectorAll('form input');
+
+findSelector.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+
+  const inputID = input.id || input.name;
+
+  input.id = inputID;
+  label.setAttribute('for', inputID);
+
+  label.textContent = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  input.setAttribute(
+    'placeholder',
+    input.name.charAt(0).toUpperCase() + input.name.slice(1),
+  );
+
+  input.parentElement.insertBefore(label, input);
+});
