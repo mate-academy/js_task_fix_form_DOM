2038e4b19115e26725a7678d166efdd62ff84676
Daria <darialys2510@gmail.com>
2025-06-02 19:19:05 +0200

    make fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,13 +7,10 @@ for (const input of inputs) {
   const label = document.createElement('label');
 
   label.className = 'field-label';
-  label.setAttribute('for', input.id)
+  label.setAttribute('for', input.id);
   label.textContent = capitalazedName;
 
   input.placeholder = capitalazedName;
 
   input.parentElement.appendChild(label);
 }
-
-console.log(inputs[1]);
-// write code here
