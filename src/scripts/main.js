ed4da74c5f31309afb4710908c955b301daf05f2
Bernavschi Natalia <natb@list.ru>
2025-07-03 07:22:35 +0300

    add task solution for js_task_fix_form_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+
+  label.setAttribute('for', input.id);
+
+  const labelText = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  label.textContent = labelText;
+
+  input.parentNode.insertBefore(label, input);
+
+  input.placeholder = labelText;
+});
