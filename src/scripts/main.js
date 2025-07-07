5a6765e63def061d7e1710a216fb65e002b194cf
Sofiia Kogut <kogutsofia288@gmail.com>
2025-07-07 15:16:49 +0300

    add

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
+const forms = document.querySelectorAll('form');
+
+forms.forEach((form) => {
+  const inputs = form.querySelectorAll('input');
+
+  inputs.forEach((input) => {
+    if (!input.id) {
+      input.id = input.name;
+    }
+
+    const label = document.createElement('label');
+
+    label.classList.add('field-label');
+    label.setAttribute('for', input.id);
+
+    label.textContent =
+      input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+    input.placeholder =
+      input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+    input.parentElement.append(label);
+  });
+});
 // write code here
