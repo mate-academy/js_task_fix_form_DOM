7d51aaa82eb1b420579b7a0ab46a09ad009de903
Andriï Zakhartchenko <andriizakhartchenko@outlook.fr>
2025-07-10 13:34:37 +0200

    solved task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
-'use strict';
+const allInputs = document.querySelectorAll('input');
+let count = 0;
 
-// write code here
+allInputs.forEach(function (item) {
+  count++;
+  item.setAttribute('placeholder', item.name);
+
+  const labelCur = document.createElement('label');
+
+  labelCur.classList.add('field-label');
+  labelCur.setAttribute('for', count);
+  labelCur.textContent = item.name;
+  item.before(labelCur);
+});
