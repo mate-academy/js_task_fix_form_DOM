d3c2a4969b12aff9ade546a55cc62e6d4f6a85d7
Olga Berezhna <57196165+oberezhnay@users.noreply.github.com>
2025-07-15 21:57:06 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+  const InputName = input.name;
+
+  label.classList.add('field-label');
+  label.innerText = normalizeString(InputName);
+  input.insertAdjacentElement('beforebegin', label);
+
+  input.placeholder = normalizeString(InputName);
+}
+
+function normalizeString(str) {
+  const normalizedStr = str.charAt(0).toUpperCase() + str.slice(1);
+
+  return normalizedStr.split(/(?=[A-Z])/).join(' ');
+}
