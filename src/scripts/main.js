a49b85f756d0d2223a5c6cbd1ffe1ad1b1ddf8b8
Dina Zhyliaieva <din.zhyliaieva@gmail.com>
2025-07-22 15:40:21 +0300

    Auth form fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputEls = document.querySelectorAll('.field-text');
+
+inputEls.forEach((el) => {
+  const inputName = el.getAttribute('name');
+  const inputId = el.getAttribute('id');
+
+  const inputLabel = document.createElement('label');
+
+  inputLabel.classList.add('field-label');
+  inputLabel.setAttribute('for', inputId);
+
+  inputLabel.textContent = inputName[0].toUpperCase() + inputName.slice(1);
+  el.before(inputLabel);
+  el.setAttribute('placeholder', inputName.toUpperCase());
+});
