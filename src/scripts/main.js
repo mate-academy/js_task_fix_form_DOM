8274b35d037cb6657d1dafba46abad1f2b7ee0d4
Dmytro Yakovchuk <iknowtooyou@gmail.com>
2025-07-24 11:28:32 +0300

    add placeholder in label

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+inputs.forEach((input) => {
+  const inputName = input.getAttribute('name');
+
+  if (!inputName) {
+    return;
+  }
+
+  const inputId = input.getAttribute('id');
+
+  const inputLabel = document.createElement('label');
+
+  inputLabel.classList.add('field-label');
+  inputLabel.setAttribute('name', inputName);
+  inputLabel.setAttribute('id', `${inputId}-lable`);
+  inputLabel.setAttribute('for', inputId);
+  inputLabel.textContent = inputName;
+
+  input.parentElement.insertBefore(inputLabel, input);
+
+  input.setAttribute('placeholder', `Введите ${inputName}`);
+});
