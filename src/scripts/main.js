4a3ed05afb7d78da9e816df43a6b130154a81605
Vladyslav Nechepurenko <fablevlados@gmail.com>
2025-07-28 19:45:50 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
 // write code here
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+const forms = document.querySelectorAll('form');
+
+const inputs = [];
+
+forms.forEach(form => {
+  const formInputs = form.querySelectorAll('input');
+
+  inputs.push(...formInputs);
+});
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.setAttribute('class', 'field-label');
+  label.setAttribute('for', `${input.getAttribute('id')}`);
+  label.textContent = `${capitalize(input.getAttribute('name'))}`;
+
+  input.setAttribute('placeholder', `${capitalize(input.getAttribute('name'))}`);
+  input.parentElement.appendChild(label);
+}
