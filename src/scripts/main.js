8bf0fdccf70efa809d1420546a97bf02aeaffc1e
CwaR228 <sosok2gt@gmail.com>
2025-07-30 19:14:15 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const arrayOfInputs = document.querySelectorAll('form input');
+
+arrayOfInputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.textContent = input.name;
+  input.parentNode.appendChild(label);
+
+  const capitalize = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  input.placeholder = capitalize;
+});
