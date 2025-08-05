817a2a96608502204aff7d1b3ea8db01e86aea3d
Maxim <makson202007@gmail.com>
2025-08-05 12:11:36 +0300

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write code here
+const $inputElements = document.querySelectorAll('input');
+
+$inputElements.forEach((element) => {
+  const inputName = element.getAttribute('name');
+  const capitalizedName = `${inputName[0].toLocaleUpperCase() + inputName.slice(1)}`;
+
+  element.setAttribute('placeholder', capitalizedName);
+
+  const inputLabel = document.createElement('label');
+
+  inputLabel.textContent = capitalizedName;
+  inputLabel.for = element.getAttribute('id');
+  inputLabel.className = 'field-label';
+
+  element.parentElement.prepend(inputLabel);
+});
