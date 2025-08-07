f4a8d4c17b704a8494af849a4260fe3d29cee64f
Dmytro Nykorovych <dev.dmytronykorovych@gmail.com>
2025-08-07 16:59:01 +0200

    Task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+[...inputs].forEach((input) => {
+  const inputId = input.getAttribute('id');
+  const inputName = input.getAttribute('name');
+
+  input.insertAdjacentHTML(
+    'beforebegin',
+    `<label class="field-label" for="${inputId}">${inputName}</label>`,
+  );
+
+  input.setAttribute(
+    'placeholder',
+    inputName[0].toLocaleUpperCase() + inputName.slice(1),
+  );
+});
