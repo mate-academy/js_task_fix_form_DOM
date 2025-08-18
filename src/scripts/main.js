a1f0b247d3c57122704c0b0e77038ee59e278c13
Nikita Chupahin <nik123chupahin@gmail.com>
2025-08-18 17:21:16 +0300

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const inputs = Array.from(document.querySelectorAll('input'));
+
+inputs.forEach((item) => {
+  const label = `<label class="field-label" for="${item.id}">${item.name}</label>`;
+
+  item.insertAdjacentHTML('beforebegin', label);
+
+  const capitalizedString =
+    item.name.charAt(0).toUpperCase() + item.name.slice(1);
+
+  item.setAttribute('placeholder', capitalizedString);
+});
