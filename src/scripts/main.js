5a9ad0521a70a299fb9565d6185187f77c7e787a
Volodymyr Kolosov <kolosovvova0@gmail.com>
2025-08-23 13:28:50 +0300

    Fix form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+function textParser(text) {
+  let parsText = '';
+
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] !== text[i].toUpperCase()) {
+      parsText += text[i];
+    } else {
+      parsText += ' ' + text[i];
+    }
+  }
+  parsText = parsText[0].toUpperCase() + parsText.slice(1);
+
+  return parsText;
+}
+
+const inputList = document.querySelectorAll('input');
+
+inputList.forEach((element) => {
+  const inputLablel = document.createElement('label');
+
+  inputLablel.classList.add('field-label');
+
+  inputLablel.setAttribute('for', element.getAttribute('id'));
+
+  element.after(inputLablel);
+
+  inputLablel.textContent = textParser(element.getAttribute('name'));
+
+  element.setAttribute('placeholder', textParser(element.getAttribute('name')));
+});
