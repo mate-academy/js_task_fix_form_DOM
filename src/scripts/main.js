dbba827fbb5de25e50b3f02e4999de834ae62ecc
alina <kostenkoalina97@icloud.com>
2025-08-24 21:15:54 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+function addLabelAndPlaceholders() {
+  const form = document.querySelectorAll('form .field input');
+
+  form.forEach((field) => {
+    const label = document.createElement('label');
+
+    label.className = 'field-label';
+    label.setAttribute('for', field.id);
+    label.textContent = field.name;
+    field.setAttribute('placeholder', field.name);
+
+    field.parentNode.insertBefore(label, field);
+  });
+}
+
+addLabelAndPlaceholders();
