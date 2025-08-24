7cadac61472d792e4218e444ea380bdf2175ca3c
alina <kostenkoalina97@icloud.com>
2025-08-24 21:33:33 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+Array.from(inputs).forEach((input) => {
+  const capitalizedName =
+    input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.textContent = capitalizedName;
+
+  input.parentElement.appendChild(label);
+  input.placeholder = capitalizedName;
+});
