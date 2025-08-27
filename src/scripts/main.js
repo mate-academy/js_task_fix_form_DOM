c87a32a3cb12084e144a4e6f1a250dc238537d32
kirky359 <lol783176@gmail.com>
2025-08-27 21:18:29 +0300

    task completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+for (const el of inputs) {
+  if (el.name) {
+    const text = el.name[0].toUpperCase() + el.name.slice(1).toLowerCase();
+
+    el.placeholder = text;
+
+    const lab = document.createElement('label');
+
+    lab.setAttribute('for', el.id);
+    lab.textContent = text.toUpperCase();
+
+    lab.style.fontWeight = 'bold';
+    lab.style.fontSize = '13px';
+
+    el.parentNode.insertBefore(lab, el);
+  }
+}
