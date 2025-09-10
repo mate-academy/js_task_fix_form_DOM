79fcb4c85f6d1306b390f1fb5cd5150cbb47da84
Nazariy <nazariy.holovach2007@gmail.com>
2025-09-10 14:27:16 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+// Helper function to capitalize string
+function capitalizeFirstLetter(str) {
+  if (!str) {
+    return '';
+  }
+
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function createLabel(inputEl) {
+  const label = document.createElement('label');
+
+  // Set label attributes
+  label.classList.add('field-label');
+  label.setAttribute('for', inputEl.id);
+  label.textContent = inputEl.name;
+
+  inputEl.parentNode.insertBefore(label, inputEl);
+}
+
+function setPlaceholder(inputEl) {
+  inputEl.setAttribute('placeholder', capitalizeFirstLetter(inputEl.name));
+}
+
+const inputElements = [...document.getElementsByTagName('input')];
+
+inputElements.forEach((el) => {
+  createLabel(el);
+  setPlaceholder(el);
+});
