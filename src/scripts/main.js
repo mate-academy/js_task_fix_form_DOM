26360eba815e3a9d6ef55ab37f83a21bf92b06d0
Vladyslav Kostiuk <K4nezoor@gmail.com>
2025-09-12 11:00:28 +0200

    finished task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const imputs = document.querySelectorAll('form input');
+
+  for (const input of imputs) {
+    const label = document.createElement('label');
+
+    label.classList.add('field-label');
+    label.setAttribute('for', input.id);
+
+    label.textContent = input.name;
+
+    input.placeholder =
+      input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+    input.parentElement.appendChild(label);
+  }
+});
