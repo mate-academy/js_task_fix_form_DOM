17e85dbe11049067b132eb241f3e1830e50553b9
Yuliia Kulish <kulish.yulechka@gmail.com>
2025-09-23 22:54:59 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputMap = [...document.querySelectorAll('.field-text')];
+
+inputMap.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+
+  const capitalized = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  input.setAttribute('placeholder', capitalized);
+
+  input.parentElement.insertBefore(label, input);
+});
