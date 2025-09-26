5957017fb536da00cd8787ebcad974ecd2958637
pvcificv <pvcificv@quantum.gate>
2025-09-26 13:53:55 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
+
+  const names = input.name;
+
+  label.textContent = names.charAt(0).toUpperCase() + names.slice(1);
+
+  input.placeholder = label.textContent;
+
+  input.parentNode.insertBefore(label, input);
+});
