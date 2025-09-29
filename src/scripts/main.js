7647efe23102ce11014d6936edfd84838cc48bd4
Aleksandr Chaika <alekschayka97@gmail.com>
2025-09-29 19:57:10 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+for (const element of inputs) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', element.id);
+  label.textContent = getFirstUpperChar(element.name);
+  element.placeholder = getFirstUpperChar(element.name);
+  element.parentElement.insertBefore(label, element);
+}
+
+function getFirstUpperChar(name) {
+  const firstChar = name.charAt(0).toUpperCase() + name.slice(1);
+
+  return firstChar;
+}
