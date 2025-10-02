d9d93ae89f9bf9fcd0c7cb96aff162ae1280b09a
Yaroslav Molchanov <y.molchanoff@gmail.com>
2025-10-02 12:26:00 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
 // write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const inputs = document.querySelectorAll('form input');
+
+  inputs.forEach((input) => {
+    const inputName = input.getAttribute('name'); // <--- renamed
+
+    if (!inputName) {
+      return;
+    }
+
+    const words = inputName.replace(/([A-Z])/g, '$1').trim();
+    const placeholder =
+      words.charAt(0).toUpperCase() + words.slice(1).toLowerCase();
+
+    const label = document.createElement('label');
+
+    label.classList.add('field-label');
+
+    // Якщо інпуту немає id — задаємо один (щоб label[for] працював)
+    if (!input.id) {
+      input.id = inputName;
+    }
+    label.setAttribute('for', input.id);
+    label.textContent = words.toUpperCase();
+
+    input.setAttribute('placeholder', placeholder);
+
+    input.parentNode.insertBefore(label, input);
+  });
+});
