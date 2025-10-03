3022156ea21a0006b820614857b416a1512d71c4
Roman <hpp1ovh@gmail.com>
2025-10-03 21:11:38 +0300

    complite task v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,17 @@
 'use strict';
 
-// write code here
 document.addEventListener('DOMContentLoaded', () => {
   const forms = document.querySelectorAll('form');
 
   forms.forEach((form) => {
     const inputs = form.querySelectorAll('input');
 
-    inputs.forEach((input) => {
+    inputs.forEach((input, index) => {
       const inputName = input.name;
+
+      if (!inputName) {
+        return;
+      }
 
       const placeholder = inputName
         .replace(/([A-Z])/g, ' $1')
@@ -16,12 +19,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
       input.placeholder = placeholder;
 
+      if (!input.id) {
+        const sanitizedId =
+          inputName.toLowerCase().replace(/\s+/g, '-') + '-' + index;
+
+        input.id = sanitizedId;
+      }
+
       const label = document.createElement('label');
 
       label.className = 'field-label';
       label.setAttribute('for', input.id);
-      label.textContent = placeholder.toUpperCase();
-
+      label.textContent = placeholder;
       input.parentNode.appendChild(label);
     });
   });
