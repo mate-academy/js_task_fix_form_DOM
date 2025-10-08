9652d3560ee9430b0ecf366fecf7295cb4222cf0
Artem Yakhno <artem.yakhno.ua@gmail.com>
2025-10-08 14:54:11 +0300

    fixed isuess

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,37 @@
 'use strict';
 
 function formatName(string) {
-  if (!string) {
-    return '';
-  }
-
   const formatted = string.replace(/([A-Z])/g, ' $1').toLowerCase();
 
   return formatted.charAt(0).toUpperCase() + formatted.slice(1);
 }
 
-const inputs = document.querySelectorAll('form input');
+function generateLabels(listInputs) {
+  listInputs.forEach((input, index) => {
+    if (!input.name) {
+      return;
+    }
 
-if (inputs) {
-  inputs.forEach((input) => {
+    if (!input.id) {
+      const sanitizedId =
+        input.name.toLowerCase().replace(/\s+/g, '-') + '-' + index;
+
+      input.id = sanitizedId;
+    }
+
     const label = document.createElement('label');
 
     label.classList.add('field-label');
     label.setAttribute('for', input.id);
-    label.textContent = formatName(input.name).toUpperCase();
+    label.textContent = formatName(input.name);
     input.setAttribute('placeholder', formatName(input.name));
 
     input.before(label);
   });
+}
+
+const inputs = document.querySelectorAll('form input');
+
+if (inputs) {
+  generateLabels(inputs);
 }
