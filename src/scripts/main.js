72f203a52f5b07280769d813d68b8e03e4b1d90e
Artem Yakhno <artem.yakhno.ua@gmail.com>
2025-10-08 14:19:56 +0300

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+function formatName(string) {
+  if (!string) {
+    return '';
+  }
+
+  const formatted = string.replace(/([A-Z])/g, ' $1').toLowerCase();
+
+  return formatted.charAt(0).toUpperCase() + formatted.slice(1);
+}
+
+const inputs = document.querySelectorAll('form input');
+
+if (inputs) {
+  inputs.forEach((input) => {
+    const label = document.createElement('label');
+
+    label.classList.add('field-label');
+    label.setAttribute('for', input.id);
+    label.textContent = formatName(input.name).toUpperCase();
+    input.setAttribute('placeholder', formatName(input.name));
+
+    input.before(label);
+  });
+}
