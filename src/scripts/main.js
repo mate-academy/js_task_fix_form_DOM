dff6cf7ee9795a0c8c2cb92e25211aecc334c20b
Roma Ivashchenko <rrrrgvvggh@gmail.com>
2025-10-10 16:31:06 +0300

    add

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const allInputs = document.querySelectorAll('input');
+const allInputs = document.querySelectorAll('form input');
 
 for (const input of allInputs) {
   const allLabel = document.createElement('label');
 
   allLabel.classList.add('field-label');
-  allLabel.textContent = input.name[0].toUpperCase() + input.name.slice(1);
+  allLabel.textContent = input.name;
   allLabel.setAttribute('for', input.id);
-  input.placeholder = allLabel.textContent;
+  input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
   input.parentElement.prepend(allLabel);
 }
