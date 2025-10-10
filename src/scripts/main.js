3fa895902fb49fb8c1ebf02cd841404035c41b36
Roma Ivashchenko <rrrrgvvggh@gmail.com>
2025-10-10 16:24:52 +0300

    add task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,10 +4,10 @@ const allInputs = document.querySelectorAll('input');
 
 for (const input of allInputs) {
   const allLabel = document.createElement('label');
+
   allLabel.classList.add('field-label');
   allLabel.textContent = input.name[0].toUpperCase() + input.name.slice(1);
   allLabel.setAttribute('for', input.id);
   input.placeholder = allLabel.textContent;
   input.parentElement.prepend(allLabel);
 }
-
