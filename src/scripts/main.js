f287c09b21554b8b18c15dcf9fb1e32058ed7831
Dima <dima2003strik@gmail.com>
2025-10-11 04:13:51 +0300

    add task solution-2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,20 +5,26 @@ const inputs = [...document.querySelectorAll('form input')];
 
 inputs.forEach((input, index) => {
   const nameElement = input.getAttribute('name');
-  const capitalized = nameElement.charAt(0).toUpperCase() + nameElement.slice(1);
 
-  if (!nameElement) return;
+  if (!nameElement) {
+    return;
+  }
+
+  const capitalized =
+    nameElement.charAt(0).toUpperCase() + nameElement.slice(1);
 
   if (!input.id) {
     input.id = `${nameElement}-${index}`;
   }
 
   const label = document.createElement('label');
+
   label.classList.add('field-label');
   label.setAttribute('for', input.id);
   label.textContent = capitalized;
 
   const parentElement = input.parentElement;
+
   parentElement.prepend(label);
 
   input.placeholder = capitalized;
