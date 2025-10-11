da6f1c9b1a246c0eaa5f105babda821075e2e1da
Dima <dima2003strik@gmail.com>
2025-10-11 03:59:10 +0300

    fixed problems

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,25 @@
 'use strict';
 
 // write code here
-const inputs = [...document.querySelectorAll('input')];
+const inputs = [...document.querySelectorAll('form input')];
 
-inputs.forEach((input) => {
+inputs.forEach((input, index) => {
+  const nameElement = input.getAttribute('name');
+  const capitalized = nameElement.charAt(0).toUpperCase() + nameElement.slice(1);
+
+  if (!nameElement) return;
+
+  if (!input.id) {
+    input.id = `${nameElement}-${index}`;
+  }
+
   const label = document.createElement('label');
-
   label.classList.add('field-label');
-  label.setAttribute('for', `${input.getAttribute('id')}`);
-  label.textContent = `${input.getAttribute('name')}`;
+  label.setAttribute('for', input.id);
+  label.textContent = capitalized;
 
   const parentElement = input.parentElement;
-
   parentElement.prepend(label);
 
-  const nameInput = input.getAttribute('name');
-
-  input.placeholder = nameInput.charAt(0).toUpperCase() + nameInput.slice(1);
+  input.placeholder = capitalized;
 });
