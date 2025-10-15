3c2f3ed8f329265b7afa3243c7c9c654a012a772
Ivan Shcherbatiuk <shcherbatyuk13@gmail.com>
2025-10-16 00:40:23 +0300

    add task solution2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,15 +4,18 @@
 const formInputs = document.querySelectorAll('form input');
 
 // eslint-disable-next-line no-unused-vars
-const formatName = (text) =>
-  text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+const formatName = (text) => text.charAt(0).toUpperCase() + text.slice(1);
 
-formInputs.forEach((input) => {
+formInputs.forEach((input, index) => {
+  if (!input.id) {
+    input.id = `${input.name || 'input'}-${index}`;
+  }
+
   const label = document.createElement('label');
-
   label.className = 'field-label';
   label.setAttribute('for', input.id);
-  label.textContent = formatName(input.name);
+  label.textContent = input.name;
   input.setAttribute('placeholder', formatName(input.name));
+
   input.parentElement.appendChild(label);
 });
