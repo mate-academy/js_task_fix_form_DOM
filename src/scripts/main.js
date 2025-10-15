7f87c95b0ad80e092f291adb201ab00dde3adb30
Ivan Shcherbatiuk <shcherbatyuk13@gmail.com>
2025-10-15 23:50:17 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+// eslint-disable-next-line no-unused-vars
+const formInputs = document.querySelectorAll('form input');
+
+// eslint-disable-next-line no-unused-vars
+const formatName = (text) =>
+  text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+
+formInputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.textContent = formatName(input.name);
+  input.setAttribute('placeholder', formatName(input.name));
+  input.parentElement.appendChild(label);
+});
