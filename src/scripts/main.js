c58171bdabd10e3604f95ed163a083047ec3aab4
Valentyna Chernova <valentyna19960410@gmail.com>
2025-11-24 21:48:41 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write code here
+const inputs = [...document.querySelectorAll('input')];
+inputs.map(elem => {
+ const label = document.createElement('label');
+ label.setAttribute('for', elem.id);
+ label.classList.add('field-label');
+ label.textContent = elem.name;
+ const parrent = elem.parentElement;
+ parrent.appendChild(label);
+ elem.placeholder = elem.name.slice(0,1).toUpperCase() + elem.name.slice(1);
+})
+
