b4977630ca9119cc12683ceac880648c72452723
Dmytro Odnoroh <falcon121299@icloud.com>
2025-11-27 17:34:28 +0200

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+function fixInputs() {
+  const form = document.querySelector('.form');
+  const inputs = form.querySelectorAll('input');
+
+  inputs.forEach((input) => {
+    const label = document.createElement('label');
+
+    label.classList.add('field-label');
+    label.setAttribute('for', input.id);
+    label.textContent = input.name;
+
+    input.placeholder =
+      input.name.charAt(0).toUpperCase() + input.name.slice(1);
+    input.parentElement.append(label);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', fixInputs);
