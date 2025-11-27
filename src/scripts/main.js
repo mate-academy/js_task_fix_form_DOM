41620ce4462c9f50f8e546864ba626ad23c510cc
SageUA <panchyshyn33@gmail.com>
2025-11-27 16:59:12 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+inputs.forEach((el) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.htmlFor = el.id;
+  label.textContent = el.name;
+
+  el.parentElement.prepend(label);
+  el.placeholder = el.name.toUpperCase();
+});
