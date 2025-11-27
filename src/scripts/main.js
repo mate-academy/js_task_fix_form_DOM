69cc791f1306e1bc44fce37db1ed41c0029f7296
Stas <oleksii.stanislav@student.uzhnu.edu.ua>
2025-11-27 18:58:28 +0200

    Add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write code here
+const inputs = document.querySelectorAll('input');
+const arr = Array.from(inputs);
+
+for (const input of arr) {
+  if (!input.id) {
+    input.id = input.name + '-id';
+  }
+
+  const label = document.createElement('label');
+
+  input.placeholder = capitalize(input.name);
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
+  label.textContent = capitalize(input.name);
+  input.insertAdjacentElement('beforebegin', label);
+}
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
