a8f850c6fed55b44c81caf698635143563748add
Aleksandra <escovaaleksandra@gmail.com>
2025-12-03 15:19:35 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
 // write code here
+const lablesMap = {
+  firstName: 'First Name',
+  lastName: 'Last Name',
+  email: 'Email',
+  password: 'Password'
+};
+
+const inputs = document.querySelectorAll('form input');
+
+inputs.forEach(input => {
+
+  const name = input.getAttribute('name');
+
+  if (!lablesMap[name]) {
+    return;
+  }
+
+  const lable = document.createElement('lable');
+  lable.classList.add('field-label');
+  lable.textContent = lablesMap[name];
+  lable.setAttribute('for', name);
+
+  input.before(lable);
+
+  input.setAttribute('placeholder', lablesMap[name]);
+})
+
+
+
+
+
