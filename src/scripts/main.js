58fc0adf7f3ab07b2524c150bf85afe0aaa48858
Andrey <kagamlykas@gmail.com>
2025-12-09 13:14:16 +0200

    Solved task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const blocks = document.querySelectorAll('.field');
+
+if (blocks) {
+  for (let element of blocks) {
+    const inputElement = element.querySelector('input');
+
+    if (inputElement){
+      const inputName = inputElement.getAttribute('name');
+      const el = document.createElement('label')
+      el.setAttribute('for', inputElement.getAttribute('id'));
+      el.setAttribute('class', 'field-label');
+      el.textContent = inputName.toUpperCase();
+
+      //element.prepend(el);
+      element.insertAdjacentElement('afterbegin', el);
+
+      const placeholderValue = inputName[0].toUpperCase().concat(inputName.slice(1));
+      inputElement.setAttribute('placeholder', placeholderValue);
+    }
+  }
+}
