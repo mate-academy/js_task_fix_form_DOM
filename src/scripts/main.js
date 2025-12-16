2cdcdd181af363cfd4581d4923131983662330fc
Evg3n22 <evgen.gulevaty@gmail.com>
2025-12-16 14:11:26 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // write code here
+const inputs = document.querySelectorAll('.field-text');
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', input.getAttribute('id'));
+  label.textContent = input.getAttribute('name');
+
+  input.parentNode.insertBefore(label, input);
+
+  input.setAttribute('placeholder', input.getAttribute('name'));
+});
