a797c24debabd676c392daffe5d86e743e442d2c
Dmytro <dimusgamer2.0@gmail.com>
2025-12-20 23:01:35 +0200

    Add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input').values()];
+
+const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
+for (const input of inputs) {
+  const { name: inputName, id: inputId, parentElement } = input;
+
+  input.placeholder = capitalize(inputName);
+
+  const label = document.createElement('label');
+
+  label.textContent = inputName;
+  label.htmlFor = inputId;
+  label.classList.add('field-label');
+
+  parentElement.append(label);
+}
