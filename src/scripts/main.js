c6a4ea755b83b958e75d2fe4c30a2e3337ea8c8d
Aleks Pashkevych <alekspashkevych@gmail.com>
2025-12-21 08:12:12 +0200

    completed home task, test padded

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+inputs.forEach((input) => {
+  const value = input.name;
+  const toShow = value.toUpperCase();
+  const lbl = document.createElement('label');
+  const forWho = input.id;
+
+  lbl.className = 'field-label';
+  lbl.id = value;
+  lbl.innerText = toShow;
+  lbl.for = forWho;
+
+  input.placeholder = value.charAt(0).toUpperCase() + value.slice(1);
+  input.before(lbl);
+});
