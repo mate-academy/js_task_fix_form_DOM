b7681c7c0153a2df3fa6806e9d4ba73adc07cdac
Nazarii Lesniak <naziksay@gmail.com>
2026-01-07 09:42:59 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const inputsArray = [...document.querySelectorAll('input[name]')];
+
+inputsArray.forEach((item) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.htmlFor = item.id;
+  label.textContent = capitalizeFirstLetter(item.name);
+  item.parentNode.insertBefore(label, item);
+  item.placeholder = capitalizeFirstLetter(item.name);
+});
+
+function capitalizeFirstLetter(string) {
+  if (string.length === 0) {
+    return '';
+  }
+
+  return string.charAt(0).toUpperCase() + string.toLowerCase().slice(1);
+}
