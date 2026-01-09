c586e831e401898b864b391a329217244d3d01b0
DevT <dev.tymofiichuk@gmail.com>
2026-01-09 10:25:23 +0200

    Auth form fix task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const inputElements = document.querySelectorAll('form input');
+
+inputElements.forEach((el) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', el.id);
+  label.textContent = el.name;
+
+  el.placeholder = el.name[0].toUpperCase() + el.name.slice(1);
+  el.before(label);
+});
