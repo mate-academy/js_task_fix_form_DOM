a2e17ec049b1c33d0d2575fbe8699e084222f059
Illia Safonov <safonov.illia@gmail.com>
2026-01-14 13:52:55 +0100

    js_task_fix_form_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+
+  const inputName = input.name;
+  const capitalizedName = inputName[0].toUpperCase() + inputName.slice(1);
+
+  input.placeholder = capitalizedName;
+
+  const parentElement = input.parentElement;
+
+  parentElement.append(label);
+});
