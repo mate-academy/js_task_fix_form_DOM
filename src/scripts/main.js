d593797d4c8590a40d846bd9e79b40e748f71434
Mykyta Oliinyk <mykyta.oliinyk0@gmail.com>
2026-01-16 17:52:34 +0100

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
+/* eslint-disable prettier/prettier */
 'use strict';
 
-// write code here
+const inputsFromForm = document.querySelectorAll('input');
+
+inputsFromForm.forEach((element) =>
+  element.setAttribute(
+    'placeholder',
+    element.name[0].toUpperCase() + element.name.slice(1),
+  ));
+
+inputsFromForm.forEach((element) => {
+  const labelElement = document.createElement('label');
+
+  labelElement.classList.add('field-label');
+  labelElement.setAttribute('for', element.id);
+  labelElement.textContent = element.name;
+
+  element.parentNode.appendChild(labelElement);
+});
