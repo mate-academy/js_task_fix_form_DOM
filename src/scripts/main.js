f753b1a7cb76ed207e4924ccdced6296211e9e73
Olia Shymanska <oliaa.shymanska@gmail.com>
2026-01-23 02:04:27 +0200

    add label and placeholder for each input

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const pathInputs = document.querySelectorAll('input');
+
+pathInputs.forEach((targetInput) => {
+  const labelInput = document.createElement('label');
+
+  labelInput.textContent = targetInput.name;
+  labelInput.className = 'field-label';
+  labelInput.htmlFor = targetInput.id;
+
+  targetInput.after(labelInput);
+});
+
+pathInputs.forEach((targetInput) => {
+  const textPlaceholder =
+    targetInput.name[0].toUpperCase() + targetInput.name.slice(1);
+
+  targetInput.setAttribute('placeholder', textPlaceholder);
+});
