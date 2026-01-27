af448430f703121bef7c7c65e6761000a20adc8a
vladahvala <80634246+vladahvala@users.noreply.github.com>
2026-01-27 15:39:38 +0200

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+[...document.querySelectorAll('form input')].map((input) => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+
+  const placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  input.placeholder = placeholder;
+
+  input.before(label);
+
+  return input;
+});
