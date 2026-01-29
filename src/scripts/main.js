3f98e90bf6f413f2690d1798e4ed9d339c050a57
DenisNosov22 <dennosov2210@gmail.com>
2026-01-29 16:40:43 +0200

    js task fix from DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+inputs.forEach((input) => {
+  const labelText = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.textContent = labelText;
+  input.placeholder = labelText;
+
+  const parentField = input.parentElement;
+
+  parentField.insertBefore(label, input);
+});
