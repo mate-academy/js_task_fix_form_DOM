8afa7e8c9d40ff1c2ba11ffdcd969ed64eab1a3a
Marta Kurdylo <martakurdylo17@gmail.com>
2026-02-02 10:26:52 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write code here
+const allInputs = document.querySelectorAll('form input');
+
+allInputs.forEach((input, index) => {
+  if (!input.id) {
+    input.id = input.name + '-' + index;
+  }
+
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.htmlFor = input.id;
+
+  let text = input.name;
+
+  text = text.replace(/[-_]/g, ' ');
+  text = text.charAt(0).toUpperCase() + text.slice(1);
+
+  label.textContent = text;
+  input.placeholder = text;
+
+  input.parentElement.insertBefore(label, input);
+});
