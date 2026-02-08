2ac90491f6286ec6039f72bac7337de897663175
freelinex <freelinex7@gmail.com>
2026-02-08 23:29:26 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const fields = document.querySelectorAll('form input');
+
+fields.forEach((item) => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', item.id);
+  label.textContent = item.name;
+
+  item.placeholder = item.name[0].toUpperCase() + item.name.slice(1);
+
+  item.before(label);
+});
