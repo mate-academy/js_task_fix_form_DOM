f3cac74ba904247c321984a9b7a6df93c8313d40
Ola <aleksandra-chomicz@outlook.com>
2026-03-05 13:09:49 +0100

    add labels to inputs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const inputList = Array.from(document.querySelectorAll('form input'));
+
+inputList.forEach((input) => {
+  if (!input.id) {
+    input.id = input.name;
+  }
+
+  const newLabel = document.createElement('label');
+
+  newLabel.classList.add('field-label');
+
+  newLabel.setAttribute('for', input.id);
+
+  newLabel.textContent = input.name;
+
+  input.placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  input.before(newLabel);
+});
