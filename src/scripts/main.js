f38eb9fc6e24b264da3473f13e578c73f06ca692
Taras Pidkuimukha <taraspidkuimukha@gmail.com>
2026-03-08 16:59:45 +0100

    lets try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+inputs.forEach((input, index) => {
+  if (!input.id) {
+    input.id = `input-${index}`;
+  }
+
+  if (!input.parentElement.querySelector(`label[for="${input.id}"]`)) {
+    const label = document.createElement('label');
+
+    label.className = 'field-label';
+    label.htmlFor = input.id;
+    label.textContent = toLabelText(input.name);
+    input.parentElement.appendChild(label);
+  }
+  input.placeholder = toLabelText(input.name);
+});
+
+function toLabelText(str) {
+  const clean = str.replace(/([A-Z])/g, ' $1').toLowerCase();
+
+  return clean.charAt(0).toUpperCase() + clean.slice(1);
+}
