176f1d87df40f9615a30b80526a5cf916c21386a
Yevhen Serdiuk <yevhen.srdk@gmail.com>
2026-03-10 16:19:58 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
+/* eslint-disable no-console */
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+  const inputParent = input.closest('div');
+
+  label.classList.add('field-label');
+  label.for = input.id;
+  label.textContent = input.name.toUpperCase();
+
+  const inputPlaceholder =
+    input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  input.placeholder = inputPlaceholder;
+
+  inputParent.append(label);
+}
