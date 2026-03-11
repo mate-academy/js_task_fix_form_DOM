68d3af761471931e9629a3827a3dbb79be023833
Valentyna <pr.valya1989@gmail.com>
2026-03-11 13:12:48 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const allForms = document.querySelectorAll('form');
+
+allForms.forEach((form) => {
+  const inputInThisForm = form.querySelectorAll('input');
+
+  inputInThisForm.forEach((input) => {
+    const inputName = input.name;
+    const formattedName =
+      inputName.charAt(0).toUpperCase() + inputName.slice(1);
+
+    input.placeholder = formattedName;
+
+    const newLabel = document.createElement('label');
+
+    newLabel.classList.add('field-label');
+    newLabel.htmlFor = input.id;
+    newLabel.textContent = formattedName;
+    input.parentElement.appendChild(newLabel);
+  });
+});
