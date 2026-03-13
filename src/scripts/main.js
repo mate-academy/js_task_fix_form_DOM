efc02f7462e91c99a11d16e7478be65305119567
Taras Demianiuk <demtaras1@gamil.com>
2026-03-13 10:37:42 +0200

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const forms = document.getElementsByTagName('form');
+
+for (const form of forms) {
+  const inputs = form.getElementsByTagName('input');
+
+  for (const input of inputs) {
+    const inputName = input.name;
+
+    input.placeholder = inputName[0].toUpperCase() + inputName.slice(1);
+
+    const label = document.createElement('label');
+
+    label.setAttribute('for', input.id);
+    label.classList.add('field-label');
+    label.textContent = inputName;
+    input.before(label);
+  }
+}
