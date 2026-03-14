9ce29fae383d830d67dc669ff435950a22c0ac00
Taras Yelahin <inflades@gmail.com>
2026-03-14 17:17:07 +0200

    dom_fix_form_task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((input) => {
+  if (!input.id) {
+    input.id = `input-${input.name}`;
+  }
+
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+
+  const text = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  label.textContent = text;
+  input.placeholder = text;
+  input.parentElement.insertBefore(label, input);
+});
