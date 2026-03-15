05a8e09a2233672eef8b8c4013a94c56b3902958
Mariana Klochnyk <klochnykmaryana@gmail.com>
2026-03-15 20:29:59 +0200

    edit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const getInput = document.querySelectorAll('input');
+
+getInput.forEach((input) => {
+  const formLabel = document.createElement('label');
+
+  formLabel.classList.add('field-label');
+  formLabel.setAttribute('for', input.id);
+  formLabel.textContent = input.name;
+  input.placeholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+  input.parentElement.append(formLabel);
+});
