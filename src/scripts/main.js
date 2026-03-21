5e5f58549b02be705e078cc6d67c8daf3bb8b635
Yurii Leniv <supusman32@gmail.com>
2026-03-21 20:01:45 +0200

    fdsgfsdgsd

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+  let k = document.querySelectorAll('form .field .field-text');
+
+  let m = Array.from(k);
+
+  m.forEach((el) => {
+    let k = el.getAttribute('name');
+    let p = el.getAttribute('id');
+
+    k = k[0].toUpperCase() + k.slice(1);
+    el.setAttribute('placeholder', k);
+    const label = document.createElement('label');
+    label.className = 'field-label';
+    label.htmlFor = p;
+    label.textContent = k;
+
+    el.before(label);
+  });
