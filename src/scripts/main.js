3af7e5c0b202a5e566e4a709fbe6fa3989f0d289
Maksym Davydiuk <krytan53@gmail.com>
2026-03-29 17:27:42 +0300

    Fix auth form DOM task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+function capitalize(text) {
+  return text[0].toUpperCase() + text.slice(1);
+}
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+  const capitalizedName = capitalize(input.name);
+
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
+  label.textContent = capitalizedName;
+
+  input.placeholder = capitalizedName;
+  input.parentElement.append(label);
+});
