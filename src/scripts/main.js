38e547203b544696b546d95b0f6654953ac9886a
Banderos14 <anton.shyshenko@gmail.com>
2026-04-02 13:38:34 +0200

    feat: task_solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const inputs = Array.from(document.querySelectorAll('form input'));
+
+inputs.forEach((input) => {
+  const text = input.name[0].toUpperCase() + input.name.slice(1);
+
+  input.setAttribute('placeholder', text);
+
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.htmlFor = input.id;
+  label.textContent = text;
+
+  input.parentElement.insertBefore(label, input);
+});
