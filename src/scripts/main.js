38df042ff36cbca598c1eec4d80b702a8e3c73fe
Maksim Vashuk <maximka.vashuk@gmail.com>
2026-04-04 22:33:10 +0200

    fix form dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 // write code here
+const inputs = document.querySelectorAll('form input');
+
+inputs.forEach((input) => {
+  const inputName = input.getAttribute('name');
+  const id = input.getAttribute('id');
+
+  const formatedName = inputName.charAt(0).toUpperCase() + inputName.slice(1);
+
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', id);
+  label.textContent = formatedName;
+
+  input.placeholder = formatedName;
+
+  input.parentElement.appendChild(label);
+});
