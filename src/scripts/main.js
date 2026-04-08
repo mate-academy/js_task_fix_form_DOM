fa15cbc5c201ed27e98b9dd529314103cf35d636
Aakash Sadhu <aakashsadhu@hotmail.com>
2026-04-08 15:57:27 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+inputs.forEach((input) => {
+  const inputName = input.getAttribute('name');
+
+  if (!input.id) {
+    input.id = `input-${inputName}`;
+  }
+
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.textContent = inputName;
+
+  const capitaliseName = inputName.charAt(0).toUpperCase() + inputName.slice(1);
+
+  input.placeholder = capitaliseName;
+
+  input.parentElement.prepend(label);
+});
