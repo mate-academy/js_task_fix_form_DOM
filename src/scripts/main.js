7419e596236ee7819edab940b7d5b479f6b5e4f0
Vital <vitaligna@gmail.com>
2026-04-12 11:01:18 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const allInputs = document.querySelectorAll('form input');
+
+allInputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+
+  const capitalizedName =
+    input.name
+      .charAt(0)
+
+      .toUpperCase() + input.name.slice(1);
+
+  input.placeholder = capitalizedName;
+
+  input.parentElement.prepend(label);
+});
