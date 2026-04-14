79ce2c6c11b689698fae506dbbb6bab3fc59046f
OlehYuriev <urievoleg94@gmail.com>
2026-04-14 15:23:33 +0300

    fix script

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,19 +5,28 @@ const forms = document.querySelectorAll('form');
 forms.forEach((form) => {
   const inputs = form.querySelectorAll('input');
 
-  inputs.forEach((item) => {
-    if (item.tagName === 'INPUT') {
-      const idInput = item.getAttribute('id');
-      const nameInput = item.getAttribute('name');
-      const label = document.createElement('label');
+  inputs.forEach((item, index) => {
+    const nameInput = item.getAttribute('name');
 
-      const capitalize = nameInput[0].toLocaleUpperCase() + nameInput.slice(1);
-
-      label.classList.add('field-label');
-      item.setAttribute('placeholder', capitalize);
-      label.setAttribute('for', idInput);
-      label.textContent = nameInput;
-      item.before(label);
+    if (!nameInput) {
+      return;
     }
+
+    let id = item.id;
+
+    if (!id) {
+      id = `generated-id-${index}`;
+      item.id = id;
+    }
+
+    const label = document.createElement('label');
+
+    const capitalize = nameInput[0].toLocaleUpperCase() + nameInput.slice(1);
+
+    label.classList.add('field-label');
+    item.setAttribute('placeholder', capitalize);
+    label.setAttribute('for', id);
+    label.textContent = capitalize;
+    item.before(label);
   });
 });
