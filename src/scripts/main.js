9cc2a8a714bed32da9b0123db9959064e802a95b
Damian Kurzydlo <duchozywia2024@gmail.com>
2026-05-11 22:18:36 +0100

    feat: generate labels and placeholders for form inputs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write code here
+const formInputs = document.querySelectorAll('input');
+
+formInputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', input.getAttribute('id'));
+  label.textContent = input.getAttribute('name');
+  input.insertAdjacentElement('beforebegin', label);
+  input.setAttribute('placeholder', input.getAttribute('name'));
+});
