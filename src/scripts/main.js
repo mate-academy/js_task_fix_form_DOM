4cbdcef792fe4b78f3a122054d53ba1fb97fe8d7
Damian Kurzydlo <duchozywia2024@gmail.com>
2026-05-11 22:56:29 +0100

    select inputs from form and capitalize placeholders

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,22 @@
 'use strict';
 
 // write code here
-const formInputs = document.querySelectorAll('input');
+const forms = document.querySelectorAll('form');
 
-formInputs.forEach((input) => {
-  const label = document.createElement('label');
+forms.forEach((form) => {
+  const inputs = form.querySelectorAll('input');
+  inputs.forEach((input) => {
+    const label = document.createElement('label');
 
-  label.classList.add('field-label');
-  label.setAttribute('for', input.getAttribute('id'));
-  label.textContent = input.getAttribute('name');
-  input.insertAdjacentElement('beforebegin', label);
-  input.setAttribute('placeholder', input.getAttribute('name'));
+    label.classList.add('field-label');
+    label.setAttribute('for', input.id);
+    label.textContent = input.name;
+
+    input.insertAdjacentElement('beforebegin', label);
+    input.placeholder = capitalize(input.name);
+  });
 });
+
+function capitalize(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
