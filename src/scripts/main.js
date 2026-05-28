94f557ccb63dad557d556d247bd0080badf2e9fd
ILLYA SMYK <smykillya@gmail.com>
2026-05-28 20:37:23 +0200

    add task solution2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,17 @@
 'use strict';
 
-const form = document.querySelector('form');
+const forms = [...document.querySelectorAll('form')];
 
-const inputs = [...form.querySelectorAll('input')];
+for (const form of forms) {
+  const inputs = [...form.querySelectorAll('input')];
 
-for (const input of inputs) {
-  const label = document.createElement('label');
+  for (const input of inputs) {
+    const label = document.createElement('label');
 
-  label.classList.add('field-label');
-  label.setAttribute('for', input.id);
-  label.textContent = input.name;
-  input.placeholder = input.name[0].toLocaleUpperCase() + input.name.slice(1);
-  input.parentElement.append(label);
+    label.classList.add('field-label');
+    label.setAttribute('for', input.id);
+    label.textContent = input.name;
+    input.placeholder = input.name[0].toUpperCase() + input.name.slice(1);
+    input.parentElement.append(label);
+  }
 }
