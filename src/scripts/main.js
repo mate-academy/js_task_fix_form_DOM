8143d3bf9b623626455feafa2d78f5cafd0569ae
interpunktus <240490702+interpunktus@users.noreply.github.com>
2026-06-09 16:01:54 +0300

    try1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write code here
+
+const allInputs = document.querySelectorAll('form input');
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+allInputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.classList.add('class');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
+  label.textContent = capitalize(input.name);
+
+  input.placeholder = capitalize(input.name);
+  input.parentElement.append(label);
+});
