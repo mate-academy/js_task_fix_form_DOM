4ebd11093a12dd9e959484f94f5bf8d991b2675c
Vitalii <vtysiachnyk@gmail.com>
2026-06-21 19:03:56 +0300

    add function to format inputName

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,8 @@
 
 const allInputs = document.querySelectorAll('form input');
 
+const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
 for (const input of allInputs) {
   const inputName = input.getAttribute('name');
   const inputId = input.getAttribute('id');
@@ -9,15 +11,17 @@ for (const input of allInputs) {
 
   const label = document.createElement('label');
 
+  const formattedName = capitalize(inputName);
+
   label.classList.add('field-label');
 
   if (inputId) {
     label.setAttribute('for', inputId);
   }
 
-  label.textContent = inputName;
+  label.textContent = formattedName;
 
-  input.setAttribute('placeholder', inputName);
+  input.setAttribute('placeholder', formattedName);
 
   parentContainer.append(label);
 }
