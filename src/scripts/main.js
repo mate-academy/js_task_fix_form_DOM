734f5625845a0ec06997d2b09fc90f14706896a3
Olexander <parfenov4589@gmail.com>
2026-06-23 17:40:30 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const form = document.querySelectorAll('form');
+const inputs1 = form[0].querySelectorAll('input');
+const inputs2 = form[1].querySelectorAll('input');
+
+for (const input of inputs1) {
+  input.insertAdjacentHTML(
+    'beforebegin',
+    `<label class="field-label" for="${input.id}">
+      ${input.name}
+    </label>`,
+  );
+
+  const placeholders =
+    input.name[0].toUpperCase() + input.name.slice(1).toLowerCase();
+
+  input.placeholder = placeholders;
+}
+
+for (const input of inputs2) {
+  input.insertAdjacentHTML(
+    'beforebegin',
+    `<label class="field-label" for="${input.id}">
+      ${input.name}
+    </label>`,
+  );
+
+  const placeholders =
+    input.name[0].toUpperCase() + input.name.slice(1).toLowerCase();
+
+  input.placeholder = placeholders;
+}
