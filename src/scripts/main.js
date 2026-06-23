dfde5281c34a7043ba768d5818db63c5304e5cf5
Olexander <parfenov4589@gmail.com>
2026-06-23 17:46:03 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,33 +1,20 @@
 'use strict';
 
-const form = document.querySelectorAll('form');
-const inputs1 = form[0].querySelectorAll('input');
-const inputs2 = form[1].querySelectorAll('input');
+const forms = document.querySelectorAll('form');
 
-for (const input of inputs1) {
-  input.insertAdjacentHTML(
-    'beforebegin',
-    `<label class="field-label" for="${input.id}">
-      ${input.name}
-    </label>`,
-  );
+for (const form of forms) {
+  const inputs = form.querySelectorAll('input');
 
-  const placeholders =
-    input.name[0].toUpperCase() + input.name.slice(1).toLowerCase();
+  for (const input of inputs) {
+    input.parentElement.insertAdjacentHTML(
+      'afterbegin',
+      `<label class="field-label" for="${input.id}">
+        ${input.name}
+      </label>`,
+    );
 
-  input.placeholder = placeholders;
-}
+    const placeholder = input.name[0].toUpperCase() + input.name.slice(1);
 
-for (const input of inputs2) {
-  input.insertAdjacentHTML(
-    'beforebegin',
-    `<label class="field-label" for="${input.id}">
-      ${input.name}
-    </label>`,
-  );
-
-  const placeholders =
-    input.name[0].toUpperCase() + input.name.slice(1).toLowerCase();
-
-  input.placeholder = placeholders;
+    input.placeholder = placeholder;
+  }
 }
