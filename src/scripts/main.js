3d569f7d7b7d0e61431a10ad6f730c7e19052162
student1232354 <tarasromanenko33@gmail.com>
2026-07-04 11:13:05 +0300

    first update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+ const inputs = document.querySelectorAll('form input');
+
+ inputs.forEach(function (oneinput) {
+  const inputName = oneinput.name;
+
+  const normalname = inputName.charAt(0).toUpperCase() + inputName.slice(1);
+
+  oneinput.placeholder = normalname;
+
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('for', oneinput.id);
+
+  label.textContent = normalname;
+
+  const div = oneinput.parentElement;
+
+  div.insertBefore(label, oneinput);
+ });
