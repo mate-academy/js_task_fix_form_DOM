c59bb4dd40f2f37ef7da41ead08ddc4a772f5703
Ol. <holmate@dontsp.am>
2026-07-22 19:50:30 +0300

    Solution added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+function splitCamelCaseWord(match) {
+  return ' ' + match.toLowerCase();
+}
+
+function formatFieldName(text) {
+  const result = text.replace(/[A-Z]/g, splitCamelCaseWord);
+
+  return result.charAt(0).toUpperCase() + result.slice(1);
+}
+
+const inputs = Array.from(document.querySelectorAll('input'));
+
+inputs.forEach((item) => {
+  const newLabel = document.createElement('label');
+  const fieldName = formatFieldName(item.name);
+
+  newLabel.setAttribute('for', item.id);
+  newLabel.classList.add('field-label');
+  newLabel.textContent = fieldName;
+  item.setAttribute('placeholder', fieldName);
+  item.insertAdjacentElement('beforebegin', newLabel);
+});
