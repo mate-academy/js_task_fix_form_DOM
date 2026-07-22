73c7d92ec99aa4bf8da72a38226cc5cb5dbcd0fc
Yehor Mishchenko <130080322+myehor@users.noreply.github.com>
2026-07-22 18:34:24 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write code here
+const inputs = [...document.querySelectorAll('input')];
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+  const parentElement = input.parentElement;
+
+  label.classList.add('field-label');
+  label.htmlFor = input.id;
+  label.textContent = input.name;
+  input.setAttribute('placeholder', capitalize(input.name));
+  parentElement.append(label);
+}
+
+function capitalize(value) {
+  const symbols = value.split('');
+
+  symbols[0] = symbols[0].toUpperCase();
+
+  return symbols.join('');
+}
