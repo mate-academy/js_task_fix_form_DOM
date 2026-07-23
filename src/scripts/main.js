790beb8cb95c7455a12746a37058e06daf05ad8a
Nik200309 <nikitapidlypnij@gmail.com>
2026-07-23 21:45:40 +0300

    First attempt, created loop for adding <label> tags in front of inputs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write code here
+const inputs = document.getElementsByTagName('input');
+
+for (let i = 0; i < inputs.length; i++) {
+  const input = inputs[i];
+  const placeholder = input.name.toUpperCase();
+
+  input.insertAdjacentHTML(
+    'beforebegin',
+    `<label class="field-label" for="${input.id}">${placeholder}</label>`,
+  );
+}
