69fc10a797202501eb0d06b7018fad8ee74ec779
nataliandr <fnnny.yann@mail.com>
2026-07-28 18:29:12 +0200

    fixed form solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+function formatName(n) {
+  return n
+    .replace(/([a-z])([A-Z])/g, '$1 $2')
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
+}
+
+const inputs = document.querySelectorAll('form input');
+
+inputs.forEach((input) => {
+  const labelText = formatName(input.name);
+
+  input.placeholder = labelText;
+
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.setAttribute('for', input.id);
+  label.textContent = labelText;
+
+  input.parentElement.appendChild(label);
+});
