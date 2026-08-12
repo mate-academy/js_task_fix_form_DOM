48923f29cfd0454324bbaa9c05d2f2f28dc31948
SquareSquirrell <zettahedron@gmail.com>
2026-08-12 19:07:19 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+function capitalize(text) {
+  if (!text) {
+    return '';
+  }
+
+  return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
+inputs.forEach((input) => {
+  const inputName = input.name;
+  const inputId = input.id;
+  const formattedName = capitalize(inputName);
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.htmlFor = inputId;
+  label.textContent = formattedName;
+
+  input.placeholder = formattedName;
+
+  const parentContainer = input.parentElement;
+
+  parentContainer.prepend(label);
+});
