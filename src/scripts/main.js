fa9ce9ff96dc88bb874d2d206e2d0ae35c2920c7
Jakub Kumorek <jkumorek01@gmail.com>
2026-08-17 19:51:09 +0200

    first task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+function createLabels() {
+  for (const input of inputs) {
+    const label = document.createElement('label');
+
+    label.setAttribute('class', 'field-label');
+
+    const id = input.getAttribute('id');
+
+    label.setAttribute('for', id);
+
+    const text = input.getAttribute('name');
+
+    label.textContent = text[0].toUpperCase() + text.slice(1);
+
+    const containerElement = input.parentElement;
+
+    containerElement.append(label);
+  }
+}
+
+createLabels();
