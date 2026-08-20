e77d1d72648245cdc134798015fc8ebc09c4bdd0
Vitalii <vitaliy.doronin4@gmail.com>
2026-08-20 11:07:10 +0300

    Add a solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const inputContainers = document.querySelectorAll('.field');
+
+const inputList = document.querySelectorAll('form input');
+
+function fixTheForm(list) {
+  list.forEach((element, index) => {
+    const label = document.createElement('label');
+
+    label.htmlFor = element.id;
+    label.className = 'field-label';
+    label.textContent = element.name.toUpperCase();
+
+    inputContainers[index].appendChild(label);
+
+    element.placeholder =
+      element.name.charAt(0).toUpperCase() + element.name.slice(1);
+  });
+}
+
+fixTheForm(inputList);
