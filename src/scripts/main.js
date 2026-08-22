63bb8e2c47a395d3780ee38c76aa99ae578cc20c
Gregory Lyon <ggylyon@gmail.com>
2026-08-22 20:34:01 +0300

    Added the solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const forms = document.querySelectorAll('form');
+
+for (const form of forms) {
+  const fields = form.querySelectorAll('.field');
+
+  for (const field of fields) {
+    const fieldName = field.querySelector('input').name;
+
+    let capitalizedFieldName = '';
+
+    for (const char of fieldName) {
+      if (char.toUpperCase() === char) {
+        capitalizedFieldName += ` ${char}`;
+        continue;
+      }
+
+      capitalizedFieldName += char;
+    }
+    capitalizedFieldName = capitalizedFieldName.split('');
+    capitalizedFieldName[0] = capitalizedFieldName[0].toUpperCase();
+
+    capitalizedFieldName = capitalizedFieldName.join('');
+
+    const fieldInput = field.querySelector('input');
+
+    fieldInput.placeholder = capitalizedFieldName;
+    fieldInput.insertAdjacentHTML('beforebegin', '<label>');
+
+    const fieldLabel = fieldInput.previousElementSibling;
+
+    fieldLabel.className = 'field-label';
+    fieldLabel.htmlFor = fieldInput.id;
+    fieldLabel.textContent = capitalizedFieldName;
+  }
+}
