cdc9bbb901461bb901ca4638128c050869225eea
Sumar Illya <illasumar002@gmail.com>
2026-08-23 21:49:13 +0300

    Tasks complited

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field input');
+
+inputs.forEach((input) => {
+  input.placeholder = `${input.name.at(0).toUpperCase() + input.name.slice(1)}`;
+
+  const parentField = input.parentElement;
+
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+
+  label.htmlFor = input.id;
+
+  label.textContent = `${input.name}`;
+
+  parentField.insertBefore(label, input);
+});
