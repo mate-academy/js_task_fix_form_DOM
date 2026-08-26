85d438f9d4fb300551185f60ead037ebb6ee6c48
Anastasi Bielikova <anastasiabelikova39@gmail.com>
2026-08-26 15:09:47 +0300

    add

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const formInput = document.querySelectorAll('form input');
+
+const capitalize = (str) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+for (const input of formInput) {
+  const inputName = input.name;
+  const inputId = input.id;
+
+  input.placeholder = capitalize(inputName);
+
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.htmlFor = inputId;
+  label.textContent = inputName;
+
+  input.before(label);
+}
