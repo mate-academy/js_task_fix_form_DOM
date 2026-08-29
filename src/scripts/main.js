0bd6fca5f99f9eb30d8c9d070c539772b9e0424f
Jonereh <gnomushka0@gmail.com>
2026-08-29 07:46:36 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((input) => {
+  const newLabel = document.createElement('label');
+
+  newLabel.classList.add('field-label');
+  newLabel.setAttribute('for', input.id);
+
+  const capitalizedLabel =
+    input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  newLabel.textContent = capitalizedLabel;
+  input.placeholder = capitalizedLabel;
+
+  input.parentNode.appendChild(newLabel);
+});
