2be7436918fa03d5cf7fc3c49efc2c0a983ada91
Your Name <you@example.com>
2026-08-31 23:40:47 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const formNodeList = document.querySelectorAll('.field-text');
+
+function capitalize(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+function placeholderImplementator(list, attrName) {
+  for (const element of list) {
+    const value = element.name;
+
+    element.setAttribute(attrName, capitalize(value));
+  }
+}
+
+function labelImplementator(list) {
+  for (const element of list) {
+    const label = document.createElement('label');
+
+    label.setAttribute('class', 'field-label');
+    label.setAttribute('for', element.id);
+    label.textContent = element.name;
+
+    element.parentNode.prepend(label);
+  }
+}
+
+placeholderImplementator(formNodeList, 'placeholder');
+labelImplementator(formNodeList);
