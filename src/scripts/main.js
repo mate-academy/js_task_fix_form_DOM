b37d2cbc78bba0e4c21f556b67f88de857cd9b22
Max <143881777+ZloyHoma@users.noreply.github.com>
2026-09-13 12:24:57 +0300

    *

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write code here
+const inputs = [...document.querySelectorAll('input')];
+
+inputs.forEach((input) => {
+  const inputName = replacer(input.getAttribute('name'));
+  const label = document.createElement('label');
+  const id = input.getAttribute('id');
+
+  input.setAttribute('placeholder', inputName);
+  input.parentNode.insertBefore(label, input);
+  label.setAttribute('for', id);
+  label.classList.add(`field-label`);
+  label.textContent = inputName;
+});
+
+function replacer(str) {
+  return str
+    .replace(/([A-Z])/g, ' $git add ./src1')
+    .replace(/^./, (firstChar) => firstChar.toUpperCase());
+}
